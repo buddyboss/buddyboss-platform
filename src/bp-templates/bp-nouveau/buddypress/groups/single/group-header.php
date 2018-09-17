a58dbcd45ecfbbedf146d1065eea9320fe553665
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2018-09-17 19:38:55 +0530

    removed groups timestamp js for dates

--- src/bp-templates/bp-nouveau/buddypress/groups/single/group-header.php
+++ src/bp-templates/bp-nouveau/buddypress/groups/single/group-header.php
@@ -23,7 +23,7 @@
 
 	<p class="highlight group-status"><strong><?php echo esc_html( bp_nouveau_group_meta()->status ); ?></strong></p>
 
-	<p class="activity" data-livestamp="<?php bp_core_iso8601_date( bp_get_group_last_active( 0, array( 'relative' => false ) ) ); ?>">
+	<p class="activity">
 		<?php
 		$created_date = date('F Y', strtotime( bp_get_group_date_created() ));
 		echo esc_html( sprintf( __( 'Created %s', 'buddyboss' ), $created_date ) );
