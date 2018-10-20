e9656b7f393620ca60f867ab53d2dda2944e0314
Michael Eisenwasser <michael@buddyboss.com>
2018-10-20 14:00:36 -0700

    Memberpress => MemberPress

--- src/bp-integrations/memberpress/admin/bp-admin-memberpress-tab.php
+++ src/bp-integrations/memberpress/admin/bp-admin-memberpress-tab.php
@@ -3,7 +3,7 @@
 class BP_Admin_Integration_Memberpress extends BP_Admin_Integration_tab {
 
 	public function initialize() {
-		$this->tab_label       = __( 'Memberpress', 'buddyboss' );
+		$this->tab_label       = __( 'MemberPress', 'buddyboss' );
 		$this->tab_name        = 'bp-memberpress';
 		$this->required_plugin = 'memberpress/memberpress.php';
 		$this->intro_template  = buddypress()->plugin_dir . '/bp-integrations/memberpress/admin/templates/tab-intro.php';
