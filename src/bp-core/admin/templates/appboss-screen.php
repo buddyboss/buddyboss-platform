00f3fd37fe14ba91ff01393036c2c0507a54b93d
Michael Eisenwasser <michael@buddyboss.com>
2018-10-20 14:21:04 -0700

    nicer AppBoss dummy content

--- src/bp-core/admin/templates/appboss-screen.php
+++ src/bp-core/admin/templates/appboss-screen.php
@@ -2,5 +2,35 @@
 <div class="wrap">
     <h1><?php _e( 'Mobile App', 'buddyboss' ); ?> </h1>
 
-	<p>Consider extending your community into your own companion mobile app via AppBoss.</p>
+	<div id="welcome-panel" class="welcome-panel">
+		<div class="welcome-panel-content">
+		<h2>AppBoss</h2>
+		<p class="about-description">Consider extending your community into your own native mobile app via AppBoss.</p>
+		<div class="welcome-panel-column-container">
+		<div class="welcome-panel-column">
+						<h3>Get Started</h3>
+				<a class="button button-primary button-hero load-customize hide-if-no-customize" href="http://localhost:8888/buddyboss/wp-admin/customize.php">Customize Your Site</a>
+					<a class="button button-primary button-hero hide-if-customize" href="http://localhost:8888/buddyboss/wp-admin/themes.php">Customize Your Site</a>
+									<p class="hide-if-no-customize">or, <a href="http://localhost:8888/buddyboss/wp-admin/customize.php?autofocus[panel]=themes">change your theme completely</a></p>
+				</div>
+		<div class="welcome-panel-column">
+			<h3>Next Steps</h3>
+			<ul>
+						<li><a href="http://localhost:8888/buddyboss/wp-admin/post-new.php" class="welcome-icon welcome-write-blog">Write your first blog post</a></li>
+				<li><a href="http://localhost:8888/buddyboss/wp-admin/post-new.php?post_type=page" class="welcome-icon welcome-add-page">Add an About page</a></li>
+						<li><a href="http://localhost:8888/buddyboss/" class="welcome-icon welcome-view-site">View your site</a></li>
+			</ul>
+		</div>
+		<div class="welcome-panel-column welcome-panel-last">
+			<h3>More Actions</h3>
+			<ul>
+						<li><div class="welcome-icon welcome-widgets-menus">Manage <a href="http://localhost:8888/buddyboss/wp-admin/widgets.php">widgets</a> or <a href="http://localhost:8888/buddyboss/wp-admin/nav-menus.php">menus</a></div></li>
+								<li><a href="http://localhost:8888/buddyboss/wp-admin/options-discussion.php" class="welcome-icon welcome-comments">Turn comments on or off</a></li>
+						<li><a href="https://codex.wordpress.org/First_Steps_With_WordPress" class="welcome-icon welcome-learn-more">Learn more about getting started</a></li>
+			</ul>
+		</div>
+		</div>
+		</div>
+	</div>
+
 </div>
