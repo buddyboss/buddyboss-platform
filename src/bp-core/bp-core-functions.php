390a3c51eee7247718e652222ff9a7cc11078690
Michael Eisenwasser <michael@buddyboss.com>
2018-10-29 20:42:44 -0700

    Forum text

--- src/bp-core/bp-core-functions.php
+++ src/bp-core/bp-core-functions.php
@@ -2452,8 +2452,8 @@ function bp_core_get_components( $type = 'all' ) {
 			'description' => __( 'Groups allow your users to organize themselves into specific public, private or hidden social areas with separate activity feeds and member listings.', 'buddyboss' )
 		),
 		'forums'   => array(
-			'title'       => __( 'Discussion Forums', 'buddyboss' ),
-			'description' => __( 'Forums allow your users to have discussions within Q&A style message boards. Forums can be standalone or connected to social groups.', 'buddyboss' )
+			'title'       => __( 'Forum Discussions', 'buddyboss' ),
+			'description' => __( 'Forums allow your users to have discussions using Q&A style message boards. Forums can be standalone or connected to social groups.', 'buddyboss' )
 		)
 	);
 
