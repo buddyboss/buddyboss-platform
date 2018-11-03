20cee5b94a7456b04ad8a85ae61b5077ddfdd54d
Michael Eisenwasser <michael@buddyboss.com>
2018-11-03 10:53:13 -0700

    description text for User Connections

--- src/bp-core/bp-core-functions.php
+++ src/bp-core/bp-core-functions.php
@@ -2429,7 +2429,7 @@ function bp_core_get_components( $type = 'all' ) {
 		),
 		'friends'  => array(
 			'title'       => __( 'User Connections', 'buddyboss' ),
-			'description' => __( 'Let your users make connections so they can follow the activity of others and focus on the people they care about the most.', 'buddyboss' )
+			'description' => __( 'Let your users make connections with each other and focus on the people they care about the most.', 'buddyboss' )
 		),
 		'messages' => array(
 			'title'       => __( 'Private Messaging', 'buddyboss' ),
