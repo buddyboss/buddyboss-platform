bd92321324865a42b300b9a7c5aa01ae23dcc8b2
andrey <annrra@gmail.com>
2018-11-06 14:11:52 +0200

    #230 - Members Directory

--- src/bp-templates/bp-nouveau/buddypress/common/search-and-filters-bar.php
+++ src/bp-templates/bp-nouveau/buddypress/common/search-and-filters-bar.php
@@ -16,11 +16,8 @@
 	</div>
 	<?php endif; ?>
     
-    <?php if ( bp_is_members_directory() ): ?>
-        <div class="grid-filters">
-			<a href="#" class="layout-grid-view active"><i class="dashicons dashicons-screenoptions" aria-hidden="true"></i></a>
-			<a href="#" class="layout-list-view"><i class="dashicons dashicons-menu" aria-hidden="true"></i></a>
-		</div>
+    <?php if ( 'members' == bp_current_component() || 'groups' == bp_current_component() || 'friends' == bp_current_component() ): ?>
+        <?php bp_get_template_part( 'common/filters/grid-filters' ); ?>
     <?php endif; ?>
 
 	<?php if ( ! ( bp_is_user() && ! bp_is_current_action( 'requests' ) ) && 'groups' !== bp_current_component() ): ?>
