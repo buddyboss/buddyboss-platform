0d676645e66c7e33b6ce24a37c1a1812625ab7d2
Sagar Jadhav <sagar@buddyboss.com>
2018-11-13 19:23:56 +0530

    Repeater Set UI

--- src/bp-xprofile/bp-xprofile-repeaters.php
+++ src/bp-xprofile/bp-xprofile-repeaters.php
@@ -556,7 +556,7 @@ function bp_print_add_repeater_set_button () {
     $group_id = bp_get_current_profile_group_id();
     $is_repeater_enabled = 'on' == BP_XProfile_Group::get_group_meta( $group_id, 'is_repeater_enabled' ) ? true : false;
     if ( $is_repeater_enabled ) {
-        echo "<button id='btn_add_repeater_set' data-nonce='". wp_create_nonce( 'bp_xprofile_add_repeater_set' ) ."' data-group='{$group_id}'>";
+        echo "<button id='btn_add_repeater_set' class='button outline' data-nonce='". wp_create_nonce( 'bp_xprofile_add_repeater_set' ) ."' data-group='{$group_id}'>";
         echo '<span class="dashicons dashicons-plus-alt"></span>';
         printf(
             /* translators: %s = profile field group name */
