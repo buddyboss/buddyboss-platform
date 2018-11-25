997807594941ead3b63bd60cbf60a0bd012703f8
Michael Eisenwasser <michael@buddyboss.com>
2018-11-25 17:05:29 +0800

    only display Member Type filters on MEMBERS directory

--- src/bp-templates/bp-nouveau/buddypress/common/filters/member-type-filters.php
+++ src/bp-templates/bp-nouveau/buddypress/common/filters/member-type-filters.php
@@ -7,52 +7,51 @@
 ?>
 
 <?php
-if ( bp_current_component() === 'activity' ) {
-	return '';
-}
+if ( bp_current_component() === 'members' ) {
 
-$member_types = bp_get_active_member_types();
-$display_arr = array();
-foreach ( $member_types as $member_type_id ) {
+	$member_types = bp_get_active_member_types();
+	$display_arr = array();
+	foreach ( $member_types as $member_type_id ) {
 
-	if ( !get_post_meta( $member_type_id, '_bp_member_type_enable_directory', true ) ) {
-		continue;
+		if ( !get_post_meta( $member_type_id, '_bp_member_type_enable_directory', true ) ) {
+			continue;
+		}
+
+		$type_name = bp_get_member_type_key( $member_type_id );
+		$type_id = bp_member_type_term_taxonomy_id( $type_name );
+		$members_count = count(  bp_member_type_by_type( $type_id ));
+		$member_type_name = get_post_meta( $member_type_id, '_bp_member_type_label_name', true );
+
+		if ( !empty( $type_id ) ) {
+			$display_arr[] = array(
+				'id' => $type_id,
+				'name' => $member_type_name,
+			);
+		}
+
 	}
 
-	$type_name = bp_get_member_type_key( $member_type_id );
-	$type_id = bp_member_type_term_taxonomy_id( $type_name );
-	$members_count = count(  bp_member_type_by_type( $type_id ));
-	$member_type_name = get_post_meta( $member_type_id, '_bp_member_type_label_name', true );
-
-	if ( !empty( $type_id ) ) {
-		$display_arr[] = array(
-			'id' => $type_id,
-			'name' => $member_type_name,
-		);
-	}
-
-}
-
-if ( isset( $display_arr ) && !empty( $display_arr )) {
-	?>
-	<div id="member-type-filters" class="component-filters clearfix">
-		<div id="member-type-select" class="last filter">
-			<label class="bp-screen-reader-text" for="member-type-order-by">
-				<span ><?php bp_nouveau_filter_label(); ?></span>
-			</label>
-			<div class="select-wrap">
-				<select id="member-type-order-by" data-bp-filter="members">
-					<option value="member_type_all"><?php echo __( 'All Types', 'buddyboss' ); ?></option><?php
-					foreach ( $display_arr as $member ) {
+	if ( isset( $display_arr ) && !empty( $display_arr )) {
+		?>
+		<div id="member-type-filters" class="component-filters clearfix">
+			<div id="member-type-select" class="last filter">
+				<label class="bp-screen-reader-text" for="member-type-order-by">
+					<span ><?php bp_nouveau_filter_label(); ?></span>
+				</label>
+				<div class="select-wrap">
+					<select id="member-type-order-by" data-bp-filter="members">
+						<option value="member_type_all"><?php echo __( 'All Types', 'buddyboss' ); ?></option><?php
+						foreach ( $display_arr as $member ) {
+							?>
+							<option value="member_type_<?php echo $member['id']; ?>"><?php echo __( $member['name'], 'buddyboss' ); ?></option><?php
+						}
 						?>
-						<option value="member_type_<?php echo $member['id']; ?>"><?php echo __( $member['name'], 'buddyboss' ); ?></option><?php
-					}
-					?>
-				</select>
-				<span class="select-arrow" aria-hidden="true"></span>
+					</select>
+					<span class="select-arrow" aria-hidden="true"></span>
+				</div>
 			</div>
 		</div>
-	</div>
-	<?php
-}
+		<?php
+	}
 
+}
