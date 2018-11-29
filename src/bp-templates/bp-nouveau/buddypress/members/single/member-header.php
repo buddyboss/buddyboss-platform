3f7c4da79c169d263b8386b8ffb5b07339394756
Sagar Jadhav <sagar@buddyboss.com>
2018-11-29 19:13:32 +0530

    member profile header fix

--- src/bp-templates/bp-nouveau/buddypress/members/single/member-header.php
+++ src/bp-templates/bp-nouveau/buddypress/members/single/member-header.php
@@ -21,13 +21,6 @@
 	<div id="item-header-content">
 		<h2 class="user-nicename"><?php echo bp_core_get_user_displayname( bp_displayed_user_id() ); ?></h2>
 
-		<?php if ( bp_is_active( 'activity' ) && bp_activity_do_mentions() ) : ?>
-			<span class="mention-name">@<?php bp_displayed_user_mentionname(); ?></span>
-			<span class="separator">&bull;</span>
-		<?php endif; ?>
-
-		<?php bp_nouveau_member_hook( 'before', 'header_meta' ); ?>
-
 		<?php
 
 		if ( true === bp_member_type_enable_disable() ) {
@@ -54,12 +47,24 @@
 		}
 		?>
 
-		<?php if ( bp_nouveau_member_has_meta() ) : ?>
+		<?php bp_nouveau_member_hook( 'before', 'header_meta' ); ?>
+
+		<?php if ( ( bp_is_active( 'activity' ) && bp_activity_do_mentions() ) || bp_nouveau_member_has_meta() ) : ?>
 			<div class="item-meta">
+				<?php if ( bp_is_active( 'activity' ) && bp_activity_do_mentions() ) : ?>
+					<span class="mention-name">@<?php bp_displayed_user_mentionname(); ?></span>
+				<?php endif; ?>
 
-				<?php bp_nouveau_member_meta(); ?>
+				<?php if ( bp_is_active( 'activity' ) && bp_activity_do_mentions() && bp_nouveau_member_has_meta() ) : ?>
+					<span class="separator">&bull;</span>
+				<?php endif; ?>
 
-			</div><!-- #item-meta -->
+				<?php bp_nouveau_member_hook( 'before', 'header_meta' ); ?>
+
+				<?php if ( bp_nouveau_member_has_meta() ) : ?>
+					<?php bp_nouveau_member_meta(); ?>
+				<?php endif; ?>
+			</div>	
 		<?php endif; ?>
 
 		<?php bp_nouveau_member_header_buttons( array( 'container_classes' => array( 'member-header-actions' ) ) ); ?>
