300753a53c6614050ab1ce61c6e6ebc8ecbf1aa9
andrey <annrra@gmail.com>
2018-12-03 12:25:02 +0200

    remove status field - new topic form

--- src/bp-forums/templates/default/bbpress/form-topic.php
+++ src/bp-forums/templates/default/bbpress/form-topic.php
@@ -129,18 +129,6 @@
 
 						<?php do_action( 'bbp_theme_after_topic_form_type' ); ?>
 
-						<?php do_action( 'bbp_theme_before_topic_form_status' ); ?>
-
-						<p>
-
-							<label for="bbp_topic_status"><?php _e( 'Status:', 'buddyboss' ); ?></label><br />
-
-							<?php bbp_form_topic_status_dropdown(); ?>
-
-						</p>
-
-						<?php do_action( 'bbp_theme_after_topic_form_status' ); ?>
-
 					<?php endif; ?>
 
 					<?php if ( bbp_is_subscriptions_active() && !bbp_is_anonymous() && ( !bbp_is_topic_edit() || ( bbp_is_topic_edit() && !bbp_is_topic_anonymous() ) ) ) : ?>
@@ -191,7 +179,7 @@
 
 						<?php do_action( 'bbp_theme_before_topic_form_submit_button' ); ?>
 
-						<button type="submit" tabindex="<?php bbp_tab_index(); ?>" id="bbp_topic_submit" name="bbp_topic_submit" class="button submit"><?php _e( 'Submit', 'buddyboss' ); ?></button>
+						<button type="submit" tabindex="<?php bbp_tab_index(); ?>" id="bbp_topic_submit" name="bbp_topic_submit" class="button submit"><?php _e( 'Post', 'buddyboss' ); ?></button>
 
 						<?php do_action( 'bbp_theme_after_topic_form_submit_button' ); ?>
 
