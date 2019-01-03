080786492ba37de18e17c4d85ba10072148b70df
Chetan Satasiya <chetan@buddyboss.com>
2019-01-03 18:24:51 +0530

    Fixed the member search based on the profile visibility settings

--- src/bp-core/profile-search/bps-search.php
+++ src/bp-core/profile-search/bps-search.php
@@ -205,6 +205,8 @@ function bp_ps_search ( $request, $users = null ) {
 
     $fields = bp_ps_parse_request( $request );
 
+    $copied_arr = array();
+
     foreach ( $fields as $f ) {
         if ( !isset( $f->filter ) )
             continue;
@@ -215,6 +217,23 @@ function bp_ps_search ( $request, $users = null ) {
 
         $found = call_user_func( $f->search, $f );
         $found = apply_filters( 'bp_ps_field_search_results', $found, $f );
+
+	    $copied_arr = $found;
+	    if ( isset( $copied_arr ) && !empty( $copied_arr ) ) {
+		    foreach ( $copied_arr as $key => $user ) {
+			    $field_visibility = xprofile_get_field_visibility_level( intval( $f->id ), intval( $user ) );
+			    if ( 'adminsonly' === $field_visibility && ! current_user_can( 'administrator' ) ) {
+				    if ( ( $key = array_search( $user, $found ) ) !== false ) {
+					    unset( $found[ $key ] );
+				    }
+			    }
+			    if ( 'friends' === $field_visibility && ! current_user_can( 'administrator' ) && false === friends_check_friendship( intval( $user ), bp_loggedin_user_id() ) ) {
+				    if ( ( $key = array_search( $user, $found ) ) !== false ) {
+					    unset( $found[ $key ] );
+				    }
+			    }
+		    }
+	    }
 
         $match_all = apply_filters( 'bp_ps_match_all', true );
         if ( $match_all ) {
