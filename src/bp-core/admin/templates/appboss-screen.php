7367612a4dc3d256e0d89c49fcc380609251ffc2
Michael Eisenwasser <michael@buddyboss.com>
2019-01-25 00:38:27 +0800

    AppBoss admin - section 1

--- src/bp-core/admin/templates/appboss-screen.php
+++ src/bp-core/admin/templates/appboss-screen.php
@@ -1,40 +1,50 @@
 
-<div class="wrap">
-    <h1><?php _e( 'AppBoss', 'buddyboss' ); ?></h1>
+<div class="wrap wc_addons_wrap">
+    <h1 class="screen-reader-text"><?php _e( 'AppBoss Mobile App', 'buddyboss' ); ?></h1>
 
-	<div id="welcome-panel" class="welcome-panel">
-		<div class="welcome-panel-content">
-		
-			<h2><?php _e( 'Stand Out With A High-Quality iOS & Android App', 'buddyboss' ); ?></h2>
-			<p class="about-description"><?php _e( 'Create and launch a native mobile app that syncs with your WordPress community. Install our AppBoss plugin to get started, and then contact us to finalize and launch your app. This is a <strong>paid</strong> service.', 'buddyboss' ); ?></p>
-			
-			<div class="welcome-panel-column-container">			
-				<div class="welcome-panel-column">
-					<h3><?php _e( 'Get Started', 'buddyboss' ); ?></h3>
-					<a class="button button-primary button-hero" href="#"><?php _e( 'Install Plugin', 'buddyboss' ); ?></a>
-					<p class="hide-if-no-customize"><?php _e( 'or, ', 'buddyboss' ); ?><a class="web" href="https://appboss.com/"><?php _e( 'learn about AppBoss', 'buddyboss' ); ?></a></p>
+		<div class="addons-featured">
+			<div class="addons-banner-block">
+				<h1><?php _e( 'Take your store beyond the typical - sell anything', 'buddyboss' ); ?></h1>
+				<p><?php _e( 'From services to content, there&#039;s no limit to what you can sell with WooCommerce.', 'buddyboss' ); ?></p>
+
+				<div class="addons-banner-block-items">
+					
+					<div class="addons-banner-block-item">
+						<div class="addons-banner-block-item-icon">
+							<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/subscriptions-icon@2x.png" />
+						</div>
+						<div class="addons-banner-block-item-content">
+							<h3><?php _e( 'Subscriptions', 'buddyboss' ); ?></h3>
+							<p><?php _e( 'Let customers subscribe to your products or services and pay on a weekly, monthly or annual basis.', 'buddyboss' ); ?></p>
+							<a class="addons-button addons-button-solid" href="#"><?php _e( 'From: $199', 'buddyboss' ); ?></a>
+						</div>
+					</div>
+
+					<div class="addons-banner-block-item">
+						<div class="addons-banner-block-item-icon">
+							<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/bookings-icon@2x.png" />
+						</div>
+						<div class="addons-banner-block-item-content">
+							<h3><?php _e( 'Bookings', 'buddyboss' ); ?></h3>
+							<p><?php _e( 'Allow customers to book appointments for services without leaving your site.', 'buddyboss' ); ?></p>
+							<a class="addons-button addons-button-solid" href="#"><?php _e( 'From: $249', 'buddyboss' ); ?></a>
+						</div>
+					</div>
+
+					<div class="addons-banner-block-item">
+						<div class="addons-banner-block-item-icon">
+							<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/memberships-icon@2x.png" />
+						</div>
+						<div class="addons-banner-block-item-content">
+							<h3><?php _e( 'Memberships', 'buddyboss' ); ?></h3>
+							<p><?php _e( 'Give members access to restricted content or products, for a fee or for free.', 'buddyboss' ); ?></p>
+							<a class="addons-button addons-button-solid" href="#"><?php _e( 'From: $149', 'buddyboss' ); ?></a>
+						</div>
+					</div>
+					
 				</div>
-				
-				<div class="welcome-panel-column">
-					<h3><?php _e( 'Features', 'buddyboss' ); ?></h3>
-					<ul>
-						<li><?php _e( 'Push Notifications', 'buddyboss' ); ?></li>
-						<li><?php _e( 'Synchronized with your site', 'buddyboss' ); ?></li>
-						<li><?php _e( 'Social Login', 'buddyboss' ); ?></li>
-					</ul>
-				</div>
-				
-				<div class="welcome-panel-column welcome-panel-last">
-					<h3><?php _e( 'Customize', 'buddyboss' ); ?></h3>
-					<ul>
-						<li><?php _e( 'Custom Branding', 'buddyboss' ); ?></li>
-						<li><?php _e( 'Custom Pages', 'buddyboss' ); ?></li>
-						<li><?php _e( 'Custom Menus', 'buddyboss' ); ?></li>
-					</ul>
-				</div>
+
 			</div>
-
 		</div>
-	</div>
 
 </div>
