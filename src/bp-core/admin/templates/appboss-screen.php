4ab62c67e5a0692b2720c5e4f14808f7d0653ed0
Michael Eisenwasser <michael@buddyboss.com>
2019-01-25 01:25:30 +0800

    appboss class

--- src/bp-core/admin/templates/appboss-screen.php
+++ src/bp-core/admin/templates/appboss-screen.php
@@ -1,48 +1,48 @@
 
-<div class="wrap wc_addons_wrap">
+<div class="wrap wc_appboss_wrap">
     <h1 class="screen-reader-text"><?php _e( 'AppBoss Mobile App', 'buddyboss' ); ?></h1>
-	<div class="addons-featured">
+	<div class="appboss-featured">
 
 			<?php
 			/*
-			 * addons-banner-block
+			 * appboss-banner-block
 			 */
 			?>
-			<div class="addons-banner-block">
+			<div class="appboss-banner-block">
 				<h1><?php _e( 'Take your store beyond the typical - sell anything', 'buddyboss' ); ?></h1>
 				<p><?php _e( 'From services to content, there&#039;s no limit to what you can sell with WooCommerce.', 'buddyboss' ); ?></p>
-				<div class="addons-banner-block-items">
+				<div class="appboss-banner-block-items">
 					
-					<div class="addons-banner-block-item">
-						<div class="addons-banner-block-item-icon">
-							<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/subscriptions-icon@2x.png" />
+					<div class="appboss-banner-block-item">
+						<div class="appboss-banner-block-item-icon">
+							<img class="appboss-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/subscriptions-icon@2x.png" />
 						</div>
-						<div class="addons-banner-block-item-content">
+						<div class="appboss-banner-block-item-content">
 							<h3><?php _e( 'Subscriptions', 'buddyboss' ); ?></h3>
 							<p><?php _e( 'Let customers subscribe to your products or services and pay on a weekly, monthly or annual basis.', 'buddyboss' ); ?></p>
-							<a class="addons-button addons-button-solid" href="#"><?php _e( 'From: $199', 'buddyboss' ); ?></a>
+							<a class="appboss-button appboss-button-solid" href="#"><?php _e( 'From: $199', 'buddyboss' ); ?></a>
 						</div>
 					</div>
 
-					<div class="addons-banner-block-item">
-						<div class="addons-banner-block-item-icon">
-							<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/bookings-icon@2x.png" />
+					<div class="appboss-banner-block-item">
+						<div class="appboss-banner-block-item-icon">
+							<img class="appboss-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/bookings-icon@2x.png" />
 						</div>
-						<div class="addons-banner-block-item-content">
+						<div class="appboss-banner-block-item-content">
 							<h3><?php _e( 'Bookings', 'buddyboss' ); ?></h3>
 							<p><?php _e( 'Allow customers to book appointments for services without leaving your site.', 'buddyboss' ); ?></p>
-							<a class="addons-button addons-button-solid" href="#"><?php _e( 'From: $249', 'buddyboss' ); ?></a>
+							<a class="appboss-button appboss-button-solid" href="#"><?php _e( 'From: $249', 'buddyboss' ); ?></a>
 						</div>
 					</div>
 
-					<div class="addons-banner-block-item">
-						<div class="addons-banner-block-item-icon">
-							<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/memberships-icon@2x.png" />
+					<div class="appboss-banner-block-item">
+						<div class="appboss-banner-block-item-icon">
+							<img class="appboss-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/memberships-icon@2x.png" />
 						</div>
-						<div class="addons-banner-block-item-content">
+						<div class="appboss-banner-block-item-content">
 							<h3><?php _e( 'Memberships', 'buddyboss' ); ?></h3>
 							<p><?php _e( 'Give members access to restricted content or products, for a fee or for free.', 'buddyboss' ); ?></p>
-							<a class="addons-button addons-button-solid" href="#"><?php _e( 'From: $149', 'buddyboss' ); ?></a>
+							<a class="appboss-button appboss-button-solid" href="#"><?php _e( 'From: $149', 'buddyboss' ); ?></a>
 						</div>
 					</div>
 					
@@ -51,76 +51,76 @@
 
 			<?php
 			/*
-			 * addons-wcs-banner-block
+			 * appboss-wcs-banner-block
 			 */
 			?>
-			<div class="addons-wcs-banner-block">
+			<div class="appboss-wcs-banner-block">
 
-				<div class="addons-wcs-banner-block-image">
-					<img class="addons-img" src="http://localhost:8888/buddyboss-theme/wp-content/plugins/woocommerce/assets/images/wcs-extensions-banner-3x.png" alt="WooCommerce Services" />
+				<div class="appboss-wcs-banner-block-image">
+					<img class="appboss-img" src="http://localhost:8888/buddyboss-theme/wp-content/plugins/woocommerce/assets/images/wcs-extensions-banner-3x.png" alt="WooCommerce Services" />
 				</div>
 				
-				<div class="addons-wcs-banner-block-content">
+				<div class="appboss-wcs-banner-block-content">
 					<h1><?php _e( 'Buy discounted shipping labels — then print them from your dashboard.', 'buddyboss' ); ?></h1>
 					<p><?php _e( 'Integrate your store with USPS to buy discounted shipping labels, and print them directly from your WooCommerce dashboard. Powered by WooCommerce Services.', 'buddyboss' ); ?></p>
-					<a class="addons-button addons-button-outline-dark" href="#"><?php _e( 'Free - Install now', 'buddyboss' ); ?></a>
+					<a class="appboss-button appboss-button-outline-dark" href="#"><?php _e( 'Free - Install now', 'buddyboss' ); ?></a>
 				</div>
 
 			</div>
 
 			<?php
 			/*
-			 * addons-column-section
+			 * appboss-column-section
 			 */
 			?>
-			<div class="addons-column-section">
+			<div class="appboss-column-section">
 
 				<?php
 				/*
-				 * addons-column
+				 * appboss-column
 				 */
 				?>			
-				<div class="addons-column">
+				<div class="appboss-column">
 					
 					<?php
 					/*
-					 * addons-column-block
+					 * appboss-column-block
 					 */
 					?>
-					<div class="addons-column-block">
+					<div class="appboss-column-block">
 					
 						<h1><?php _e( 'Improve the main features of your online store', 'buddyboss' ); ?></h1>
 						<p><?php _e( 'Sell more by helping customers find the products and options they want.', 'buddyboss' ); ?></p>
 
-						<div class="addons-column-block-item">
-							<div class="addons-column-block-item-icon">
-								<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/product-add-ons-icon@2x.png" />
+						<div class="appboss-column-block-item">
+							<div class="appboss-column-block-item-icon">
+								<img class="appboss-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/product-add-ons-icon@2x.png" />
 							</div>
-							<div class="addons-column-block-item-content">
+							<div class="appboss-column-block-item-content">
 								<h2><?php _e( 'Product Add-ons', 'buddyboss' ); ?></h2>
-								<a class="addons-button addons-button-solid" href="#"><?php _e( 'From: $49', 'buddyboss' ); ?></a>
+								<a class="appboss-button appboss-button-solid" href="#"><?php _e( 'From: $49', 'buddyboss' ); ?></a>
 								<p><?php _e( 'Give your customers the option to customize their purchase or add personalization.', 'buddyboss' ); ?></p>
 							</div>
 						</div>
 
-						<div class="addons-column-block-item">
-							<div class="addons-column-block-item-icon">
-								<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/product-search-icon@2x.png" />
+						<div class="appboss-column-block-item">
+							<div class="appboss-column-block-item-icon">
+								<img class="appboss-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/product-search-icon@2x.png" />
 							</div>
-							<div class="addons-column-block-item-content">
+							<div class="appboss-column-block-item-content">
 								<h2><?php _e( 'Product Search', 'buddyboss' ); ?></h2>
-								<a class="addons-button addons-button-solid" href="#"><?php _e( 'From: $49', 'buddyboss' ); ?></a>
+								<a class="appboss-button appboss-button-solid" href="#"><?php _e( 'From: $49', 'buddyboss' ); ?></a>
 								<p><?php _e( 'Make sure customers find what they want when they search your site.', 'buddyboss' ); ?></p>
 							</div>
 						</div>
 
-						<div class="addons-column-block-item">
-							<div class="addons-column-block-item-icon">
-								<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/checkout-add-ons-icon@2x.png" />
+						<div class="appboss-column-block-item">
+							<div class="appboss-column-block-item-icon">
+								<img class="appboss-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/checkout-add-ons-icon@2x.png" />
 							</div>
-							<div class="addons-column-block-item-content">
+							<div class="appboss-column-block-item-content">
 								<h2><?php _e( 'Checkout Add-ons', 'buddyboss' ); ?></h2>
-								<a class="addons-button addons-button-solid" href="#"><?php _e( 'From: $49', 'buddyboss' ); ?></a>
+								<a class="appboss-button appboss-button-solid" href="#"><?php _e( 'From: $49', 'buddyboss' ); ?></a>
 								<p><?php _e( 'Highlight relevant products, offers like free shipping and other upsells during checkout.', 'buddyboss' ); ?></p>
 							</div>
 						</div>
@@ -129,17 +129,17 @@
 
 					<?php
 					/*
-					 * addons-small-light-block
+					 * appboss-small-light-block
 					 */
 					?>
-					<div class="addons-small-light-block">
-						<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/storefront-screen@2x.png" />
-						<div class="addons-small-light-block-content">
+					<div class="appboss-small-light-block">
+						<img class="appboss-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/storefront-screen@2x.png" />
+						<div class="appboss-small-light-block-content">
 							<h1><?php _e( 'Get the official WooCommerce theme', 'buddyboss' ); ?></h1>
 							<p><?php _e( 'Storefront is the lean, flexible, and free theme, built by the people who make WooCommerce - everything you need to get started.', 'buddyboss' ); ?></p>
-							<div class="addons-small-light-block-buttons">
-								<a class="addons-button addons-button-solid" href="#"><?php _e( 'Install', 'buddyboss' ); ?></a>
-								<a class="addons-button addons-button-solid" href="#"><?php _e( 'Read More', 'buddyboss' ); ?></a>
+							<div class="appboss-small-light-block-buttons">
+								<a class="appboss-button appboss-button-solid" href="#"><?php _e( 'Install', 'buddyboss' ); ?></a>
+								<a class="appboss-button appboss-button-solid" href="#"><?php _e( 'Read More', 'buddyboss' ); ?></a>
 							</div>
 						</div>
 					</div>
@@ -148,65 +148,65 @@
 
 				<?php
 				/*
-				 * addons-column
+				 * appboss-column
 				 */
 				?>
-				<div class="addons-column">
+				<div class="appboss-column">
 	
 					<?php
 					/*
-					 * addons-small-dark-block
+					 * appboss-small-dark-block
 					 */
 					?>
-					<div class="addons-small-dark-block">
+					<div class="appboss-small-dark-block">
 						<h1><?php _e( 'Square + WooCommerce', 'buddyboss' ); ?></h1>
 						<p><?php _e( 'Keep your WooCommerce and brick-and-mortar stores in sync. Use Square to take payments both online and offline, keep inventory updated between the two and sync product changes.', 'buddyboss' ); ?></p>
-						<div class="addons-small-dark-items">
-							<div class="addons-small-dark-item">
-								<a class="addons-button addons-button-outline-light" href="#"><?php _e( 'Free!', 'buddyboss' ); ?>		</a>
+						<div class="appboss-small-dark-items">
+							<div class="appboss-small-dark-item">
+								<a class="appboss-button appboss-button-outline-light" href="#"><?php _e( 'Free!', 'buddyboss' ); ?>		</a>
 							</div>
 						</div>
 					</div>
 
 					<?php
 					/*
-					 * addons-column-block
+					 * appboss-column-block
 					 */
 					?>					
-					<div class="addons-column-block">
+					<div class="appboss-column-block">
 					
 						<h1><?php _e( 'Get deeper insights', 'buddyboss' ); ?></h1>
 						<p><?php _e( 'Learn how your store is performing with enhanced reporting.', 'buddyboss' ); ?></p>
 
-						<div class="addons-column-block-item">
-							<div class="addons-column-block-item-icon">
-								<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/ga-icon@2x.png" />
+						<div class="appboss-column-block-item">
+							<div class="appboss-column-block-item-icon">
+								<img class="appboss-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/ga-icon@2x.png" />
 							</div>
-							<div class="addons-column-block-item-content">
+							<div class="appboss-column-block-item-content">
 								<h2><?php _e( 'Google Analytics', 'buddyboss' ); ?></h2>
-								<a class="addons-button addons-button-solid" href="#"><?php _e( 'Free', 'buddyboss' ); ?></a>
+								<a class="appboss-button appboss-button-solid" href="#"><?php _e( 'Free', 'buddyboss' ); ?></a>
 								<p><?php _e( 'Understand your customers and increase revenue with the world’s leading analytics platform.', 'buddyboss' ); ?></p>
 							</div>
 						</div>
 
-						<div class="addons-column-block-item">
-							<div class="addons-column-block-item-icon">
-								<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/cart-reports-icon@2x.png" />
+						<div class="appboss-column-block-item">
+							<div class="appboss-column-block-item-icon">
+								<img class="appboss-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/cart-reports-icon@2x.png" />
 							</div>
-							<div class="addons-column-block-item-content">
+							<div class="appboss-column-block-item-content">
 								<h2><?php _e( 'Cart reports', 'buddyboss' ); ?></h2>
-								<a class="addons-button addons-button-solid" href="#"><?php _e( 'From: $79', 'buddyboss' ); ?></a>
+								<a class="appboss-button appboss-button-solid" href="#"><?php _e( 'From: $79', 'buddyboss' ); ?></a>
 								<p><?php _e( 'Get real-time reports on what customers are leaving in their cart.', 'buddyboss' ); ?></p>
 							</div>
 						</div>
 
-						<div class="addons-column-block-item">
-							<div class="addons-column-block-item-icon">
-								<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/cost-of-goods-icon@2x.png" />
+						<div class="appboss-column-block-item">
+							<div class="appboss-column-block-item-icon">
+								<img class="appboss-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/cost-of-goods-icon@2x.png" />
 							</div>
-							<div class="addons-column-block-item-content">
+							<div class="appboss-column-block-item-content">
 								<h2><?php _e( 'Cost of Goods', 'buddyboss' ); ?></h2>
-								<a class="addons-button addons-button-solid" href="#"><?php _e( 'From: $79', 'buddyboss' ); ?></a>
+								<a class="appboss-button appboss-button-solid" href="#"><?php _e( 'From: $79', 'buddyboss' ); ?></a>
 								<p><?php _e( 'Easily track profit by including cost of goods in your reports.', 'buddyboss' ); ?></p>
 							</div>
 						</div>
@@ -219,44 +219,44 @@
 
 			<?php
 			/*
-			 * addons-banner-block
+			 * appboss-banner-block
 			 */
 			?>
-			<div class="addons-banner-block">
+			<div class="appboss-banner-block">
 				<h1><?php _e( 'Promote your products and increase sales', 'buddyboss' ); ?></h1>
 				<p><?php _e( 'From coupons to emails, these extensions can power up your marketing efforts.', 'buddyboss' ); ?></p>
-				<div class="addons-banner-block-items">
+				<div class="appboss-banner-block-items">
 					
-					<div class="addons-banner-block-item">
-						<div class="addons-banner-block-item-icon">
-							<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/smart-coupons-icon@2x.png" />
+					<div class="appboss-banner-block-item">
+						<div class="appboss-banner-block-item-icon">
+							<img class="appboss-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/smart-coupons-icon@2x.png" />
 						</div>
-						<div class="addons-banner-block-item-content">
+						<div class="appboss-banner-block-item-content">
 							<h3><?php _e( 'Smart Coupons', 'buddyboss' ); ?></h3>
 							<p><?php _e( 'Enhance your coupon options - create gift certificates, store credit, coupons based on purchases and more.', 'buddyboss' ); ?></p>
-							<a class="addons-button addons-button-solid" href="#"><?php _e( 'From: $99', 'buddyboss' ); ?></a>
+							<a class="appboss-button appboss-button-solid" href="#"><?php _e( 'From: $99', 'buddyboss' ); ?></a>
 						</div>
 					</div>
 
-					<div class="addons-banner-block-item">
-						<div class="addons-banner-block-item-icon">
-							<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/follow-up-emails-icon@2x.png" />
+					<div class="appboss-banner-block-item">
+						<div class="appboss-banner-block-item-icon">
+							<img class="appboss-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/follow-up-emails-icon@2x.png" />
 						</div>
-						<div class="addons-banner-block-item-content">
+						<div class="appboss-banner-block-item-content">
 							<h3><?php _e( 'Follow Up Emails', 'buddyboss' ); ?></h3>
 							<p><?php _e( 'Automatically contact customers after purchase - be it everyone, your most loyal or your biggest spenders - and keep your store top-of-mind.', 'buddyboss' ); ?></p>
-							<a class="addons-button addons-button-solid" href="#"><?php _e( 'From: $99', 'buddyboss' ); ?></a>
+							<a class="appboss-button appboss-button-solid" href="#"><?php _e( 'From: $99', 'buddyboss' ); ?></a>
 						</div>
 					</div>
 
-					<div class="addons-banner-block-item">
-						<div class="addons-banner-block-item-icon">
-							<img class="addons-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/google-product-feed-icon@2x.png" />
+					<div class="appboss-banner-block-item">
+						<div class="appboss-banner-block-item-icon">
+							<img class="appboss-img" src="https://d3t0oesq8995hv.cloudfront.net/add-ons/google-product-feed-icon@2x.png" />
 						</div>
-						<div class="addons-banner-block-item-content">
+						<div class="appboss-banner-block-item-content">
 							<h3><?php _e( 'Google Product Feed', 'buddyboss' ); ?></h3>
 							<p><?php _e( 'Let customers find you when shopping for products via Google.', 'buddyboss' ); ?></p>
-							<a class="addons-button addons-button-solid" href="#"><?php _e( 'From: $79', 'buddyboss' ); ?></a>
+							<a class="appboss-button appboss-button-solid" href="#"><?php _e( 'From: $79', 'buddyboss' ); ?></a>
 						</div>
 					</div>
 					
