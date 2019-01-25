3bd768ad9983817ac65df8440ff7ac57c8059085
Chetan Satasiya <chetan@buddyboss.com>
2019-01-25 16:46:51 +0530

    override the Wordpress core functions to plugin

--- src/bp-core/admin/bp-core-admin-pages.php
+++ src/bp-core/admin/bp-core-admin-pages.php
@@ -24,7 +24,7 @@ function bp_core_admin_pages_settings() {
         <form action="" method="post">
 			<?php
 			settings_fields( 'bp-pages' );
-			do_settings_sections( 'bp-pages' );
+			bp_custom_pages_do_settings_sections( 'bp-pages' );
 
 			printf(
 				'<p class="submit">
@@ -37,6 +37,69 @@ function bp_core_admin_pages_settings() {
     </div>
 
 	<?php
+}
+
+function bp_custom_pages_do_settings_sections( $page ) {
+	global $wp_settings_sections, $wp_settings_fields;
+
+	if ( ! isset( $wp_settings_sections[$page] ) )
+		return;
+
+	foreach ( (array) $wp_settings_sections[$page] as $section ) {
+		if ( $section['title'] )
+			echo "<div class='card'><h2>{$section['title']}</h2>\n";
+
+		if ( $section['callback'] )
+			call_user_func( $section['callback'], $section );
+
+		if ( ! isset( $wp_settings_fields ) || !isset( $wp_settings_fields[$page] ) || !isset( $wp_settings_fields[$page][$section['id']] ) )
+			continue;
+		echo '<table class="form-table">';
+		bp_custom_pages_do_settings_fields( $page, $section['id'] );
+		echo '</table></div>';
+	}
+}
+
+/**
+ * Print out the settings fields for a particular settings section
+ *
+ * Part of the Settings API. Use this in a settings page to output
+ * a specific section. Should normally be called by do_settings_sections()
+ * rather than directly.
+ *
+ * @global $wp_settings_fields Storage array of settings fields and their pages/sections
+ *
+ * @since 2.7.0
+ *
+ * @param string $page Slug title of the admin page who's settings fields you want to show.
+ * @param string $section Slug title of the settings section who's fields you want to show.
+ */
+function bp_custom_pages_do_settings_fields($page, $section) {
+	global $wp_settings_fields;
+
+	if ( ! isset( $wp_settings_fields[$page][$section] ) )
+		return;
+
+	foreach ( (array) $wp_settings_fields[$page][$section] as $field ) {
+		$class = '';
+
+		if ( ! empty( $field['args']['class'] ) ) {
+			$class = ' class="' . esc_attr( $field['args']['class'] ) . '"';
+		}
+
+		echo "<tr{$class}>";
+
+		if ( ! empty( $field['args']['label_for'] ) ) {
+			echo '<th scope="row"><label for="' . esc_attr( $field['args']['label_for'] ) . '">' . $field['title'] . '</label></th>';
+		} else {
+			echo '<th scope="row">' . $field['title'] . '</th>';
+		}
+
+		echo '<td>';
+		call_user_func($field['callback'], $field['args']);
+		echo '</td>';
+		echo '</tr>';
+	}
 }
 
 /**
