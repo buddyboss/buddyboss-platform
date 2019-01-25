8073c850fb5bc16a354e7b6e9a3ed9a71eb574fe
Chetan Satasiya <chetan@buddyboss.com>
2019-01-25 16:31:33 +0530

    override the Wordpress core functions to plugin

--- src/bp-core/classes/class-bp-admin-tab.php
+++ src/bp-core/classes/class-bp-admin-tab.php
@@ -215,7 +215,7 @@ abstract class BP_Admin_Tab {
 	 */
 	public function form_html() {
 		settings_fields( $this->tab_name );
-		do_settings_sections( $this->tab_name );
+		$this->bp_custom_do_settings_sections( $this->tab_name );
 
 		printf(
 			'<p class="submit">
@@ -397,6 +397,82 @@ abstract class BP_Admin_Tab {
 
 	protected function get_input_value( $key, $default = '' ) {
 		return bp_get_option( $key, $default );
+	}
+
+	/**
+	 * Prints out all settings sections added to a particular settings page
+	 *
+	 * Part of the Settings API. Use this in a settings page callback function
+	 * to output all the sections and fields that were added to that $page with
+	 * add_settings_section() and add_settings_field()
+	 *
+	 * @global $wp_settings_sections Storage array of all settings sections added to admin pages
+	 * @global $wp_settings_fields Storage array of settings fields and info about their pages/sections
+	 * @since 2.7.0
+	 *
+	 * @param string $page The slug name of the page whose settings sections you want to output
+	 */
+	public function bp_custom_do_settings_sections( $page ) {
+		global $wp_settings_sections, $wp_settings_fields;
+
+		if ( ! isset( $wp_settings_sections[$page] ) )
+			return;
+
+		foreach ( (array) $wp_settings_sections[$page] as $section ) {
+			if ( $section['title'] )
+				echo "<div class='card'><h2>{$section['title']}</h2>\n";
+
+			if ( $section['callback'] )
+				call_user_func( $section['callback'], $section );
+
+			if ( ! isset( $wp_settings_fields ) || !isset( $wp_settings_fields[$page] ) || !isset( $wp_settings_fields[$page][$section['id']] ) )
+				continue;
+			echo '<table class="form-table">';
+			$this->bp_custom_do_settings_fields( $page, $section['id'] );
+			echo '</table></div>';
+		}
+	}
+
+	/**
+	 * Print out the settings fields for a particular settings section
+	 *
+	 * Part of the Settings API. Use this in a settings page to output
+	 * a specific section. Should normally be called by do_settings_sections()
+	 * rather than directly.
+	 *
+	 * @global $wp_settings_fields Storage array of settings fields and their pages/sections
+	 *
+	 * @since 2.7.0
+	 *
+	 * @param string $page Slug title of the admin page who's settings fields you want to show.
+	 * @param string $section Slug title of the settings section who's fields you want to show.
+	 */
+	public function bp_custom_do_settings_fields($page, $section) {
+		global $wp_settings_fields;
+
+		if ( ! isset( $wp_settings_fields[$page][$section] ) )
+			return;
+
+		foreach ( (array) $wp_settings_fields[$page][$section] as $field ) {
+			$class = '';
+
+			if ( ! empty( $field['args']['class'] ) ) {
+				$class = ' class="' . esc_attr( $field['args']['class'] ) . '"';
+			}
+
+			echo "<tr{$class}>";
+
+			if ( ! empty( $field['args']['label_for'] ) ) {
+				echo '<th scope="row"><label for="' . esc_attr( $field['args']['label_for'] ) . '">' . $field['title'] . '</label></th>';
+			} else {
+				echo '<th scope="row">' . $field['title'] . '</th>';
+			}
+
+			echo '<td>';
+			call_user_func($field['callback'], $field['args']);
+			echo '</td>';
+			echo '</tr>';
+		}
 	}
 }
 
