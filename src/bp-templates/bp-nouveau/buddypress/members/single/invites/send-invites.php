0028a6c96d2c2645f39e96c8b9edde5e955dda9f
Michael Eisenwasser <michael@buddyboss.com>
2019-01-28 19:25:27 +0800

    validation text

--- src/bp-templates/bp-nouveau/buddypress/members/single/invites/send-invites.php
+++ src/bp-templates/bp-nouveau/buddypress/members/single/invites/send-invites.php
@@ -122,7 +122,7 @@ bp_nouveau_member_hook( 'before', 'invites_send_template' ); ?>
 		<?php
 	}
 	?>
-	<input type="hidden" value="<?php _e('Please fill out all the required fields to invite a new member.', 'buddyboss') ?>" name="error-message-required-field" id="error-message-required-field">
+	<input type="hidden" value="<?php _e('Please fill out all required fields to invite a new member.', 'buddyboss') ?>" name="error-message-required-field" id="error-message-required-field">
 	<?php bp_nouveau_submit_button( 'member-invites-submit' ); ?>
 
 </form>
