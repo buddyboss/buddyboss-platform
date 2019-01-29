5aef6ae2a6bdde77f236f293cd01ce479cf936c0
Michael Eisenwasser <michael@buddyboss.com>
2019-01-29 18:13:18 +0800

    text changes

--- src/bp-core/admin/bp-core-admin-slugs.php
+++ src/bp-core/admin/bp-core-admin-slugs.php
@@ -35,7 +35,7 @@ function bp_core_admin_get_directory_pages() {
 	}
 
 	if ( bp_nouveau_get_appearance_settings( 'user_front_page' ) ) {
-		$directory_pages['profile_dashboard'] = __( 'Dashboard', 'buddyboss' );
+		$directory_pages['profile_dashboard'] = __( 'Profile Dashboard', 'buddyboss' );
 	}
 
 	/** Directory Display *****************************************************/
