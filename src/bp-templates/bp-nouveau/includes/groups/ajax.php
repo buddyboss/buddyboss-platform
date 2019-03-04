4a02cac4b62e7656c095f62311a29ba30dca0116
Chetan Satasiya <chetan@buddyboss.com>
2019-03-04 17:57:50 +0530

    Group Invites Sub Group invite changes

--- src/bp-templates/bp-nouveau/includes/groups/ajax.php
+++ src/bp-templates/bp-nouveau/includes/groups/ajax.php
@@ -285,6 +285,22 @@ function bp_nouveau_ajax_get_users_to_invite() {
 
 	if ( 'groups_get_group_potential_invites' === $request['action'] ) {
 
+		// check if subgroup.
+		$parent_group_id = bp_get_parent_group_id( $request['group_id'] );
+
+		if ( isset( $parent_group_id ) && $parent_group_id > 0 ) {
+
+			$check_admin = groups_is_user_admin( bp_loggedin_user_id(), $parent_group_id );
+			$check_moder = groups_is_user_mod( bp_loggedin_user_id(), $parent_group_id );
+
+			// Check role of current logged in user for this group.
+			if ( false === $check_admin && false === $check_moder ) {
+				wp_send_json_error( array(
+					'feedback' => __( 'You are not authorized to send invites to other users.', 'buddyboss' ),
+					'type'     => 'info',
+				) );
+			}
+		}
 
 		$group_type = bp_groups_get_group_type( $request['group_id'] );
 
