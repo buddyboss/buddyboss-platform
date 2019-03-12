249348b9cf954f35447aac9c9093575c19ec869d
Paresh Radadiya <pareshradadiya@hotmail.com>
2019-03-12 14:52:17 +0530

    search result template tweaks

--- src/bp-templates/bp-nouveau/buddypress/search/loop/activity.php
+++ src/bp-templates/bp-nouveau/buddypress/search/loop/activity.php
@@ -7,20 +7,17 @@
 		</div>
 
 		<div class="item activity-content">
+			<div class="activity-header">
+				<?php echo bp_get_activity_action( [ 'no_timestamp' => true ] ); ?>
+			</div>
 			<?php if ( bp_nouveau_activity_has_content() ) : ?>
-				<h3 class="entry-title item-title activity-content">
-					<a href="<?php echo bp_activity_get_permalink( bp_get_activity_id() ) ?>">
-						<?php bp_nouveau_activity_content(); ?>
-					</a>
-				</h3>
+				<div class="activity-inner">
+					<?php echo wp_trim_words( $GLOBALS['activities_template']->activity->content, '20', '...' ) ?>
+				</div>
 			<?php endif; ?>
-			<div class="item-meta activity-header">
-				<strong>
-					<a href="<?php bp_activity_user_link(); ?>"><?php echo bp_core_get_user_displayname( bp_get_activity_user_id() ) ?></a>
-				</strong>
-				<span class="middot">&middot;</span>
+			<div class="item-meta">
 				<time>
-					<?php echo human_time_diff( bp_nouveau_get_activity_timestamp() ) . ' ago' ?>
+					<?php echo human_time_diff( bp_nouveau_get_activity_timestamp() ) . '&nbsp;' . esc_html__( 'ago', 'buddyboss' ) ?>
 				</time>
 			</div>
 		</div>
