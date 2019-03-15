57e10662e4605f3d8795821f45862e7836938c68
Michael Eisenwasser <michael@buddyboss.com>
2019-03-15 14:03:10 +0800

    text

--- src/bp-integrations/appboss/templates/admin/integration-tab-intro.php
+++ src/bp-integrations/appboss/templates/admin/integration-tab-intro.php
@@ -4,7 +4,7 @@
 		<h2><?php _e( 'AppBoss <span>&mdash; purchase to activate</span>', 'buddyboss' ); ?></h2>
 		<p><?php
 			printf(
-				__('Extend your community into a native mobile app using %s. AppBoss is a paid service provided by BuddyBoss, which will launch native iOS and Android apps for you, published under your own Apple and Google Play accounts. The apps will be branded to match your site, and can sync community data (profiles, activity, etc.) back and forth with BuddyBoss Platform. If using LearnDash they will also sync course data with your website. ', 'buddyboss'),
+				__('Extend your community into a native mobile app using %s. AppBoss is a paid service provided by BuddyBoss, which will launch native iOS and Android apps for you, published under your own Apple and Google Play accounts. The apps will be branded to match your site, and can sync community data (profiles, forums, etc.) back and forth with BuddyBoss Platform. If using LearnDash they will also sync course data with your website. ', 'buddyboss'),
 				sprintf(
 					'<a href="%s">%s</a>',
 					'https://appboss.com',
