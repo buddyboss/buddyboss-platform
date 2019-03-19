361034b6fb161a58cec20f56f3770598d382aa65
Ketan Chawda <chawda.ketan.a@gmail.com>
2019-03-18 22:52:49 -0700

    Buddyboss Memberships :  Order change for tabs

--- src/bp-integrations/appboss/bp-admin-appboss-tab.php
+++ src/bp-integrations/appboss/bp-admin-appboss-tab.php
@@ -1,24 +1,24 @@
 <?php
 /**
  * AppBoss integration admin tab
- * 
+ *
  * @package BuddyBoss\AppBoss
  * @since BuddyBoss 1.0.0
- */ 
+ */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 /**
  * Setup AppBoss integration admin tab class.
- * 
+ *
  * @since BuddyBoss 1.0.0
  */
 class BP_Appboss_Admin_Integration_Tab extends BP_Admin_Integration_tab {
 
 	public function initialize() {
-		$this->tab_order             = 10;
-		$this->intro_template        = $this->root_path . '/templates/admin/integration-tab-intro.php';
+		$this->tab_order = 30;
+		$this->intro_template = $this->root_path . '/templates/admin/integration-tab-intro.php';
 	}
 
 	public function settings_save() {
@@ -28,7 +28,7 @@ class BP_Appboss_Admin_Integration_Tab extends BP_Admin_Integration_tab {
 	public function register_fields() {
 		$this->add_section(
 			'appboss-section',
-			__( 'Section Heading', 'buddyboss' )
+			__('Section Heading', 'buddyboss')
 		);
 	}
 }
--- src/bp-integrations/learndash/bp-learndash-integration.php
+++ src/bp-integrations/learndash/bp-learndash-integration.php
@@ -7,7 +7,7 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 /**
  * Setup the bp learndash class.
@@ -19,29 +19,29 @@ class BP_Learndash_Integration extends BP_Integration {
 	public function __construct() {
 		$this->start(
 			'learndash',
-			__( 'LearnDash', 'buddyboss' ),
+			__('LearnDash', 'buddyboss'),
 			'learndash',
 			[
-				'required_plugin' => 'sfwd-lms/sfwd_lms.php'
+				'required_plugin' => 'sfwd-lms/sfwd_lms.php',
 			]
 		);
 	}
 
 	public function setup_admin_integartion_tab() {
-		require_once trailingslashit( $this->path ) . 'bp-admin-learndash-tab.php';
+		require_once trailingslashit($this->path) . 'bp-admin-learndash-tab.php';
 
 		new BP_Learndash_Admin_Integration_Tab(
 			"bp-{$this->id}",
 			$this->name,
 			[
 				'root_path' => $this->path,
-				'root_url'  => $this->url,
+				'root_url' => $this->url,
 				'required_plugin' => $this->required_plugin,
 			]
 		);
 	}
 
-	public function includes( $includes = array() ) {
+	public function includes($includes = array()) {
 		parent::includes([
 			'functions',
 			'groups-sync/loader.php',
