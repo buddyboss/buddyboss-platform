b10e268a270f2c95784edd7e410ded9f48baadcd
Paresh Radadiya <pareshradadiya@hotmail.com>
2019-03-25 10:10:58 +0530

    updated search result default thumbnail function

--- src/bp-templates/bp-nouveau/buddypress/search/loop/reply.php
+++ src/bp-templates/bp-nouveau/buddypress/search/loop/reply.php
@@ -7,7 +7,7 @@ $topic_id = bbp_get_reply_topic_id($reply_id);
 		<div class="item-avatar">
 			<a href="<?php bbp_reply_url( $reply_id ); ?>">
 				<img
-					src="<?php echo bbp_get_forum_thumbnail_src( bbp_get_forum_id( $reply_id ) ) ?: buddypress()->plugin_url . "bp-core/images/forum.svg"; ?>"
+					src="<?php echo bbp_get_forum_thumbnail_src( bbp_get_forum_id( $reply_id ) ) ?: bp_search_get_post_thumbnail_default( get_post_type() ); ?>"
 					class="avatar forum-avatar"
 					height="150"
 					width="150"
