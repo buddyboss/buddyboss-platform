f91a21f28a25a124a9e164b018d6ff19fb2a1534
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2019-03-26 18:36:04 +0530

    media privacy class fixes

--- src/bp-media/classes/class-bp-media-privacy.php
+++ src/bp-media/classes/class-bp-media-privacy.php
@@ -38,17 +38,18 @@ class BP_Media_Privacy {
 
 	/**
 	 * get options for visibility
-	 * @param bool $friend
+	 *
+	 * @since BuddyBoss 1.0.0
 	 * @param bool $group
 	 * @return array
 	 */
 	function get_visibility_options( $is_group = false ){
 
 		$options = array(
-			'public' => __( 'Everyone', 'buddyboss' ),
+			'public'   => __( 'Everyone', 'buddyboss' ),
 			'loggedin' => __( 'Logged In Users', 'buddyboss' ),
-			'onlyme' => __('Only Me', 'buddyboss' ),
-			'friends' => __('My Friends', 'buddyboss' ),
+			'onlyme'   => __( 'Only Me', 'buddyboss' ),
+			'friends'  => __( 'My Friends', 'buddyboss' ),
 		);
 
 		if( $is_group && bp_is_active( 'groups' ) ) {
@@ -58,57 +59,76 @@ class BP_Media_Privacy {
 		return $options;
 	}
 
+	/**
+	 * Get visibility of media
+	 *
+	 * @since BuddyBoss 1.0.0
+	 * @param $media_id
+	 *
+	 * @return WP_Error
+	 */
 	function get_visibility( $media_id ) {
-		$media_model = new BP_Media();
-		$media = $media_model::get( $media_id );
+		$result = bp_media_get_specific( array( 'media_ids' => $media_id ) );
 
-		if ( empty( $media ) || is_wp_error( $media ) ) {
+		if ( empty( $result['medias'] ) || empty( $result['medias'][0] ) ) {
 			return new WP_Error( 'no_media', __( 'There is no media.', 'buddyboss' ), array( 'status' => 500 ) );
 		}
 
-		return $media->privacy;
+		return $result['medias'][0]->privacy;
 	}
 
-	function is_media_visible( $media_id = 0 ) {
+	/**
+	 * Check if media is visible or not to the logged in user
+	 *
+	 * @since BuddyBoss 1.0.0
+	 * @param bool $media_id
+	 *
+	 * @return bool|mixed|void|WP_Error
+	 */
+	function is_media_visible( $media_id = false ) {
+		$result = bp_media_get_specific( array( 'media_ids' => $media_id ) );
 
-		$media = new BP_Media( $media_id );
-
-		if ( empty( $media->id ) ) {
-			return new \WP_Error( 'no_media', __( 'There is no media.', 'buddyboss' ), array( 'status' => 500 ) );
+		if ( empty( $result['medias'] ) || empty( $result['medias'][0] ) ) {
+			return new WP_Error( 'no_media', __( 'There is no media.', 'buddyboss' ), array( 'status' => 500 ) );
 		}
 
+		$media      = $result['medias'][0];
 		$visibility = $media->privacy;
-		$visible = true;
+		$visible    = true;
 
-		if( bp_loggedin_user_id() != $media->user_id ) {
+		if ( bp_loggedin_user_id() != $media->user_id ) {
 
 			switch ( $visibility ) {
 				//Logged in users
 				case 'loggedin' :
-					if( !bp_loggedin_user_id() )
+					if ( ! bp_loggedin_user_id() ) {
 						$visible = false;
+					}
 					break;
 
 				//My friends
 				case 'friends' :
 					if ( bp_is_active( 'friends' ) ) {
 						$is_friend = friends_check_friendship( bp_loggedin_user_id(), $media->user_id );
-						if( !$is_friend )
+						if ( ! $is_friend ) {
 							$visible = false;
+						}
 					}
 					break;
 
 				//Only group members
 				case 'grouponly' :
 					$group_is_user_member = groups_is_user_member( bp_loggedin_user_id(), $media->activity_id );
-					if( !$group_is_user_member )
+					if ( ! $group_is_user_member ) {
 						$visible = false;
+					}
 					break;
 
 				//Only Me
 				case 'onlyme' :
-					if( bp_loggedin_user_id() != $media->user_id )
+					if ( bp_loggedin_user_id() != $media->user_id ) {
 						$visible = false;
+					}
 					break;
 
 				default:
@@ -121,18 +141,22 @@ class BP_Media_Privacy {
 			$visible = true;
 		}
 
-		$visible = apply_filters( 'bp_media_is_media_visible', $visible, $visibility, $media_id );
-
-		return $visible;
+		return apply_filters( 'bp_media_is_media_visible', $visible, $visibility, $media_id );
 	}
 
+	/**
+	 * Get visible media count for the user
+	 *
+	 * @since BuddyBoss 1.0.0
+	 * @param int $user_id
+	 *
+	 * @return int
+	 */
 	function get_visible_media_count( $user_id = 0 ) {
 
 		if ( empty( $user_id ) ) {
 			return 0;
 		}
-
-		$media_model = new BP_Media();
 
 		if ( is_user_logged_in() ) {
 			$visibility = array( 'public', 'loggedin' );
@@ -150,7 +174,9 @@ class BP_Media_Privacy {
 			$visibility = array_keys( $this->get_visibility_options() );
 		}
 
-		return $media_model::rows( array( 'author_id' => $user_id, 'privacy' => $visibility ) );
+		$result = bp_media_get_specific( array( 'user_id' => $user_id, 'privacy' => $visibility ) );
+
+		return ! empty( $result['total'] ) ? $result['total'] : 0;
 	}
 
 }
