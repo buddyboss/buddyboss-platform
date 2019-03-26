fc2ff8a5c0910c7a8b46564d27b6b77129f401d3
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2019-03-26 15:08:20 +0530

    media directory layout changes

--- src/bp-templates/bp-nouveau/buddypress/media/media-loop.php
+++ src/bp-templates/bp-nouveau/buddypress/media/media-loop.php
@@ -12,7 +12,7 @@ bp_nouveau_before_loop(); ?>
 <?php if ( bp_has_media( bp_ajax_querystring( 'media' ) ) ) : ?>
 
 	<?php if ( empty( $_POST['page'] ) || 1 === (int) $_POST['page'] ) : ?>
-        <ul class="media-list item-list bp-list bb-member-photos-list">
+        <ul class="media-list item-list bp-list bb-photo-list grid">
 	<?php endif; ?>
 
 	<?php while ( bp_media() ) :
