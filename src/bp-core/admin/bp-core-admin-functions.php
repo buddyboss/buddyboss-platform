fc9b390b6fd3644afb0dca7c111ed40fa936ba94
Michael Eisenwasser <michael@buddyboss.com>
2019-03-28 00:08:54 +0800

    text

--- src/bp-core/admin/bp-core-admin-functions.php
+++ src/bp-core/admin/bp-core-admin-functions.php
@@ -2449,7 +2449,7 @@ function bp_core_get_emails_admin_tabs( $active_tab = '') {
 
 	$tabs[] = array(
 		'href'  => bp_get_admin_url( add_query_arg( array( 'page' => 'bp-emails-customizer-redirect' ), 'themes.php' ) ),
-		'name'  => __( 'Email Layout', 'buddypress' ),
+		'name'  => __( 'Customize Layout', 'buddypress' ),
 		'class' => 'bp-emails-customizer',
 	);
 
