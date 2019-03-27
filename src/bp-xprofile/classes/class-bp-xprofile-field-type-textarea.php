c1752cf4cd7714174b25dfc2a5dc898dd3c01cec
andrey <annrra@gmail.com>
2019-03-27 09:36:01 +0200

    profile fields helper texts position

--- src/bp-xprofile/classes/class-bp-xprofile-field-type-textarea.php
+++ src/bp-xprofile/classes/class-bp-xprofile-field-type-textarea.php
@@ -70,8 +70,10 @@ class BP_XProfile_Field_Type_Textarea extends BP_XProfile_Field_Type {
 				<?php bp_the_profile_field_required_label(); ?>
 			<?php endif; ?>
 		</legend>
-
-		<?php
+        
+        <?php if ( bp_get_the_profile_field_description() ) : ?>
+			<p class="description" id="<?php bp_the_profile_field_input_name(); ?>-3"><?php bp_the_profile_field_description(); ?></p>
+		<?php endif;
 
 		/** This action is documented in bp-xprofile/bp-xprofile-classes */
 		do_action( bp_get_the_profile_field_errors_action() );
@@ -121,9 +123,6 @@ class BP_XProfile_Field_Type_Textarea extends BP_XProfile_Field_Type {
 			);
 		}
 
-		if ( bp_get_the_profile_field_description() ) : ?>
-			<p class="description" id="<?php bp_the_profile_field_input_name(); ?>-3"><?php bp_the_profile_field_description(); ?></p>
-		<?php endif;
 	}
 
 	/**
