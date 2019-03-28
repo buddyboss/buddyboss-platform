2e367cb38f25ad14c0da6a808c791f4cf2a896bb
Sagar Jadhav <sagar@buddyboss.com>
2019-03-28 20:44:16 +0530

    Media modal

--- src/bp-templates/bp-nouveau/buddypress/media/theatre.php
+++ src/bp-templates/bp-nouveau/buddypress/media/theatre.php
@@ -1,6 +1,6 @@
 <div class="bb-media-model-wrapper bb-internal-model" style="display: none;">
 
-    <a data-balloon-pos="left" data-balloon="<?php _e( 'Close', 'buddyboss' ); ?>" class="bb-close-media-theatre" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"><path fill="none" stroke="#FFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 1L1 13m12 0L1 1" opacity=".7"/></svg></a>
+    <a data-balloon-pos="left" data-balloon="<?php _e( 'Close', 'buddyboss' ); ?>" class="bb-close-media-theatre bb-close-model" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"><path fill="none" stroke="#FFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 1L1 13m12 0L1 1" opacity=".7"/></svg></a>
 
     <div id="bb-media-model-container" class="bb-media-model-container">
         <div class="bb-media-model-inner">
@@ -13,16 +13,12 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="30"><path fill="none" stroke="#FFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1l14 14L1 29" opacity=".7"/></svg>
                 </a>
 
-<!--                <span><i class="bb-icon-spin5 animate-spin"></i></span>-->
-
                 <figure class="">
                     <img src="" alt="" />
                 </figure>
             </div>
             <div class="bb-media-info-section">
-                <ul class="activity-list item-list bp-list">
-
-                </ul>
+                <ul class="activity-list item-list bp-list"><span><i class="bb-icon-spin5 animate-spin"></i></span></ul>
             </div>
         </div>
     </div>
