4b09430cd0848f08b20bfca61e33c66f72b41f8e
Chetan Satasiya <chetan@buddyboss.com>
2019-03-29 14:25:53 +0530

    send-invites tab first open fix

--- src/bp-invites/bp-invites-filters.php
+++ src/bp-invites/bp-invites-filters.php
@@ -25,7 +25,7 @@ function bp_set_send_invites_group_default_tab( $component ) {
 
 	// Get the group nav order based on the customizer settings.
 	$nav_tabs = bp_nouveau_get_appearance_settings( 'group_nav_order' );
-	if ( isset( $nav_tabs[0] ) && 'send-invites' === $nav_tabs[0] && bp_is_active( $nav_tabs[0] ) ) {
+	if ( isset( $nav_tabs[0] ) && 'send-invites' === $nav_tabs[0] && bp_is_active( 'invites' ) ) {
 		if ( bp_group_is_member()  ) {
 			return $nav_tabs[0];
 		}
