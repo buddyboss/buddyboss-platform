0cce584c2724c710a259e7e84667195b8f628fde
Sagar Jadhav <sagar@buddyboss.com>
2019-04-01 20:28:18 +0530

    Fixed media issue

--- src/bp-templates/bp-nouveau/buddypress/media/activity-entry.php
+++ src/bp-templates/bp-nouveau/buddypress/media/activity-entry.php
@@ -8,21 +8,19 @@
 
 <?php global $media_template; ?>
 
-<div class="bb-activity-media-wrap <?php echo 'bb-media-length-' . $media_template->media_count; echo $media_template->media_count > 5 ? 'bb-media-length-more' : ''; ?>">
-    <div class="bb-activity-media-elem <?php echo $media_template->media_count == 1 || $media_template->media_count > 1 && $media_template->current_media == 0 ? 'act-grid-1-1 ' : ''; echo $media_template->media_count > 1 && $media_template->current_media > 0 ? 'act-grid-1-2 ' : ''; echo $media_template->media->attachment_data->meta['width'] > $media_template->media->attachment_data->meta['height'] ? 'bb-horizontal-layout' : ''; echo $media_template->media->attachment_data->meta['height'] > $media_template->media->attachment_data->meta['width'] ? 'bb-vertical-layout' : ''; ?>">
-        <a href="#"
-           class="bb-open-media-theatre entry-img"
-           data-id="<?php bp_media_id(); ?>"
-           data-attachment-full="<?php bp_media_attachment_image(); ?>"
-           data-activity-id="<?php bp_media_activity_id(); ?>">
+<div class="bb-activity-media-elem <?php echo $media_template->media_count == 1 || $media_template->media_count > 1 && $media_template->current_media == 0 ? 'act-grid-1-1 ' : ''; echo $media_template->media_count > 1 && $media_template->current_media > 0 ? 'act-grid-1-2 ' : ''; echo $media_template->media->attachment_data->meta['width'] > $media_template->media->attachment_data->meta['height'] ? 'bb-horizontal-layout' : ''; echo $media_template->media->attachment_data->meta['height'] > $media_template->media->attachment_data->meta['width'] ? 'bb-vertical-layout' : ''; ?>">
+	<a href="#"
+	   class="bb-open-media-theatre entry-img"
+	   data-id="<?php bp_media_id(); ?>"
+	   data-attachment-full="<?php bp_media_attachment_image(); ?>"
+	   data-activity-id="<?php bp_media_activity_id(); ?>">
 
-            <img src="<?php bp_media_attachment_image_activity_thumbnail(); ?>" class="no-round photo" alt="<?php bp_media_title(); ?>" />
+		<img src="<?php bp_media_attachment_image_activity_thumbnail(); ?>" class="no-round photo" alt="<?php bp_media_title(); ?>" />
 
-            <?php if ( $media_template->media_count > 5 && $media_template->current_media == 4 ) {
-                ?>
-                <span class="bb-photos-length"><span><strong>+<?php echo $media_template->media_count - 5; ?></strong> <span><?php _e( 'More Photos', 'buddyboss' ); ?></span></span></span>
-                <?php
-            } ?>
-        </a>
-    </div>
+		<?php if ( $media_template->media_count > 5 && $media_template->current_media == 4 ) {
+			?>
+			<span class="bb-photos-length"><span><strong>+<?php echo $media_template->media_count - 5; ?></strong> <span><?php _e( 'More Photos', 'buddyboss' ); ?></span></span></span>
+			<?php
+		} ?>
+	</a>
 </div>
