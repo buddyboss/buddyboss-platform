2c7b5c10a61c31a8d1c55d33394f558aee474c8b
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2019-04-04 15:32:20 +0530

    changes to uploader

--- src/bp-templates/bp-nouveau/buddypress/media/uploader.php
+++ src/bp-templates/bp-nouveau/buddypress/media/uploader.php
@@ -26,12 +26,10 @@
 						</a>
                     </header>
 
-                    <div class="bb-dropzone-wrap bb-has-items bp-media-upload-tab-content" id="bp-dropzone-content">
-	                    <?php if ( ! is_bbpress() ) : ?>
-                            <div class="media-uploader-post-content">
-                                <textarea name="bp-media-post-content" id="bp-media-post-content" placeholder="<?php _e( 'Write something about media.' ); ?>"></textarea>
-                            </div>
-	                    <?php endif; ?>
+                    <div class="bb-dropzone-wrap bp-media-upload-tab-content" id="bp-dropzone-content">
+                        <div class="media-uploader-post-content">
+                            <textarea name="bp-media-post-content" id="bp-media-post-content" placeholder="<?php _e( 'Write something about media.' ); ?>"></textarea>
+                        </div>
                         <div class="media-uploader-wrapper">
                             <div class="dropzone" id="media-uploader"></div>
                         </div>
