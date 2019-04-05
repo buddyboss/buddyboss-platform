9828d3370c79f03a3760b92df423b72d93eb73e3
Chetan Satasiya <chetan@buddyboss.com>
2019-04-05 13:06:44 +0530

    Private Network > Allow Page Selection for Exclude

--- src/bp-core/admin/settings/bp-admin-setting-general.php
+++ src/bp-core/admin/settings/bp-admin-setting-general.php
@@ -28,6 +28,7 @@ class BP_Admin_Setting_General extends BP_Admin_Setting_tab {
 		$this->add_field( 'bp-enable-site-registration', __( 'Registrations', 'buddyboss' ), 'bp_admin_setting_callback_register', 'intval' );
 		$this->add_field( 'hide-loggedout-adminbar', __( 'Toolbar', 'buddyboss' ), 'bp_admin_setting_callback_admin_bar', 'intval' );
 		$this->add_field( 'bp-disable-account-deletion', __( 'Account Deletion', 'buddyboss' ), 'bp_admin_setting_callback_account_deletion', 'intval' );
+		$this->add_section( 'bp_privacy', __( 'Privacy', 'buddyboss' ) );
 		$this->add_field( 'bp-enable-private-network', __( 'Private Network', 'buddyboss' ), 'bp_admin_setting_callback_private_network', 'intval' );
 	}
 }
