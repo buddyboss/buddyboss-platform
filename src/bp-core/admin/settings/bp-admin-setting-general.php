02a38565bd7c3fbbb03d95650f495fb20e5b7fb4
Michael Eisenwasser <michael@buddyboss.com>
2019-04-05 16:44:56 +0800

    settings order

--- src/bp-core/admin/settings/bp-admin-setting-general.php
+++ src/bp-core/admin/settings/bp-admin-setting-general.php
@@ -26,8 +26,8 @@ class BP_Admin_Setting_General extends BP_Admin_Setting_tab {
 	public function register_fields() {
 		$this->add_section( 'bp_main', __( 'General Settings', 'buddyboss' ) );
 		$this->add_field( 'bp-enable-site-registration', __( 'Registrations', 'buddyboss' ), 'bp_admin_setting_callback_register', 'intval' );
-		$this->add_field( 'hide-loggedout-adminbar', __( 'Toolbar', 'buddyboss' ), 'bp_admin_setting_callback_admin_bar', 'intval' );
 		$this->add_field( 'bp-disable-account-deletion', __( 'Account Deletion', 'buddyboss' ), 'bp_admin_setting_callback_account_deletion', 'intval' );
+		$this->add_field( 'hide-loggedout-adminbar', __( 'Toolbar', 'buddyboss' ), 'bp_admin_setting_callback_admin_bar', 'intval' );
 		$this->add_section( 'bp_privacy', __( 'Privacy', 'buddyboss' ) );
 		$this->add_field( 'bp-enable-private-network', __( 'Private Network', 'buddyboss' ), 'bp_admin_setting_callback_private_network', 'intval' );
 		$this->add_field( 'bp-enable-private-network-public-content', __( 'Public Content', 'buddyboss' ), 'bp_admin_setting_callback_private_network_public_content' );
