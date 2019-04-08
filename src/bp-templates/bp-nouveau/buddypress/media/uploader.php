0abff4265d4eda4db58d2c6bf3622f22534bdd4c
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2019-04-08 22:17:06 +0530

    translation fix

--- src/bp-templates/bp-nouveau/buddypress/media/uploader.php
+++ src/bp-templates/bp-nouveau/buddypress/media/uploader.php
@@ -29,7 +29,7 @@
                     <div class="bb-dropzone-wrap bp-media-upload-tab-content" id="bp-dropzone-content">
                         <?php if ( ! bbp_is_single_forum() && ! bbp_is_single_topic() ) : ?>
                         <div class="media-uploader-post-content">
-                            <textarea name="bp-media-post-content" id="bp-media-post-content" placeholder="<?php _e( 'Write something about media.' ); ?>"></textarea>
+                            <textarea name="bp-media-post-content" id="bp-media-post-content" placeholder="<?php _e( 'Write something about media.', 'buddyboss' ); ?>"></textarea>
                         </div>
                         <?php endif; ?>
                         <div class="media-uploader-wrapper">
