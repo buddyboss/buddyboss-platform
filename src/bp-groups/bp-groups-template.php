e9d8291e45e876b86ac0ffe5005e62d738ddd1e8
Michael Eisenwasser <michael@buddyboss.com>
2019-04-17 20:46:18 +0800

    <span> to allow for: group text “You’re a [role]” more compact view for Grid list

--- src/bp-groups/bp-groups-template.php
+++ src/bp-groups/bp-groups-template.php
@@ -3857,14 +3857,14 @@ function bp_group_join_button( $group = false ) {
 
 			if ( groups_is_user_admin( bp_loggedin_user_id(), $group->id ) ) {
 				if ( 'Organizer' === get_group_role_label( $group->id, 'organizer_singular_label_name') ) {
-					$button_text = __( 'You\'re an ' . get_group_role_label( $group->id, 'organizer_singular_label_name'), 'buddyboss' );
+					$button_text = __( '<span>You\'re an </span> ' . get_group_role_label( $group->id, 'organizer_singular_label_name'), 'buddyboss' );
 				} else {
-					$button_text = __( 'You\'re a ' . get_group_role_label( $group->id, 'organizer_singular_label_name'), 'buddyboss' );
+					$button_text = __( '<span>You\'re a </span> ' . get_group_role_label( $group->id, 'organizer_singular_label_name'), 'buddyboss' );
 				}
 			} elseif ( groups_is_user_mod( bp_loggedin_user_id(), $group->id ) ) {
-				$button_text = __( 'You\'re a ' . get_group_role_label( $group->id, 'moderator_singular_label_name'), 'buddyboss' );
+				$button_text = __( '<span>You\'re a </span> ' . get_group_role_label( $group->id, 'moderator_singular_label_name'), 'buddyboss' );
 			} else {
-				$button_text = __( 'You\'re a ' . get_group_role_label( $group->id, 'member_singular_label_name'), 'buddyboss' );
+				$button_text = __( '<span>You\'re a </span> ' . get_group_role_label( $group->id, 'member_singular_label_name'), 'buddyboss' );
 			}
 
 			// Setup button attributes.
