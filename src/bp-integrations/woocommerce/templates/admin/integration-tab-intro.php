4edf18eed0ef806fc0d50aca4b3b10e7f25960b3
Michael Eisenwasser <michael@buddyboss.com>
2019-04-17 15:06:49 -0700

    integration structure

--- src/bp-integrations/woocommerce/templates/admin/integration-tab-intro.php
+++ src/bp-integrations/woocommerce/templates/admin/integration-tab-intro.php
@@ -1,7 +1,7 @@
 <div class="wrap">
 	
 	<div class="bp-admin-card section-bp-woocommerce-integration">
-		<h2><?php _e( 'WooCommerce <span>&mdash; install plugin to activate</span>', 'buddyboss' ); ?></h2>
+		<h2><?php _e( 'WooCommerce <span>&mdash; requires plugin to activate</span>', 'buddyboss' ); ?></h2>
 		<p><?php
 			printf(
 				__('BuddyBoss Platform has integration settings for %s. If using LearnDash we add the ability to easily enroll users into courses based on their purchased WooCommerce product. If using our BuddyBoss Theme we also include styling for WooCommerce.', 'buddyboss'),
@@ -12,6 +12,16 @@
 				)
 			)
 		?></p>
+		<br />
+		<div class="bp-admin-card-bottom">
+			<?php
+				printf(
+					'<a href="%s" class="button-secondary">%s</a>',
+					'https://wordpress.org/plugins/woocommerce/',
+					__('Get WooCommerce', 'buddyboss')
+				);
+			?>
+		</div>
 	</div>
 
 </div>
