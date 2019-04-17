6a4494acf9cdf943faa7b6c6b2d9054a2e05fbe1
Sagar Jadhav <sagar@buddyboss.com>
2019-04-17 12:54:58 +0530

    Media fixes

--- src/bp-templates/bp-nouveau/buddypress/media/activity-entry.php
+++ src/bp-templates/bp-nouveau/buddypress/media/activity-entry.php
@@ -6,9 +6,11 @@
  */
 ?>
 
-<?php global $media_template; ?>
+<?php global $media_template;
 
-<div class="bb-activity-media-elem <?php echo $media_template->current_media > 4 ? 'hide' : ''; echo $media_template->media_count == 1 || $media_template->media_count > 1 && $media_template->current_media == 0 ? 'act-grid-1-1 ' : ''; echo $media_template->media_count > 1 && $media_template->current_media > 0 ? 'act-grid-1-2 ' : ''; echo $media_template->media->attachment_data->meta['width'] > $media_template->media->attachment_data->meta['height'] ? 'bb-horizontal-layout' : ''; echo $media_template->media->attachment_data->meta['height'] > $media_template->media->attachment_data->meta['width'] ? 'bb-vertical-layout' : ''; ?>">
+?>
+
+<div class="bb-activity-media-elem <?php echo $media_template->current_media > 4 ? 'hide' : ''; echo $media_template->media_count == 1 || $media_template->media_count > 1 && $media_template->current_media == 0 ? 'act-grid-1-1 ' : ''; echo $media_template->media_count > 1 && $media_template->current_media > 0 ? 'act-grid-1-2 ' : ''; echo $media_template->media->attachment_data->meta['width'] > $media_template->media->attachment_data->meta['height'] ? 'bb-horizontal-layout' : ''; echo $media_template->media->attachment_data->meta['height'] > $media_template->media->attachment_data->meta['width'] || $media_template->media->attachment_data->meta['width'] == $media_template->media->attachment_data->meta['height'] ? 'bb-vertical-layout' : ''; ?>">
 	<a href="#"
 	   class="bb-open-media-theatre entry-img"
 	   data-id="<?php bp_media_id(); ?>"
