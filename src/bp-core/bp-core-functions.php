dc5e3b5ba4201b998cd589acd1f14f23a1c31fcd
Michael Eisenwasser <michael@buddyboss.com>
2019-04-17 20:15:02 +0800

    Photo Uploads - new description

--- src/bp-core/bp-core-functions.php
+++ src/bp-core/bp-core-functions.php
@@ -2493,7 +2493,7 @@ function bp_core_get_components( $type = 'all' ) {
 		'media'   => array(
 			'title'       => __( 'Photo Uploading', 'buddyboss' ),
 			'settings'    => bp_get_admin_url( add_query_arg( array( 'page' => 'bp-settings', 'tab' => 'bp-media' ) , 'admin.php' ) ),
-			'description' => __( 'Allow members to upload photos and create photo albums.', 'buddyboss' ),
+			'description' => __( 'Allow members to upload photos to activity, groups, forums and messages, and to organize photos into albums.', 'buddyboss' ),
 			'default'     => false,
 		),
 		'messages' => array(
