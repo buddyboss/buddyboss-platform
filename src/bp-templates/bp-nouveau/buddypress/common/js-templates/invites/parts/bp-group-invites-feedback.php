b0d1d0968d837bfe2dc3be5c0096c527e880aa88
Paresh Radadiya <pareshradadiya@hotmail.com>
2019-04-22 19:27:01 +0530

    split group invite js templates

---
+++ src/bp-templates/bp-nouveau/buddypress/common/js-templates/invites/parts/bp-group-invites-feedback.php
@@ -0,0 +1,6 @@
+<script type="text/html" id="tmpl-bp-group-invites-feedback">
+	<div class="bp-feedback {{data.type}}">
+		<span class="bp-icon" aria-hidden="true"></span>
+		<p>{{{data.message}}}</p>
+	</div>
+</script>
