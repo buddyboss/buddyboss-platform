f2a994d72e52039f8c2c5778de01d963f88fe929
Sagar Jadhav <sagar@buddyboss.com>
2019-04-25 11:59:29 +0530

    Media and Albums

--- src/bp-templates/bp-nouveau/buddypress/groups/single/albums.php
+++ src/bp-templates/bp-nouveau/buddypress/groups/single/albums.php
@@ -6,20 +6,22 @@
  */
 ?>
 
-<?php
+<div class="bb-media-container group-albums">
+	<?php
+	switch ( bp_current_action() ) :
 
-switch ( bp_current_action() ) :
+		// Home/Media/Albums
+		case 'albums':
+			if ( ! bp_is_single_album() )
+				bp_get_template_part( 'media/albums' );
+			else
+				bp_get_template_part( 'media/single-album' );
+			break;
 
-	// Home/Media/Albums
-	case 'albums':
-		if ( ! bp_is_single_album() )
-			bp_get_template_part( 'media/albums' );
-		else
-			bp_get_template_part( 'media/single-album' );
-		break;
-
-	// Any other
-	default:
-		bp_get_template_part( 'groups/single/plugins' );
-		break;
-endswitch;
+		// Any other
+		default:
+			bp_get_template_part( 'groups/single/plugins' );
+			break;
+	endswitch;
+	?>
+</div>
