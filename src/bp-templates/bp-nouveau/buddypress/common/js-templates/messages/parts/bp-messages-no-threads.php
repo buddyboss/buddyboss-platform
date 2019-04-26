f6db231426298ad09f4ba49d8f1412de0cc3d95d
Sagar Jadhav <sagar@buddyboss.com>
2019-04-26 19:44:53 +0530

    Messages

--- src/bp-templates/bp-nouveau/buddypress/common/js-templates/messages/parts/bp-messages-no-threads.php
+++ src/bp-templates/bp-nouveau/buddypress/common/js-templates/messages/parts/bp-messages-no-threads.php
@@ -1,4 +1,9 @@
 <script type="text/html" id="tmpl-bp-messages-no-threads">
-    <?php _e( 'No new messages yet!', 'buddyboss' ); ?>
-    <a href="#" id="bp-new-message"><?php _e( 'Start a new chat', 'buddyboss' ); ?></a>
+	<div class="no-message-wrap">
+		<span class="dashicons dashicons-email-alt"></span>
+		<div class="">
+			<h2><?php _e( 'No new messages yet!', 'buddyboss' ); ?></h2>
+			<p><?php _e( 'Looks like you havent initiated a conversation with any other member.', 'buddyboss' ); ?></p>
+		</div>
+	</div>
 </script>
