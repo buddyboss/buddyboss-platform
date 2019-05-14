1c1ce4fffac2b28a367915ce489be4d0fe33a97f
Sagar Jadhav <sagar@buddyboss.com>
2019-05-14 13:02:18 +0530

    UI fixes - group albums/photos

--- src/bp-templates/bp-nouveau/buddypress/media/album-entry.php
+++ src/bp-templates/bp-nouveau/buddypress/media/album-entry.php
@@ -14,10 +14,11 @@
             <?php if ( ! empty( $media_album_template->album->media['medias'] ) ) : ?>
                 <img src="<?php echo $media_album_template->album->media['medias'][0]->attachment_data->thumb; ?>" />
             <?php endif; ?>
+
+			<div class="bb-album-content-wrap">
+				<h4><?php bp_album_title(); ?></h4>
+				<span><?php echo bp_core_format_date( $media_album_template->album->date_created ); ?></span> <span>&middot;</span> <span><?php printf( _n( '%s photo', '%s photos', $media_album_template->album->media['total'], 'buddyboss' ), number_format_i18n( $media_album_template->album->media['total'] ) ); ?></span>
+			</div>
         </a>
-		<div class="bb-album-content-wrap">
-			<h4><?php bp_album_title(); ?></h4>
-			<span><?php echo bp_core_format_date( $media_album_template->album->date_created ); ?></span> <span>&middot;</span> <span><?php printf( _n( '%s photo', '%s photos', $media_album_template->album->media['total'], 'buddyboss' ), number_format_i18n( $media_album_template->album->media['total'] ) ); ?></span>
-		</div>
     </div>
 </li>
