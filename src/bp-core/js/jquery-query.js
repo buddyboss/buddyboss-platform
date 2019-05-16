94d307e69064e155a923331c5c29ec6bc8b332e5
Vrunda <vrunda@buddyboss.com>
2019-05-16 17:17:17 +0530

    Fixed :  Community  link needs to be highlight when submenu "forum" selected.

--- src/bp-core/js/jquery-query.js
+++ src/bp-core/js/jquery-query.js
@@ -4,3 +4,21 @@ function bp_get_querystring( n ) {
 	var half = location.search.split( n + '=' )[1];
 	return half ? decodeURIComponent( half.split('&')[0] ) : null;
 }
+
+
+/* Code to make parent menu selected for all type of submenu */
+
+jQuery( document ).ready( function() {
+
+	jQuery('.menu-item-has-children .sub-menu li').each(function () { 
+
+		if(jQuery(this).hasClass('current-menu-item')){
+
+			if(!jQuery(this).parent().parent().hasClass('current-menu-parent')){
+        		jQuery(this).parent().parent().addClass('current-menu-parent');
+        	}
+        }
+    });
+
+	
+} );
