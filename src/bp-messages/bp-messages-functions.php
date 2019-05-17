9bea8e667986d058a47cbc0efc54bb130965fe33
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2019-05-17 20:32:53 +0530

    messages - remove strip all html from emails in content

--- src/bp-messages/bp-messages-functions.php
+++ src/bp-messages/bp-messages-functions.php
@@ -673,7 +673,7 @@ function messages_notification_new_message( $raw_args = array() ) {
 		bp_send_email( 'messages-unread', $ud, array(
 			'tokens' => array(
 				'message_id'  => $id,
-				'usermessage' => wp_strip_all_tags( stripslashes( $message ) ),
+				'usermessage' => stripslashes( $message ),
 				'message.url' => esc_url( bp_core_get_user_domain( $recipient->user_id ) . bp_get_messages_slug() . '/view/' . $thread_id . '/' ),
 				'sender.name' => $sender_name,
 				'usersubject' => sanitize_text_field( stripslashes( $subject ) ),
