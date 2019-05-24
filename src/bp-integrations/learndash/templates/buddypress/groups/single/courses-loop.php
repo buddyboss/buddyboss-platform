864614bdac4cafc550c84a33e695176304f05108
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2019-05-24 13:55:14 +0530

    textdomain fix

--- src/bp-integrations/learndash/templates/buddypress/groups/single/courses-loop.php
+++ src/bp-integrations/learndash/templates/buddypress/groups/single/courses-loop.php
@@ -3,7 +3,7 @@
 $is_enrolled       = false;
 $lession_list      = learndash_get_lesson_list( get_the_ID() );
 $lessons_count     = sizeof( $lession_list );
-$total_lessons     = $lessons_count > 1 ? sprintf( __( '%s Lessons', 'buddyboss-theme' ), $lessons_count ) : sprintf( __( '%s Lesson', 'buddyboss-theme' ), $lessons_count );
+$total_lessons     = $lessons_count > 1 ? sprintf( __( '%s Lessons', 'buddyboss' ), $lessons_count ) : sprintf( __( '%s Lesson', 'buddyboss' ), $lessons_count );
 $current_user_id   = get_current_user_id();
 $access_list       = learndash_get_course_meta_setting( $post->ID, 'course_access_list' );
 $admin_enrolled    = LearnDash_Settings_Section::get_section_setting( 'LearnDash_Settings_Section_General_Admin_User', 'courses_autoenroll_admin_users' );
@@ -115,7 +115,7 @@ if ( ! empty( $course_price ) && ( $course_price_type == 'paynow' || $course_pri
 				if( $lessons_count > 0 ) {
 					echo '<div class="course-lesson-count">' . $total_lessons . '</div>';
 				} else {
-					echo '<div class="course-lesson-count">' . __( '0 Lessons', 'buddyboss-theme' ) . '</div>';
+					echo '<div class="course-lesson-count">' . __( '0 Lessons', 'buddyboss' ) . '</div>';
 				}
 				?>
 				<h3 class="course-name">
