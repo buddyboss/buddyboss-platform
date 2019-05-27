7aeebe566f167eb918e54caec9fd732a37773110
Michael Eisenwasser <michael@buddyboss.com>
2019-05-27 00:58:38 -0500

    Emoji > View Tutorial

--- src/bp-media/bp-media-settings.php
+++ src/bp-media/bp-media-settings.php
@@ -65,13 +65,6 @@ function bp_media_get_settings_fields() {
 		        'class' => 'hidden'
 	        ]
         ],
-
-/*
-        'bp_photo_uploading_tutorial' => [
-			'title'             => __( 'Tutorial', 'buddyboss' ),
-			'callback'          => 'bp_photo_uploading_tutorial',
-		],
-*/
 	];
 
 	if ( bp_is_active( 'groups' ) ) {
@@ -148,6 +141,11 @@ function bp_media_get_settings_fields() {
 			'args'              => []
 		],
 
+	];
+
+	$fields['bp_media_settings_emoji']['bp_emoji_tutorial'] = [
+		'title'             => __( '&#160;', 'buddyboss' ),
+		'callback'          => 'bp_emoji_tutorial',
 	];
 
 	$fields['bp_media_settings_gifs'] = [
@@ -585,6 +583,22 @@ function bp_is_messages_emoji_support_enabled( $default = 0 ) {
  */
 function bp_is_forums_emoji_support_enabled( $default = 0 ) {
 	return (bool) apply_filters( 'bp_is_forums_emoji_support_enabled', (bool) get_option( 'bp_media_forums_emoji_support', $default ) );
+}
+
+/**
+ * Link to Emoji tutorial
+ *
+ * @since BuddyBoss 1.0.0
+ *
+ */
+function bp_emoji_tutorial() {
+	?>
+
+	<p>
+		<a class="button" href="<?php echo bp_core_help_docs_link( 'components/media/emoji.md' ); ?>"><?php _e( 'View Tutorial', 'buddyboss' ); ?></a>
+	</p>
+
+	<?php
 }
 
 /**
