aaf5c2ac2c6b684b9e4f248e9471a67f0ee0790a
Chetan Satasiya <chetan@buddyboss.com>
2019-05-31 18:50:11 +0530

    Single Course with a group layout

--- src/bp-integrations/learndash/templates/buddypress/groups/single/courses-courses.php
+++ src/bp-integrations/learndash/templates/buddypress/groups/single/courses-courses.php
@@ -1,7 +1,28 @@
+<?php
+$count   = count( bp_ld_sync('buddypress')->courses->getGroupCourses() );
+$courses_new = bp_ld_sync('buddypress')->courses->getGroupCourses();
+?>
+
 <div id="courses-group-list" class="group_courses dir-list" data-bp-list="group_courses">
     <ul id="courses-list" class="item-list courses-group-list bp-list">
-		<?php foreach (bp_ld_sync('buddypress')->courses->getGroupCourses() as $post): setup_postdata($post); ?>
-			<?php bp_locate_template('groups/single/courses-loop.php', true, false); ?>
-		<?php endforeach; wp_reset_postdata(); ?>
+		<?php
+		foreach (bp_ld_sync('buddypress')->courses->getGroupCourses() as $post): setup_postdata($post);
+			bp_locate_template('groups/single/courses-loop.php', true, false);
+
+
+		endforeach;
+
+		wp_reset_postdata(); ?>
 	</ul>
 </div>
+<?php
+if ( 1 === $count ) {
+	?>
+	<div class="single-course-content">
+		<?php
+		echo apply_filters( 'the_content', $courses_new[0]->post_content );
+		echo do_shortcode( '[course_content course_id="'.$courses_new[0]->ID.'"]' );
+		?>
+	</div>
+	<?php
+}
