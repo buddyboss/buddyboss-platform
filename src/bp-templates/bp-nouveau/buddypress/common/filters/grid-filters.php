18f74e001ba36df69c688d5106a5eab4c5540ce2
Deepak Gupta <raftaar1191@gmail.com>
2019-06-11 00:55:15 +0530

    Update Code via WP coding standard

--- src/bp-templates/bp-nouveau/buddypress/common/filters/grid-filters.php
+++ src/bp-templates/bp-nouveau/buddypress/common/filters/grid-filters.php
@@ -7,6 +7,13 @@
 ?>
 
 <div class="grid-filters" data-object="<?php echo bp_current_component(); ?>">
-	<a href="#" class="layout-view layout-grid-view active bp-tooltip" data-bp-tooltip-pos="up" data-bp-tooltip="<?php _e('Grid View', 'buddyboss'); ?>"><i class="dashicons dashicons-screenoptions" aria-hidden="true"></i></a>
-	<a href="#" class="layout-view layout-list-view bp-tooltip" data-bp-tooltip-pos="up" data-bp-tooltip="<?php _e('List View', 'buddyboss'); ?>"><i class="dashicons dashicons-menu" aria-hidden="true"></i></a>
+    <a href="#" class="layout-view layout-grid-view active bp-tooltip"  data-view="grid" data-bp-tooltip-pos="up"
+       data-bp-tooltip="<?php _e( 'Grid View', 'buddyboss' ); ?>">
+        <i class="dashicons dashicons-screenoptions" aria-hidden="true"></i>
+    </a>
+
+    <a href="#" class="layout-view layout-list-view bp-tooltip" data-view="list" data-bp-tooltip-pos="up"
+       data-bp-tooltip="<?php _e( 'List View', 'buddyboss' ); ?>">
+        <i class="dashicons dashicons-menu" aria-hidden="true"></i>
+    </a>
 </div>
