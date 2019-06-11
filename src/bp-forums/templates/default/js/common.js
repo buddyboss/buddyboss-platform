59b6f9c6bd7f7775493cf051de2fe65db8bb953d
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2019-06-12 01:15:33 +0530

    forums - common js fixes

--- src/bp-forums/templates/default/js/common.js
+++ src/bp-forums/templates/default/js/common.js
@@ -1,7 +1,12 @@
 jQuery(document).ready( function() {
-    var input = document.querySelector('input[name=bbp_topic_tags]');
-    tagify = new Tagify( input );
+    if ( typeof window.Tagify !== 'undefined' ) {
+        var input = document.querySelector('input[name=bbp_topic_tags]');
+        var tagify = new window.Tagify(input);
 
-    // "remove all tags" button event listener
-    document.querySelector('.js-modal-close').addEventListener('click', tagify.removeAllTags.bind(tagify))
+        // "remove all tags" button event listener
+        var js_modal_close = document.querySelector('.js-modal-close');
+        if (typeof js_modal_close !== 'undefined') {
+            document.querySelector('.js-modal-close').addEventListener('click', tagify.removeAllTags.bind(tagify));
+        }
+    }
 });
