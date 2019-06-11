bfb3d0698f52315f13fbbbb2f4d562918df1bd83
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2019-06-12 02:07:57 +0530

    grunt build

--- src/bp-forums/templates/default/js/common.js
+++ src/bp-forums/templates/default/js/common.js
@@ -1,12 +1,12 @@
 jQuery(document).ready( function() {
     if ( typeof window.Tagify !== 'undefined' ) {
         var input = document.querySelector('input[name=bbp_topic_tags]');
-        var tagify = new window.Tagify(input);
 
-        // "remove all tags" button event listener
-        var js_modal_close = document.querySelector('.js-modal-close');
-        if (typeof js_modal_close !== 'undefined') {
-            document.querySelector('.js-modal-close').addEventListener('click', tagify.removeAllTags.bind(tagify));
+        if ( input != null ) {
+            var tagify = new window.Tagify(input);
+
+            // "remove all tags" button event listener
+            jQuery('.js-modal-close').on('click', tagify.removeAllTags.bind(tagify));
         }
     }
 });
