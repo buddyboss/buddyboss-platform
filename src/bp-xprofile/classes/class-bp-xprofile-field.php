029e2a9cbe416fe0582adc91354423712bc2df0b
Deepak Gupta <raftaar1191@gmail.com>
2019-07-05 20:27:01 +0530

    Fix PHP notices when loading the sites #953

--- src/bp-xprofile/classes/class-bp-xprofile-field.php
+++ src/bp-xprofile/classes/class-bp-xprofile-field.php
@@ -927,15 +927,15 @@ class BP_XProfile_Field {
 	 * @return bool|null|string
 	 */
 	public static function get_type( $field_id = 0 ) {
-		global $wpdb;
+		global $wpdb, $bp;
 
 		// Bail if no field ID.
 		if ( empty( $field_id ) ) {
 			return false;
 		}
 
-		$bp   = buddypress();
-		$sql  = $wpdb->prepare( "SELECT type FROM {$bp->profile->table_name_fields} WHERE id = %d", $field_id );
+		$table_name = $bp->table_prefix . 'bp_xprofile_data';
+		$sql  = $wpdb->prepare( "SELECT type FROM {$table_name} WHERE id = %d", $field_id );
 		$type = $wpdb->get_var( $sql );
 
 		// Return field type.
