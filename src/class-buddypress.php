ca154c14ff0db3f9d5388668ae790c1f893c3583
Deepak Gupta <raftaar1191@gmail.com>
2019-07-17 21:20:59 +0530

    Fix issue into the bbPress #810

--- src/class-buddypress.php
+++ src/class-buddypress.php
@@ -325,7 +325,7 @@ class BuddyPress {
 	private function setup_globals() {
 
 		/** Versions **********************************************************/
-		$this->version    = defined('BP_PLATFORM_VERSION' ) ? BP_PLATFORM_VERSION : '1.0.0';
+		$this->version    = defined('BP_VERSION' ) ? BP_VERSION : defined('BP_PLATFORM_VERSION' ) ? BP_PLATFORM_VERSION : '1.0.0';;
 		$this->db_version = 13731;
 
 		/** Loading ***********************************************************/
