eb4cc363865ea38312a375bd622f895052b1dd40
Michael Eisenwasser <michael@buddyboss.com>
2019-08-15 01:29:07 +0800

    removing the “Activity Scopes/Tabs” option
    - This feature is not ready for release

--- src/bp-core/admin/settings/bp-admin-setting-activity.php
+++ src/bp-core/admin/settings/bp-admin-setting-activity.php
@@ -86,7 +86,7 @@ class BP_Admin_Setting_Activity extends BP_Admin_Setting_tab {
 		$this->add_field( '_bp_enable_activity_autoload', __( 'Activity auto-load', 'buddyboss' ), 'bp_admin_setting_callback_enable_activity_autoload', 'intval' );
 
 		// Allow scopes/tabs.
-		$this->add_field( '_bp_enable_activity_scopes', __( 'Activity tabs', 'buddyboss' ), 'bp_admin_setting_callback_enable_activity_scopes', 'intval' );
+//		$this->add_field( '_bp_enable_activity_scopes', __( 'Activity tabs', 'buddyboss' ), 'bp_admin_setting_callback_enable_activity_scopes', 'intval' );
 
 		// Allow follow.
 		$this->add_field( '_bp_enable_activity_follow', __( 'Follow', 'buddyboss' ), 'bp_admin_setting_callback_enable_activity_follow', 'intval' );
