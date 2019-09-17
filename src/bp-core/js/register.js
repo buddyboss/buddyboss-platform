7b7f836711af8581b568c5abe4d4f34468ca2127
Manish Patolia <manishpatolia09@gmail.com>
2019-09-17 20:35:44 +0530

    T#1200 prevent tab key

--- src/bp-core/js/register.js
+++ src/bp-core/js/register.js
@@ -167,8 +167,14 @@ jQuery( document ).ready( function() {
 	});
 
 	// Bind signup_email to keyup events in the email fields
-	jQuery( '#signup_email' ).val( '' ).on('keyup change', bp_register_validate_email );
-	jQuery( '#signup_email_confirm' ).val( '' ).on('keyup change', bp_register_validate_confirm_email );
+	if ( jQuery( '#signup_email' ). length ) {
+        jQuery( '#signup_email' ).val( '' ).on('keyup change', bp_register_validate_email );
+    }
+
+    if ( jQuery( '#signup_email_confirm' ). length ) {
+        jQuery( '#signup_email_confirm' ).val( '' ).on( 'keyup change' , bp_register_validate_confirm_email );
+    }
+
 	
 	function bp_register_validate_email() {
 		var email1 = jQuery( '#signup_email' ).val(),
@@ -176,16 +182,16 @@ jQuery( document ).ready( function() {
 		    regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 		// Reset classes and result text
 		jQuery( '#email-strength-result' ).removeClass( 'show mismatch bad' );
-		if( ( email1 =='' && email2 =='' ) && regex.test( email1 ) ) {
+		if( ( email1 == '' && email2 == '' ) && regex.test( email1 ) ) {
 			jQuery( '#email-strength-result' ).html( '' );
 			return;
 		}else{
 			jQuery( '#email-strength-result' ).html( '' );
-			if ( ( email1 !='' || email2 !='' ) && !regex.test( email1 ) ) {
+			if ( ( email1 != '' || email2 != '' ) && !regex.test( email1 ) ) {
 				jQuery( '#email-strength-result' ).addClass( 'show bad' ).html( BP_Register.valid_email );
 				return;
 			}
-			if ( ( email2 !='' ) && ( email1 != email2 ) ) {
+			if ( ( email2 != '' ) && ( email1 != email2 ) ) {
 				jQuery( '#email-strength-result' ).addClass( 'show mismatch' ).html( BP_Register.mismatch_email );
 				return;
 			}
@@ -193,24 +199,24 @@ jQuery( document ).ready( function() {
 	}
 
 	function bp_register_validate_confirm_email() {
+		
+		if(	window.event.keyCode == 9 ){
+			return;
+		}
+
 		var email1 = jQuery( '#signup_email' ).val(),
 		    email2 = jQuery( '#signup_email_confirm' ).val(),
 		    regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 		// Reset classes and result text
 		jQuery( '#email-strength-result' ).removeClass( 'show mismatch bad' );
-		if ( ( email1 =='' && email2 =='' ) || ( regex.test( email2 ) && regex.test( email1 ) && ( email1 == email2	) ) ) {
+		if ( ( email1 == '' && email2 == '' ) || ( email2 == '') || ( regex.test( email2 ) && regex.test( email1 ) && ( email1 == email2	) ) ) {
 			jQuery( '#email-strength-result' ).html( '' );
 			return;
 		}
-		// if ( (!regex.test( email1 ) || !regex.test( email2 ) ) ) {
-		// 	jQuery( '#email-strength-result' ).addClass( 'show bad' ).html( BP_Register.valid_email );
-		// 	return;
-		// }else{
-			if ( email1 !== email2 ) {
-				jQuery( '#email-strength-result' ).addClass( 'show mismatch' ).html( BP_Register.mismatch_email );
-				return;
-			}
-		//}
+		if ( email1 !== email2 ) {
+			jQuery( '#email-strength-result' ).addClass( 'show mismatch' ).html( BP_Register.mismatch_email );
+			return;
+		}
 	}
 
 } );
