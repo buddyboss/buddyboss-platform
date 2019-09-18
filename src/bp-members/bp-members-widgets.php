3121ed12078fa377c90f68e4e3b8ec37358b7363
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2019-09-18 19:53:15 +0530

    bp-members phpcbf fixes

--- src/bp-members/bp-members-widgets.php
+++ src/bp-members/bp-members-widgets.php
@@ -17,9 +17,24 @@ defined( 'ABSPATH' ) || exit;
  * @since BuddyPress 2.2.0
  */
 function bp_members_register_widgets() {
-	add_action( 'widgets_init', function() { return register_widget( 'BP_Core_Members_Widget' );         } );
-	add_action( 'widgets_init', function() { return register_widget( 'BP_Core_Whos_Online_Widget' );     } );
-	add_action( 'widgets_init', function() { return register_widget( 'BP_Core_Recently_Active_Widget' ); } );
+	add_action(
+		'widgets_init',
+		function() {
+			return register_widget( 'BP_Core_Members_Widget' );
+		}
+	);
+	add_action(
+		'widgets_init',
+		function() {
+			return register_widget( 'BP_Core_Whos_Online_Widget' );
+		}
+	);
+	add_action(
+		'widgets_init',
+		function() {
+			return register_widget( 'BP_Core_Recently_Active_Widget' );
+		}
+	);
 }
 add_action( 'bp_register_widgets', 'bp_members_register_widgets' );
 
@@ -31,23 +46,23 @@ add_action( 'bp_register_widgets', 'bp_members_register_widgets' );
  * @see BP_Core_Members_Widget
  */
 function bp_core_ajax_widget_members() {
-    global $members_template;
+	global $members_template;
 	check_ajax_referer( 'bp_core_widget_members' );
 
 	// Setup some variables to check.
-	$filter      = ! empty( $_POST['filter']      ) ? $_POST['filter']                : 'recently-active-members';
+	$filter      = ! empty( $_POST['filter'] ) ? $_POST['filter'] : 'recently-active-members';
 	$max_members = ! empty( $_POST['max-members'] ) ? absint( $_POST['max-members'] ) : 5;
 
 	// Determine the type of members query to perform.
 	switch ( $filter ) {
 
 		// Newest activated.
-		case 'newest-members' :
+		case 'newest-members':
 			$type = 'newest';
 			break;
 
 		// Popular by friends.
-		case 'popular-members' :
+		case 'popular-members':
 			if ( bp_is_active( 'friends' ) ) {
 				$type = 'popular';
 			} else {
@@ -56,8 +71,8 @@ function bp_core_ajax_widget_members() {
 			break;
 
 		// Default.
-		case 'recently-active-members' :
-		default :
+		case 'recently-active-members':
+		default:
 			$type = 'active';
 			break;
 	}
@@ -70,48 +85,53 @@ function bp_core_ajax_widget_members() {
 		'max'             => $max_members,
 		'populate_extras' => true,
 		'search_terms'    => false,
-		'exclude'		  => bp_get_users_of_removed_member_types(),
+		'exclude'         => bp_get_users_of_removed_member_types(),
 	);
 
 	// Query for members.
 	if ( bp_has_members( $members_args ) ) : ?>
 		<?php echo '0[[SPLIT]]'; // Return valid result. TODO: remove this. ?>
-		<?php while ( bp_members() ) : bp_the_member(); ?>
+		<?php
+		while ( bp_members() ) :
+			bp_the_member();
+			?>
 			<li class="vcard">
-                <div class="item-avatar">
-                    <a href="<?php bp_member_permalink() ?>">
+				<div class="item-avatar">
+					<a href="<?php bp_member_permalink(); ?>">
 						<?php bp_member_avatar(); ?>
 						<?php
 						$current_time = current_time( 'mysql', 1 );
-						$diff =  strtotime( $current_time ) - strtotime( $members_template->member->last_activity );
-						if ( $diff < 300 ) { // 5 minutes  =  5 * 60 ?>
+						$diff         = strtotime( $current_time ) - strtotime( $members_template->member->last_activity );
+						if ( $diff < 300 ) { // 5 minutes  =  5 * 60
+							?>
 							<span class="member-status online"></span>
 						<?php } ?>
 					</a>
-                </div>
+				</div>
 
-                <div class="item">
-                    <div class="item-title fn"><a href="<?php bp_member_permalink(); ?>"><?php bp_member_name(); ?></a></div>
-                    <div class="item-meta">
+				<div class="item">
+					<div class="item-title fn"><a href="<?php bp_member_permalink(); ?>"><?php bp_member_name(); ?></a></div>
+					<div class="item-meta">
 						<?php if ( 'newest' == $settings['member_default'] ) : ?>
-                            <span class="activity" data-livestamp="<?php bp_core_iso8601_date( bp_get_member_registered( array( 'relative' => false ) ) ); ?>"><?php bp_member_registered(); ?></span>
+							<span class="activity" data-livestamp="<?php bp_core_iso8601_date( bp_get_member_registered( array( 'relative' => false ) ) ); ?>"><?php bp_member_registered(); ?></span>
 						<?php elseif ( 'active' == $settings['member_default'] ) : ?>
-                            <span class="activity" data-livestamp="<?php bp_core_iso8601_date( bp_get_member_last_active( array( 'relative' => false ) ) ); ?>"><?php bp_member_last_active(); ?></span>
+							<span class="activity" data-livestamp="<?php bp_core_iso8601_date( bp_get_member_last_active( array( 'relative' => false ) ) ); ?>"><?php bp_member_last_active(); ?></span>
 						<?php else : ?>
-                            <span class="activity"><?php bp_member_total_friend_count(); ?></span>
+							<span class="activity"><?php bp_member_total_friend_count(); ?></span>
 						<?php endif; ?>
-                    </div>
-                </div>
-                <div class="member_last_visit"></div>
+					</div>
+				</div>
+				<div class="member_last_visit"></div>
 			</li>
 
 		<?php endwhile; ?>
 
-	<?php else: ?>
-		<?php echo "-1[[SPLIT]]<li>"; ?>
-		<?php esc_html_e( 'There were no members found, please try another filter.', 'buddyboss' ) ?>
-		<?php echo "</li>"; ?>
-	<?php endif;
+	<?php else : ?>
+		<?php echo '-1[[SPLIT]]<li>'; ?>
+		<?php esc_html_e( 'There were no members found, please try another filter.', 'buddyboss' ); ?>
+		<?php echo '</li>'; ?>
+		<?php
+	endif;
 }
-add_action( 'wp_ajax_widget_members',        'bp_core_ajax_widget_members' );
+add_action( 'wp_ajax_widget_members', 'bp_core_ajax_widget_members' );
 add_action( 'wp_ajax_nopriv_widget_members', 'bp_core_ajax_widget_members' );
