558448e115e2f90ec7f85aecbd9d14713bae8447
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2019-09-25 19:44:44 +0530

    T1468 - delete album permission check

--- src/bp-templates/bp-nouveau/includes/media/ajax.php
+++ src/bp-templates/bp-nouveau/includes/media/ajax.php
@@ -690,6 +690,15 @@ function bp_nouveau_ajax_media_album_delete() {
 		wp_send_json_error( $response );
 	}
 
+	if ( ! bp_album_user_can_delete( $_POST['album_id'] ) ) {
+		$response['feedback'] = sprintf(
+			'<div class="bp-feedback error"><span class="bp-icon" aria-hidden="true"></span><p>%s</p></div>',
+			esc_html__( 'You do not have permission to delete this album.', 'buddyboss' )
+		);
+
+		wp_send_json_error( $response );
+	}
+
 	// delete album
 	$album_id = bp_album_delete( array( 'id' => $_POST['album_id'] ) );
 
