e2e71d7d2db9b79401594f160dbcc38405b82454
Deepak Gupta <raftaar1191@gmail.com>
2019-10-10 23:57:48 +0530

    T:git diff --cathed remove unwanted code

--- src/bp-templates/bp-nouveau/buddypress/members/single/courses/certificates.php
+++ src/bp-templates/bp-nouveau/buddypress/members/single/courses/certificates.php
@@ -1,41 +1,40 @@
 <?php
-$user_id = bp_displayed_user_id();
-$certificates  = bp_learndash_get_users_certificates( $user_id );
+$user_id      = bp_displayed_user_id();
+$certificates = bp_learndash_get_users_certificates( $user_id );
 ?>
 
 <div id="bb-learndash-profile" class="bb-certificates-wrapper">
 	<?php if ( ! empty( $certificates ) ) { ?>
-		
-		<ul id="certificate_list" class="bb-grid">
+
+        <ul id="certificate_list" class="bb-grid">
 			<?php foreach ( $certificates as $certificate ) { ?>
-				
-				<li class="sm-grid-1-1">
-					<div class="bb-certificate-wrap">
-						<div class="bb-certificate-content">
-							<h3 class="bb-certificate-title"><a href="<?php echo $certificate->link; ?>"><?php echo $certificate->title; ?></a></h3>
-							<div class="bb-certificate-text"><?php echo wp_trim_words( $certificate->content, 10, '&hellip;' ); ?></div>
-							<div class="bb-certificate-date"><?php echo mysql2date( 'F j, Y', $certificate->date ); ?></div>
-						</div>
 
-						<?php if( !empty($certificate->image) ) { ?>
-							<a class="bb-certificate-img" href="<?php echo $certificate->link; ?>" title="<?php echo $certificate->title; ?>">
-								<img src="<?php echo $certificate->image; ?>" alt="<?php echo $certificate->title; ?>"/>
-							</a>
+                <li class="sm-grid-1-1">
+                    <div class="bb-certificate-wrap">
+                        <div class="bb-certificate-content">
+                            <h3 class="bb-certificate-title"><a
+                                        href="<?php echo $certificate->link; ?>"><?php echo $certificate->title; ?></a>
+                            </h3>
+                            <div class="bb-certificate-text"><?php echo wp_trim_words( $certificate->content, 10, '&hellip;' ); ?></div>
+                            <div class="bb-certificate-date"><?php echo mysql2date( 'F j, Y', $certificate->date ); ?></div>
+                        </div>
+
+						<?php if ( ! empty( $certificate->image ) ) { ?>
+                            <a class="bb-certificate-img" href="<?php echo $certificate->link; ?>"
+                               title="<?php echo $certificate->title; ?>">
+                                <img src="<?php echo $certificate->image; ?>" alt="<?php echo $certificate->title; ?>"/>
+                            </a>
 						<?php } ?>
-					</div>
-	            </li>
-
-	        ?>
+                    </div>
+                </li>
+			<?php } ?>
         </ul>
-        
-    <?php } } else { ?>
-
-		<aside class="bp-feedback bp-messages info">
-			
-			<span class="bp-icon" aria-hidden="true"></span>
-			<p><?php _e( 'Sorry, no certificates were found.', 'buddyboss' ); ?></p>
-			
-		</aside>
+		<?php
+	} else { ?>
+        <aside class="bp-feedback bp-messages info">
+            <span class="bp-icon" aria-hidden="true"></span>
+            <p><?php _e( 'Sorry, no certificates were found.', 'buddyboss' ); ?></p>
+        </aside>
 
 	<?php } ?>
 </div>
