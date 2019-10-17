4f03b5be1724ea090f68a455ba114311e0952f38
Michael Eisenwasser <michael@buddyboss.com>
2019-10-17 14:38:01 -0700

    Courses - removing “Certificate Image” as its actually the background image

--- src/bp-templates/bp-nouveau/buddypress/members/single/courses/certificates.php
+++ src/bp-templates/bp-nouveau/buddypress/members/single/courses/certificates.php
@@ -18,13 +18,6 @@ $certificates = bp_learndash_get_users_certificates( $user_id );
                             <div class="bb-certificate-text"><?php echo wp_trim_words( $certificate->content, 10, '&hellip;' ); ?></div>
                             <div class="bb-certificate-date"><?php echo mysql2date( 'F j, Y', $certificate->date ); ?></div>
                         </div>
-
-						<?php if ( ! empty( $certificate->image ) ) { ?>
-                            <a class="bb-certificate-img" href="<?php echo $certificate->link; ?>"
-                               title="<?php echo $certificate->title; ?>">
-                                <img src="<?php echo $certificate->image; ?>" alt="<?php echo $certificate->title; ?>"/>
-                            </a>
-						<?php } ?>
                     </div>
                 </li>
 			<?php } ?>
