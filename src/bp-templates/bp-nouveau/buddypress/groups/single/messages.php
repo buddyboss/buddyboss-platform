a46ecdb2f8a6c473b8a6664bad0d323d41f49eff
Michael Eisenwasser <michael@buddyboss.com>
2019-10-26 20:17:46 -0700

    #T1499 - change text “Search” to “Search Members”

--- src/bp-templates/bp-nouveau/buddypress/groups/single/messages.php
+++ src/bp-templates/bp-nouveau/buddypress/groups/single/messages.php
@@ -8,10 +8,10 @@
 			<div class="bp-search">
 				<form action="" method="get" id="group_messages_search_form" class="bp-messages-search-form" data-bp-search="group-messages">
 					<label for="group_messages_search" class="bp-screen-reader-text"><?php bp_nouveau_search_default_text( __( 'Search Members', 'buddyboss' ), false ); ?></label>
-					<input type="search" id="group_messages_search" placeholder="<?php esc_attr_e( 'Search', 'buddyboss' ); ?>"/>
+					<input type="search" id="group_messages_search" placeholder="<?php esc_attr_e( 'Search Members', 'buddyboss' ); ?>"/>
 					<button type="submit" id="group_messages_search_submit" class="nouveau-search-submit">
 						<span class="dashicons dashicons-search" aria-hidden="true"></span>
-						<span id="button-text" class="bp-screen-reader-text"><?php esc_html_e( 'Search', 'buddyboss' ); ?></span>
+						<span id="button-text" class="bp-screen-reader-text"><?php esc_html_e( 'Search Members', 'buddyboss' ); ?></span>
 					</button>
 				</form>
 			</div>
