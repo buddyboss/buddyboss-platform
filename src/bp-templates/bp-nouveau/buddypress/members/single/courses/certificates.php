016a5693eb06a5ea0c5210f8a74f33f416050c1e
Deepak Gupta <raftaar1191@gmail.com>
2019-10-28 10:37:20 +0530

    Fix date and course title in certificate

--- src/bp-templates/bp-nouveau/buddypress/members/single/courses/certificates.php
+++ src/bp-templates/bp-nouveau/buddypress/members/single/courses/certificates.php
@@ -7,16 +7,25 @@ $certificates = bp_learndash_get_users_certificates( $user_id );
 	<?php if ( ! empty( $certificates ) ) { ?>
 
         <ul id="certificate_list" class="bb-grid">
-			<?php foreach ( $certificates as $certificate ) { ?>
+			<?php foreach ( $certificates as $certificate ) {
 
+				?>
                 <li class="sm-grid-1-1">
                     <div class="bb-certificate-wrap">
                         <div class="bb-certificate-content">
                             <h3 class="bb-certificate-title">
-                                <?php _e( 'Certificate in', 'buddyboss' ); ?> <?php echo $certificate->title; ?>
+								<?php
+								printf(
+									__( 'Certificate in <a href="%s">%s</a>', 'buddyboss' ),
+									get_permalink( $certificate->ID ),
+									$certificate->title
+								);
+								?>
                             </h3>
-                            <div class="bb-certificate-date"><?php _e( 'Earned on', 'buddyboss' ); ?> <?php echo mysql2date( 'F j, Y', $certificate->date ); ?></div>
-                            <p><a href="<?php echo $certificate->link; ?>"><?php _e( 'Download PDF', 'buddyboss' ); ?></a></p>
+                            <div class="bb-certificate-date"><?php printf( __( 'Earned on %s', 'buddyboss' ), bp_core_get_format_date( $certificate->date ) ); ?></div>
+                            <p>
+                                <a href="<?php echo $certificate->link; ?>"><?php _e( 'Download PDF', 'buddyboss' ); ?></a>
+                            </p>
                         </div>
                     </div>
                 </li>
