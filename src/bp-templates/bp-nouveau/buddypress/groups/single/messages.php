8dcdb75530009bedcb320d8da24398b9e304cebf
Sagar Jadhav <sagarjadhav27@gmail.com>
2019-11-18 22:07:14 +0530

    Groups messages styling changes

--- src/bp-templates/bp-nouveau/buddypress/groups/single/messages.php
+++ src/bp-templates/bp-nouveau/buddypress/groups/single/messages.php
@@ -6,12 +6,14 @@
 <!--					<option value="all">--><?php //_e( 'All Group Members', 'buddyboss' ); ?><!--</option>-->
 <!--					<option value="single">--><?php //_e( 'Select Members', 'buddyboss' ); ?><!--</option>-->
 <!--				</select>-->
-				<h4 class="total-members-text"></h4>
-				<h5><?php esc_html_e( 'All Members', 'buddyboss' ); ?></h5>
-				<label class="bp-group-message-switch">
-					<input type="checkbox" checked>
-					<span class="bp-group-message-slider bp-group-message-round"></span>
-				</label>
+				<!--<h5><?php //esc_html_e( 'All Members', 'buddyboss' ); ?></h5>-->
+				<div class="bb-panel-subhead">
+					<h4 class="total-members-text"></h4>
+					<label class="bp-group-message-switch bp-tooltip" data-bp-tooltip-pos="left" data-bp-tooltip="<?php _e( 'Select Members', 'buddyboss' ); ?>">
+						<input type="checkbox" checked>
+						<span class="bp-group-message-slider bp-group-message-round"></span>
+					</label>
+				</div>
 				<div id="bp-message-dropdown-options" class="bp-message-dropdown-options-hide">
 					<div>
 						<i class="dashicons dashicons-update animate-spin"></i>
