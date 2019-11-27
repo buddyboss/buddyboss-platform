f92f965709bd0c8d3915e8aabc895d72b25d41be
Michael Eisenwasser <michael@buddyboss.com>
2019-11-27 11:39:57 -0600

    #247 - styling and text for “offline” notice in Help section

--- src/bp-core/admin/bp-core-admin-functions.php
+++ src/bp-core/admin/bp-core-admin-functions.php
@@ -1301,7 +1301,6 @@ function bp_admin_email_add_codex_notice() {
 	bp_core_add_admin_notice(
 		sprintf(
 			__( 'Phrases wrapped in braces <code>{{ }}</code> are email tokens. <a href="%s">Learn about email tokens</a>.', 'buddyboss' ),
-			//bp_get_admin_url( 'admin.php?page=bp-help&article=62844' )
 			bp_get_admin_url( 
 				add_query_arg( 
 					array( 
