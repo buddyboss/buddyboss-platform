1bd1d92a39590f81d6b1c0c3fadc0a8d40a707c4
Michael Eisenwasser <michael@buddyboss.com>
2019-12-11 10:56:08 -0800

    Activity Preview Styling - making it look more like wp-embed

--- src/bp-templates/bp-nouveau/buddypress/common/js-templates/activity/parts/bp-activity-link-preview.php
+++ src/bp-templates/bp-nouveau/buddypress/common/js-templates/activity/parts/bp-activity-link-preview.php
@@ -4,10 +4,15 @@
 	<span class="activity-url-scrapper-loading activity-ajax-loader"><?php esc_html_e( 'Loading preview...', 'buddyboss' ) ?></span>
 	<# } #>
 	<# if ( data.link_success || data.link_error ) { #>
-	<div id="activity-url-scrapper" class="activity-link-preview-container">
+	<div class="activity-link-preview-container">		
+	
+		<# if ( data.link_success && ! data.link_error ) { #>
+		<div class="activity-link-preview-title">{{data.link_title}}</div>
+		<# } #>
+
 		<# if ( data.link_images.length && data.link_success && ! data.link_error ) { #>
 		<div id="activity-url-scrapper-img-holder">
-			<div id="activity-url-scrapper-img" class="activity-link-preview-image">
+			<div class="activity-link-preview-image">
 				<img src="{{{data.link_images[data.link_image_index]}}}"/>
 				<a title="Cancel Preview Image" href="#" id="activity-link-preview-close-image">
 					<i class="dashicons dashicons-no-alt"></i>
@@ -24,21 +29,20 @@
 			<# } #>
 		</div>
 		<# } #>
-		<div id="activity-url-scrapper-text-holder" class="activity-link-preview-content">
-			<# if ( data.link_success && ! data.link_error ) { #>
-			<div id="activity-url-scrapper-title" class="activity-link-preview-title">{{data.link_title}}</div>
-			<div id="activity-url-scrapper-description" class="activity-link-preview-excerpt">{{{data.link_description}}}</div>
-			<# } #>
-			<# if ( data.link_error && ! data.link_success ) { #>
-			<div id="activity-url-error" class="activity-url-error">{{data.link_error_msg}}</div>
-			<# } #>
-			<# if ( data.link_description.indexOf('iframe') > -1 ) { #>
-			<a style="display: none;" title="Cancel Preview" href="#" id="activity-close-link-suggestion"><i class="dashicons dashicons-no-alt"></i></a>
-			<# } else { #>
-			<a title="Cancel Preview" href="#" id="activity-close-link-suggestion"><i class="dashicons dashicons-no-alt"></i></a>
-			<# } #>
 
-		</div>
+		<# if ( data.link_success && ! data.link_error ) { #>
+		<div class="activity-link-preview-excerpt">{{{data.link_description}}}</div>
+		<# } #>
+
+		<# if ( data.link_error && ! data.link_success ) { #>
+		<div id="activity-url-error" class="activity-url-error">{{data.link_error_msg}}</div>
+		<# } #>
+		<# if ( data.link_description.indexOf('iframe') > -1 ) { #>
+		<a style="display: none;" title="Cancel Preview" href="#" id="activity-close-link-suggestion"><i class="dashicons dashicons-no-alt"></i></a>
+		<# } else { #>
+		<a title="Cancel Preview" href="#" id="activity-close-link-suggestion"><i class="dashicons dashicons-no-alt"></i></a>
+		<# } #>
+
 	</div>
 	<# } #>
 	<# } #>
