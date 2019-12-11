ddc6af574484a8997d495542f815249af3df1c5d
Michael Eisenwasser <michael@buddyboss.com>
2019-12-11 10:36:14 -0800

    Activity Preview Styling - making it look more like wp-embed

--- src/bp-templates/bp-nouveau/buddypress/common/js-templates/activity/parts/bp-activity-link-preview.php
+++ src/bp-templates/bp-nouveau/buddypress/common/js-templates/activity/parts/bp-activity-link-preview.php
@@ -1,12 +1,12 @@
 <script type="text/html" id="tmpl-activity-link-preview">
 	<# if ( data.link_scrapping ) { #>
 	<# if ( data.link_loading ) { #>
-	<span class="activity-url-scrapper-loading activity-ajax-loader"><?php esc_html_e( 'Processing...', 'buddyboss' ) ?></span>
+	<span class="activity-url-scrapper-loading activity-ajax-loader"><?php esc_html_e( 'Loading preview...', 'buddyboss' ) ?></span>
 	<# } #>
 	<# if ( data.link_success || data.link_error ) { #>
 	<div id="activity-url-scrapper" class="activity-link-preview-container">
 		<# if ( data.link_images.length && data.link_success && ! data.link_error ) { #>
-		<div id="activity-url-scrapper-img-holder" class="activity-link-preview-image-wrap">
+		<div id="activity-url-scrapper-img-holder">
 			<div id="activity-url-scrapper-img" class="activity-link-preview-image">
 				<img src="{{{data.link_images[data.link_image_index]}}}"/>
 				<a title="Cancel Preview Image" href="#" id="activity-link-preview-close-image">
@@ -27,7 +27,7 @@
 		<div id="activity-url-scrapper-text-holder" class="activity-link-preview-content">
 			<# if ( data.link_success && ! data.link_error ) { #>
 			<div id="activity-url-scrapper-title" class="activity-link-preview-title">{{data.link_title}}</div>
-			<div id="activity-url-scrapper-description" class="activity-link-preview-body">{{{data.link_description}}}</div>
+			<div id="activity-url-scrapper-description" class="activity-link-preview-excerpt">{{{data.link_description}}}</div>
 			<# } #>
 			<# if ( data.link_error && ! data.link_success ) { #>
 			<div id="activity-url-error" class="activity-url-error">{{data.link_error_msg}}</div>
