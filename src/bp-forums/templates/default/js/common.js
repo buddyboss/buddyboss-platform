f7fdfa3d3578ea2db34cb95871e50c385553a65f
Manish Patolia <manishpatolia09@gmail.com>
2019-12-12 22:31:19 +0530

    T#1795 When using two topic shortcodes the reply textarea doesnt work
    https://trello.com/c/5ZFzSCn4/1795-when-using-two-shortcodes-discussion-topic-display-topicx1-and-discussion-topic-display-topicx2-the-reply-textarea-doesnt-work

--- src/bp-forums/templates/default/js/common.js
+++ src/bp-forums/templates/default/js/common.js
@@ -1,94 +1,95 @@
 jQuery( document ).ready(
 	function() {
 
-			var $tagsSelect   = jQuery( 'body' ).find( '#bbp_topic_tags_dropdown' );
-			var tagsArrayData = [];
+		var $tagsSelectAll   = jQuery( 'body' ).find( '.bbp_topic_tags_dropdown' );
+		var tagsArrayData = [];
+		$tagsSelectAll.each( function( key, value ) {
+			var $tagsSelect =	jQuery( this )
+			if ( $tagsSelect.length ) {
+				$tagsSelect.select2({
+					placeholder: $tagsSelect.attr('placeholder'),
+					minimumInputLength: 1,
+					closeOnSelect: true,
+					tags: true,
+					dropdownCssClass: 'bb-select-dropdown',
+					containerCssClass: 'bb-select-container',
+					tokenSeparators: [',', ' '],
+					ajax: {
+						url: bbpCommonJsData.ajax_url,
+						dataType: 'json',
+						delay: 1000,
+						data: function (params) {
+							return jQuery.extend({}, params, {
+								_wpnonce: bbpCommonJsData.nonce,
+								action: 'search_tags',
+							});
+						},
+						cache: true,
+						processResults: function (data) {
 
-	if ( $tagsSelect.length ) {
-		$tagsSelect.select2({
-			placeholder: $tagsSelect.attr('placeholder'),
-			minimumInputLength: 1,
-			closeOnSelect: true,
-			tags: true,
-			dropdownCssClass: 'bb-select-dropdown',
-			containerCssClass: 'bb-select-container',
-			tokenSeparators: [',', ' '],
-			ajax: {
-				url: bbpCommonJsData.ajax_url,
-				dataType: 'json',
-				delay: 1000,
-				data: function (params) {
-					return jQuery.extend({}, params, {
-						_wpnonce: bbpCommonJsData.nonce,
-						action: 'search_tags',
-					});
-				},
-				cache: true,
-				processResults: function (data) {
-
-					// Removed the element from results if already selected.
-					if (false === jQuery.isEmptyObject(tagsArrayData)) {
-						jQuery.each(tagsArrayData, function (index, value) {
-							for (var i = 0; i < data.data.results.length; i++) {
-								if (data.data.results[i].id === value) {
-									data.data.results.splice(i, 1);
-								}
+							// Removed the element from results if already selected.
+							if (false === jQuery.isEmptyObject(tagsArrayData)) {
+								jQuery.each(tagsArrayData, function (index, value) {
+									for (var i = 0; i < data.data.results.length; i++) {
+										if (data.data.results[i].id === value) {
+											data.data.results.splice(i, 1);
+										}
+									}
+								});
 							}
-						});
+
+							return {
+								results: data && data.success ? data.data.results : []
+							};
+						}
 					}
+				});
 
-					return {
-						results: data && data.success ? data.data.results : []
-					};
-				}
+				// Add element into the Arrdata array.
+				$tagsSelect.on('select2:select', function (e) {
+					var data = e.params.data;
+					tagsArrayData.push(data.id);
+					var tags = tagsArrayData.join(',');
+					jQuery('body #bbp_topic_tags').val(tags);
+
+					jQuery( 'body .select2-search__field' ).trigger( 'click' );
+					console.log('1');
+					jQuery( 'body .select2-search__field' ).trigger( 'click' );
+					console.log('111');
+				});
+
+				// Remove element into the Arrdata array.
+				$tagsSelect.on('select2:unselect', function (e) {
+					var data = e.params.data;
+					tagsArrayData = jQuery.grep(tagsArrayData, function (value) {
+						return value !== data.id;
+					});
+					var tags = tagsArrayData.join(',');
+					jQuery('body #bbp_topic_tags').val(tags);
+					if (tags.length === 0) {
+						jQuery(window).scrollTop(jQuery(window).scrollTop() + 1);
+					}
+				});
+
 			}
-		});
-
-		// Add element into the Arrdata array.
-		$tagsSelect.on('select2:select', function (e) {
-			var data = e.params.data;
-			tagsArrayData.push(data.id);
-			var tags = tagsArrayData.join(',');
-			jQuery('body #bbp_topic_tags').val(tags);
-
-			jQuery( 'body .select2-search__field' ).trigger( 'click' );
-			console.log('1');
-			jQuery( 'body .select2-search__field' ).trigger( 'click' );
-			console.log('111');
-		});
-
-		// Remove element into the Arrdata array.
-		$tagsSelect.on('select2:unselect', function (e) {
-			var data = e.params.data;
-			tagsArrayData = jQuery.grep(tagsArrayData, function (value) {
-				return value !== data.id;
+			// "remove all tags" button event listener
+			jQuery( 'body' ).on('click', '.js-modal-close', function() {
+				$tagsSelect.val('');
+				$tagsSelect.trigger( 'change' ); // Notify any JS components that the value changed
+				jQuery( 'body' ).removeClass( 'popup-modal-reply' );
 			});
-			var tags = tagsArrayData.join(',');
-			jQuery('body #bbp_topic_tags').val(tags);
-			if (tags.length === 0) {
-				jQuery(window).scrollTop(jQuery(window).scrollTop() + 1);
-			}
-		});
-
-	}
-	// "remove all tags" button event listener
-	jQuery( 'body' ).on('click', '.js-modal-close', function() {
-		$tagsSelect.val('');
-		$tagsSelect.trigger( 'change' ); // Notify any JS components that the value changed
-		jQuery( 'body' ).removeClass( 'popup-modal-reply' );
-	});
 
 			var topicReplyButton = jQuery( 'body .bbp-topic-reply-link' );
-		if ( topicReplyButton.length ) {
-			topicReplyButton.click(
-				function () {
-						jQuery( 'body' ).addClass( 'popup-modal-reply' );
-						$tagsSelect.val( '' );
-						$tagsSelect.trigger( 'change' ); // Notify any JS components that the value changed
-				}
-			);
-		}
-
+			if ( topicReplyButton.length ) {
+				topicReplyButton.click(
+					function () {
+							jQuery( 'body' ).addClass( 'popup-modal-reply' );
+							$tagsSelect.val( '' );
+							$tagsSelect.trigger( 'change' ); // Notify any JS components that the value changed
+					}
+				);
+			}
+		});
 		if (typeof BP_Nouveau !== 'undefined' && typeof BP_Nouveau.media !== 'undefined' && typeof BP_Nouveau.media.emoji !== 'undefined' ) {
 			  var bbp_editor_content_elem = false;
 			if ( jQuery( '#bbp_editor_topic_content' ).length ) {
