3aa3addf76c495c966224e6475282e3a3f3bc8f3
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2019-12-18 22:09:28 +0530

    Revert "Revert "Search > support for discussion tags""
    This reverts commit a747ebac630401516c44f66430b3cce2c9c94a96.

--- src/bp-search/classes/class-bp-search-bbpress-forums-topics.php
+++ src/bp-search/classes/class-bp-search-bbpress-forums-topics.php
@@ -33,27 +33,55 @@ if ( ! class_exists( 'Bp_Search_bbPress_Topics' ) ) :
 
 			$from = "{$wpdb->posts} p LEFT JOIN {$wpdb->postmeta} pm ON pm.post_id = p.ID AND pm.meta_key = '_bbp_forum_id'";
 
+			$tax = array();
+
+			if ( bp_is_search_post_type_taxonomy_enable( bbpress()->topic_tag_tax_id, $this->type ) ) {
+				$tax[] = bbpress()->topic_tag_tax_id;
+			}
+
+			$where_clause = ' WHERE ';
+
+			$tax_sql = '';
+			// Tax query left join
+			if ( ! empty( $tax ) ) {
+				$tax_sql = " LEFT JOIN {$wpdb->term_relationships} r ON p.ID = r.object_id ";
+			}
+
+			// Tax query
+			if ( ! empty( $tax ) ) {
+
+				$tax_in_arr = array_map( function ( $t_name ) {
+					return "'" . $t_name . "'";
+				},
+					$tax );
+
+				$tax_in = implode( ', ', $tax_in_arr );
+
+				$tax_sql             .= " WHERE r.term_taxonomy_id IN (SELECT tt.term_taxonomy_id FROM {$wpdb->term_taxonomy} tt INNER JOIN {$wpdb->terms} t ON 
+					  t.term_id = tt.term_id WHERE ( t.slug LIKE %s OR t.name LIKE %s ) AND  tt.taxonomy IN ({$tax_in}) )";
+				$query_placeholder[] = '%' . $search_term . '%';
+				$query_placeholder[] = '%' . $search_term . '%';
+				$where_clause        = ' OR ';
+			}
+
 			$group_memberships = '';
 			if ( bp_is_active( 'groups' ) ) {
-				$group_memberships = bp_get_user_groups(
-					get_current_user_id(),
+				$group_memberships = bp_get_user_groups( get_current_user_id(),
 					array(
 						'is_admin' => null,
 						'is_mod'   => null,
-					)
-				);
+					) );
 			}
 
 			$group_query = '';
 			if ( ! empty( $group_memberships ) ) {
-				$in = array_reduce(
-					array_keys( $group_memberships ),
+				$in = array_reduce( array_keys( $group_memberships ),
 					function ( $carry, $group_id ) {
 						return $carry . ',\'' . maybe_serialize( array( $group_id ) ) . '\'';
-					}
-				);
+					} );
 
-				$group_query = ' pm.meta_value IN ( SELECT post_id FROM ' . $wpdb->postmeta . ' WHERE meta_key = \'_bbp_group_ids\' AND meta_value IN(' . trim( $in, ',' ) . ') ) OR ';
+				$group_query = ' pm.meta_value IN ( SELECT post_id FROM ' . $wpdb->postmeta . ' WHERE meta_key = \'_bbp_group_ids\' AND meta_value IN(' . trim( $in,
+						',' ) . ') ) OR ';
 			}
 
 			if ( current_user_can( 'read_hidden_forums' ) ) {
@@ -70,32 +98,33 @@ if ( ! class_exists( 'Bp_Search_bbPress_Topics' ) ) :
 			$where[] = "post_type = '{$this->type}'";
 
 			$where[] = '(' . $group_query . '
-			pm.meta_value IN ( SELECT ID FROM ' . $wpdb->posts . ' WHERE post_type = \'forum\' AND post_status IN (' . join( ',', $post_status ) . ') )
+			pm.meta_value IN ( SELECT ID FROM ' . $wpdb->posts . ' WHERE post_type = \'forum\' AND post_status IN (' . join( ',',
+					$post_status ) . ') )
 			)';
 
 			$query_placeholder[] = '%' . $search_term . '%';
 			$query_placeholder[] = '%' . $search_term . '%';
 
-			$sql   = 'SELECT ' . $columns . ' FROM ' . $from . ' WHERE ' . implode( ' AND ', $where );
+
+			$sql = 'SELECT ' . $columns . ' FROM ' . $from . $tax_sql . $where_clause . implode( ' AND ', $where );
+
 			$query = $wpdb->prepare( $sql, $query_placeholder );
 
-			return apply_filters(
-				'Bp_Search_Forums_sql',
+			return apply_filters( 'Bp_Search_Forums_sql',
 				$query,
 				array(
 					'search_term'         => $search_term,
 					'only_totalrow_count' => $only_totalrow_count,
-				)
-			);
+				) );
 		}
 
 		/**
 		 * Insures that only one instance of Class exists in memory at any
 		 * one time. Also prevents needing to define globals all over the place.
 		 *
+		 * @return object Bp_Search_Forums
 		 * @since BuddyBoss 1.0.0
 		 *
-		 * @return object Bp_Search_Forums
 		 */
 		public static function instance() {
 			// Store the instance locally to avoid private static replication
