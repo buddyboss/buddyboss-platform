80c01c5d13efbce543170bb5bfbec9dd7953c3d5
Chetan Satasiya <chetan@buddyboss.com>
2019-12-27 11:01:10 +0530

    #420 Error plugin file not exists error

--- src/bp-loader.php
+++ src/bp-loader.php
@@ -128,9 +128,38 @@ if ( empty( $is_bp_active ) && empty( $is_bb_active ) && empty( $bp_incompatible
 		global $bp_plugin_file, $bb_plugin_file, $bp_is_multisite, $buddyboss_platform_plugin_file;
 
 		// Do not add the "bbpress/bbpress.php" & "buddypress/bp-loader.php" on "/wp-admin/plugins.php" page otherwise it will show the plugin file not exists error.
+
+		$plugin_string = '/wp-admin/plugins.php';
+		$ajax_string   = '/wp-admin/admin-ajax.php';
+
+		// Hide My WP plugin compatibility
+		if ( class_exists( 'HideMyWP' ) ) {
+			if ( is_multisite() ) {
+				// Get the current site ID
+				$site_id = get_current_blog_id();
+				$options = get_blog_option( $site_id, 'hide_my_wp' );
+			} else {
+				$options = get_option( 'hide_my_wp' );
+			}
+
+			$admin_path = $options['new_admin_path'];
+			$ajax_path  = $options['replace_admin_ajax'];
+
+			if ( '' !== $admin_path ) {
+				$plugin_string = '/' . $admin_path . '/plugins.php';
+			}
+
+			if ( '' !== $admin_path && '' !== $ajax_path ) {
+				$ajax_string = '/' . $admin_path . '/' . $ajax_path;
+			} elseif ( '' !== $admin_path && '' === $ajax_path ) {
+				$ajax_string = '/' . $admin_path . '/admin-ajax.php';
+			}
+
+		}
+
 		if ( is_network_admin()
-			 || strpos( $_SERVER['REQUEST_URI'], '/wp-admin/plugins.php' ) !== false
-			 || strpos( $_SERVER['REQUEST_URI'], '/wp-admin/admin-ajax.php' ) !== false
+			 || strpos( $_SERVER['REQUEST_URI'], $plugin_string ) !== false
+			 || strpos( $_SERVER['REQUEST_URI'], $ajax_string ) !== false
 		) {
 
 			/**
