d9d489a19aa11b1b6125cfee60c44c222d47cdd6
Sagar Jadhav <sagar@buddyboss.com>
2020-01-15 22:37:32 +0530

    #306 - Removed icheck JS

--- src/bp-templates/bp-nouveau/buddypress/members/single/settings/capabilities.php
+++ src/bp-templates/bp-nouveau/buddypress/members/single/settings/capabilities.php
@@ -14,10 +14,10 @@ bp_nouveau_member_hook( 'before', 'settings_template' ); ?>
 
 <form action="<?php echo esc_url( bp_displayed_user_domain() . bp_get_settings_slug() . '/capabilities/' ); ?>" name="account-capabilities-form" id="account-capabilities-form" class="standard-form" method="post">
 
-	<label for="user-spammer">
-		<input type="checkbox" name="user-spammer" id="user-spammer" value="1" <?php checked( bp_is_user_spammer( bp_displayed_user_id() ) ); ?> />
-			<?php esc_html_e( 'This member is a spammer.', 'buddyboss' ); ?>
-	</label>
+	<div class="bp-checkbox-wrap">
+		<input type="checkbox" name="user-spammer" id="user-spammer" class="bs-styled-checkbox" value="1" <?php checked( bp_is_user_spammer( bp_displayed_user_id() ) ); ?> />
+		<label for="user-spammer"><?php esc_html_e( 'This member is a spammer.', 'buddyboss' ); ?></label>
+	</div>
 
 	<?php bp_nouveau_submit_button( 'member-capabilities' ); ?>
 
