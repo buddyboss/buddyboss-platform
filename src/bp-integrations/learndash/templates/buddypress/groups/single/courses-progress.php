ea42ba2460f6b0467ff8c20880e95fef893b6a3a
Michael Eisenwasser <michael@buddyboss.com>
2020-01-26 21:38:22 -0800

    use text “Complete” instead of “Completed”

--- src/bp-integrations/learndash/templates/buddypress/groups/single/courses-progress.php
+++ src/bp-integrations/learndash/templates/buddypress/groups/single/courses-progress.php
@@ -1,5 +1,5 @@
 <div class="bp-learndash-progress-bar">
 	<p class="bp-learndash-progress-bar-label"><?php echo $label; ?></p>
 	<progress value="<?php echo $progress; ?>" max="100"></progress>
-	<span class="bp-learndash-progress-bar-percentage"><?php echo $progress; ?>% <?php _e( 'Completed', 'buddyboss' ); ?></span>
+	<span class="bp-learndash-progress-bar-percentage"><?php echo $progress; ?>% <?php _e( 'Complete', 'buddyboss' ); ?></span>
 </div>
