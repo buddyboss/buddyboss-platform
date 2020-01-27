01e8cc203fd9820973520b32ebfe2131d52f61f9
Chetan Satasiya <chetan@buddyboss.com>
2020-01-27 21:07:16 +0530

    #326 code formatting

--- src/bp-templates/bp-nouveau/buddypress/members/single/profile/widget.php
+++ src/bp-templates/bp-nouveau/buddypress/members/single/profile/widget.php
@@ -8,53 +8,53 @@
 $bp_nouveau = bp_nouveau();
 
 // If no Profile Completion Progress found then stop.
-if( !isset( $bp_nouveau->xprofile->profile_completion_widget_para ) ){
+if ( ! isset( $bp_nouveau->xprofile->profile_completion_widget_para ) ) {
 	return;
 }
 
 $user_progress = $bp_nouveau->xprofile->profile_completion_widget_para;
 
-$progress_label = sprintf( __( '%s Complete', 'buddyboss' ), $user_progress['completion_percentage'].'%' );
+$progress_label = sprintf( __( '%s Complete', 'buddyboss' ), $user_progress['completion_percentage'] . '%' );
 
 ?>
-<div class="profile_completion_wrap" >
-	
-	<div class="pc_progress_wrap" >
-		<div class="progress_text_wrap" >
-			<span class="progress_text" ><?php echo $progress_label; ?></span>
+<div class="profile_completion_wrap">
+
+	<div class="pc_progress_wrap">
+		<div class="progress_text_wrap">
+			<span class="progress_text"><?php echo $progress_label; ?></span>
 		</div>
-		<div class="progress_container" >
-			<div class="pc_progress" style="width: <?php echo $user_progress['completion_percentage']; ?>%;" ></div>
+		<div class="progress_container">
+			<div class="pc_progress" style="width: <?php echo $user_progress['completion_percentage']; ?>%;"></div>
 		</div>
 	</div>
-	
-	<div class="pc_detailed_progress_wrap" >
-		
-		<ul class="pc_detailed_progress" >
-		
-		<?php // Loop through all sections and show progress. ?>
-		<?php foreach( $user_progress['groups'] as $single_section_details ): ?>
 
-		<?php $user_progress_status = ($single_section_details['completed'] == 0 && $single_section_details['total'] > 0) ? 'progress_not_started' : ''; ?>
+	<div class="pc_detailed_progress_wrap">
 
-		<li class="single_section_wrap <?php echo ( $single_section_details['is_group_completed'] ) ? 'completed ' : 'incomplete '; echo $user_progress_status; ?>" >
-			<span class="section_number" >
-				<?php echo $single_section_details['number']; ?>
-			</span>
-			<span class="section_name" >
-				<a href="<?php echo $single_section_details['link']; ?>" class="group_link" ><?php echo $single_section_details['label']; ?></a>
-			</span>
-			<span class="progress" >
-				<span class="completed_staus" >
-				<span class="completed_steps" ><?php echo $single_section_details['completed']; ?></span>/<span class="total_steps" ><?php echo $single_section_details['total']; ?></span>
-				</span>
-			</span>
-		</li>
-		
-		<?php endforeach; ?>
-			
+		<ul class="pc_detailed_progress">
+
+			<?php // Loop through all sections and show progress. ?>
+			<?php foreach ( $user_progress['groups'] as $single_section_details ): ?>
+
+				<?php $user_progress_status = ( $single_section_details['completed'] == 0 && $single_section_details['total'] > 0 ) ? 'progress_not_started' : ''; ?>
+
+				<li class="single_section_wrap <?php echo ( $single_section_details['is_group_completed'] ) ? 'completed ' : 'incomplete '; echo $user_progress_status; ?>">
+					<span class="section_number">
+						<?php echo $single_section_details['number']; ?>
+					</span>
+					<span class="section_name">
+						<a href="<?php echo esc_url( $single_section_details['link'] ); ?>" class="group_link"><?php echo $single_section_details['label']; ?></a>
+					</span>
+					<span class="progress">
+						<span class="completed_staus">
+							<span class="completed_steps"><?php echo absint( $single_section_details['completed'] ); ?></span>/<span class="total_steps"><?php echo absint( $single_section_details['total'] ); ?></span>
+						</span>
+					</span>
+				</li>
+
+			<?php endforeach; ?>
+
 		</ul>
-		
+
 	</div>
-	
+
 </div>
--- src/bp-xprofile/bp-xprofile-widgets.php
+++ src/bp-xprofile/bp-xprofile-widgets.php
@@ -3,7 +3,7 @@
  * BuddyBoss xProfile Component Widgets.
  *
  * Functions here registers Profile Completion Widget.
- * 
+ *
  * @package BuddyBoss\XProfile
  * @since BuddyBoss 1.0.0
  */
@@ -14,19 +14,17 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Register bp-profile widgets.
  *
- * @since BuddyBoss 1.0.0
+ * @since BuddyBoss 1.2.5
  */
 function bp_xprofile_register_widgets() {
-	
+
 	/**
 	 * Profile Completion Widget
 	 */
-	add_action(
-		'widgets_init',
-		function() {
-			register_widget( 'BP_Xprofile_Profile_Completion_Widget' );
-		}
-	);
-	
+	add_action( 'widgets_init', function () {
+		register_widget( 'BP_Xprofile_Profile_Completion_Widget' );
+	} );
+
 }
+
 add_action( 'bp_register_widgets', 'bp_xprofile_register_widgets' );
