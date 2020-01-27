3bbaf7cf933a7a874359e443cd4cd67c4cad0625
Kartik Suthar <kartik@buddyboss.com>
2020-01-27 20:13:32 +0530

    #456 - added string translation to dropzone js file

--- src/bp-core/bp-core-cssjs.php
+++ src/bp-core/bp-core-cssjs.php
@@ -269,17 +269,17 @@ function bp_core_register_common_scripts() {
 		'bp-media-dropzone',
 		'bp_media_dropzone',
 		array(
-			'dictDefaultMessage' => __( "Drop files here to upload", 'buddyboss' ),
-			'dictFallbackMessage' => __( "Your browser does not support drag'n'drop file uploads.", 'buddyboss' ),
-			'dictFallbackText' => __( "Please use the fallback form below to upload your files like in the olden days.", 'buddyboss' ),
-			'dictFileTooBig' => __( "File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.", 'buddyboss' ),
-			'dictInvalidFileType' => __( "You can't upload files of this type.", 'buddyboss' ),
-			'dictResponseError' => __( "Server responded with {{statusCode}} code.", 'buddyboss' ),
-			'dictCancelUpload' => __( "Cancel upload", 'buddyboss' ),
-			'dictUploadCanceled' => __( "Upload canceled.", 'buddyboss' ),
+			'dictDefaultMessage'           => __( "Drop files here to upload", 'buddyboss' ),
+			'dictFallbackMessage'          => __( "Your browser does not support drag'n'drop file uploads.", 'buddyboss' ),
+			'dictFallbackText'             => __( "Please use the fallback form below to upload your files like in the olden days.", 'buddyboss' ),
+			'dictFileTooBig'               => __( "File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.", 'buddyboss' ),
+			'dictInvalidFileType'          => __( "You can't upload files of this type.", 'buddyboss' ),
+			'dictResponseError'            => __( "Server responded with {{statusCode}} code.", 'buddyboss' ),
+			'dictCancelUpload'             => __( "Cancel upload", 'buddyboss' ),
+			'dictUploadCanceled'           => __( "Upload canceled.", 'buddyboss' ),
 			'dictCancelUploadConfirmation' => __( "Are you sure you want to cancel this upload?", 'buddyboss' ),
-			'dictRemoveFile' => __( "Remove file", 'buddyboss' ),
-			'dictMaxFilesExceeded' => __( "You can not upload any more files.", 'buddyboss' ),
+			'dictRemoveFile'               => __( "Remove file", 'buddyboss' ),
+			'dictMaxFilesExceeded'         => __( "You can not upload any more files.", 'buddyboss' ),
 		)
 	);
 }
--- src/bp-core/js/vendor/dropzone.js
+++ src/bp-core/js/vendor/dropzone.js
@@ -412,56 +412,56 @@ class Dropzone extends Emitter {
 			/**
 			 * The text used before any files are dropped.
 			 */
-			dictDefaultMessage: "Drop files here to upload",
+			dictDefaultMessage: bp_media_dropzone.dictDefaultMessage,
 
 			/**
 			 * The text that replaces the default message text it the browser is not supported.
 			 */
-			dictFallbackMessage: "Your browser does not support drag'n'drop file uploads.",
+			dictFallbackMessage: bp_media_dropzone.dictFallbackMessage,
 
 			/**
 			 * The text that will be added before the fallback form.
 			 * If you provide a  fallback element yourself, or if this option is `null` this will
 			 * be ignored.
 			 */
-			dictFallbackText: "Please use the fallback form below to upload your files like in the olden days.",
+			dictFallbackText: bp_media_dropzone.dictFallbackText,
 
 			/**
 			 * If the filesize is too big.
 			 * `{{filesize}}` and `{{maxFilesize}}` will be replaced with the respective configuration values.
 			 */
-			dictFileTooBig: "File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.",
+			dictFileTooBig: bp_media_dropzone.dictFileTooBig,
 
 			/**
 			 * If the file doesn't match the file type.
 			 */
-			dictInvalidFileType: "You can't upload files of this type.",
+			dictInvalidFileType: bp_media_dropzone.dictInvalidFileType,
 
 			/**
 			 * If the server response was invalid.
 			 * `{{statusCode}}` will be replaced with the servers status code.
 			 */
-			dictResponseError: "Server responded with {{statusCode}} code.",
+			dictResponseError: bp_media_dropzone.dictResponseError,
 
 			/**
 			 * If `addRemoveLinks` is true, the text to be used for the cancel upload link.
 			 */
-			dictCancelUpload: "Cancel upload",
+			dictCancelUpload: bp_media_dropzone.dictCancelUpload,
 
 			/**
 			 * The text that is displayed if an upload was manually canceled
 			 */
-			dictUploadCanceled: "Upload canceled.",
+			dictUploadCanceled: bp_media_dropzone.dictUploadCanceled,
 
 			/**
 			 * If `addRemoveLinks` is true, the text to be used for confirmation when cancelling upload.
 			 */
-			dictCancelUploadConfirmation: "Are you sure you want to cancel this upload?",
+			dictCancelUploadConfirmation: bp_media_dropzone.dictCancelUploadConfirmation,
 
 			/**
 			 * If `addRemoveLinks` is true, the text to be used to remove a file.
 			 */
-			dictRemoveFile: "Remove file",
+			dictRemoveFile: bp_media_dropzone.dictRemoveFile,
 
 			/**
 			 * If this is not null, then the user will be prompted before removing a file.
@@ -472,7 +472,7 @@ class Dropzone extends Emitter {
 			 * Displayed if `maxFiles` is st and exceeded.
 			 * The string `{{maxFiles}}` will be replaced by the configuration value.
 			 */
-			dictMaxFilesExceeded: "You can not upload any more files.",
+			dictMaxFilesExceeded: bp_media_dropzone.dictMaxFilesExceeded,
 
 			/**
 			 * Allows you to translate the different units. Starting with `tb` for terabytes and going down to
