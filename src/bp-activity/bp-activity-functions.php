fe345092ae69515560daff3923082096785b8e15
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2020-03-02 18:06:22 +0530

    #269 - mentions in forums editors added

--- src/bp-activity/bp-activity-functions.php
+++ src/bp-activity/bp-activity-functions.php
@@ -4313,42 +4313,6 @@ function bp_activity_do_heartbeat() {
 }
 
 /**
- * AJAX endpoint for Suggestions API lookups.
- *
- * @since BuddyPress 2.1.0
- */
-function bp_ajax_get_suggestions() {
-	if ( ! bp_is_user_active() || empty( $_GET['term'] ) || empty( $_GET['type'] ) ) {
-		wp_send_json_error( 'missing_parameter' );
-		exit;
-	}
-
-	$args = array(
-		'term' => sanitize_text_field( $_GET['term'] ),
-		'type' => sanitize_text_field( $_GET['type'] ),
-	);
-
-	if ( ! empty( $_GET['only_friends'] ) ) {
-		$args['only_friends'] = absint( $_GET['only_friends'] );
-	}
-
-	// Support per-Group suggestions.
-	if ( ! empty( $_GET['group-id'] ) ) {
-		$args['group_id'] = absint( $_GET['group-id'] );
-	}
-
-	$results = bp_core_get_suggestions( $args );
-
-	if ( is_wp_error( $results ) ) {
-		wp_send_json_error( $results->get_error_message() );
-		exit;
-	}
-
-	wp_send_json_success( $results );
-}
-add_action( 'wp_ajax_bp_get_suggestions', 'bp_ajax_get_suggestions' );
-
-/**
  * AJAX endpoint for activity comments.
  *
  * @since BuddyBoss 1.0.0
