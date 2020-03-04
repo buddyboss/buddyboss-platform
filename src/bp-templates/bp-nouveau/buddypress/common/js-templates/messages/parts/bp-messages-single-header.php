54c10ea00ba5bb1fa9059e99866aa03a63318929
Chetan Satasiya <chetan@buddyboss.com>
2020-03-04 09:17:28 +0530

    #357 removed space

--- src/bp-templates/bp-nouveau/buddypress/common/js-templates/messages/parts/bp-messages-single-header.php
+++ src/bp-templates/bp-nouveau/buddypress/common/js-templates/messages/parts/bp-messages-single-header.php
@@ -34,12 +34,7 @@
 						<span class="participants-name">
                             <# if ( other_recipients[i].is_deleted ) { #>
 								{{other_recipients[i].user_name}}
-							<# } else { #>
-								<a href="{{other_recipients[i].user_link}}">{{other_recipients[i].user_name}}</a>
-							<# } #>
-							<# if ( i != other_recipients.length -1 || ( i == other_recipients.length -1 && include_you ) ) { #>
-								<?php _e(',', 'buddyboss'); ?>
-							<# } #>
+							<# } else { #><a href="{{other_recipients[i].user_link}}">{{other_recipients[i].user_name}}</a><# } #><# if ( i != other_recipients.length -1 || ( i == other_recipients.length -1 && include_you ) ) { #><?php _e(',', 'buddyboss'); ?><# } #>
 		                </span>
 					<# } #>
 
