c9492c6f840d6b47fab1f715b7a173102ac11e72
Chetan Satasiya <chetan@buddyboss.com>
2020-03-18 15:56:53 +0530

    #604 Fix link preview image not showing

--- src/bp-activity/bp-activity-functions.php
+++ src/bp-activity/bp-activity-functions.php
@@ -5203,6 +5203,45 @@ function bp_activity_media_sideload_attachment( $file ) {
 	$file_array = array();
 
 	if ( empty( $matches ) ) {
+
+		$upload_dir = wp_upload_dir();
+
+		$image_data = file_get_contents( $file );
+
+		if ( $image_data ) {
+
+			$filename = basename( $file ) . '.jpg';
+
+			if ( wp_mkdir_p( $upload_dir['path'] ) ) {
+				$file = $upload_dir['path'] . '/' . $filename;
+			} else {
+				$file = $upload_dir['basedir'] . '/' . $filename;
+			}
+
+			if ( false === file_put_contents( $file, $image_data ) ) {
+				return;
+			}
+
+			$wp_filetype = wp_check_filetype( $filename, null );
+
+			$attachment = array(
+				'post_mime_type' => $wp_filetype['type'],
+				'post_title'     => sanitize_file_name( $filename ),
+				'post_content'   => '',
+				'post_status'    => 'inherit'
+			);
+
+			$attach_id = wp_insert_attachment( $attachment, $file );
+			require_once( ABSPATH . 'wp-admin/includes/image.php' );
+			$attach_data = wp_generate_attachment_metadata( $attach_id, $file );
+			wp_update_attachment_metadata( $attach_id, $attach_data );
+
+			if ( $attach_id ) {
+				return $attach_id;
+			}
+
+		}
+
 		return;
 	}
 
