e62f65b6ff844fb3605d78dbb5d83a0d5b2e75de
Chetan Satasiya <chetan@buddyboss.com>
2020-04-09 12:20:30 +0530

    #266 document theatre in directory content

--- src/bp-templates/bp-nouveau/buddypress/document/index.php
+++ src/bp-templates/bp-nouveau/buddypress/document/index.php
@@ -7,44 +7,45 @@
  */
 ?>
 
-	<?php bp_nouveau_before_document_directory_content(); ?>
+<?php bp_nouveau_before_document_directory_content(); ?>
 
-	<?php bp_nouveau_template_notices(); ?>
+<?php bp_nouveau_template_notices(); ?>
 
-	<div class="screen-content">
+<?php bp_get_template_part( 'document/theatre' ); ?>
 
-		<?php bp_nouveau_document_hook( 'before_directory', 'list' ); ?>
+<div class="screen-content">
 
-		<?php
-		/**
-		 * Fires before the display of the members list tabs.
-		 *
-		 * @since BuddyPress 1.8.0
-		 */
-		do_action( 'bp_before_directory_document_tabs' );
-		?>
+	<?php bp_nouveau_document_hook( 'before_directory', 'list' ); ?>
 
-		<?php if ( ! bp_nouveau_is_object_nav_in_sidebar() ) : ?>
+	<?php
+	/**
+	 * Fires before the display of the members list tabs.
+	 *
+	 * @since BuddyPress 1.8.0
+	 */
+	do_action( 'bp_before_directory_document_tabs' );
+	?>
 
-			<?php bp_get_template_part( 'common/nav/directory-nav' ); ?>
+	<?php if ( ! bp_nouveau_is_object_nav_in_sidebar() ) : ?>
 
-		<?php endif; ?>
-			
-			<div class="document-options">
-				<?php bp_get_template_part( 'common/search-and-filters-bar' ); ?>
-				<a href="#" id="bp-add-document" class="bb-add-document button small"><i class="bb-icon-upload"></i><?php esc_html_e( 'Add Documents', 'buddyboss' ); ?></a>
-				<a href="#" id="bb-create-folder" class="bb-create-folder button small"><i class="bb-icon-plus"></i><?php esc_html_e( 'Add Folder', 'buddyboss' ); ?></a>
-				<?php bp_get_template_part( 'document/document-uploader' ); ?>
-				<?php bp_get_template_part( 'document/create-folder' ); ?>
-			</div>
+		<?php bp_get_template_part( 'common/nav/directory-nav' ); ?>
 
-			
+	<?php endif; ?>
 
-		<div id="media-stream" class="media document-parent" data-bp-list="document">
-			<div id="bp-ajax-loader"><?php bp_nouveau_user_feedback( 'directory-media-document-loading' ); ?></div>
-		</div><!-- .media -->
+    <div class="document-options">
+		<?php bp_get_template_part( 'common/search-and-filters-bar' ); ?>
+        <a href="#" id="bp-add-document" class="bb-add-document button small"><i class="bb-icon-upload"></i><?php esc_html_e( 'Add Documents', 'buddyboss' ); ?></a>
+        <a href="#" id="bb-create-folder" class="bb-create-folder button small"><i class="bb-icon-plus"></i><?php esc_html_e( 'Add Folder', 'buddyboss' ); ?></a>
+		<?php bp_get_template_part( 'document/document-uploader' ); ?>
+		<?php bp_get_template_part( 'document/create-folder' ); ?>
+    </div>
 
-		<?php bp_nouveau_after_document_directory_content(); ?>
 
-	</div><!-- // .screen-content -->
+    <div id="media-stream" class="media document-parent" data-bp-list="document">
+        <div id="bp-ajax-loader"><?php bp_nouveau_user_feedback( 'directory-media-document-loading' ); ?></div>
+    </div><!-- .media -->
+
+	<?php bp_nouveau_after_document_directory_content(); ?>
+
+</div><!-- // .screen-content -->
 
