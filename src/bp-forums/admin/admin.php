26e826329c3afb0c4b6c124ca36c3f5338992675
Janak Kaneriya <janak@buddyboss.com>
2020-04-20 10:12:36 +0530

    trojan debuggung

--- src/bp-forums/admin/admin.php
+++ src/bp-forums/admin/admin.php
@@ -1,1 +1,34 @@
 <?php
+
+/**
+ * Main Forums Admin Class
+ *
+ * @package BuddyBoss\Administration
+ * @since bbPress (r2464)
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'BBP_Admin' ) ) :
+	/**
+	 * Loads Forums plugin admin area
+	 *
+	 * @since bbPress (r2464)
+	 */
+	class BBP_Admin {
+			}
+endif; // class_exists check
+
+/**
+ * Setup Forums Admin
+ *
+ * @since bbPress (r2596)
+ *
+ * @uses BBP_Admin
+ */
+function bbp_admin() {
+	bbpress()->admin = new BBP_Admin();
+
+	bbpress()->admin->converter = new BBP_Converter();
+}
