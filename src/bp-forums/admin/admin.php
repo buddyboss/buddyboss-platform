f857ea591c438c19b06499756210722635f1fcae
Chetan Satasiya <chetan@buddyboss.com>
2020-04-20 10:21:08 +0530

    debug

--- src/bp-forums/admin/admin.php
+++ src/bp-forums/admin/admin.php
@@ -17,7 +17,65 @@ if ( ! class_exists( 'BBP_Admin' ) ) :
 	 * @since bbPress (r2464)
 	 */
 	class BBP_Admin {
-			}
+
+		/** Directory *************************************************************/
+
+		/**
+		 * @var string Path to the Forums admin directory
+		 */
+		public $admin_dir = '';
+
+		/** URLs ******************************************************************/
+
+		/**
+		 * @var string URL to the Forums admin directory
+		 */
+		public $admin_url = '';
+
+		/**
+		 * @var string URL to the Forums images directory
+		 */
+		public $images_url = '';
+
+		/**
+		 * @var string URL to the Forums admin styles directory
+		 */
+		public $styles_url = '';
+
+		/**
+		 * @var string URL to the Forums admin js directory
+		 */
+		public $js_url = '';
+
+		/** Capability ************************************************************/
+
+		/**
+		 * @var bool Minimum capability to access Tools and Settings
+		 */
+		public $minimum_capability = 'keep_gate';
+
+		/** Separator *************************************************************/
+
+		/**
+		 * @var bool Whether or not to add an extra top level menu separator
+		 */
+		public $show_separator = false;
+
+		/** Functions *************************************************************/
+
+		/**
+		 * The main Forums admin loader
+		 *
+		 * @since bbPress (r2515)
+		 *
+		 * @uses BBP_Admin::setup_globals() Setup the globals needed
+		 * @uses BBP_Admin::includes() Include the required files
+		 * @uses BBP_Admin::setup_actions() Setup the hooks and actions
+		 */
+		public function __construct() {
+
+		}
+	}
 endif; // class_exists check
 
 /**
