bf36bef49ab79f0db0288dffaa25ea40bd3f69c8
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2020-04-22 20:30:17 +0530

    #733 - fix parse url for activity preview

--- src/bp-activity/bp-activity-functions.php
+++ src/bp-activity/bp-activity-functions.php
@@ -5018,213 +5018,24 @@ add_action( 'rest_after_insert_post', 'bp_update_activity_feed_of_post', 99, 3 )
  * @since BuddyBoss 1.0.0
  */
 function bp_activity_action_parse_url() {
-	$url       = $_POST['url'];
-	$json_data = array();
+	// Get URL.
+	$url = $_POST['url'];
 
-	$cache_key = 'bp_activity_oembed_' . md5( serialize( $url ) );
-	$data      = get_transient( $cache_key );
-	if ( ! empty( $data ) ) {
-		wp_send_json( $data );
+	// Check if URL is validated.
+	if ( ! filter_var( $url, FILTER_VALIDATE_URL ) ) {
+		wp_send_json( array( 'error' => __( 'URL is not valid.', 'buddyboss' ) ) );
 	}
 
-	// Fetch the oembed code for URL.
-	$embed_code = wp_oembed_get( $url );
-	if ( ! empty( $embed_code ) ) {
-		$json_data['title']       = ' ';
-		$json_data['description'] = $embed_code;
-		$json_data['images']      = '';
-		$json_data['error']       = '';
-		$json_data['wp_embed']    = true;
+	// Get URL parsed data.
+	$parse_url_data = bp_core_parse_url( $url );
 
-		set_transient( $cache_key, $json_data, DAY_IN_SECONDS );
-
-		wp_send_json( $json_data );
+	// If empty data then send error.
+	if ( empty( $parse_url_data ) ) {
+		wp_send_json( array( 'error' => __( 'Sorry! preview is not available right now. Please try again later.', 'buddyboss' ) ) );
 	}
 
-	// include wensite parser
-	require_once trailingslashit( buddypress()->plugin_dir . 'bp-activity/vendors' ) . '/website-parser/website_parser.php';
-
-	// curling
-	if ( class_exists( 'WebsiteParser' ) ) {
-
-		$parser = new WebsiteParser( $url );
-		$body   = wp_remote_get( $url );
-
-		if ( ! is_wp_error( $body ) && isset( $body['body'] ) ) {
-
-			$title       = '';
-			$description = '';
-			$images      = array();
-
-			$parser->content = $body['body'];
-
-			$meta_tags = $parser->getMetaTags( false );
-
-			if ( empty( $meta_tags ) ) {
-				$doc = new DOMDocument();
-				$doc->loadHTML( $parser->content );
-				$xpath = new DOMXPath( $doc );
-				$query = '//*/meta[starts-with(@property, \'og:\')]';
-				$metas = $xpath->query( $query );
-				foreach ( $metas as $meta ) {
-					$property    = $meta->getAttribute( 'property' );
-					$content     = $meta->getAttribute( 'content' );
-					$meta_tags[] = array( $property, $content );
-				}
-			}
-
-			if ( is_array( $meta_tags ) && ! empty( $meta_tags ) ) {
-				foreach ( $meta_tags as $tag ) {
-					if ( is_array( $tag ) && ! empty( $tag ) ) {
-						if ( $tag[0] == 'og:title' ) {
-							$title = $tag[1];
-						}
-						if ( $tag[0] == 'og:description' ) {
-							$description = html_entity_decode( $tag[1], ENT_QUOTES, 'utf-8' );
-						} elseif ( strtolower( $tag[0] ) == 'description' && $description == '' ) {
-							$description = html_entity_decode( $tag[1], ENT_QUOTES, 'utf-8' );
-						}
-						if ( $tag[0] == 'og:image' ) {
-							$images[] = $tag[1];
-						}
-					}
-				}
-			}
-
-			if ( $title == '' ) {
-				$title = $parser->getTitle( false );
-			}
-
-			$images_sources = $parser->getImageSources( false );
-			if ( ! empty( $images_sources ) ) {
-				foreach ( $images_sources as $key => $img ) {
-					if ( strpos( $url, 'youtube.com' ) > 0 ) {
-						$img = 'https://www.youtube.com' . $img;
-					}
-					if ( @fopen( $img, 'r' ) ) {
-						$images[] = $img;
-					}
-				}
-			}
-
-			// Generate Image URL Previews
-			if ( empty( $images ) ) {
-				$content_type = wp_remote_retrieve_header( $body, 'content-type' );
-				if ( false !== strpos( $content_type, 'image' ) ) {
-					$images = array( $url );
-				}
-			}
-
-			$json_data['title']       = $title;
-			$json_data['description'] = $description;
-			$json_data['images']      = $images;
-			$json_data['error']       = '';
-
-			set_transient( $cache_key, $json_data, DAY_IN_SECONDS );
-		} else {
-			// Extract HTML using curl
-			$ch = curl_init();
-
-			curl_setopt( $ch, CURLOPT_HEADER, 1 );
-			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-			curl_setopt( $ch, CURLOPT_URL, $url );
-			curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
-
-			$data = curl_exec( $ch );
-			curl_close( $ch );
-
-			// Load HTML to DOM Object
-			$dom = new DOMDocument();
-			@$dom->loadHTML( $data );
-
-			// Parse DOM to get Meta Description
-			$metas       = $dom->getElementsByTagName( 'meta' );
-			$meta_tags   = array();
-			$images      = array();
-			$description = '';
-			$title       = '';
-
-			$xpath = new DOMXPath( $dom );
-			$query = '//*/meta[starts-with(@property, \'og:\')]';
-			$metas_query = $xpath->query( $query );
-			foreach ( $metas_query as $meta ) {
-				$property    = $meta->getAttribute( 'property' );
-				$content     = $meta->getAttribute( 'content' );
-				$meta_tags[] = array( $property, $content );
-			}
-
-			if ( is_array( $meta_tags ) && ! empty( $meta_tags ) ) {
-				foreach ( $meta_tags as $tag ) {
-					if ( is_array( $tag ) && ! empty( $tag ) ) {
-						if ( $tag[0] == 'og:title' ) {
-							$title = $tag[1];
-						}
-						if ( $tag[0] == 'og:description' || 'description' === strtolower( $tag[0] ) ) {
-							$description = html_entity_decode( $tag[1], ENT_QUOTES, 'utf-8' );
-						}
-						if ( $tag[0] == 'og:image' ) {
-							$images[] = $tag[1];
-						}
-					}
-				}
-			}
-
-			if ( empty( $title ) ) {
-				// Parse DOM to get Title
-				$nodes = $dom->getElementsByTagName( 'title' );
-				$title = $nodes->item( 0 )->nodeValue;
-			}
-
-			if ( empty( $title ) ) {
-				$nodes = $dom->getElementsByTagName( 'h1' );
-				$title = $nodes->item( 0 )->nodeValue;
-			}
-
-			if ( empty( $title ) ) {
-				$nodes = $dom->getElementsByTagName( 'h2' );
-				$title = $nodes->item( 0 )->nodeValue;
-			}
-
-			if ( empty( $description ) ) {
-				for ( $i = 0; $i < $metas->length; $i ++ ) {
-					$meta = $metas->item( $i );
-					if ( $meta->getAttribute( 'name' ) == 'description' ) {
-						$description = $meta->getAttribute( 'content' );
-					}
-				}
-			}
-
-			// Parse DOM to get Images
-			$image_elements = $dom->getElementsByTagName( 'img' );
-			for ( $i = 0; $i < $image_elements->length; $i ++ ) {
-				$image = $image_elements->item( $i );
-				$src   = $image->getAttribute( 'src' );
-
-				if ( filter_var( $src, FILTER_VALIDATE_URL ) ) {
-					$images[] = $src;
-				}
-			}
-
-			if ( isset( $images ) && isset( $description ) && '' === trim( $title ) ) {
-				$title = $description;
-			}
-
-			if ( isset( $title ) && isset( $description ) && isset( $images ) ) {
-				$json_data['title']       = $title;
-				$json_data['description'] = $description;
-				$json_data['images']      = $images;
-				$json_data['error']       = '';
-
-				set_transient( $cache_key, $json_data, DAY_IN_SECONDS );
-			} else {
-				$json_data['error'] = __( 'Sorry! preview is not available right now. Please try again later.', 'buddyboss' );
-			}
-		}
-	} else {
-		$json_data['error'] = __( 'Sorry! preview is not available right now. Please try again later.', 'buddyboss' );
-	}
-
-	wp_send_json( $json_data );
+	// send json success.
+	wp_send_json( $parse_url_data );
 }
 
 add_action( 'wp_ajax_bp_activity_parse_url', 'bp_activity_action_parse_url' );
