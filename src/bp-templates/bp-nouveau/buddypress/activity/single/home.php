83b96082a1f90dfeef96729c24ba6a55a4330e32
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2020-04-23 17:11:40 +0530

    #731 - added single activity before content hook

--- src/bp-templates/bp-nouveau/buddypress/activity/single/home.php
+++ src/bp-templates/bp-nouveau/buddypress/activity/single/home.php
@@ -11,6 +11,8 @@
 
 	<div class="activity" data-bp-single="<?php echo esc_attr( bp_current_action() ); ?>">
 
+		<?php do_action( 'bp_before_single_activity_content' ); ?>
+
 		<ul id="activity-stream" class="activity-list item-list bp-list" data-bp-list="activity">
 
 			<li id="bp-ajax-loader"><?php bp_nouveau_user_feedback( 'single-activity-loading' ); ?></li>
