a605fb4a27c030671f85d79360fdb13b90837119
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2020-05-11 22:06:19 +0530

    set filters for subnav and jquery datetime picker lib register

--- src/bp-core/bp-core-functions.php
+++ src/bp-core/bp-core-functions.php
@@ -4715,3 +4715,61 @@ function bp_core_parse_url( $url ) {
 	 */
 	return apply_filters( 'bp_core_parse_url', $parsed_url_data );
 }
+
+/**
+ * Format file size units
+ *
+ * @param int $bytes
+ * @param bool $unit_label
+ * @param string $type
+ *
+ * @return string
+ * @since BuddyBoss 1.3.5
+ *
+ */
+function bp_core_format_size_units( $bytes, $unit_label = false, $type = '' ) {
+
+	if ( $bytes > 0 && ! $unit_label ) {
+		if ( 'GB' === $type ) {
+			return $bytes / 1073741824;
+		} elseif ( 'MB' === $type ) {
+			return $bytes / 1048576;
+		} elseif ( 'KB' === $type ) {
+			return $bytes / 1024;
+		} else {
+			return $bytes;
+		}
+	}
+
+	if ( empty( $type ) ) {
+		if ( $bytes >= 1073741824 ) {
+			$bytes = number_format( ( $bytes / 1073741824 ), 2, '.', '') . ' GB';
+		} elseif ( $bytes >= 1048576 ) {
+			$bytes = number_format( ( $bytes / 1048576 ), 2, '.', '') . ' MB';
+		} elseif ( $bytes >= 1024 ) {
+			$bytes = number_format( ( $bytes / 1024 ), 2, '.', '') . ' KB';
+		} elseif ( $bytes > 1 ) {
+			$bytes = $bytes . ' bytes';
+		} elseif ( $bytes == 1 ) {
+			$bytes = $bytes . ' byte';
+		} else {
+			$bytes = '0' . ' bytes';
+		}
+	} else {
+		if ( 'GB' === $type ) {
+			$bytes = number_format( ( $bytes / 1073741824 ), 2, '.', '') . ' GB';
+		} elseif ( 'MB' === $type ) {
+			$bytes = number_format( ( $bytes / 1048576 ), 2, '.', '') . ' MB';
+		} elseif ( 'KB' === $type ) {
+			$bytes = number_format( ( $bytes / 1024 ), 2, '.', '') . ' KB';
+		} elseif ( 'bytes' === $type ) {
+			$bytes = $bytes . ' bytes';
+		} elseif ( 1 === $bytes ) {
+			$bytes = $bytes . ' byte';
+		} else {
+			$bytes = '0' . ' bytes';
+		}
+	}
+
+	return $bytes;
+}
