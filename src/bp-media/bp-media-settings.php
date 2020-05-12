fd588e4011c5d8e2719673bb39831c01cc2fb90d
Michael Eisenwasser <michael@buddyboss.com>
2020-05-12 00:27:31 -0600

    re-ordering Description and MIME Type

--- src/bp-media/bp-media-settings.php
+++ src/bp-media/bp-media-settings.php
@@ -1073,8 +1073,8 @@ function bp_media_settings_callback_extension_document_support() {
 		<th class="ext-head ext-head-enable"></th>
 		<th class="ext-head ext-head-extension"><?php echo esc_html__( 'Name', 'buddyboss' ); ?></th>
 		<th class="ext-head ext-head-extension"><?php echo esc_html__( 'Extension', 'buddyboss' ); ?></th>
-		<th class="ext-head ext-head-mime"><?php echo esc_html__( 'MIME Type', 'buddyboss' ); ?></th>
 		<th class="ext-head ext-head-desc"><?php echo esc_html__( 'Description', 'buddyboss' ); ?></th>
+		<th class="ext-head ext-head-mime"><?php echo esc_html__( 'MIME Type', 'buddyboss' ); ?></th>
 		<th class="ext-head ext-head-control"></th>
 		</thead>
 		<tbody>
@@ -1098,10 +1098,10 @@ function bp_media_settings_callback_extension_document_support() {
 					<input <?php echo esc_attr( $edit ); ?> name="<?php echo esc_attr( $name . '[extension]' ); ?>" id="<?php echo esc_attr( $name ) . 'extension'; ?>" type="text" value="<?php echo esc_attr( $extension['extension'] ); ?>"/>
 				</td>
 				<td>
-					<input <?php echo esc_attr( $edit ); ?> name="<?php echo esc_attr( $name . '[mime_type]' ); ?>" id="<?php echo esc_attr( $name ) . 'mime'; ?>" type="text" value="<?php echo esc_attr( $extension['mime_type'] ); ?>"/>
+					<input name="<?php echo esc_attr( $name . '[description]' ); ?>" id="<?php echo esc_attr( $name ) . 'desc'; ?>" type="text" value="<?php echo esc_attr( $extension['description'] ); ?>"/>
 				</td>
 				<td>
-					<input name="<?php echo esc_attr( $name . '[description]' ); ?>" id="<?php echo esc_attr( $name ) . 'desc'; ?>" type="text" value="<?php echo esc_attr( $extension['description'] ); ?>"/>
+					<input <?php echo esc_attr( $edit ); ?> name="<?php echo esc_attr( $name . '[mime_type]' ); ?>" id="<?php echo esc_attr( $name ) . 'mime'; ?>" type="text" value="<?php echo esc_attr( $extension['mime_type'] ); ?>"/>
 				</td>
 				<?php
 				if ( ! $is_default ) {
@@ -1135,10 +1135,10 @@ function bp_media_settings_callback_extension_document_support() {
 				<input name="extension-extension" data-name="<?php echo esc_attr( $name . '[extension]' ); ?>" type="text" class="extension-extension"/>
 			</td>
 			<td>
-				<input name="extension-mime" data-name="<?php echo esc_attr( $name . '[mime_type]' ); ?>" type="text" class="extension-mime"/>
+				<input name="extension-desc" data-name="<?php echo esc_attr( $name . '[description]' ); ?>" type="text" class="extension-desc"/>
 			</td>
 			<td>
-				<input name="extension-desc" data-name="<?php echo esc_attr( $name . '[description]' ); ?>" type="text" class="extension-desc"/>
+				<input name="extension-mime" data-name="<?php echo esc_attr( $name . '[mime_type]' ); ?>" type="text" class="extension-mime"/>
 			</td>
 			<td>
 				<span class="dashicons dashicons-dismiss" id="btn-remove-extensions"></span>
@@ -1148,8 +1148,6 @@ function bp_media_settings_callback_extension_document_support() {
 		<tfoot>
 		<tr>
 			<td colspan="6">
-				
-				<div id="btn-add-extensions" class="button-primary"><?php echo esc_html__( 'Add Extension', 'buddyboss' ); ?></div>
 				
 				<?php
 				$check_mime_type_link = add_query_arg(
@@ -1162,6 +1160,8 @@ function bp_media_settings_callback_extension_document_support() {
 				);
 				?>
 				<a href="<?php echo esc_url( $check_mime_type_link ); ?>" id="btn-check-mime-type" class="button" target="_blank"><?php echo esc_html__( 'Find MIME Type', 'buddyboss' ); ?></a>
+
+				<div id="btn-add-extensions" class="button-primary"><?php echo esc_html__( 'Add Extension', 'buddyboss' ); ?></div>
 
 			</td>
 		</tr>
