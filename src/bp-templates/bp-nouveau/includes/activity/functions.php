4b84aca1f4292eae4e58157400f54a5c478ed89a
Chetan Satasiya <chetan@buddyboss.com>
2020-06-03 16:17:39 +0530

    #266 - document network search popup GIF fix

--- src/bp-templates/bp-nouveau/includes/activity/functions.php
+++ src/bp-templates/bp-nouveau/includes/activity/functions.php
@@ -53,7 +53,7 @@ function bp_nouveau_activity_enqueue_scripts() {
 	wp_enqueue_style( 'bp-medium-editor-beagle' );
 
 	// Enqueue activity form parts and js required for single activity
-	if ( bp_nouveau_current_user_can( 'publish_activity' ) && bp_is_single_activity() ) {
+	if ( bp_nouveau_current_user_can( 'publish_activity' ) ) {
 		wp_enqueue_script( 'bp-nouveau-activity-post-form' );
 		bp_get_template_part( 'common/js-templates/activity/form' );
 	}
