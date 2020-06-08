8706eaaec98e80ec8084f25c59c95570f0b7ae6e
Kartik Suthar <kartik.7013@gmail.com>
2020-06-08 14:47:04 +0530

    fix activity display based on privacy

--- src/bp-activity/classes/class-bp-activity-activity.php
+++ src/bp-activity/classes/class-bp-activity-activity.php
@@ -534,7 +534,7 @@ class BP_Activity_Activity {
 		}
 
 		// The filter activities by their privacy
-		if ( ! empty( $r['privacy'] ) && empty( $r['in'] ) ) {
+		if ( ! empty( $r['privacy'] ) ) {
 			$privacy                     = "'" . implode( "', '", $r['privacy'] ) . "'";
 			$where_conditions['privacy'] = "a.privacy IN ({$privacy})";
 		}
