999428b84327502546837aec26abc42349de5123
Chetan Satasiya <chetan@buddyboss.com>
2020-06-10 00:19:39 +0530

    Fix PDF preview broken on older upload

--- src/bp-templates/bp-nouveau/includes/document/functions.php
+++ src/bp-templates/bp-nouveau/includes/document/functions.php
@@ -1069,7 +1069,7 @@ function bp_document_get_preview_image_url( $document_id, $extension, $preview_a
 		}
 		$document_id        = 'forbidden_' . $document_id;
 		$attachment_id      = 'forbidden_' . $preview_attachment_id;
-		if ( ! empty( $preview_attachment_id ) ) {
+		if ( ! empty( $preview_attachment_id ) && wp_attachment_is_image( $preview_attachment_id ) ) {
 			$attachment_url     = trailingslashit( buddypress()->plugin_url ) . 'bp-templates/bp-nouveau/includes/document/preview.php?id=' . base64_encode( $attachment_id ) . '&id1=' . base64_encode( $document_id );
 		}
 	}
