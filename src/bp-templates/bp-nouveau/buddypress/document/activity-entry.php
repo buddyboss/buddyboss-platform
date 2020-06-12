ea51685d042c01690ae6995019b4d3339ad0df0b
Chetan Satasiya <chetan@buddyboss.com>
2020-06-12 10:52:13 +0530

    Text changes on forum activity and message preview

--- src/bp-templates/bp-nouveau/buddypress/document/activity-entry.php
+++ src/bp-templates/bp-nouveau/buddypress/document/activity-entry.php
@@ -56,7 +56,7 @@ $click_text 	= apply_filters( 'bp_document_activity_click_to_view_text', __( ' v
 if ( 'forums' === bp_get_db_document_privacy() ) {
 	//$class_theatre 	= '';
 	//$class_popup	= '';
-	$click_text 	= apply_filters( 'bp_document_activity_click_to_download_text', __( ' Download', 'buddyboss' ) );
+	$click_text 	= apply_filters( 'bp_document_activity_click_to_download_text', __( ' View', 'buddyboss' ) );
 }
 
 $bp_document_music_preview = apply_filters( 'bp_document_music_preview', true );
