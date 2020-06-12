05cde11f048a9877f9d4ce1221de74b2abd4e038
Chetan Satasiya <chetan@buddyboss.com>
2020-06-12 18:17:49 +0530

    Fix ABSPATH warning

--- src/bp-templates/bp-nouveau/includes/document/player.php
+++ src/bp-templates/bp-nouveau/includes/document/player.php
@@ -1,11 +1,11 @@
 <?php
 
-if(!defined(ABSPATH)){
-	$pagePath = explode('/wp-content/', dirname(__FILE__));
-	include_once(str_replace('wp-content/' , '', $pagePath[0] . '/wp-load.php'));
+if ( ! defined( 'ABSPATH' ) ) {
+	$pagePath = explode( '/wp-content/', dirname( __FILE__ ) );
+	include_once str_replace( 'wp-content/', '', $pagePath[0] . '/wp-load.php' );
 }
 
-define('WP_USE_THEMES', true);
+define( 'WP_USE_THEMES', true );
 
 global $wpdb, $bp;
 
@@ -14,19 +14,19 @@ if ( empty( $_REQUEST ) && empty( $_REQUEST['id'] ) && empty( $_REQUEST['id1'] )
 	exit();
 }
 
-$encode_id      = base64_decode( $_REQUEST['id'] );
-$encode_id1     = base64_decode( $_REQUEST['id1'] );
-$explode_arr    = explode( 'forbidden_', $encode_id);
-$explode_arr1   = explode( 'forbidden_', $encode_id1);
+$encode_id    = base64_decode( $_REQUEST['id'] );
+$encode_id1   = base64_decode( $_REQUEST['id1'] );
+$explode_arr  = explode( 'forbidden_', $encode_id );
+$explode_arr1 = explode( 'forbidden_', $encode_id1 );
 
-if ( isset( $explode_arr ) && !empty( $explode_arr ) && isset( $explode_arr[1] ) && (int) $explode_arr[1] > 0 &&
-     isset( $explode_arr1 ) && !empty( $explode_arr1 ) && isset( $explode_arr1[1] ) && (int) $explode_arr1[1] > 0  ) {
-	$id                 = (int) $explode_arr[1];
-	$id1                = (int) $explode_arr1[1];
-	$document_privacy   = bp_document_user_can_manage_document( $id1, bp_loggedin_user_id() );
-	$can_view           = ( true === (bool) $document_privacy['can_view'] ) ? true : false;
+if ( isset( $explode_arr ) && ! empty( $explode_arr ) && isset( $explode_arr[1] ) && (int) $explode_arr[1] > 0 &&
+	 isset( $explode_arr1 ) && ! empty( $explode_arr1 ) && isset( $explode_arr1[1] ) && (int) $explode_arr1[1] > 0 ) {
+	$id               = (int) $explode_arr[1];
+	$id1              = (int) $explode_arr1[1];
+	$document_privacy = bp_document_user_can_manage_document( $id1, bp_loggedin_user_id() );
+	$can_view         = ( true === (bool) $document_privacy['can_view'] ) ? true : false;
 	if ( $can_view ) {
-		$type = get_post_mime_type( $id );
+		$type            = get_post_mime_type( $id );
 		$output_file_src = bp_document_scaled_image_path( $id );
 
 		if ( ! file_exists( $output_file_src ) ) {
@@ -41,10 +41,10 @@ if ( isset( $explode_arr ) && !empty( $explode_arr ) && isset( $explode_arr[1] )
 		$end    = $size - 1;       // End byte
 		header( "Content-Type: $type" );
 		header( "Accept-Ranges: 0-$length" );
-		header( "Accept-Ranges: bytes" );
+		header( 'Accept-Ranges: bytes' );
 		if ( isset( $_SERVER['HTTP_RANGE'] ) ) {
-			$c_start = $start;
-			$c_end   = $end;
+			$c_start         = $start;
+			$c_end           = $end;
 			list( , $range ) = explode( '=', $_SERVER['HTTP_RANGE'], 2 );
 			if ( strpos( $range, ',' ) !== false ) {
 				header( 'HTTP/1.1 416 Requested Range Not Satisfiable' );
@@ -71,7 +71,7 @@ if ( isset( $explode_arr ) && !empty( $explode_arr ) && isset( $explode_arr[1] )
 			header( 'HTTP/1.1 206 Partial Content' );
 		}
 		header( "Content-Range: bytes $start-$end/$size" );
-		header( "Content-Length: " . $length );
+		header( 'Content-Length: ' . $length );
 		$buffer = 1024 * 8;
 		while ( ! feof( $fp ) && ( $p = ftell( $fp ) ) <= $end ) {
 			if ( $p + $buffer > $end ) {
--- src/bp-templates/bp-nouveau/includes/document/preview.php
+++ src/bp-templates/bp-nouveau/includes/document/preview.php
@@ -1,11 +1,11 @@
 <?php
 
-if(!defined(ABSPATH)){
-	$pagePath = explode('/wp-content/', dirname(__FILE__));
-	include_once(str_replace('wp-content/' , '', $pagePath[0] . '/wp-load.php'));
+if ( ! defined( 'ABSPATH' ) ) {
+	$pagePath = explode( '/wp-content/', dirname( __FILE__ ) );
+	include_once str_replace( 'wp-content/', '', $pagePath[0] . '/wp-load.php' );
 }
 
-define('WP_USE_THEMES', true);
+define( 'WP_USE_THEMES', true );
 
 global $wpdb, $bp;
 
@@ -14,27 +14,27 @@ if ( empty( $_REQUEST ) && empty( $_REQUEST['id'] ) && empty( $_REQUEST['id1'] )
 	exit();
 }
 
-$encode_id      = base64_decode( $_REQUEST['id'] );
-$encode_id1     = base64_decode( $_REQUEST['id1'] );
-$explode_arr    = explode( 'forbidden_', $encode_id);
-$explode_arr1   = explode( 'forbidden_', $encode_id1);
+$encode_id    = base64_decode( $_REQUEST['id'] );
+$encode_id1   = base64_decode( $_REQUEST['id1'] );
+$explode_arr  = explode( 'forbidden_', $encode_id );
+$explode_arr1 = explode( 'forbidden_', $encode_id1 );
 
-if ( isset( $explode_arr ) && !empty( $explode_arr ) && isset( $explode_arr[1] ) && (int) $explode_arr[1] > 0 &&
-     isset( $explode_arr1 ) && !empty( $explode_arr1 ) && isset( $explode_arr1[1] ) && (int) $explode_arr1[1] > 0  ) {
-	$id                 = (int) $explode_arr[1];
-	$id1                = (int) $explode_arr1[1];
-	$document_privacy   = bp_document_user_can_manage_document( $id1, bp_loggedin_user_id() );
-	$can_view           = ( true === (bool) $document_privacy['can_view'] ) ? true : false;
+if ( isset( $explode_arr ) && ! empty( $explode_arr ) && isset( $explode_arr[1] ) && (int) $explode_arr[1] > 0 &&
+	 isset( $explode_arr1 ) && ! empty( $explode_arr1 ) && isset( $explode_arr1[1] ) && (int) $explode_arr1[1] > 0 ) {
+	$id               = (int) $explode_arr[1];
+	$id1              = (int) $explode_arr1[1];
+	$document_privacy = bp_document_user_can_manage_document( $id1, bp_loggedin_user_id() );
+	$can_view         = ( true === (bool) $document_privacy['can_view'] ) ? true : false;
 	if ( $can_view && wp_attachment_is_image( $id ) ) {
-		$type = get_post_mime_type( $id );
+		$type            = get_post_mime_type( $id );
 		$output_file_src = bp_document_scaled_image_path( $id );
 
 		if ( ! file_exists( $output_file_src ) ) {
 			echo '// Silence is golden.';
 			exit();
 		}
-		header("Content-Type: $type");
-		readfile("$output_file_src");
+		header( "Content-Type: $type" );
+		readfile( "$output_file_src" );
 	} else {
 		echo '// Silence is golden.';
 		exit();
