6ffee28e40fa5a036ce627e1b4432c94bc12227c
Chetan Satasiya <chetan@buddyboss.com>
2020-06-15 10:54:50 +0530

    Fix breadcrums

--- src/bp-document/bp-document-functions.php
+++ src/bp-document/bp-document-functions.php
@@ -1957,7 +1957,7 @@ function bp_document_folder_bradcrumb( $folder_id ) {
             (SELECT @r := %d, @l := 0) vars, {$document_folder_table} m
         WHERE @r <> 0) d
     JOIN {$document_folder_table} c
-    ON d._id = c.id d.level DESC",
+    ON d._id = c.id ORDER BY d.level ASC",
 		$folder_id
 	);
 
