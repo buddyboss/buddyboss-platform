f2fadd387e6522b0a33b3f57f808645622da8724
Kartik Suthar <kartik.7013@gmail.com>
2020-06-19 16:37:44 +0530

    #1024 - removed multiple buttons and placed the notice there

--- src/bp-templates/bp-nouveau/buddypress/groups/single/request-membership.php
+++ src/bp-templates/bp-nouveau/buddypress/groups/single/request-membership.php
@@ -10,73 +10,39 @@ bp_nouveau_group_hook( 'before', 'request_membership_content' );
 
 if ( groups_check_user_has_invite( bp_loggedin_user_id(), bp_get_current_group_id() ) ) {
 
-	bp_nouveau_group_hook( 'before', 'invites_content' );
 	?>
 
-	<div class="entry-content">
-		<ul id="groups-list" class="invites item-list bp-list item-list groups-list" data-bp-list="groups_invites">
-
-			<li <?php bp_group_class( array( 'item-entry' ) ); ?> data-bp-item-id="<?php bp_group_id(); ?>" data-bp-item-component="groups">
-				<div class="list-wrap">
-
-					<?php if ( ! bp_disable_group_avatar_uploads() ) : ?>
-						<div class="item-avatar">
-							<a href="<?php bp_group_permalink(); ?>"><?php bp_group_avatar( bp_nouveau_avatar_args() ); ?></a>
-						</div>
-					<?php endif; ?>
-
-					<div class="item">
-
-						<div class="item-block">
-
-							<h2 class="list-title groups-title"><?php bp_group_link(); ?></h2>
-
-							<p class="item-meta group-details">
-								<?php $inviter = bp_groups_get_invited_by( bp_loggedin_user_id(), bp_get_current_group_id() ); ?>
-								<?php if ( ! empty( $inviter ) ) : ?>
-									<?php
-									printf(
-										__( 'Invited by %1$s &middot; %2$s.', 'buddyboss-theme' ),
-										sprintf(
-											'<a href="%s">%s</a>',
-											$inviter['url'],
-											$inviter['name']
-										),
-										sprintf(
-											'<span class="last-activity">%s</span>',
-											bp_core_time_since( $inviter['date_modified'] )
-										)
-									);
-									?>
-								<?php endif; ?>
-							</p>
-
-							<p class="desc item-meta invite-message">
-								<?php echo bp_groups_get_invite_messsage_for_user( bp_displayed_user_id(), bp_get_current_group_id() ); ?>
-							</p>
-
-						</div>
-
-						<?php bp_nouveau_group_hook( '', 'invites_item' ); ?>
-
-						<?php
-						bp_nouveau_groups_invite_buttons(
-							array(
-								'container'      => 'ul',
-								'button_element' => 'button',
-							)
-						);
-						?>
-
-					</div>
-				</div>
-			</li>
-		</ul>
-	</div>
+	<aside class="bp-feedback bp-messages loading">
+		<span class="bp-icon" aria-hidden="true"></span>
+		<p>
+			<?php
+			$inviter = bp_groups_get_invited_by( bp_loggedin_user_id(), bp_get_current_group_id() );
+			if ( ! empty( $inviter ) ) :
+				$groups_link = trailingslashit( bp_loggedin_user_domain() . bp_get_groups_slug() );
+				printf(
+					__( 'You are already invited to this group by %1$s %2$s. %3$s', 'buddyboss' ),
+					sprintf(
+						'<a href="%s">%s</a>',
+						$inviter['url'],
+						$inviter['name']
+					),
+					sprintf(
+						'<span class="last-activity">%s</span>',
+						bp_core_time_since( $inviter['date_modified'] )
+					),
+					sprintf(
+						'<a href="%s" >%s</a>',
+						esc_url( trailingslashit( $groups_link . 'invites' ) ),
+						__( 'View Invitation', 'buddyboss' ),
+					)
+				);
+				?>
+			<?php endif; ?>
+		</p>
+	</aside>
 
 	<?php
 
-	bp_nouveau_group_hook( 'after', 'invites_content' );
 
 } else if ( ! bp_group_has_requested_membership() ) : ?>
 	<p>
