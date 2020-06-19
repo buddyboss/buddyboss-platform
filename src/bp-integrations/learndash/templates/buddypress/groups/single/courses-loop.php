0af6d7c402d458dd5d0fe081369a285658d5046f
Manish Patolia <manishpatolia09@gmail.com>
2020-06-19 16:50:56 +0530

    Formate issue

--- src/bp-integrations/learndash/templates/buddypress/groups/single/courses-loop.php
+++ src/bp-integrations/learndash/templates/buddypress/groups/single/courses-loop.php
@@ -1,7 +1,7 @@
 <?php
 
 $is_enrolled            = false;
-$lesson_list           = learndash_get_lesson_list( get_the_ID(), array( 'num' => - 1 ) );
+$lesson_list            = learndash_get_lesson_list( get_the_ID(), array( 'num' => - 1 ) );
 $current_user_id        = get_current_user_id();
 $access_list            = learndash_get_course_meta_setting( $post->ID, 'course_access_list' );
 $admin_enrolled         = LearnDash_Settings_Section::get_section_setting( 'LearnDash_Settings_Section_General_Admin_User', 'courses_autoenroll_admin_users' );
