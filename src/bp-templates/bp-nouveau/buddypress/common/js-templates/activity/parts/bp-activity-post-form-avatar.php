a2f16c1cd5d46ef3debb2aaf626a1a27e47f2d76
Mh Shohel <mhshohel89@gmail.com>
2020-07-09 12:42:07 +0600

    Fixed Wrong avatar on the new activity post form #1048

--- src/bp-templates/bp-nouveau/buddypress/common/js-templates/activity/parts/bp-activity-post-form-avatar.php
+++ src/bp-templates/bp-nouveau/buddypress/common/js-templates/activity/parts/bp-activity-post-form-avatar.php
@@ -1,7 +1,7 @@
 <script type="text/html" id="tmpl-activity-post-form-avatar">
-	<# if ( data.display_avatar ) { #>
+	<# if ( data.display_avatar ) {  #>
 	<a class="activity-post-avatar" href="{{data.user_domain}}">
-		<img src="{{data.avatar_url}}" class="avatar user-{{data.user_id}}-avatar avatar-{{data.avatar_width}} photo" width="{{data.avatar_width}}" height="{{data.avatar_width}}" alt="{{data.avatar_alt}}" />
+		<img src="{{data.avatar_url.split("#038;").join("")}}" class="avatar user-{{data.user_id}}-avatar avatar-{{data.avatar_width}} photo" width="{{data.avatar_width}}" height="{{data.avatar_width}}" alt="{{data.avatar_alt}}" />
 		<span class="user-name">{{data.user_display_name}}</span>
 	</a>
 	<# } #>
