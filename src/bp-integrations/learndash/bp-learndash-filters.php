9863a47c093760840e634c5741ab8fdff5af999c
Chetan Satasiya <chetan@buddyboss.com>
2020-07-13 20:39:00 +0530

    Fix slug issue with LD

--- src/bp-integrations/learndash/bp-learndash-filters.php
+++ src/bp-integrations/learndash/bp-learndash-filters.php
@@ -15,6 +15,8 @@ defined( 'ABSPATH' ) || exit;
 add_filter( 'bp_activity_pre_transition_post_type_status', 'bp_activity_pre_transition_post_type_status', 10, 4 );
 
 add_filter( 'bp_core_wpsignup_redirect', 'bp_ld_popup_register_redirect', 10 );
+add_filter( 'sfwd_cpt_options', 'bb_ld_group_archive_slug_change', 999, 2 );
+add_filter( 'learndash_settings_fields', 'bb_ld_group_archive_backend_slug_print', 9999, 2 );
 
 /* Actions *******************************************************************/
 add_action( 'add_meta_boxes', 'bp_activity_add_meta_boxes', 50 );
@@ -295,11 +297,47 @@ function bb_group_wp_admin_bar_updates_menu() {
 		//Get a reference to the edit node to modify.
 		$new_content_node = $wp_admin_bar->get_node('edit');
 
-		//Change href
-		$new_content_node->href = get_edit_post_link( $page_ids['groups'] );
+		if ( isset( $new_content_node ) && ! empty( $new_content_node->href ) ) {
+			//Change href
+			$new_content_node->href = get_edit_post_link( $page_ids['groups'] );
+		}
 
 		//Update Node.
 		$wp_admin_bar->add_node($new_content_node);
 
 	}
+}
+
+/**
+ * Learndash Plugin fix conflict with groups directory page.
+ *
+ * @since BuddyBoss 1.4.7
+ */
+function bb_ld_group_archive_slug_change( $post_options, $post_type ) {
+	$page_ids = bp_core_get_directory_page_ids();
+
+	if ( bp_is_active( 'groups') && is_array( $page_ids ) && isset( $page_ids['groups'] ) && !empty( $page_ids['groups'] ) && learndash_get_post_type_slug( 'group' ) === $post_type ) {
+		$post_options['rewrite']['slug'] = 'ld-groups';
+	}
+
+	return $post_options;
+}
+
+/**
+ * Learndash Plugin fix conflict with groups directory page and show the proper label on  http://localhost/platform/wp-admin/admin.php?page=groups-options page.
+ *
+ * @since BuddyBoss 1.4.7
+ */
+function bb_ld_group_archive_backend_slug_print( $setting_option_fields, $settings_section_key) {
+
+	if ( is_admin() && isset( $_REQUEST ) && isset( $_REQUEST['page'] ) && 'groups-options' === $_REQUEST['page'] && 'cpt_options' === $settings_section_key && is_array( $setting_option_fields ) && isset( $setting_option_fields['has_archive']['options']['yes'] ) ) {
+		$setting_option_fields['has_archive']['options']['yes'] = sprintf(
+		// translators: placeholder: URL for CPT Archive.
+			esc_html_x( 'Archive URL: %s', 'placeholder: URL for CPT Archive', 'buddyboss' ),
+			'<code><a target="blank" href="' . home_url( 'ld-groups' ) . '">' . home_url( 'ld-groups' ) . '</a></code>'
+		);
+	}
+
+	return $setting_option_fields;
+
 }
