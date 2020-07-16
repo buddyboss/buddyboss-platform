a863ce449ac4b2b44cda915b8556129197426876
Mehul Kaklotar <mehul.kaklotar@gmail.com>
2020-07-16 19:36:03 +0530

    #1004 - message & reply content empty check fix

--- src/bp-templates/bp-nouveau/includes/messages/ajax.php
+++ src/bp-templates/bp-nouveau/includes/messages/ajax.php
@@ -132,7 +132,7 @@ function bp_nouveau_ajax_messages_send_message() {
 	}
 
 	// Validate subject and message content.
-	if ( empty( $_POST['message_content'] ) ) {
+	if ( empty( $_POST['message_content'] ) || ! strlen( trim( html_entity_decode( wp_strip_all_tags( $_POST['content'] ) ) ) ) ) {
 		$response['feedback'] = __( 'Your message was not sent. Please enter some content.', 'buddyboss' );
 
 		wp_send_json_error( $response );
@@ -311,7 +311,7 @@ function bp_nouveau_ajax_messages_send_reply() {
 		wp_send_json_error( $response );
 	}
 
-	if ( empty( $_POST['content'] ) || empty( $_POST['thread_id'] ) ) {
+	if ( empty( $_POST['content'] ) || ! strlen( trim( html_entity_decode( wp_strip_all_tags( $_POST['content'] ) ) ) ) || empty( $_POST['thread_id'] ) ) {
 		$response['feedback'] = __( 'Please add some content to your message.', 'buddyboss' );
 
 		wp_send_json_error( $response );
