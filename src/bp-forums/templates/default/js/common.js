de4b145123cf5cafdebb8e362ce7f12fb178984b
amin <amin@buddyboss.com>
2020-07-21 17:53:04 +0530

    #1235 - Fixed HTML loads in forum topic,reply edit page

--- src/bp-forums/templates/default/js/common.js
+++ src/bp-forums/templates/default/js/common.js
@@ -111,12 +111,13 @@ jQuery( document ).ready(
 								ready: function () {
 									if (typeof window.forums_medium_topic_editor !== 'undefined' && typeof window.forums_medium_topic_editor[key] !== 'undefined') {
 										window.forums_medium_topic_editor[key].setContent( jQuery('#'+elem_id).closest('form').find( '#bbp_topic_content' ).val() );
+										window.forums_medium_topic_editor[key].resetContent();
 									}
 									if (typeof window.forums_medium_reply_editor !== 'undefined' && typeof window.forums_medium_reply_editor[key] !== 'undefined') {
-										window.forums_medium_reply_editor[key].setContent( jQuery('#'+elem_id).closest('form').find( '#bbp_reply_content' ).val() );
+										window.forums_medium_reply_editor[key].resetContent();
 									}
 									if (typeof window.forums_medium_forum_editor !== 'undefined' && typeof window.forums_medium_forum_editor[key] !== 'undefined') {
-										window.forums_medium_forum_editor[key].setContent( jQuery('#'+elem_id).closest('form').find( '#bbp_forum_content' ).val() );
+										window.forums_medium_forum_editor[key].resetContent();
 									}
 								},
 								emojibtn_click: function () {
