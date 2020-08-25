ce82c6690b38d52f12e1ee5e9d146397fac17aef
Chetan Satasiya <chetan@buddyboss.com>
2020-08-25 18:03:21 +0530

    Fix emoji issue

--- src/bp-templates/bp-nouveau/js/buddypress-activity-post-form.js
+++ src/bp-templates/bp-nouveau/js/buddypress-activity-post-form.js
@@ -1375,7 +1375,7 @@ window.bp = window.bp || {};
 				var startIndex  = urlText.indexOf( prefix );
 				var responseUrl = '';
 
-				if ( typeof $( $.parseHTML( urlText ) ).attr( 'href' ) !== 'undefined' ) {
+				if ( typeof $( urlText ).attr( 'href' ) !== 'undefined' ) {
 					urlString = $( urlText ).attr( 'href' );
 				} else {
 					for ( var i = startIndex; i < urlText.length; i++ ) {
@@ -2626,7 +2626,7 @@ window.bp = window.bp || {};
 				}
 
 				// validation for content editor.
-				if ( $( $.parseHTML( content ) ).text().trim() === '' && ( ( typeof self.model.get( 'document' ) !== 'undefined' && ! self.model.get( 'document' ).length ) && ( typeof self.model.get( 'media' ) !== 'undefined' && ! self.model.get( 'media' ).length ) && ( typeof self.model.get( 'gif_data' ) !== 'undefined' && ! Object.keys( self.model.get( 'gif_data' ) ).length ) ) ) {
+				if ( $( content ).text().trim() === '' && ( ( typeof self.model.get( 'document' ) !== 'undefined' && ! self.model.get( 'document' ).length ) && ( typeof self.model.get( 'media' ) !== 'undefined' && ! self.model.get( 'media' ).length ) && ( typeof self.model.get( 'gif_data' ) !== 'undefined' && ! Object.keys( self.model.get( 'gif_data' ) ).length ) ) ) {
 					self.model.set(
 						'errors',
 						{
