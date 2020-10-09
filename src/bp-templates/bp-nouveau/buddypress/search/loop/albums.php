f7aa81e3d6ae7ac47a24bf4e5ee4120d33f2e8e4
amin <amin@buddyboss.com>
2020-10-09 19:18:15 +0530

    #1316 - Styling album and photo search result

--- src/bp-templates/bp-nouveau/buddypress/search/loop/albums.php
+++ src/bp-templates/bp-nouveau/buddypress/search/loop/albums.php
@@ -6,21 +6,35 @@ $albums_link = bp_get_album_link();
 	<div class="list-wrap">
 		<div class="item">
 
-			<div class="media-folder_items ac-folder-list">
-				<div class="media-folder_icon">
+			<div class="media-album_items ac-album-list">
+				<div class="media-album_thumb">
 					<a href="<?php echo esc_url( $albums_link ); ?>">
-						<i class="bb-icon-folder-stacked"></i>
+						<img src="https://picsum.photos/400/270" alt="<?php echo bp_get_album_title(); ?>" />
 					</a>
 				</div>
 
-				<div class="media-folder_details">
-					<a class="media-folder_name " href="<?php echo esc_url( $albums_link ); ?>">
+				<div class="media-album_details">
+					<a class="media-album_name " href="<?php echo esc_url( $albums_link ); ?>">
 						<span><?php echo bp_get_album_title(); ?></span>
 					</a>
+					<span class="media-photo_count">2 photos</span>
 				</div>
 
-				<div class="media-folder_visibility">
-					<div class="media-folder_details__bottom">
+				<div class="media-album_modified">
+					<div class="media-album_details__bottom">
+						<span class="media-album_date">October 9, 2020</span>
+						<span class="media-album_author">by <a href="http://localhost/buddyboss/members/john/documents/">John Smith</a></span>
+					</div>
+				</div>
+
+				<div class="media-album_group">
+					<div class="media-album_details__bottom">
+						<span class="media-album_group_name">Group Name</span>
+					</div>
+				</div>
+
+				<div class="media-album_visibility">
+					<div class="media-album_details__bottom">
 						<span>
 							<?php echo bp_get_album_privacy(); ?>
 						</span>
