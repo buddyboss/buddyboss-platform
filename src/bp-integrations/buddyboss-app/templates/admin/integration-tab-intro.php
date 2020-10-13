2a89c5a185b4dc28afdeff64193caeb2a15c8704
Michael Eisenwasser <michael@buddyboss.com>
2020-10-12 17:18:31 -0700

    App Integration sales content

--- src/bp-integrations/buddyboss-app/templates/admin/integration-tab-intro.php
+++ src/bp-integrations/buddyboss-app/templates/admin/integration-tab-intro.php
@@ -7,7 +7,7 @@
 			printf(
 				__( 'Access your community from a native mobile app using the %s. BuddyBoss App is a paid product built by BuddyBoss, providing native iOS and Android apps for WordPress, published under your own Apple and Google Play accounts. The apps are branded to match your site, and sync community data (members, groups, forums, etc.) back and forth with WordPress. If using LearnDash your members can also take their courses in the app.', 'buddyboss' ),
 				sprintf(
-					'<a href="%s">%s</a>',
+					'<a href="%s" target="_blank">%s</a>',
 					'https://buddyboss.com/app',
 					__( 'BuddyBoss App', 'buddyboss' )
 				)
@@ -17,9 +17,16 @@
 		<br />
 		<?php
 			printf(
-				'<a href="%s" class="button-secondary">%s</a>',
+				'<a href="%s" class="button-primary">%s</a>',
 				esc_url( bp_get_admin_url( '?hello=buddyboss-app' ) ),
-				__( 'Watch the demo', 'buddyboss' )
+				__( 'Video Demo', 'buddyboss' )
+			);
+			?>
+		<?php
+			printf(
+				'<a href="%s" target="_blank" class="button-secondary">%s</a>',
+				'https://buddyboss.com/app',
+				__( 'More Info', 'buddyboss' )
 			);
 			?>
 	</div>
