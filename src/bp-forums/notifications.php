ed41a7598f6b1b111e8ecc4d7a9a8c7bda3226eb
Janak Kaneriya <janak@buddyboss.com>
2020-10-20 22:27:33 +0530

    1299 - Fixed the notification date

--- src/bp-forums/notifications.php
+++ src/bp-forums/notifications.php
@@ -201,7 +201,7 @@ function bbp_buddypress_add_notification( $reply_id = 0, $topic_id = 0, $forum_i
 		'item_id'          => $reply_id,
 		'component_name'   => bbp_get_component_name(),
 		'component_action' => 'bbp_new_reply',
-		'date_notified'    => get_post( $reply_id )->post_date,
+		'date_notified'    => bp_core_current_time(),
 	);
 
 	// Notify the topic author if not the current reply author
