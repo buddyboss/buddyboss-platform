13101ca4476b483df96e973618c9245a372bdfbe
Janak Kaneriya <janak@buddyboss.com>
2020-10-29 17:20:58 +0530

    Added moderation list table

--- src/bp-moderation/bp-moderation-functions.php
+++ src/bp-moderation/bp-moderation-functions.php
@@ -21,13 +21,14 @@ defined( 'ABSPATH' ) || exit;
  * The bp_moderation_get() function shares all arguments with
  * BP_Moderation::get().
  *
+ * @since BuddyBoss 1.5.4
+ *
  * @param array|string $args See BP_Moderation::get() for description.
  *
  * @return array $moderation See BP_Moderation::get() for description.
  * @see   BP_Moderation::get() For more information on accepted arguments
  *        and the format of the returned value.
  *
- * @since BuddyBoss 1.5.4
  */
 function bp_moderation_get( $args = '' ) {
 
@@ -105,10 +106,11 @@ function bp_moderation_get( $args = '' ) {
 	/**
 	 * Filters the requested moderation item(s).
 	 *
-	 * @param BP_Moderation $moderation Requested moderation object.
-	 * @param array $r                  Arguments used for the moderation query.
-	 *
 	 * @since BuddyBoss 1.5.4
+	 *
+	 * @param array         $r          Arguments used for the moderation query.
+	 *
+	 * @param BP_Moderation $moderation Requested moderation object.
 	 */
 	return apply_filters_ref_array(
 		'bp_moderation_get',
@@ -122,11 +124,12 @@ function bp_moderation_get( $args = '' ) {
 /**
  * Retrieve sitewide hidden items ids of particular item type.
  *
+ * @since BuddyBoss 1.5.4
+ *
  * @param string $type Moderation items type.
  *
  * @return array $moderation See BP_Moderation::get() for description.
  *
- * @since BuddyBoss 1.5.4
  */
 function bp_moderation_get_sitewide_hidden_item_ids( $type ) {
 	return bp_moderation_get(
@@ -138,4 +141,85 @@ function bp_moderation_get_sitewide_hidden_item_ids( $type ) {
 			),
 		)
 	);
+}
+
+/**
+ * Output the tabs in the admin area.
+ *
+ * @since BuddyBoss 1.5.4
+ *
+ * @param string $active_tab Name of the tab that is active. Optional.
+ */
+function bp_core_admin_moderation_tabs( $active_tab = '' ) {
+
+	$tabs_html    = '';
+	$idle_class   = 'nav-tab';
+	$active_class = 'nav-tab nav-tab-active';
+
+	/**
+	 * Filters the admin tabs to be displayed.
+	 *
+	 * @since BuddyBoss 1.0.0
+	 *
+	 * @param array $value Array of tabs to output to the admin area.
+	 */
+	$tabs = apply_filters( 'bp_core_admin_moderation_tabs', bp_core_get_moderation_admin_tabs( $active_tab ) );
+
+	// Loop through tabs and build navigation.
+	foreach ( array_values( $tabs ) as $tab_data ) {
+		$is_current = (bool) ( $tab_data['name'] == $active_tab );
+		$tab_class  = $is_current ? $tab_data['class'] . ' ' . $active_class : $tab_data['class'] . ' ' . $idle_class;
+		$tabs_html  .= '<a href="' . esc_url( $tab_data['href'] ) . '" class="' . esc_attr( $tab_class ) . '">' . esc_html( $tab_data['name'] ) . '</a>';
+	}
+
+	echo $tabs_html;
+
+	/**
+	 * Fires after the output of tabs for the admin area.
+	 *
+	 * @since BuddyBoss 1.5.4
+	 */
+	do_action( 'bp_admin_moderation_tabs' );
+}
+
+/**
+ * Register tabs for the BuddyBoss > Moderation screens.
+ *
+ * @since BuddyBoss 1.5.4
+ *
+ * @param string $active_tab
+ *
+ * @return array
+ */
+function bp_core_get_moderation_admin_tabs( $active_tab = '' ) {
+
+	$tabs = array();
+
+	$tabs[] = array(
+		'href'  => bp_get_admin_url( add_query_arg( array( 'page' => 'bp-moderation' ), 'admin.php' ) ),
+		'name'  => __( 'Reported Content', 'buddyboss' ),
+		'class' => 'bp-reported-content',
+	);
+
+
+	$tabs[] = array(
+		'href'  => bp_get_admin_url( add_query_arg( array( 'page' => 'bp-moderation', 'tab' => 'blocked-members' ), 'admin.php' ) ),
+		'name'  => __( 'Blocked Members', 'buddyboss' ),
+		'class' => 'bp-blocked-members',
+	);
+
+	$tabs[] = array(
+		'href'  => bp_get_admin_url( add_query_arg( array( 'page' => 'bp-moderation', 'tab' => 'report-categories' ), 'admin.php' ) ),
+		'name'  => __( 'Report Categories', 'buddyboss' ),
+		'class' => 'bp-report-categories',
+	);
+
+	/**
+	 * Filters the tab data used in our wp-admin screens.
+	 *
+	 * @since BuddyBoss 1.0.0
+	 *
+	 * @param array $tabs Tab data.
+	 */
+	return apply_filters( 'bp_core_get_moderation_admin_tabs', $tabs );
 }
