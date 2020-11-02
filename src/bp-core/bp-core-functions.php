c3841928eefeaed005d4db1b481900f74a2e6db1
Chetan Satasiya <chetansatasiya88@gmail.com>
2020-11-02 19:41:38 +0530

    fix phpcs

--- src/bp-core/bp-core-functions.php
+++ src/bp-core/bp-core-functions.php
@@ -5146,15 +5146,15 @@ function bp_xprofile_get_selected_options_user_progress( $settings ) {
 		foreach ( $profile_photo_type as $option ) {
 			if ( 'profile_photo' === $option && isset( $get_user_data['photo_type'] ) && isset( $get_user_data['photo_type']['profile_photo'] ) ) {
 				$response['photo_type']['profile_photo'] = $get_user_data['photo_type']['profile_photo'];
-				$total_count                             = ++$total_count;
+				$total_count                             = ++ $total_count;
 				if ( isset( $get_user_data['photo_type']['profile_photo']['is_uploaded'] ) && 1 === (int) $get_user_data['photo_type']['profile_photo']['is_uploaded'] ) {
-					$total_completed_count = ++$total_completed_count;
+					$total_completed_count = ++ $total_completed_count;
 				}
 			} elseif ( 'cover_photo' === $option && isset( $get_user_data['photo_type'] ) && isset( $get_user_data['photo_type']['cover_photo'] ) ) {
 				$response['photo_type']['cover_photo'] = $get_user_data['photo_type']['cover_photo'];
-				$total_count                           = ++$total_count;
+				$total_count                           = ++ $total_count;
 				if ( isset( $get_user_data['photo_type']['cover_photo']['is_uploaded'] ) && 1 === (int) $get_user_data['photo_type']['cover_photo']['is_uploaded'] ) {
-					$total_completed_count = ++$total_completed_count;
+					$total_completed_count = ++ $total_completed_count;
 				}
 			}
 		}
@@ -5204,11 +5204,11 @@ function bp_xprofile_get_selected_options_user_progress( $settings ) {
 function bp_core_xprofile_clear_all_user_progress_cache() {
 
 	delete_metadata(
-		'user',        // the meta type
-		0,             // this doesn't actually matter in this call
-		'bp_profile_completion_widgets', // the meta key to be removed everywhere
-		'',            // this also doesn't actually matter in this call
-		true           // tells the function "yes, please remove them all"
+		'user',        // the meta type.
+		0,             // this doesn't actually matter in this call.
+		'bp_profile_completion_widgets', // the meta key to be removed everywhere.
+		'',            // this also doesn't actually matter in this call.
+		true           // tells the function "yes, please remove them all".
 	);
 
 }
