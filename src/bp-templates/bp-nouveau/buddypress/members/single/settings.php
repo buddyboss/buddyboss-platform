9aeab2c3ebf86abf2dbc3a75a5240b4d5c13a100
Janak Kaneriya <janak@buddyboss.com>
2020-11-15 00:59:19 +0530

    Added support to non theme users for blocked members and reported content

--- src/bp-templates/bp-nouveau/buddypress/members/single/settings.php
+++ src/bp-templates/bp-nouveau/buddypress/members/single/settings.php
@@ -36,6 +36,10 @@ switch ( bp_current_action() ) :
 	case 'export':
 		bp_get_template_part( 'members/single/settings/export-data' );
 		break;
+	case 'reported-content':
+	case 'blocked-members':
+		bp_get_template_part( 'members/single/settings/moderation' );
+		break;
 	default:
 		bp_get_template_part( 'members/single/plugins' );
 		break;
