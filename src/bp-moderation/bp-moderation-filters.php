a6350d2fa858b87ab7186905d6662073016da2e9
Janak Kaneriya <janak@buddyboss.com>
2020-11-15 14:47:39 +0530

    Added member blocking popup and functionality

--- src/bp-moderation/bp-moderation-filters.php
+++ src/bp-moderation/bp-moderation-filters.php
@@ -32,6 +32,7 @@ function bp_moderation_js_strings( $params ) {
 
 	return $params;
 }
+
 add_filter( 'bp_core_get_js_strings', 'bp_moderation_js_strings' );
 
 /**
@@ -100,6 +101,78 @@ function bp_moderation_content_report() {
 
 add_action( 'wp_ajax_bp_moderation_content_report', 'bp_moderation_content_report' );
 add_action( 'wp_ajax_nopriv_bp_moderation_content_report', 'bp_moderation_content_report' );
+
+
+/**
+ * Function to handle frontend block member form submission.
+ *
+ * @since BuddyBoss 2.0.0
+ */
+function bp_moderation_block_member() {
+	$response = array(
+		'success' => false,
+		'message' => '',
+	);
+
+	$nonce     = filter_input( INPUT_POST, '_wpnonce', FILTER_SANITIZE_STRING );
+	$item_id   = filter_input( INPUT_POST, 'content_id', FILTER_SANITIZE_NUMBER_INT );
+	$item_type = filter_input( INPUT_POST, 'content_type', FILTER_SANITIZE_STRING );
+
+	if ( empty( $item_id ) || empty( $item_type ) ) {
+		$response['message'] = new WP_Error( 'bp_moderation_missing_data', esc_html__( 'Required field missing.', 'buddyboss' ) );
+	}
+
+	if ( bp_moderation_report_exist( $item_id, $item_type ) ) {
+		$response['message'] = new WP_Error( 'bp_moderation_already_reported', esc_html__( 'Already reported this item.', 'buddyboss' ) );
+	}
+
+	if ( wp_verify_nonce( $nonce, 'bp-moderation-content' ) && ! is_wp_error( $response['message'] ) ) {
+		$moderation = bp_moderation_add(
+			array(
+				'content_id'   => $item_id,
+				'content_type' => $item_type,
+				'category_id'  => 0,
+				'note'         => '',
+			)
+		);
+
+		if ( ! empty( $moderation->id ) && ! empty( $moderation->report_id ) ) {
+			$response['success']    = true;
+			$response['moderation'] = $moderation;
+
+			if ( bp_is_friend( $item_id ) ) {
+				friends_remove_friend( bp_loggedin_user_id(), $item_id );
+			}
+
+			if ( bp_is_following( array( 'leader_id' => $item_id, 'follower_id' => bp_loggedin_user_id() ) ) ) {
+				bp_stop_following( array( 'leader_id' => $item_id, 'follower_id' => bp_loggedin_user_id() ) );
+			}
+
+			$response['button'] = bp_moderation_get_report_button(
+				array(
+					'button_attr' => array(
+						'data-bp-content-id'   => $item_id,
+						'data-bp-content-type' => $item_type,
+					),
+				),
+				false
+			);
+		}
+
+		$response['message'] = $moderation->errors;
+	}
+
+	if ( empty( $response['success'] ) && empty( $response['message'] ) ) {
+		$response['message'] = new WP_Error( 'bp_moderation_missing_error', esc_html__( 'Sorry, Something happened wrong', 'buddyboss' ) );
+	}
+
+	echo wp_json_encode( $response );
+	exit();
+
+}
+
+add_action( 'wp_ajax_bp_moderation_block_member', 'bp_moderation_block_member' );
+add_action( 'wp_ajax_nopriv_bp_moderation_block_member', 'bp_moderation_block_member' );
 
 /**
  * Function to handle frontend unblock user request.
@@ -188,6 +261,56 @@ add_action( 'wp_ajax_nopriv_bp_moderation_content_actions_request', 'bp_moderati
  */
 function bb_moderation_content_report_popup() {
 	include BP_PLUGIN_DIR . 'src/bp-moderation/screens/content-report-form.php';
+	include BP_PLUGIN_DIR . 'src/bp-moderation/screens/block-member-form.php';
 }
 
 add_action( 'wp_footer', 'bb_moderation_content_report_popup' );
+
+/**
+ * Function to add the content in member block form.
+ *
+ * @since BuddyBoss 2.0.0
+ */
+function bp_moderation_block_member_form_content() {
+	?>
+    <span>
+        <?php
+        esc_html_e( 'This member will no longer be able to:', 'buddyboss' );
+        ?>
+    </span>
+    <ul>
+        <li>
+			<?php
+			esc_html_e( 'See your post', 'buddyboss' );
+			?>
+        </li>
+        <li>
+			<?php
+			esc_html_e( 'Tag you', 'buddyboss' );
+			?>
+        </li>
+        <li>
+			<?php
+			esc_html_e( 'Invite you in event and groups', 'buddyboss' );
+			?>
+        </li>
+        <li>
+			<?php
+			esc_html_e( 'Message you', 'buddyboss' );
+			?>
+        </li>
+        <li>
+			<?php
+			esc_html_e( 'Add you as a friend', 'buddyboss' );
+			?>
+        </li>
+    </ul>
+    <span>
+        <?php
+        esc_html_e( 'If you\'re friends, blocking member will also unfriend them', 'buddyboss' );
+        ?>
+    </span>
+	<?php
+}
+
+add_action( 'bp_moderation_block_member_before_form', 'bp_moderation_block_member_form_content' );
--- src/bp-templates/bp-nouveau/includes/members/template-tags.php
+++ src/bp-templates/bp-nouveau/includes/members/template-tags.php
@@ -595,7 +595,7 @@ function bp_nouveau_members_loop_buttons( $args = array() ) {
 			unset( bp_nouveau()->members->button_args );
 		}
 
-		if ( bp_is_active( 'moderation' ) ) {
+		if ( bp_is_active( 'moderation' ) && bp_is_moderation_member_blocking_enable() ) {
 			$buttons['member_report'] = bp_member_get_report_link(
 				array(
 					'parent_element' => $parent_element,
