3eea3802630fef79bfa18257a3e319fa8c746edd
Janak Kaneriya <janak@buddyboss.com>
2020-11-23 16:18:10 +0530

    Excluded product post type from having comment on it's activity as WC product has it's own commenting settings

--- src/bp-blogs/bp-blogs-activity.php
+++ src/bp-blogs/bp-blogs-activity.php
@@ -130,7 +130,16 @@ function bp_blogs_register_post_tracking_args( $params = null, $post_type = 0 ) 
 		$post_types = get_post_types( array( 'public' => true ) );
 
 		// Exclude BP CPT
-		$bp_exclude_cpt = array( 'forum', 'topic', 'reply', 'page', 'attachment', 'bp-group-type', 'bp-member-type' );
+		$bp_exclude_cpt = array(
+			'forum',
+			'topic',
+			'reply',
+			'page',
+			'attachment',
+			'bp-group-type',
+			'bp-member-type',
+			'product',
+		);
 
 		$bp_allowed_cpt = array();
 		foreach ( $post_types as $p_type ) {
