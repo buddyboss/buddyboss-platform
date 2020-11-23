722af666343c29a90ec8745b0ee76d5bff676537
Chetan Satasiya <chetansatasiya88@gmail.com>
2020-11-23 16:06:17 +0530

    #1818 - Fix document preview taking time

--- src/bp-templates/bp-nouveau/includes/document/functions.php
+++ src/bp-templates/bp-nouveau/includes/document/functions.php
@@ -1068,17 +1068,17 @@ function bp_document_get_preview_text_from_attachment( $attachment_id ) {
 function bp_document_get_preview_image_url( $document_id, $extension, $preview_attachment_id ) {
 	$attachment_url = '';
 
-	if ( in_array( $extension, bp_get_document_preview_doc_extensions(), true ) ) {
-		$get_preview            = $preview_attachment_id;
-		$preview_attachment_id  = bp_document_get_meta( $document_id, 'preview_attachment_id', true );
-		if ( ! $preview_attachment_id ) {
+	if( in_array( $extension, bp_get_document_preview_doc_extensions(), true ) ) {
+		$get_preview           = $preview_attachment_id;
+		$preview_attachment_id = bp_document_get_meta( $document_id, 'preview_attachment_id', true );
+		if( ! $preview_attachment_id ) {
 			$preview_attachment_id = $get_preview;
 		}
-		$document_id        = 'forbidden_' . $document_id;
-		$attachment_id      = 'forbidden_' . $preview_attachment_id;
-		$output_file_src     = bp_document_scaled_image_path( $preview_attachment_id );
-		if ( ! empty( $preview_attachment_id ) && wp_attachment_is_image( $preview_attachment_id ) && file_exists( $output_file_src ) ) {
-			$attachment_url     = trailingslashit( buddypress()->plugin_url ) . 'bp-templates/bp-nouveau/includes/document/preview.php?id=' . base64_encode( $attachment_id ) . '&id1=' . base64_encode( $document_id );
+		$document_id     = 'forbidden_' . $document_id;
+		$attachment_id   = 'forbidden_' . $preview_attachment_id;
+		$output_file_src = bp_document_scaled_image_path( $preview_attachment_id );
+		if( ! empty( $preview_attachment_id ) && wp_attachment_is_image( $preview_attachment_id ) && file_exists( $output_file_src ) ) {
+			$attachment_url = home_url( '/' ) . 'document-preview?id=' . base64_encode( $attachment_id ) . '&id1=' . base64_encode( $document_id );
 		}
 	}
 
