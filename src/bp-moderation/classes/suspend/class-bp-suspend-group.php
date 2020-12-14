1a3f3f42f989f4c625f1046cae413d2f80c6c83d
Kartik Suthar <kartik.7013@gmail.com>
2020-12-14 15:10:01 +0530

    handle delete moderation data while deleting group

--- src/bp-moderation/classes/suspend/class-bp-suspend-group.php
+++ src/bp-moderation/classes/suspend/class-bp-suspend-group.php
@@ -38,6 +38,9 @@ class BP_Suspend_Group extends BP_Suspend_Abstract {
 
 		add_action( 'groups_group_after_save', array( $this, 'update_group_after_save' ), 10, 1 );
 
+		// Delete moderation data when group is deleted.
+		add_action( 'bp_groups_delete_group', array( $this, 'update_group_after_delete' ), 10, 1 );
+
 		/**
 		 * Suspend code should not add for WordPress backend or IF component is not active or Bypass argument passed for admin
 		 */
@@ -312,5 +315,19 @@ class BP_Suspend_Group extends BP_Suspend_Abstract {
 		}
 
 		self::handle_new_suspend_entry( $suspended_record, $group->id, $group->creator_id );
+	}
+
+	/**
+	 * Update the suspend table to delete the group.
+	 *
+	 * @param BP_Groups_Group $group Current instance of the group item being deleted. Passed by reference.
+	 */
+	public function update_group_after_delete( $group ) {
+
+		if ( empty( $group ) ) {
+			return;
+		}
+
+		BP_Core_Suspend::delete_suspend( $group->id, $this->item_type );
 	}
 }
