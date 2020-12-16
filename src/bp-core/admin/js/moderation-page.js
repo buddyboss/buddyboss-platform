1e91e2c10847ebd6d099b91b6691993a658dcdc8
Janak Kaneriya <janak@buddyboss.com>
2020-12-16 21:31:24 +0530

    Revamped the user block ajax action

--- src/bp-core/admin/js/moderation-page.js
+++ src/bp-core/admin/js/moderation-page.js
@@ -1,6 +1,6 @@
 /* global Bp_Moderation */
 jQuery(document).ready(function ($) {
-	$(document).on('click', '.bp-hide-request, .bp-block-user', function ( event ) {
+	$(document).on('click', '.bp-hide-request', function ( event ) {
 		event.preventDefault();
 		if (!confirm(Bp_Moderation.strings.confirm_msg)) {
 			return false;
@@ -75,4 +75,81 @@ jQuery(document).ready(function ($) {
 			curObj.removeClass('disabled');
 		});
 	});
+
+	$( document ).on( 'click', '.bp-block-user', function ( event ) {
+		event.preventDefault();
+
+		if ( !confirm( Bp_Moderation.strings.confirm_msg ) ) {
+			return false;
+		}
+
+		$( '.bp-moderation-ajax-msg p' ).text( '' ).parent().addClass( 'hidden' );
+
+		var curObj = $( this );
+		curObj.addClass( 'disabled' );
+		var id = curObj.attr( 'data-id' );
+		var type = curObj.attr( 'data-type' );
+		var nonce = curObj.attr( 'data-nonce' );
+		var sub_action = curObj.attr( 'data-action' );
+		var data = {
+			action: 'bp_moderation_user_actions_request',
+			id: id,
+			type: type,
+			sub_action: sub_action,
+			nonce: nonce,
+		};
+
+		$.post( ajaxurl, data, function ( response ) {
+			var result = $.parseJSON( response );
+			var hideArg = '';
+			if ( true === result.success ) {
+				var url = window.location.href;
+				if ( 'hide' === sub_action ) {
+					curObj.attr( 'data-action', 'unhide' );
+					curObj.attr( 'title', Bp_Moderation.strings.unhide_label );
+					if ( curObj.hasClass( 'single-report-btn' ) && 'user' !== type ) {
+						curObj.text( Bp_Moderation.strings.unhide_label );
+					} else if ( 'user' === type && curObj.hasClass( 'content-author' ) ) {
+						curObj.text( Bp_Moderation.strings.unsuspend_author_label );
+					} else if ( curObj.hasClass( 'single-report-btn' ) ) {
+						curObj.text( Bp_Moderation.strings.unsuspend_member_label );
+					} else {
+						if ( 'user' === type ) {
+							curObj.text( Bp_Moderation.strings.unsuspend_label );
+						} else {
+							curObj.text( Bp_Moderation.strings.unhide_label );
+						}
+					}
+					hideArg = ( 'user' === type ) ? 'suspended' : 'hidden';
+				} else if ( 'unhide' === sub_action ) {
+					curObj.attr( 'data-action', 'hide' );
+					curObj.attr( 'title', Bp_Moderation.strings.hide_label );
+					if ( curObj.hasClass( 'single-report-btn' ) && 'user' !== type ) {
+						curObj.text( Bp_Moderation.strings.hide_label );
+					} else if ( 'user' === type && curObj.hasClass( 'content-author' ) ) {
+						curObj.text( Bp_Moderation.strings.suspend_author_label );
+					} else if ( curObj.hasClass( 'single-report-btn' ) ) {
+						curObj.text( Bp_Moderation.strings.suspend_member_label );
+					} else {
+						if ( 'user' === type ) {
+							curObj.text( Bp_Moderation.strings.suspend_label );
+						} else {
+							curObj.text( Bp_Moderation.strings.hide_label );
+						}
+					}
+					hideArg = ( 'user' === type ) ? 'unsuspended' : 'unhide';
+				}
+
+				if ( url.indexOf( '?' ) > -1 ) {
+					url += '&' + hideArg + '=1';
+				} else {
+					url += '?' + hideArg + '=1';
+				}
+				window.location.href = url;
+			} else {
+				$( '.bp-moderation-ajax-msg p' ).text( result.message.errors.bp_moderation_user_missing_data ).parent().removeClass( 'hidden' );
+			}
+			curObj.removeClass( 'disabled' );
+		} );
+	} );
 });
