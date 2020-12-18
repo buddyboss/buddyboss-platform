3bfb3b560dd8f3c5daa6cb29a2b7d3fcd372d152
shohelbuddyboss <mhshohel89@gmail.com>
2020-12-18 14:54:24 +0600

    Activating action btn video

--- src/bp-templates/bp-nouveau/buddypress/video/entry.php
+++ src/bp-templates/bp-nouveau/buddypress/video/entry.php
@@ -5,6 +5,26 @@
  * @since BuddyBoss 1.0.0
  */
 
+
+global $video_template;
+
+$attachment_id = bp_get_video_attachment_id();
+$download_url  = bp_video_download_link( $attachment_id, bp_get_video_id() );
+$group_id      = bp_get_media_group_id();
+$move_id       = '';
+$move_type     = '';
+$video_privacy = bp_video_user_can_manage_video( bp_get_video_id(), bp_loggedin_user_id() );
+$can_manage    = ( true === (bool) $video_privacy['can_manage'] ) ? true : false;
+
+if ( $group_id > 0 ) {
+	$move_id   = $group_id;
+	$move_type = 'group';
+} else {
+	$move_id   = bp_get_video_user_id();
+	$move_type = 'profile';
+}
+
+
 ?>
 <li class="lg-grid-1-5 md-grid-1-3 sm-grid-1-3" data-id="<?php bp_video_id(); ?>" data-date-created="<?php bp_video_date_created(); ?>">
 
@@ -16,14 +36,15 @@
             <div class="video-action_list item-action_list">
                 <ul>
                     <li class="edit_video">
-                        <a href="#" data-action="video" data-media-id="567" data-parent-activity-id="" data-item-activity-id="194811" data-type="profile" id="2" class="ac-video-edit">Edit</a>
-                    </li>    
+                        <a href="#" data-action="video" data-video-id="<?php bp_video_id(); ?>" data-parent-activity-id="<?php bp_video_parent_activity_id(); ?>" data-item-activity-id="<?php bp_video_activity_id(); ?>" data-type="<?php echo esc_attr( $move_type ); ?>" id="<?php echo esc_attr( $move_id ); ?>" class="ac-video-edit"><?php esc_html_e( 'Edit', 'buddyboss' ); ?></a>
+                    </li>
                     <li class="move_video">
-                        <a href="#" data-action="video" data-media-id="567" data-parent-activity-id="" data-item-activity-id="194811" data-type="profile" id="2" class="ac-video-move">Move</a>
+                        <a href="#" data-action="video" data-video-id="<?php bp_video_id(); ?>" data-parent-activity-id="<?php bp_video_parent_activity_id(); ?>" data-item-activity-id="<?php bp_video_activity_id(); ?>" data-type="<?php echo esc_attr( $move_type ); ?>" id="<?php echo esc_attr( $move_id ); ?>" class="ac-video-move"><?php esc_html_e( 'Move', 'buddyboss' ); ?></a>
                     </li>
                     <li class="delete_file">
-                        <a class="video-file-delete" data-media-id="567" data-parent-activity-id="" data-item-activity-id="194811" data-item-from="media" data-item-id="567" data-type="video" href="#">Delete</a>
+                        <a class="video-file-delete" data-video-id="<?php bp_video_id(); ?>" data-parent-activity-id="<?php bp_video_parent_activity_id(); ?>" data-item-activity-id="<?php bp_video_activity_id(); ?>" data-item-from="media" data-item-id="<?php bp_video_id(); ?>" data-type="video" href="#"><?php esc_html_e( 'Delete', 'buddyboss' ); ?></a>
                     </li>
+
                 </ul>
             </div>
         </div>
@@ -42,7 +63,7 @@
         <?php
 		$video_privacy  = bp_video_user_can_manage_video( bp_get_video_id(), bp_loggedin_user_id() );
 		$can_manage = ( true === (bool) $video_privacy['can_manage'] ) ? true : false;
-		if ( ( bp_is_my_profile() || bp_current_user_can( 'bp_moderate' ) ) || ( bp_is_group() && ( ( bp_is_group_video() && $can_manage ) || ( bp_is_group_albums() && $can_manage ) ) ) ) : ?>
+		if ( ( ( bp_is_my_profile() || bp_current_user_can( 'bp_moderate' ) ) || ( bp_is_group() && ( ( bp_is_group_video() && $can_manage ) || ( bp_is_group_albums() && $can_manage ) ) ) ) && !bp_is_video_directory() ) : ?>
             <div class="bb-video-check-wrap bb-action-check-wrap">
                 <input id="bb-video-<?php bp_video_id(); ?>" class="bb-custom-check" type="checkbox" value="<?php bp_video_id(); ?>" name="bb-video-select" />
                 <label class="bp-tooltip" data-bp-tooltip-pos="up" data-bp-tooltip="<?php _e( 'Select', 'buddyboss' ); ?>" for="bb-video-<?php bp_video_id(); ?>"><span class="bb-icon bb-icon-check"></span></label>
--- src/bp-templates/bp-nouveau/js/buddypress-media.js
+++ src/bp-templates/bp-nouveau/js/buddypress-media.js
@@ -244,7 +244,7 @@ window.bp = window.bp || {};
 			// Documents.
 			$(document).on('click', '.directory.document  .media-folder_action__anchor, .directory.document  .media-folder_action__anchor li a, .bb-media-container .media-folder_action__anchor, .bb-media-container  .media-folder_action__list li a', this.fileActionButton.bind(this));
 			$(document).on('click', '.bb-activity-media-elem .copy_download_file_url a, .media-folder_action__list .copy_download_file_url a, .media .bb-photo-thumb .copy_download_file_url a', this.copyDownloadLink.bind(this));
-			$(document).on('click', '.bb-activity-media-elem.media-activity .media-action-wrap .media-action_more, #media-stream.media .bb-photo-thumb .media-action-wrap .media-action_more, .bb-activity-media-elem.document-activity .document-action-wrap .document-action_more, .bb-activity-media-elem.document-activity .document-action-wrap .document-action_list li a', this.fileActivityActionButton.bind(this));
+			$(document).on('click', '.bb-activity-media-elem.media-activity .media-action-wrap .media-action_more, #media-stream.media .bb-photo-thumb .media-action-wrap .media-action_more, .bb-activity-media-elem.document-activity .document-action-wrap .document-action_more, .bb-activity-media-elem.document-activity .document-action-wrap .document-action_list li a, #video-stream.video .bb-video-thumb .video-action-wrap .video-action_more', this.fileActivityActionButton.bind(this));
 			$(document).click(this.toggleFileActivityActionButton);
 			$(document).on('click', '.bb-activity-media-elem.document-activity .document-expand .document-expand-anchor', this.expandCodePreview.bind(this));
 			$(document).on('click', '.bb-activity-media-elem.document-activity .document-action-wrap .document-action_collapse', this.collapseCodePreview.bind(this));
@@ -5466,6 +5466,10 @@ window.bp = window.bp || {};
 				$(event.currentTarget).closest('.bb-photo-thumb').toggleClass('is-visible').parent().siblings().find('.bb-photo-thumb').removeClass('is-visible').removeClass('is-visible');
 			}
 
+			if ( $(event.currentTarget).hasClass( 'video-action_more' ) ) {
+				$(event.currentTarget).closest( '.bb-video-thumb' ).toggleClass('is-visible').parent().siblings().find('.bb-video-thumb').removeClass('is-visible');
+			}
+
 			if (event.currentTarget.tagName.toLowerCase() == 'a' && ( !$(event.currentTarget).hasClass('document-action_more') && !$(event.currentTarget).hasClass('media-action_more') )) {
 				$(event.currentTarget).closest('.bb-activity-media-elem').removeClass('is-visible');
 				$(event.currentTarget).closest('.bb-photo-thumb').removeClass('is-visible');
@@ -5494,7 +5498,7 @@ window.bp = window.bp || {};
 				return event;
 			}
 
-			if ($(element).hasClass('document-action_more') || $(element).parent().hasClass('document-action_more') || $(element).hasClass('media-folder_action__anchor') || $(element).parent().hasClass('media-folder_action__anchor') || $(element).hasClass('media-action_more') || $(element).parent().hasClass('media-action_more')) {
+			if ($(element).hasClass('document-action_more') || $(element).parent().hasClass('document-action_more') || $(element).hasClass('media-folder_action__anchor') || $(element).parent().hasClass('media-folder_action__anchor') || $(element).hasClass('media-action_more') || $(element).parent().hasClass('media-action_more') ) {
 				return event;
 			}
 
