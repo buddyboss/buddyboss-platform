49a29648800a5d788b947eb82b511c54d074b0ad
Kartik Suthar <kartik.7013@gmail.com>
2020-12-21 20:58:58 +0530

    variable update

--- src/bp-moderation/classes/suspend/class-bp-suspend-message.php
+++ src/bp-moderation/classes/suspend/class-bp-suspend-message.php
@@ -157,17 +157,17 @@ class BP_Suspend_Message extends BP_Suspend_Abstract {
 	 *
 	 * @since BuddyBoss 2.0.0
 	 *
-	 * @param int      $folder_id     message thread id.
+	 * @param int      $message_id    message thread id.
 	 * @param int|null $hide_sitewide Item hidden sitewide or user specific.
 	 * @param array    $args          Parent args.
 	 */
-	public function manage_hidden_message( $folder_id, $hide_sitewide, $args = array() ) {
+	public function manage_hidden_message( $message_id, $hide_sitewide, $args = array() ) {
 		global $bp_background_updater;
 
 		$suspend_args = wp_parse_args(
 			$args,
 			array(
-				'item_id'   => $folder_id,
+				'item_id'   => $message_id,
 				'item_type' => self::$type,
 			)
 		);
@@ -179,12 +179,12 @@ class BP_Suspend_Message extends BP_Suspend_Abstract {
 		BP_Core_Suspend::add_suspend( $suspend_args );
 
 		if ( $this->backgroup_diabled || ! empty( $args ) ) {
-			$this->hide_related_content( $folder_id, $hide_sitewide, $args );
+			$this->hide_related_content( $message_id, $hide_sitewide, $args );
 		} else {
 			$bp_background_updater->push_to_queue(
 				array(
 					'callback' => array( $this, 'hide_related_content' ),
-					'args'     => array( $folder_id, $hide_sitewide, $args ),
+					'args'     => array( $message_id, $hide_sitewide, $args ),
 				)
 			);
 			$bp_background_updater->save()->schedule_event();
