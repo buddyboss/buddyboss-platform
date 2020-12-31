5aa7f92011845f00aa216ab7c6f325e074a15c6c
Kartik Suthar <kartik.7013@gmail.com>
2020-12-31 17:28:53 +0530

    fixed issue with privacy update of media and document when activity disabled

--- src/bp-templates/bp-nouveau/includes/activity/template-tags.php
+++ src/bp-templates/bp-nouveau/includes/activity/template-tags.php
@@ -1188,7 +1188,7 @@ function bp_nouveau_activity_privacy() {
 
 		$privacy_items = bp_activity_get_visibility_levels();
 
-		if ( $media_activity && ( ( $parent_activity_id && $parent_activity_permalink ) || ( $album_id && ! empty( $album_url ) ) ) ) {
+		if ( $media_activity && ( ( $parent_activity_id && $parent_activity_permalink && bp_user_can_create_activity() ) || ( $album_id && ! empty( $album_url ) ) ) ) {
 			?>
             <div class="bb-media-privacy-wrap">
             <span class="bp-tooltip privacy-wrap" data-bp-tooltip-pos="up"
@@ -1208,7 +1208,7 @@ function bp_nouveau_activity_privacy() {
 				<?php endif; ?>
             </ul>
             </div><?php
-		} elseif ( $document_activity && ( ( $parent_activity_id && $parent_activity_permalink ) || ( $folder_id && ! empty( $folder_url ) ) ) ) {
+		} elseif ( $document_activity && ( ( $parent_activity_id && $parent_activity_permalink && bp_user_can_create_activity() ) || ( $folder_id && ! empty( $folder_url ) ) ) ) {
 			?>
             <div class="bb-media-privacy-wrap">
             <span class="bp-tooltip privacy-wrap" data-bp-tooltip-pos="up"
