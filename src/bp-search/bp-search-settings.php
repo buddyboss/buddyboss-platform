503d390e5a71869240dd9a9cc192c7a378c35cf5
Chetan Satasiya <chetansatasiya88@gmail.com>
2021-01-12 13:37:05 +0530

    #823 - Removed video album from search setting as it always follow the media rule

--- src/bp-search/bp-search-settings.php
+++ src/bp-search/bp-search-settings.php
@@ -225,14 +225,7 @@ function bp_search_get_settings_fields() {
 				'class' => 'bp-search-parent-field',
 			),
 		);
-		$fields['bp_search_settings_community']['bp_search_video_albums'] = array(
-			'title'             => '&#65279;',
-			'callback'          => 'bp_search_settings_callback_video_albums',
-			'sanitize_callback' => 'intval',
-			'args'              => array(
-				'class' => 'bp-search-child-field',
-			),
-		);
+
 	}
 
 	if ( bp_is_active( 'media' ) && ( bp_is_group_document_support_enabled() || bp_is_profile_document_support_enabled() ) ) {
@@ -821,28 +814,6 @@ function bp_search_settings_callback_videos() {
 /**
  * Allow Post Type search setting field
  *
- * @since BuddyBoss 1.8.0
- *
- * @uses checked() To display the checked attribute
- */
-function bp_search_settings_callback_video_albums() {
-	?>
-	<input
-			name="bp_search_video_albums"
-			id="bp_search_video_albums"
-			type="checkbox"
-			value="1"
-			<?php checked( bp_is_search_video_albums_enable( false ) ); ?>
-	/>
-	<label for="bp_search_video_albums">
-		<?php esc_html_e( 'Albums', 'buddyboss' ); ?>
-	</label>
-	<?php
-}
-
-/**
- * Allow Post Type search setting field
- *
  * @since BuddyBoss 1.4.0
  *
  * @uses checked() To display the checked attribute
@@ -934,19 +905,6 @@ function bp_is_search_albums_enable( $default = 0 ) {
  */
 function bp_is_search_videos_enable( $default = 0 ) {
 	return (bool) apply_filters( 'bp_is_search_videos_enable', (bool) get_option( 'bp_search_videos', $default ) );
-}
-
-/**
- * Checks if video albums search is enabled.
- *
- * @since BuddyBoss 1.8.0
- *
- * @param $default integer
- *
- * @return bool Is video albums search enabled or not
- */
-function bp_is_search_video_albums_enable( $default = 0 ) {
-	return (bool) apply_filters( 'bp_is_search_video_albums_enable', (bool) get_option( 'bp_search_video_albums', $default ) );
 }
 
 /**
