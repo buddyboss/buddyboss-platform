58c88bdd325c4c356a170bde4c55f2dee3970404
Krunal Palaniya <krunal@buddyboss.com>
2021-01-22 18:34:57 +0530

    #823 - Fixed the issue When User enter in popup mode from actvity video fast forward and backward buttons are not added instantly

--- src/bp-templates/bp-nouveau/buddypress/video/single-video.php
+++ src/bp-templates/bp-nouveau/buddypress/video/single-video.php
@@ -8,7 +8,7 @@
  */
 
 ?>
-<video id="video-<?php bp_video_id(); ?>" class="video-js" controls poster="<?php bp_video_attachment_image(); ?>" data-setup='{"fluid": true,"playbackRates": [0.5, 1, 1.5, 2] }'>
+<video id="theatre-video-<?php bp_video_id(); ?>" class="video-js" controls poster="<?php bp_video_attachment_image(); ?>" data-setup='{"fluid": true,"playbackRates": [0.5, 1, 1.5, 2] }'>
 	<source src="<?php bp_video_link(); ?>">
 	</source>
 </video>
