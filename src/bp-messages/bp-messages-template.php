da0f697b874adfacb2199391e495c31f7adfc013
jitendrabanjara1991 <jitendrabanjara@gmail.com>
2021-02-05 02:17:54 +0530

    Added recepeint count in thread object and display limited recepeint list

--- src/bp-messages/bp-messages-template.php
+++ src/bp-messages/bp-messages-template.php
@@ -1752,7 +1752,7 @@ function bp_get_thread_recipients_count() {
 	 *
 	 * @param int $count Total recipients number.
 	 */
-	// message_imp
+	//message_imp
 	return (int) apply_filters( 'bp_get_thread_recipients_count', $thread_template->thread->total_recipients_count );
 }
 
