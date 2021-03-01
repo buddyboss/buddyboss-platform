87f1f861812a41f733e4b960fb4f6c35f7d75172
Nitish Kaila <kailanitish90@gmail.com>
2021-03-02 00:23:16 +0530

    Fixed - Loading Groups message display instead 404 page when entered incorrect group type name

--- src/bp-groups/classes/class-bp-groups-component.php
+++ src/bp-groups/classes/class-bp-groups-component.php
@@ -173,7 +173,7 @@ class BP_Groups_Component extends BP_Component {
 			}
 
 			// Screens - Directory.
-			if ( bp_is_groups_directory() ) {
+			if ( bp_is_groups_directory() && ! empty( bp_get_current_group_directory_type() ) ) {
 				require $this->path . 'bp-groups/screens/directory.php';
 			}
 
