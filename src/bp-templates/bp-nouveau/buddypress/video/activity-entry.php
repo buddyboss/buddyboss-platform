a93b71ca9603b74d88bf088784ae406d8d4fee64
Krunal Palaniya <krunal@buddyboss.com>
2021-03-19 20:49:47 +0530

    #823 - Some change in thumbs popup as requested

--- src/bp-templates/bp-nouveau/buddypress/video/activity-entry.php
+++ src/bp-templates/bp-nouveau/buddypress/video/activity-entry.php
@@ -27,6 +27,42 @@ $can_manage     = true === (bool) $media_privacy['can_manage'];
 $can_move       = true === (bool) $media_privacy['can_move'];
 $db_privacy     = bp_get_video_privacy();
 $is_comment_vid = bp_video_is_activity_comment_video( bp_get_video_id() );
+$attachment_urls = [];
+$auto_generated_thumbnails = get_post_meta( bp_get_video_attachment_id(), 'video_preview_thumbnails', true );
+$preview_thumbnail_id      = get_post_meta( bp_get_video_attachment_id(), 'bp_video_preview_thumbnail_id', true );
+if ( $auto_generated_thumbnails ) {
+	$auto_generated_thumbnails_arr = explode( ',', $auto_generated_thumbnails );
+	if ( $auto_generated_thumbnails_arr ) {
+		foreach ( $auto_generated_thumbnails_arr as $auto_generated_thumbnail ) {
+			$attachment_urls['default_images'][] = array(
+				'id'	=> $auto_generated_thumbnail,
+				'url'	=> wp_get_attachment_image_url( $auto_generated_thumbnail, 'full' )
+			);
+		}
+	}
+	if ( $preview_thumbnail_id ) {
+		$auto_generated_thumbnails_arr = explode( ',', $auto_generated_thumbnails );
+		if ( ! in_array( $preview_thumbnail_id, $auto_generated_thumbnails_arr, true ) ) {
+			$video                      = new BP_Video( bp_get_video_id() );
+			$attachment_urls['preview'] = array(
+				'id'            => bp_get_video_id(),
+				'attachment_id' => $video->attachment_id,
+				'thumb'         => wp_get_attachment_image_url( $preview_thumbnail_id, 'bp-media-thumbnail' ),
+				'url'           => wp_get_attachment_image_url( $preview_thumbnail_id, 'full' ),
+				'name'          => $video->title,
+				'saved'         => true,
+				'dropzone'      => true
+			);
+		} else {
+			if ( $preview_thumbnail_id ) {
+				$attachment_urls['preview'] = [
+					'id'	=> $preview_thumbnail_id,
+					'url'	=> wp_get_attachment_image_url( $preview_thumbnail_id, 'full' )
+				];
+			}
+		}
+	}
+}
 ?>
 
 <div class="bb-activity-video-elem
@@ -46,42 +82,6 @@ echo ( $more_video && 2 === $video_template->current_video ) ? esc_attr( ' no_mo
 			<?php
 			$item_id = 0;
 			if ( bp_loggedin_user_id() === bp_get_video_user_id() || bp_current_user_can( 'bp_moderate' ) ) {
-				$attachment_urls = [];
-				$auto_generated_thumbnails = get_post_meta( bp_get_video_attachment_id(), 'video_preview_thumbnails', true );
-				$preview_thumbnail_id      = get_post_meta( bp_get_video_attachment_id(), 'bp_video_preview_thumbnail_id', true );
-				if ( $auto_generated_thumbnails ) {
-					$auto_generated_thumbnails_arr = explode( ',', $auto_generated_thumbnails );
-					if ( $auto_generated_thumbnails_arr ) {
-						foreach ( $auto_generated_thumbnails_arr as $auto_generated_thumbnail ) {
-							$attachment_urls['default_images'][] = array(
-								'id'	=> $auto_generated_thumbnail,
-								'url'	=> wp_get_attachment_image_url( $auto_generated_thumbnail, 'full' )
-							);
-						}
-					}
-					if ( $preview_thumbnail_id ) {
-						$auto_generated_thumbnails_arr = explode( ',', $auto_generated_thumbnails );
-						if ( ! in_array( $preview_thumbnail_id, $auto_generated_thumbnails_arr, true ) ) {
-							$video                      = new BP_Video( bp_get_video_id() );
-							$attachment_urls['preview'] = array(
-								'id'            => bp_get_video_id(),
-								'attachment_id' => $video->attachment_id,
-								'thumb'         => wp_get_attachment_image_url( $preview_thumbnail_id, 'bp-media-thumbnail' ),
-								'url'           => wp_get_attachment_image_url( $preview_thumbnail_id, 'full' ),
-								'name'          => $video->title,
-								'saved'         => true,
-								'dropzone'      => true
-							);
-						} else {
-							if ( $preview_thumbnail_id ) {
-								$attachment_urls['preview'] = [
-									'id'	=> $preview_thumbnail_id,
-									'url'	=> wp_get_attachment_image_url( $preview_thumbnail_id, 'full' )
-								];
-							}
-						}
-					}
-				}
 			?>
 				<a href="#" class="video-action_more item-action_more" data-balloon-pos="up" data-balloon="<?php esc_html_e( 'More actions', 'buddyboss' ); ?>">
 					<i class="bb-icon-menu-dots-v"></i>
