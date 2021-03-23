7e9e4e5dd1b8427b609849479f50de0a36eb9a3d
jitendrabanjara <jitendrabanjara@gmail.com>
2021-03-23 22:18:42 +0530

    jshint error solve

--- src/bp-core/js/mentions.js
+++ src/bp-core/js/mentions.js
@@ -283,7 +283,7 @@ window.bp = window.bp || {};
 			jQuery(this).on('keydown', function (e) {
 				
 				// Check backspace key down event
-				if (!isAndroid()) {
+				if (!isAndroid) {
 					if (e.keyCode == 8) {
 						jQuery(this).find('.atwho-inserted').each(function () {
 							jQuery(this).removeAttr('contenteditable');
@@ -338,7 +338,7 @@ window.bp = window.bp || {};
 			 * It was preventing the backspace somehow. So whenever
 			 * we try to backspace, keyboard was automatically closed.
 			 */
-			if (isAndroid()) {
+			if (isAndroid) {
 				
 				var new_length = jQuery(this).text().length; // Get the new text length.
 				localStorage.setItem('charCount', new_length); // Set length to local storage.
