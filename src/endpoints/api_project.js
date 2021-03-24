c461772e1be44b1435ea933f8eedfe22bf5bfd3c
Kartik Suthar <kartik.7013@gmail.com>
2021-03-24 18:06:41 +0530

    fix rest Vulnerability https://virusdie.com/verdicts/?id=67233

--- src/endpoints/api_project.js
+++ src/endpoints/api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-03-23T11:17:20.480Z",
+    "time": "2021-03-24T12:35:58.575Z",
     "url": "http://apidocjs.com",
     "version": "0.22.0"
   }
