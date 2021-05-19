720467eaa038d2aa58efabe4c10ef9f171ea5e81
Kartik Suthar <kartik.7013@gmail.com>
2021-05-19 10:54:49 +0530

    fix permission for video upload into group and profile albums based on settings

--- src/bp-templates/bp-nouveau/buddypress/media/single-album.php
+++ src/bp-templates/bp-nouveau/buddypress/media/single-album.php
@@ -56,15 +56,17 @@ if ( bp_has_albums( array( 'include' => $album_id ) ) ) : ?>
 							<a class="bb-add-photos button small outline" id="bp-add-media" href="#" >
                                     <?php _e( 'Add Photos', 'buddyboss' ); ?>
                                 </a> <?php
-						    } ?>
-							<?php
-							if ( ( bp_is_profile_video_support_enabled() || bp_is_group_video_support_enabled() ) ) {
+						    }
+
+							if (
+								( ( bp_is_my_profile() || bp_is_user_media() ) && bp_is_profile_video_support_enabled() ) ||
+								( bp_is_group() && groups_can_user_manage_video( bp_loggedin_user_id(), bp_get_group_id() ) )
+							) {
 								?>
                                 <a href="#" id="bp-add-video" class="bb-add-video button small outline"><?php esc_html_e( 'Add Videos', 'buddyboss' ); ?></a>
-								<?php
+                                <?php
 							}
-							?>
-						<?php } ?>
+						} ?>
 
 						<?php if ( ( bp_is_my_profile() || bp_is_user_media() ) && ! bp_is_group() ) : ?>
 							<select id="bb-album-privacy">
