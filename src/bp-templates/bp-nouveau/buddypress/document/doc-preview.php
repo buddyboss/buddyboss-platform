0fcd3b7f392fd4095fb6ce00cb251e8401471e1e
Kartik Suthar <kartik.7013@gmail.com>
2021-05-20 23:57:29 +0530

    fix document file preview

--- src/bp-templates/bp-nouveau/buddypress/document/doc-preview.php
+++ src/bp-templates/bp-nouveau/buddypress/document/doc-preview.php
@@ -8,7 +8,7 @@
 
 $bp_document_image_preview = apply_filters( 'bp_document_image_preview', true );
 $attachment_url            = bp_get_document_attachment_image_activity_thumbnail();
-if ( $attachment_url && $bp_document_image_preview ) {
+if ( $attachment_url && $bp_document_image_preview && ! in_array( bp_get_document_extension(), bp_get_document_preview_code_extensions(), true ) ) {
 	?>
 	<div class="document-preview-wrap">
 		<img src="<?php echo esc_url( bp_get_document_attachment_image_activity_thumbnail() ); ?>" alt="" />
