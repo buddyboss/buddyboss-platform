07228446ed0e9f5822c3e335a2b07b5084c9b1b0
Kartik Suthar <kartik.7013@gmail.com>
2021-05-20 17:29:51 +0530

    fix document review in activity

--- src/bp-templates/bp-nouveau/buddypress/document/doc-preview.php
+++ src/bp-templates/bp-nouveau/buddypress/document/doc-preview.php
@@ -7,11 +7,11 @@
  */
 
 $bp_document_image_preview = apply_filters( 'bp_document_image_preview', true );
-$attachment_url            = bp_document_get_preview_image_url( bp_get_document_id(), bp_get_document_extension(), bp_get_document_preview_attachment_id() );
+$attachment_url            = bp_get_document_attachment_image_activity_thumbnail();
 if ( $attachment_url && $bp_document_image_preview ) {
 	?>
 	<div class="document-preview-wrap">
-		<img src="<?php echo esc_url( $attachment_url ); ?>" alt="" />
+		<img src="<?php echo esc_url( bp_get_document_attachment_image_activity_thumbnail() ); ?>" alt="" />
 	</div><!-- .document-preview-wrap -->
 	<?php
 }
