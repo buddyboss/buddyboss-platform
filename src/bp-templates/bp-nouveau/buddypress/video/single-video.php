75bd0fe7778bfd82494ae51865b1a5aacaee9fee
Krunal Palaniya <krunal@buddyboss.com>
2021-05-21 17:55:03 +0530

    Remove auto play on popup

--- src/bp-templates/bp-nouveau/buddypress/video/single-video.php
+++ src/bp-templates/bp-nouveau/buddypress/video/single-video.php
@@ -8,7 +8,7 @@
  */
 
 ?>
-<video id="theatre-video-<?php bp_video_id(); ?>" autoplay class="video-js" controls poster="<?php bp_video_attachment_image(); ?>" data-setup='{"fluid": true,"playbackRates": [0.5, 1, 1.5, 2] }'>
+<video id="theatre-video-<?php bp_video_id(); ?>" class="video-js" controls poster="<?php bp_video_attachment_image(); ?>" data-setup='{"fluid": true,"playbackRates": [0.5, 1, 1.5, 2] }'>
 	<source src="<?php bp_video_link(); ?>" type="<?php bp_video_type(); ?>">
 	</source>
 </video>
