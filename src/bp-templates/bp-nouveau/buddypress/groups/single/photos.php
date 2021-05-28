5c289f83b3a1a28e35f78fbd540ac0dacaac5d24
Kartik Suthar <kartik.7013@gmail.com>
2021-05-28 12:19:10 +0530

    added delete button for the group moderator

--- src/bp-templates/bp-nouveau/buddypress/groups/single/photos.php
+++ src/bp-templates/bp-nouveau/buddypress/groups/single/photos.php
@@ -17,7 +17,14 @@ switch ( bp_current_action() ) :
 		?>
 		<div class="bb-media-actions-wrap">
 			<?php
-			if ( bp_is_group_media() && groups_can_user_manage_media( bp_loggedin_user_id(), bp_get_current_group_id() ) ) {
+			if (
+				bp_is_group_media() &&
+				(
+					groups_can_user_manage_media( bp_loggedin_user_id(), bp_get_current_group_id() ) ||
+					groups_is_user_mod( bp_loggedin_user_id(), bp_get_current_group_id() ) ||
+					groups_is_user_admin( bp_loggedin_user_id(), bp_get_current_group_id() )
+				)
+			) {
 				bp_get_template_part( 'media/add-media' );
 			} else {
 				?>
