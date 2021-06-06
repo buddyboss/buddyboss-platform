ce38e70b668b35c7a01fb9b963565465a9816302
Mahedi Hasan Noman <mahedi@Mahedis-MacBook-Pro.local>
2021-06-07 03:30:59 +0600

    Issue #2463 - Fixed

--- src/bp-document/classes/class-bp-rest-document-details-endpoint.php
+++ src/bp-document/classes/class-bp-rest-document-details-endpoint.php
@@ -170,7 +170,7 @@ class BP_REST_Document_Details_Endpoint extends WP_REST_Controller {
 	 * @return array
 	 */
 	public function get_documents_privacy() {
-		$privacy = buddypress()->document->visibility_levels;
+		$privacy = apply_filters( 'bp_document_get_visibility_levels', buddypress()->document->visibility_levels );
 		$retval  = array();
 
 		if ( ! empty( $privacy ) ) {
