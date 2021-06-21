8d0b8d29151ab618d6da7c999b6ca644d9839fa0
asaquzzamanbb <asaquzzaman@buddyboss.com>
2021-06-21 23:18:27 +0600

    New - Notifications settings file

---
+++ src/bp-core/admin/settings/bp-admin-setting-notifications.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Add admin Activity settings page in Dashboard->BuddyBoss->Settings
+ *
+ * @package BuddyBoss\Core
+ *
+ * @since BuddyBoss 1.0.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Main activity settings class.
+ *
+ * @since BuddyBoss 1.0.0
+ */
+class BP_Admin_Setting_Notifications extends BP_Admin_Setting_tab {
+
+	public function initialize() {
+		$this->tab_label = __( 'Notifications', 'buddyboss' );
+		$this->tab_name  = 'bp-notifications';
+		$this->tab_order = 40;
+	}
+
+	public function is_active() {
+		return bp_is_active( 'notifications' );
+	}
+
+	public function settings_save() {
+		parent::settings_save();
+
+        $enabel         = empty( $_POST['_bp_on_screen_notifications_enable'] ) ? 0          : sanitize_text_field( $_POST['_bp_on_screen_notifications_enable'] );
+        $position       = empty( $_POST['_bp_on_screen_notifications_position'] ) ? 'right'  : sanitize_text_field( $_POST['_bp_on_screen_notifications_position'] );
+        $mobile_support = empty( $_POST['_bp_on_screen_notifications_mobile_support'] ) ? 0  : sanitize_text_field( $_POST['_bp_on_screen_notifications_mobile_support'] );
+        $visibility     = empty( $_POST['_bp_on_screen_notifications_visibility'] ) ? 'never': sanitize_text_field( $_POST['_bp_on_screen_notifications_visibility'] );
+        $browser_tab    = empty( $_POST['_bp_on_screen_notifications_browser_tab'] ) ? 0     : sanitize_text_field( $_POST['_bp_on_screen_notifications_browser_tab'] );
+
+        bp_update_option( '_bp_on_screen_notifications_enable', $enabel );
+        bp_update_option( '_bp_on_screen_notifications_position', $position );
+        bp_update_option( '_bp_on_screen_notifications_mobile_support', $mobile_support );
+        bp_update_option( '_bp_on_screen_notifications_visibility', $visibility );
+        bp_update_option( '_bp_on_screen_notifications_browser_tab', $browser_tab );
+
+	}
+
+	public function register_fields() {
+
+		$this->add_section( 'bp_notifications', __( 'Notification Settings', 'buddyboss' ), '', 'bp_activity_settings_tutorial' );
+
+        // Allow Activity edit setting.
+		$this->add_field( '_bp_on_screen_notification_enable', __( 'On-screen notifications', 'buddyboss' ), 'bp_admin_setting_callback_on_screen_notifications_enable', 'intval' );
+		$this->add_field( "_bp_on_screen_notification_position", __( 'Position on Screen', 'buddyboss' ), 'bp_admin_setting_callback_on_screen_notifications_position', 'intval' );
+        $this->add_field( "_bp_on_screen_notification_mobile_support", __( 'Mobile Support', 'buddyboss' ), 'bp_admin_setting_callback_on_screen_notifications_mobile_support', 'intval' );
+        $this->add_field( "_bp_on_screen_notification_visibility", __( 'Automatically Hide', 'buddyboss' ), 'bp_admin_setting_callback_on_screen_notifications_visibility', 'intval' );
+        $this->add_field( "_bp_on_screen_notification_browser_tab", __( 'Show in Browser Tab', 'buddyboss' ), 'bp_admin_setting_callback_on_screen_notifications_browser_tab', 'intval' );
+
+		/**
+		 * Fires to register Activity tab settings fields and section.
+		 *
+		 * @since BuddyBoss 1.2.6
+		 *
+		 * @param Object $this BP_Admin_Setting_Notifications.
+		 */
+		do_action( 'bp_admin_setting_Notifications_register_fields', $this );
+	}
+}
+
+return new BP_Admin_Setting_Notifications();
