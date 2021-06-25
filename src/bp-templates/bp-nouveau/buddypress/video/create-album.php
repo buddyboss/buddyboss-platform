9c31c5ac582e751ea61cf34f80ea1de32182a540
Krunal Palaniya <krunal@buddyboss.com>
2021-06-25 13:18:03 +0530

    Used proper  escaping function for placeholder

--- src/bp-templates/bp-nouveau/buddypress/video/create-album.php
+++ src/bp-templates/bp-nouveau/buddypress/video/create-album.php
@@ -16,13 +16,13 @@
 				<div id="boss-video-create-album-popup" class="modal-container">
 
 					<header class="bb-model-header">
-						<h4><?php esc_html_e( 'Create Album', 'buddyboss' ); ?></h4>
+						<h4><?php esc_attr_e( 'Create Album', 'buddyboss' ); ?></h4>
 						<a class="bb-model-close-button" id="bp-video-create-album-close" href="#"><span class="bb-icon bb-icon-close"></span></a>
 					</header>
 
 					<div class="bb-field-wrap">
-						<label for="bb-album-title" class="bb-label"><?php esc_html_e( 'Title', 'buddyboss' ); ?></label>
-						<input id="bb-album-title" type="text" placeholder="<?php esc_html_e( 'Enter Album Title', 'buddyboss' ); ?>" />
+						<label for="bb-album-title" class="bb-label"><?php esc_attr_e( 'Title', 'buddyboss' ); ?></label>
+						<input id="bb-album-title" type="text" placeholder="<?php esc_attr_e( 'Enter Album Title', 'buddyboss' ); ?>" />
 					</div>
 
 					<footer class="bb-model-footer">
@@ -39,7 +39,7 @@
 								</select>
 							</div>
 						<?php endif; ?>
-						<a class="button" id="bp-video-create-album-submit" href="#"><?php esc_html_e( 'Create Album', 'buddyboss' ); ?></a>
+						<a class="button" id="bp-video-create-album-submit" href="#"><?php esc_attr_e( 'Create Album', 'buddyboss' ); ?></a>
 					</footer>
 
 				</div>
