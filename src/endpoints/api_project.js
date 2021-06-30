f8fbed21eadd5f1f8c58e731132d3b126536d838
Kartik Suthar <kartik.7013@gmail.com>
2021-06-30 18:01:18 +0530

    load api from platform

--- src/endpoints/api_project.js
+++ src/endpoints/api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-06-30T12:24:04.695Z",
+    "time": "2021-06-30T12:30:01.158Z",
     "url": "http://apidocjs.com",
     "version": "0.22.0"
   }
