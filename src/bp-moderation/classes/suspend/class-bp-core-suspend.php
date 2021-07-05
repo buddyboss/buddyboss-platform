74c731549ebc19eff9519d8d3aeec0e6af2ebf98
Chetan Satasiya <chetan@buddyboss.com>
2021-07-05 17:27:08 +0530

    Revert - Media moeration issues

--- src/bp-moderation/classes/suspend/class-bp-core-suspend.php
+++ src/bp-moderation/classes/suspend/class-bp-core-suspend.php
@@ -63,10 +63,9 @@ class BP_Core_Suspend {
 			new BP_Suspend_Media();
 		}
 
-		// Disabling video moderation / suspend until fully support.
-//		if ( bp_is_active( 'video' ) ) {
-//			new BP_Suspend_Video();
-//		}
+		if ( bp_is_active( 'video' ) ) {
+			new BP_Suspend_Video();
+		}
 
 		if ( bp_is_active( 'messages' ) ) {
 			new BP_Suspend_Message();
