fbbd332639ade4da7307f54f68e8187f1e091ec6
asaquzzamanbb <asaquzzaman@buddyboss.com>
2021-07-08 11:12:09 +0600

    Update - View post permalink

--- src/bp-blogs/bp-blogs-filters.php
+++ src/bp-blogs/bp-blogs-filters.php
@@ -158,22 +158,18 @@ function bb_nouveau_get_activity_inner_blogs_buttons( $buttons, $activity_id ) {
 		$blog_post = get_post( $activities_template->activity->secondary_item_id );
 		// If we converted $content to an object earlier, flip it back to a string.
 		if ( is_a( $blog_post, 'WP_Post' ) && ! has_post_thumbnail( $blog_post ) ) {
-			$post_link  = sprintf( '<div class="view-post-link"><a class="bb-post-title-link" href="%s"><span class="bb-post-title">%s</span></a></div>', esc_url( get_permalink( $blog_post->ID ) ), esc_html( 'View Post', 'buddyboss-theme' ) );
+			$buttons['activity_post'] = array(
+				'id'                => 'activity_post_link_wrap',
+				'position'          => 4,
+				'component'         => 'activity',
+				'button_element'    => 'a',
+				'link_text'         => sprintf( '<span class="text">%1$s</span>', esc_html( 'View Post', 'buddyboss-theme' ) ),
+				'button_attr'       => array(
+					'href'          => esc_url( get_permalink( $blog_post->ID ) ),
+					'class'             => 'button bb-icon-arrow-circle bp-secondary-action',
+				),
+			);
 		}
-	}
-
-	if ( ! empty( $post_link ) ) {
-		$buttons['activity_post'] = array(
-			'id'                => 'activity_post_link_wrap',
-			'position'          => 4,
-			'component'         => 'activity',
-			'button_element'    => 'a',
-			'link_text'         => sprintf( '<span class="text">%1$s</span>', esc_html( 'View Post', 'buddyboss-theme' ) ),
-			'button_attr'       => array(
-				'href'          => esc_url( get_permalink( $blog_post->ID ) ),
-				'class'             => 'button bb-icon-arrow-circle bp-secondary-action',
-			),
-		);
 	}
 
 	return $buttons;
