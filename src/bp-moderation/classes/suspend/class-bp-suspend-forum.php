4c1556670f5db11aa49f6f201463cf95ccf1d1ae
Janak Kaneriya <janak@buddyboss.com>
2021-07-09 18:10:40 +0530

    Updated since tag

--- src/bp-moderation/classes/suspend/class-bp-suspend-forum.php
+++ src/bp-moderation/classes/suspend/class-bp-suspend-forum.php
@@ -433,7 +433,7 @@ class BP_Suspend_Forum extends BP_Suspend_Abstract {
 	/**
 	 * Function to un-restrict activity data while deleting the activity.
 	 *
-	 * @since BuddyBoss 1.7.1
+	 * @since BuddyBoss 1.7.2
 	 *
 	 * @param boolean $restrict restrict single item or not.
 	 *
