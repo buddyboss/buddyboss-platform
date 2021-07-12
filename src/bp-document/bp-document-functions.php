a1d09fd93d75497c7dcb840c8f3081ebf5346544
Krunal Palaniya <krunal@buddyboss.com>
2021-07-12 17:40:49 +0530

    Check if extension available then only it will append

--- src/bp-document/bp-document-functions.php
+++ src/bp-document/bp-document-functions.php
@@ -4523,7 +4523,7 @@ function bb_document_video_get_symlink( $document, $generate = true ) {
 							if ( $generate ) {
 								// Generate Document Video Symlink.
 								$ext = pathinfo($attached_file, PATHINFO_EXTENSION);
-								$attachment_path = $attachment_path . ( ! empty( $ext ) ) ? '.' . $ext : '';
+								$attachment_path .= ( ! empty( $ext ) ) ? '.' . $ext : '';
 								bb_core_symlink_generator( 'document_video', $document, '', array(), $attached_file, $attachment_path );
 							}
 						}
