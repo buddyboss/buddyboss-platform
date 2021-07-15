76551e8da3f1e659edbdf45b84b1141761cf3139
asaquzzamanbb <asaquzzaman@buddyboss.com>
2021-07-15 10:32:26 +0600

    Update - Remove unnecessary function

--- src/bp-forums/groups.php
+++ src/bp-forums/groups.php
@@ -34,9 +34,9 @@ if ( ! class_exists( 'BBP_Forums_Group_Extension' ) && class_exists( 'BP_Group_E
 		public function __construct() {
 
 			// Redirect 404 page when user will input wrong URL from groups forum screen.
-			if ( ! $this->can_access() ) {
-				return;
-			}
+			// if ( ! $this->can_access() ) {
+			// 	return;
+			// }
 
 			$this->setup_variables();
 			$this->setup_actions();
