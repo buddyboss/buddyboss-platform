e2b94be103b82969544ef1edeaa74a3ab136f7a3
Janak Kaneriya <janak@buddyboss.com>
2021-07-27 19:16:16 +0530

    Added condition check

--- src/bp-moderation/classes/class-bp-moderation-activity.php
+++ src/bp-moderation/classes/class-bp-moderation-activity.php
@@ -210,7 +210,7 @@ class BP_Moderation_Activity extends BP_Moderation_Abstract {
 				break;
 			case 'bbp_topic_create':
 				$updated = true;
-				if ( bp_is_moderation_content_reporting_enable( 0, BP_Moderation_Forum_Topics::$moderation_type ) ) {
+				if ( bp_is_active( 'forum' ) && bp_is_moderation_content_reporting_enable( 0, BP_Moderation_Forum_Topics::$moderation_type ) ) {
 					$sub_items['id']   = ( 'groups' === $activity->component ) ? $activity->secondary_item_id : $activity->item_id;
 					$sub_items['type'] = BP_Moderation_Forum_Topics::$moderation_type;
 				} else {
@@ -220,7 +220,7 @@ class BP_Moderation_Activity extends BP_Moderation_Abstract {
 				break;
 			case 'bbp_reply_create':
 				$updated = true;
-				if ( bp_is_moderation_content_reporting_enable( 0, BP_Moderation_Forum_Replies::$moderation_type ) ) {
+				if ( bp_is_active( 'forum' ) && bp_is_moderation_content_reporting_enable( 0, BP_Moderation_Forum_Replies::$moderation_type ) ) {
 					$sub_items['id']   = ( 'groups' === $activity->component ) ? $activity->secondary_item_id : $activity->item_id;
 					$sub_items['type'] = BP_Moderation_Forum_Replies::$moderation_type;
 				} else {
