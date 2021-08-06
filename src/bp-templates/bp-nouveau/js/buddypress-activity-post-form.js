84e52a907220a285784f4b37475095b52ad57b6c
Kishan Jasani <kishanjasani007@yahoo.in>
2021-08-06 16:29:27 +0530

    Fix link preview does not refresh durinf changing the url

--- src/bp-templates/bp-nouveau/js/buddypress-activity-post-form.js
+++ src/bp-templates/bp-nouveau/js/buddypress-activity-post-form.js
@@ -1885,7 +1885,6 @@ window.bp = window.bp || {};
 
 			handleKeyUp: function () {
 				var self = this;
-
 				if ( this.linkTimeout != null ) {
 					clearTimeout( this.linkTimeout );
 				}
@@ -1977,8 +1976,7 @@ window.bp = window.bp || {};
 				var regexp = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,24}(:[0-9]{1,5})?(\/.*)?$/;
 				url        = $.trim( url );
 				if ( regexp.test( url ) ) {
-
-					if ( ! _.isUndefined( self.options.activity.get( 'link_success' ) ) && self.options.activity.get( 'link_success' ) == true ) {
+					if ( ( ! _.isUndefined( self.options.activity.get( 'link_success' ) ) && self.options.activity.get( 'link_success' ) == true ) && self.options.activity.get( 'link_url' ) === url ) {
 						return false;
 					}
 
