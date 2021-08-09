24c7a42460fb049eed8e75d808d9d3a26dbd86b2
Chetan Satasiya <chetan@buddyboss.com>
2021-08-09 12:11:25 +0530

    Removed media private upload and always follow the media URLs as per offload media

--- src/bp-core/compatibility/bp-wp-offload-media-helpers.php
+++ src/bp-core/compatibility/bp-wp-offload-media-helpers.php
@@ -27,6 +27,12 @@ class BB_AS3CF_Plugin_Compatibility {
 	 * BB_AS3CF_Plugin_Compatibility constructor.
 	 */
 	public function __construct() {
+
+		add_filter( 'bb_media_do_symlink', '__return_false' );
+		add_filter( 'bb_document_do_symlink', '__return_false' );
+		add_filter( 'bb_video_do_symlink', '__return_false' );
+		add_filter( 'bb_video_create_thumb_symlinks', '__return_false' );
+
 		$this->compatibility_init();
 	}
 
@@ -54,86 +60,11 @@ class BB_AS3CF_Plugin_Compatibility {
 
 		add_filter( 'as3cf_get_attached_file_copy_back_to_local', '__return_true' );
 
-		add_filter( 'bb_media_do_symlink', array( $this, 'bb_offload_do_symlink' ), PHP_INT_MAX, 4 );
-		add_filter( 'bb_document_do_symlink', array( $this, 'bb_offload_do_symlink' ), PHP_INT_MAX, 4 );
-		add_filter( 'bb_video_do_symlink', array( $this, 'bb_offload_do_symlink' ), PHP_INT_MAX, 4 );
-		add_filter( 'bb_video_create_thumb_symlinks', array( $this, 'bb_offload_do_symlink' ), PHP_INT_MAX, 4 );
-
-		add_action( 'bb_before_document_upload_handler', array( $this, 'bb_offload_media_set_private' ) );
-		add_action( 'bb_before_media_upload_handler', array( $this, 'bb_offload_media_set_private' ) );
-		add_action( 'bb_before_video_upload_handler', array( $this, 'bb_offload_media_set_private' ) );
-		add_action( 'bb_before_video_preview_image_by_js', array( $this, 'bb_offload_media_set_private' ) );
-		add_action( 'bb_video_before_preview_generate', array( $this, 'bb_offload_media_set_private' ) );
-		add_action( 'bb_before_bp_video_thumbnail_upload_handler', array( $this, 'bb_offload_media_set_private' ) );
-		add_action( 'bb_document_before_generate_document_previews', array( $this, 'bb_offload_media_set_private' ) );
-
-		add_action( 'bb_after_document_upload_handler', array( $this, 'bb_offload_media_unset_private' ) );
-		add_action( 'bb_after_media_upload_handler', array( $this, 'bb_offload_media_unset_private' ) );
-		add_action( 'bb_after_video_upload_handler', array( $this, 'bb_offload_media_unset_private' ) );
-		add_action( 'bb_after_video_preview_image_by_js', array( $this, 'bb_offload_media_unset_private' ) );
-		add_action( 'bb_video_after_preview_generate', array( $this, 'bb_offload_media_unset_private' ) );
-		add_action( 'bb_after_bp_video_thumbnail_upload_handler', array( $this, 'bb_offload_media_unset_private' ) );
-		add_action( 'bb_document_after_generate_document_previews', array( $this, 'bb_offload_media_unset_private' ) );
-
 		add_filter( 'bp_document_get_preview_url', array( $this, 'bp_document_offload_get_preview_url' ), PHP_INT_MAX, 6 );
 		add_filter( 'bp_media_get_preview_image_url', array( $this, 'bp_media_offload_get_preview_url' ), PHP_INT_MAX, 5 );
 		add_filter( 'bb_video_get_thumb_url', array( $this, 'bp_video_offload_get_thumb_preview_url' ), PHP_INT_MAX, 5 );
 		add_filter( 'bb_video_get_symlink', array( $this, 'bp_video_offload_get_video_url' ), PHP_INT_MAX, 4 );
 
-		add_action( 'bb_try_before_video_background_create_thumbnail', array( $this, 'bb_video_set_wp_offload_download_video_local' ), 99999, 1 );
-		add_action( 'bb_try_after_video_background_create_thumbnail', array( $this, 'bb_video_unset_wp_offload_download_video_local' ), 99999, 1 );
-
-	}
-
-	/**
-	 * Function to set the false to use the default media symlink instead use the offload media URL of media.
-	 *
-	 * @param bool   $can           default true.
-	 * @param int    $id            media/document/video id.
-	 * @param int    $attachment_id attachment id.
-	 * @param string $size          preview size.
-	 *
-	 * @return bool true if the offload media used.
-	 *
-	 * @since BuddyBoss 1.7.0
-	 */
-	public function bb_offload_do_symlink( $can, $id, $attachment_id, $size ) {
-
-		/**
-		 * Global object for Amazon_S3_And_CloudFront|Amazon_S3_And_CloudFront_Pro.
-		 *
-		 * @var Amazon_S3_And_CloudFront|\Amazon_S3_And_CloudFront_Pro $as3cf
-		 */
-		global $as3cf;
-
-		if ( ! $as3cf->is_attachment_served_by_provider( $attachment_id ) ) {
-			// Not served by provider, use symlink or rewrite url.
-			$can = true;
-		} else {
-			$can = false;
-		}
-
-		return $can;
-	}
-
-	/**
-	 * Set the uploaded document to make private on offload media plugin.
-	 *
-	 * @since BuddyBoss 1.7.0
-	 */
-	public function bb_offload_media_set_private() {
-		add_filter( 'as3cf_upload_acl', array( $this, 'bb_media_private_upload_acl' ), 10, 1 );
-		add_filter( 'as3cf_upload_acl_sizes', array( $this, 'bb_media_private_upload_acl' ), 10, 1 );
-	}
-
-	/**
-	 * Remove the private URL generate document preview.
-	 *
-	 * @since BuddyBoss 1.7.0
-	 */
-	public function bb_offload_media_unset_private() {
-		remove_filter( 'as3cf_upload_acl', array( $this, 'bb_media_private_upload_acl' ), 10, 1 );
-		remove_filter( 'as3cf_upload_acl_sizes', array( $this, 'bb_media_private_upload_acl' ), 10, 1 );
 	}
 
 	/**
@@ -164,13 +95,7 @@ class BB_AS3CF_Plugin_Compatibility {
 
 			if ( ! $attachment_url || empty( $image_array ) ) {
 
-				add_filter( 'as3cf_upload_acl', array( $this, 'bb_media_private_upload_acl' ), 10, 1 );
-				add_filter( 'as3cf_upload_acl_sizes', array( $this, 'bb_media_private_upload_acl' ), 10, 1 );
-
 				bp_document_generate_document_previews( $attachment_id );
-
-				remove_filter( 'as3cf_upload_acl', array( $this, 'bb_media_private_upload_acl' ), 10, 1 );
-				remove_filter( 'as3cf_upload_acl_sizes', array( $this, 'bb_media_private_upload_acl' ), 10, 1 );
 
 				if ( ! empty( $get_metadata ) && isset( $get_metadata['sizes'] ) && isset( $get_metadata['sizes'][ $size ] ) ) {
 					$attachment_url = wp_get_attachment_image_url( $attachment_id, $size );
@@ -210,48 +135,6 @@ class BB_AS3CF_Plugin_Compatibility {
 		}
 
 		return $attachment_url;
-	}
-
-	/**
-	 * Make all the media to private signed URL if someone using the offload media to store in AWS.
-	 *
-	 * @handles `as3cf_upload_acl`
-	 * @handles `as3cf_upload_acl_sizes`
-	 *
-	 * @param string $acl defaults to 'public-read'.
-	 *
-	 * @return string $acl make the media to private with signed url.
-	 *
-	 * @since   BuddyBoss 1.7.0
-	 */
-	public function bb_media_private_upload_acl( $acl ) {
-		$acl = 'private';
-
-		return $acl;
-	}
-
-	/**
-	 * Filter to download the video on local server.
-	 *
-	 * @param BP_Video $video Video object.
-	 *
-	 * @since BuddyBoss 1.7.0
-	 */
-	public function bb_video_set_wp_offload_download_video_local( $video ) {
-		add_filter( 'as3cf_upload_acl', array( $this, 'bb_media_private_upload_acl' ), 10, 1 );
-		add_filter( 'as3cf_upload_acl_sizes', array( $this, 'bb_media_private_upload_acl' ), 10, 1 );
-	}
-
-	/**
-	 * Filter to download the video on local server.
-	 *
-	 * @param BP_Video $video Video object.
-	 *
-	 * @since BuddyBoss 1.7.0
-	 */
-	public function bb_video_unset_wp_offload_download_video_local( $video ) {
-		remove_filter( 'as3cf_upload_acl', array( $this, 'bb_media_private_upload_acl' ), 10, 1 );
-		remove_filter( 'as3cf_upload_acl_sizes', array( $this, 'bb_media_private_upload_acl' ), 10, 1 );
 	}
 
 	/**
