68d15c5adaf26cc77a20c5491fbdeec0cf39f635
Janak Kaneriya <janak@buddyboss.com>
2021-08-16 19:02:54 +0530

    Fixed the activity comment report button for photos, documents and videos

--- src/bp-moderation/classes/class-bp-moderation-activity-comment.php
+++ src/bp-moderation/classes/class-bp-moderation-activity-comment.php
@@ -198,48 +198,66 @@ class BP_Moderation_Activity_Comment extends BP_Moderation_Abstract {
 		$video_ids    = bp_activity_get_meta( $comment->id, 'bp_video_ids', true );
 
 		if ( ! empty( $media_id ) || ! empty( $media_ids ) ) {
-			if ( bp_is_active( 'media' ) && bp_is_moderation_content_reporting_enable( 0, BP_Moderation_Media::$moderation_type ) ) {
-				$explode_medias = explode( ',', $media_ids );
-				if ( 1 === count( $explode_medias ) && ! empty( $explode_medias[0] ) ) {
-					$media_id = $explode_medias[0];
+			if ( bp_is_active( 'media' ) && ! empty( $media_id ) ) {
+				if ( ! bp_is_moderation_content_reporting_enable( 0, BP_Moderation_Media::$moderation_type ) ) {
+					$sub_items['id']   = false;
+					$sub_items['type'] = false;
 				}
-				$sub_items['id']   = $media_id;
-				$sub_items['type'] = BP_Moderation_Media::$moderation_type;
-				if ( 1 < count( $explode_medias ) ) {
+			} elseif ( bp_is_active( 'media' ) && ! empty( $media_ids ) ) {
+				$explode_medias = explode( ',', $media_ids );
+				if ( 1 === count( $explode_medias ) && ! empty( $explode_medias[0] ) && bp_is_moderation_content_reporting_enable( 0, BP_Moderation_Media::$moderation_type ) ) {
+					$sub_items['id']   = $explode_medias[0];
+					$sub_items['type'] = BP_Moderation_Media::$moderation_type;
+				} elseif ( 1 < count( $explode_medias ) ) {
 					$sub_items['id']   = $comment->id;
 					$sub_items['type'] = self::$moderation_type;
+				} else {
+					$sub_items['id']   = false;
+					$sub_items['type'] = false;
 				}
 			} else {
 				$sub_items['id']   = false;
 				$sub_items['type'] = false;
 			}
 		} elseif ( ! empty( $document_id ) || ! empty( $document_ids ) ) {
-			if ( bp_is_active( 'document' ) && bp_is_moderation_content_reporting_enable( 0, BP_Moderation_Document::$moderation_type ) ) {
-				$explode_documents = explode( ',', $document_ids );
-				if ( 1 === count( $explode_documents ) && ! empty( $explode_documents[0] ) ) {
-					$document_id = $explode_documents[0];
+			if ( bp_is_active( 'document' ) && ! empty( $document_id ) ) {
+				if ( ! bp_is_moderation_content_reporting_enable( 0, BP_Moderation_Document::$moderation_type ) ) {
+					$sub_items['id']   = false;
+					$sub_items['type'] = false;
 				}
-				$sub_items['id']   = $document_id;
-				$sub_items['type'] = BP_Moderation_Document::$moderation_type;
-				if ( 1 < count( $explode_documents ) ) {
+			} elseif ( bp_is_active( 'document' ) && ! empty( $document_ids ) ) {
+				$explode_documents = explode( ',', $document_ids );
+				if ( 1 === count( $explode_documents ) && ! empty( $explode_documents[0] ) && bp_is_moderation_content_reporting_enable( 0, BP_Moderation_Document::$moderation_type ) ) {
+					$sub_items['id']   = $explode_documents[0];
+					$sub_items['type'] = BP_Moderation_Document::$moderation_type;
+				} elseif ( 1 < count( $explode_documents ) ) {
 					$sub_items['id']   = $comment->id;
 					$sub_items['type'] = self::$moderation_type;
+				} else {
+					$sub_items['id']   = false;
+					$sub_items['type'] = false;
 				}
 			} else {
 				$sub_items['id']   = false;
 				$sub_items['type'] = false;
 			}
 		} elseif ( ! empty( $video_id ) || ! empty( $video_ids ) ) {
-			if ( bp_is_active( 'video' ) && bp_is_moderation_content_reporting_enable( 0, BP_Moderation_Video::$moderation_type ) ) {
-				$explode_videos = explode( ',', $video_ids );
-				if ( 1 === count( $explode_videos ) && ! empty( $explode_videos[0] ) ) {
-					$video_id = $explode_videos[0];
+			if ( bp_is_active( 'video' ) && ! empty( $video_id ) ) {
+				if ( ! bp_is_moderation_content_reporting_enable( 0, BP_Moderation_Video::$moderation_type ) ) {
+					$sub_items['id']   = false;
+					$sub_items['type'] = false;
 				}
-				$sub_items['id']   = $video_id;
-				$sub_items['type'] = BP_Moderation_Video::$moderation_type;
-				if ( 1 < count( $explode_videos ) ) {
+			} elseif ( bp_is_active( 'video' ) && ! empty( $video_ids ) ) {
+				$explode_videos = explode( ',', $video_ids );
+				if ( 1 === count( $explode_videos ) && ! empty( $explode_videos[0] ) && bp_is_moderation_content_reporting_enable( 0, BP_Moderation_Video::$moderation_type ) ) {
+					$sub_items['id']   = $explode_videos[0];
+					$sub_items['type'] = BP_Moderation_Video::$moderation_type;
+				} elseif ( 1 < count( $explode_videos ) ) {
 					$sub_items['id']   = $comment->id;
 					$sub_items['type'] = self::$moderation_type;
+				} else {
+					$sub_items['id']   = false;
+					$sub_items['type'] = false;
 				}
 			} else {
 				$sub_items['id']   = false;
