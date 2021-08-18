caed443d8d6f3f45182a038045d00c79ceb45328
milanhirapra <milan@buddyboss.com>
2021-08-18 19:01:23 +0530

    fix: The profile completion widget is not updating after profile type was changed

--- src/bp-core/bp-core-functions.php
+++ src/bp-core/bp-core-functions.php
@@ -5238,7 +5238,11 @@ function bp_core_profile_completion_steps_options() {
  *
  * @since BuddyBoss 1.4.9
  */
-function bp_core_xprofile_update_profile_completion_user_progress() {
+function bp_core_xprofile_update_profile_completion_user_progress( $user_id = '', $posted_field_ids = array(), $errors = array(), $old_values = array(), $new_values = array() ) {
+
+	if( empty( $user_id ) ){
+		$user_id 		= get_current_user_id();
+	}
 
 	$user_id            = get_current_user_id();
 	$steps_options      = bp_core_profile_completion_steps_options();
