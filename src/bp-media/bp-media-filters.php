6d1587cf86fe76d63a5e06f86624fc28307a719c
Kartik Suthar <kartik@buddyboss.com>
2021-08-27 15:28:53 +0530

    #2121 - activity comment sync fixed

--- src/bp-media/bp-media-filters.php
+++ src/bp-media/bp-media-filters.php
@@ -309,11 +309,12 @@ function bp_media_activity_comment_entry( $comment_id ) {
 		'order_by' => 'menu_order',
 		'sort'     => 'ASC',
 		'user_id'  => false,
+		'privacy'  => array(),
 	);
 
 	if ( bp_is_active( 'groups' ) && buddypress()->groups->id === $activity->component ) {
 		if ( bp_is_group_media_support_enabled() ) {
-			$args['privacy'] = array( 'grouponly' );
+			$args['privacy'][] = 'comment';
 			if ( ! bp_is_group_albums_support_enabled() ) {
 				$args['album_id'] = 'existing-media';
 			}
@@ -331,6 +332,11 @@ function bp_media_activity_comment_entry( $comment_id ) {
 		}
 	}
 
+	$args['privacy'][] = 'comment';
+	if ( ! isset( $args['album_id'] ) ) {
+		$args['album_id'] = 'existing-media';
+	}
+
 	$is_forum_activity = false;
 	if (
 			bp_is_active( 'forums' )
@@ -340,6 +346,8 @@ function bp_media_activity_comment_entry( $comment_id ) {
 		$is_forum_activity = true;
 		$args['privacy'][] = 'forums';
 	}
+
+	$args['privacy'] = array_unique( $args['privacy'] );
 
 	if ( ! empty( $media_ids ) && bp_has_media( $args ) ) {
 		?>
