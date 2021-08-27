d9b5d69a77e288a37059ce35fc3602530dcf998b
jitendrabanjara <jitendrabanjara@gmail.com>
2021-08-27 09:57:12 +0530

    #2121 - space issue in js

--- src/bp-templates/bp-nouveau/js/buddypress-media.js
+++ src/bp-templates/bp-nouveau/js/buddypress-media.js
@@ -6725,7 +6725,7 @@ window.bp = window.bp || {};
 				self.current_document = self.documents[ self.current_document_index ];
 				self.showDocument();
 				// If activity privacy is comment for doc then it will call getDocumentsDescription
-				if ( typeof BP_Nouveau.activity !== 'undefined' && self.current_document	&& typeof self.current_document.activity_id !== 'undefined'	&& self.current_document.activity_id != 0 && !self.current_document.is_forum	&& activity_id != self.current_document.activity_id && self.current_document.privacy !== 'comment' ) {
+				if ( typeof BP_Nouveau.activity !== 'undefined' && self.current_document && typeof self.current_document.activity_id !== 'undefined' && self.current_document.activity_id != 0 && !self.current_document.is_forum && activity_id != self.current_document.activity_id && self.current_document.privacy !== 'comment' ) {
 					self.getDocumentsActivity();
 				} else {
 					self.getDocumentsDescription();
