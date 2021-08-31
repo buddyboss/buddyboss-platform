e884c1e24c2bf950d1787a19113220b33d64ae0b
amin <amin@buddyboss.com>
2021-08-31 19:57:49 +0530

    #1962 - Fixed Load more member button UI issue

--- src/bp-templates/bp-nouveau/buddypress/common/js-templates/messages/parts/bp-messages-single-header.php
+++ src/bp-templates/bp-nouveau/buddypress/common/js-templates/messages/parts/bp-messages-single-header.php
@@ -169,7 +169,7 @@
 								<div class="bb-report-type-pagination">
 									<p class="page-data" data-thread-id="{{data.id}}">
 										<a href="javascript:void(0);" name="load_more_rl" id="load_more_rl"
-										   class="load_more_rl button small"
+										   class="load_more_rl button small outline"
 										   data-thread-id="{{data.id}}"
 										   data-tp="{{data.recipients.total_pages}}"
 										   data-tc="{{data.recipients.count}}"
