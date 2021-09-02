8442255959c37e2914db0e26ba195410321ac879
Janak Kaneriya <janak@buddyboss.com>
2021-09-02 17:16:11 +0530

    Fixed the popup open for media and the document

--- src/bp-templates/bp-nouveau/js/buddypress-media.js
+++ src/bp-templates/bp-nouveau/js/buddypress-media.js
@@ -6235,6 +6235,13 @@ window.bp = window.bp || {};
 							$( '.bb-media-info-section:visible .activity-list' ).removeClass( 'loading' ).html( response.data.description );
 							$( '.bb-media-info-section:visible' ).show();
 							$( window ).scroll();
+							setTimeout(
+								function () { // Waiting to load dummy image
+									bp.Nouveau.reportPopUp();
+									bp.Nouveau.reportedPopup();
+								},
+								10
+							);
 						} else {
 							$( '.bb-media-info-section.media' ).hide();
 						}
@@ -6897,6 +6904,13 @@ window.bp = window.bp || {};
 							$( '.bb-media-info-section:visible .activity-list' ).removeClass( 'loading' ).html( response.data.description );
 							$( '.bb-media-info-section:visible' ).show();
 							$( window ).scroll();
+							setTimeout(
+								function () { // Waiting to load dummy image
+									bp.Nouveau.reportPopUp();
+									bp.Nouveau.reportedPopup();
+								},
+								10
+							);
 						} else {
 							$( '.bb-media-info-section.document' ).hide();
 						}
