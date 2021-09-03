3798e532d980599b2b3d62ece336d13aafda516a
Mishu <asaquzzaman@buddyboss.com>
2021-09-03 16:47:58 +0600

    Update - Profile nav active menu

--- src/bp-members/classes/class-bp-members-admin.php
+++ src/bp-members/classes/class-bp-members-admin.php
@@ -818,11 +818,16 @@ if ( ! class_exists( 'BP_Members_Admin' ) ) :
 			$wordpress_url = add_query_arg( $query_args, $this->edit_url );
 
 			$bp_active = false;
-			$wp_active = ' nav-tab-active';
+			$wp_active = false;
+
+			if ( 'WordPress' === $active ) {
+				$wp_active = ' nav-tab-active';
+			}
+
 			if ( 'BuddyPress' === $active ) {
 				$bp_active = ' nav-tab-active';
-				$wp_active = false;
-			} ?>
+			}
+			?>
 
 		<h2 id="profile-nav" class="nav-tab-wrapper">
 			<?php
@@ -839,17 +844,7 @@ if ( ! class_exists( 'BP_Members_Admin' ) ) :
 			<?php endif; ?>
 
 			<a class="nav-tab<?php echo esc_attr( $bp_active ); ?>" href="<?php echo esc_url( $community_url ); ?>"><?php _e( 'Extended Profile', 'buddyboss' ); ?></a>
-			<?php
-			/**
-			 * User profile navigation tab.
-			 *
-			 * @since BuddyBoss x.x.x
-			 *
-			 * @param object|null $user   User to create profile navigation for.
-			 * @param string      $active Which profile to highlight.
-			 */
-			do_action( 'bb_members_admin_user_profile_nav', $user, $active );
-			?>
+			<?php do_action( 'bb_members_admin_user_profile_nav', $user, $active ); ?>
 		</h2>
 
 			<?php
