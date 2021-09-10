f8cd6c9c5a75b9ae884a189bcdb17eff6f905f24
mahedihasannoman <mahedihasannoman@gmail.com>
2021-09-10 21:21:23 +0600

    #2647 - symlink setting changes issue fixed

--- src/bp-core/admin/bp-core-admin-schema.php
+++ src/bp-core/admin/bp-core-admin-schema.php
@@ -87,7 +87,9 @@ function bp_core_install( $active_components = false ) {
 	if ( ! empty( $active_components['media'] ) ) {
 		bp_core_install_media();
 		bp_core_install_document();
-		bb_core_enable_default_symlink_support();
+		if ( (bool) get_option( 'bp_media_symlink_support', false ) ) {
+			bb_core_enable_default_symlink_support();
+		}
 	}
 
 	if ( ! empty( $active_components['moderation'] ) ) {
