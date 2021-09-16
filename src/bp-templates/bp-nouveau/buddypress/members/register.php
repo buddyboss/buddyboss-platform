046a40ef92358dd1c7fef5719c4951842f154a9c
Janak Kaneriya <janak@buddyboss.com>
2021-09-16 18:47:40 +0530

    Added xprofile fields in array form

--- src/bp-templates/bp-nouveau/buddypress/members/register.php
+++ src/bp-templates/bp-nouveau/buddypress/members/register.php
@@ -28,8 +28,37 @@
 
 					<?php /***** Basic Account Details ******/ ?>
 
-					<?php $signup_fields = bp_nouveau_get_signup_fields( 'account_details' ); ?>
+					<?php
 
+					$signup_fields            = bp_nouveau_get_signup_fields( 'account_details' );
+					$profile_signup_fields    = array();
+					$profile_signup_field_ids = '';
+					if ( bp_is_active( 'xprofile' ) && bp_nouveau_base_account_has_xprofile() ) {
+						while ( bp_profile_groups() ) : bp_the_profile_group();
+							while ( bp_profile_fields() ) : bp_the_profile_field();
+								if ( function_exists( 'bp_member_type_enable_disable' ) && false === bp_member_type_enable_disable() ) {
+									if ( function_exists( 'bp_get_xprofile_member_type_field_id' ) && bp_get_the_profile_field_id() === bp_get_xprofile_member_type_field_id() ) {
+										continue;
+									}
+								}
+								ob_start();
+								?>
+                                <div<?php bp_field_css_class( 'editfield' );
+								bp_field_data_attribute(); ?>>
+                                    <fieldset>
+										<?php
+										$field_type = bp_xprofile_create_field_type( bp_get_the_profile_field_type() );
+										$field_type->edit_field_html();
+										?>
+                                    </fieldset>
+                                </div>
+								<?php
+								$profile_signup_fields[ bp_get_the_profile_field_id() ] .= ob_get_clean();
+							endwhile;
+							$profile_signup_field_ids = bp_get_the_profile_field_ids();
+						endwhile;
+					}
+					?>
 				</div><!-- #basic-details-section -->
 
 				<?php bp_nouveau_signup_hook( 'after', 'account_details' ); ?>
@@ -40,44 +69,34 @@
 
 					<?php bp_nouveau_signup_hook( 'before', 'signup_profile' ); ?>
 
-					<div class="register-section extended-profile" id="profile-details-section">
-
+                    <div class="register-section extended-profile" id="profile-details-section">
 						<?php /* Use the profile field loop to render input fields for the 'base' profile field group */ ?>
 						<?php while ( bp_profile_groups() ) : bp_the_profile_group(); ?>
-
 							<?php while ( bp_profile_fields() ) : bp_the_profile_field();
-
-								if ( function_exists('bp_member_type_enable_disable' ) && false === bp_member_type_enable_disable() ) {
-									if ( function_exists( 'bp_get_xprofile_member_type_field_id') && bp_get_the_profile_field_id() === bp_get_xprofile_member_type_field_id() ) {
+								if ( function_exists( 'bp_member_type_enable_disable' ) && false === bp_member_type_enable_disable() ) {
+									if ( function_exists( 'bp_get_xprofile_member_type_field_id' ) && bp_get_the_profile_field_id() === bp_get_xprofile_member_type_field_id() ) {
 										continue;
 									}
 								}
 								?>
 
-								<div<?php bp_field_css_class( 'editfield' ); bp_field_data_attribute(); ?>>
-									<fieldset>
+                                <div<?php bp_field_css_class( 'editfield' );bp_field_data_attribute(); ?>>
+                                    <fieldset>
+										<?php
+										$field_type = bp_xprofile_create_field_type( bp_get_the_profile_field_type() );
+										$field_type->edit_field_html();
 
-									<?php
-									$field_type = bp_xprofile_create_field_type( bp_get_the_profile_field_type() );
-									$field_type->edit_field_html();
-
-									?>
-
-									</fieldset>
-								</div>
-
+										?>
+                                    </fieldset>
+                                </div>
 							<?php endwhile; ?>
-
-						<input type="hidden" name="signup_profile_field_ids" id="signup_profile_field_ids" value="<?php bp_the_profile_field_ids(); ?>" />
-
+                            <input type="hidden" name="signup_profile_field_ids" id="signup_profile_field_ids"
+                                   value="<?php bp_the_profile_field_ids(); ?>"/>
 						<?php endwhile; ?>
-
 						<?php bp_nouveau_signup_hook( '', 'signup_profile' ); ?>
-
-					</div><!-- #profile-details-section -->
+                    </div><!-- #profile-details-section -->
 
 					<?php bp_nouveau_signup_hook( 'after', 'signup_profile' ); ?>
-
 				<?php endif; ?>
 
 				<?php if ( bp_get_blog_signup_allowed() ) : ?>
