de2dba2eee91393688ff8de6035740a327da01c7
Krunal Palaniya <krunal@buddyboss.com>
2021-09-17 21:18:44 +0530

    #2939 - Fix SQL Injections Notification on order by columns dynamic

--- src/bp-notifications/classes/class-bp-notifications-notification.php
+++ src/bp-notifications/classes/class-bp-notifications-notification.php
@@ -84,20 +84,6 @@ class BP_Notifications_Notification {
 	 */
 	public $is_new;
 
-	/**
-	 * Columns in the notifications table.
-	 */
-	public static $columns = array(
-		'id',
-		'user_id',
-		'item_id',
-		'secondary_item_id',
-		'component_name',
-		'component_action',
-		'date_notified',
-		'is_new'
-	);
-
 	/** Public Methods ********************************************************/
 
 	/**
@@ -422,9 +408,12 @@ class BP_Notifications_Notification {
 		// Order by.
 		if ( ! empty( $args['order_by'] ) ) {
 			// Added security patch for SQL Injections vulnerability
+			global $wpdb;
 			$order_by_clean = array();
+			$table = $wpdb->prefix . 'bp_notifications';
+			$columns = $wpdb->get_col("DESC {$table}", 0);
 			foreach ( (array) $args['order_by'] as $key => $value ) {
-				if ( in_array( $value, self::$columns, true ) ) {
+				if ( in_array( $value, $columns, true ) ) {
 					$order_by_clean[] = $value;
 				}
 			}
