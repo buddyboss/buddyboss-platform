067262467879017b4841271a5d949eda94c06946
Chetan Satasiya <chetan@buddyboss.com>
2021-09-30 12:03:56 +0530

    #2857 - space

--- src/bp-forums/groups.php
+++ src/bp-forums/groups.php
@@ -256,18 +256,18 @@ if ( ! class_exists( 'BBP_Forums_Group_Extension' ) && class_exists( 'BP_Group_E
 		 *
 		 * @return array
 		 */
-		public function bbp_map_assign_topic_tags_caps( $caps, $cap, $user_id, $args ) {
+        public function bbp_map_assign_topic_tags_caps( $caps, $cap, $user_id, $args ) {
 
-            if ( 'assign_topic_tags' !== $cap ) {
-                return $caps;
-            }
+	        if ( 'assign_topic_tags' !== $cap ) {
+		        return $caps;
+	        }
 
-            if ( bbp_group_is_mod() || bbp_group_is_admin() ) {
-                return array( 'participate' );
-            }
+	        if ( bbp_group_is_mod() || bbp_group_is_admin() ) {
+		        return array( 'participate' );
+	        }
 
-			return $caps;
-		}
+	        return $caps;
+        }
 
 		/**
 		 * Remove the topic meta cap map, so it doesn't interfere with sidebars.
