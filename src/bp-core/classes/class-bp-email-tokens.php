524e55b83bf09d579992a30ae09da9b2ce56c025
kmsadrulula <kmsadrulula@gmail.com>
2021-10-27 15:34:17 +0600

    #934 - fixed wrong escaping functions use

--- src/bp-core/classes/class-bp-email-tokens.php
+++ src/bp-core/classes/class-bp-email-tokens.php
@@ -468,7 +468,7 @@ class BP_Email_Tokens {
 						<tbody>
 						<tr>
 							<td valign="middle" width="65px" style="vertical-align: middle;">
-								<a style="display: block; width: 47px;" href="<?php echo esc_attr( bp_core_get_user_domain( $activity->user_id ) ); ?>" target="_blank" rel="nofollow">
+								<a style="display: block; width: 47px;" href="<?php echo esc_url( bp_core_get_user_domain( $activity->user_id ) ); ?>" target="_blank" rel="nofollow">
 									<?php
 									$avatar_url = bp_core_fetch_avatar(
 										array(
@@ -480,7 +480,7 @@ class BP_Email_Tokens {
 										)
 									);
 									?>
-									<img alt="" src="<?php echo esc_attr( $avatar_url ); ?>" width="47" height="47" style="margin:0; padding:0; border:none; display:block; max-width: 47px; border-radius: 50%;" border="0" />
+									<img alt="" src="<?php echo esc_url( $avatar_url ); ?>" width="47" height="47" style="margin:0; padding:0; border:none; display:block; max-width: 47px; border-radius: 50%;" border="0" />
 								</a>
 							</td>
 							<td width="88%" style="vertical-align: middle;">
@@ -589,7 +589,7 @@ class BP_Email_Tokens {
 						<tbody>
 						<tr>
 							<td valign="middle" width="65px" style="vertical-align: middle;">
-								<a style="display: block; width: 47px;" href="<?php echo esc_attr( bp_core_get_user_domain( $activity_comment->user_id ) ); ?>" target="_blank" rel="nofollow"><?php
+								<a style="display: block; width: 47px;" href="<?php echo esc_url( bp_core_get_user_domain( $activity_comment->user_id ) ); ?>" target="_blank" rel="nofollow"><?php
 									$avatar_url = bp_core_fetch_avatar(
 										array(
 											'item_id' => $activity_comment->user_id,
@@ -600,7 +600,7 @@ class BP_Email_Tokens {
 										)
 									);
 									?>
-									<img alt="" src="<?php echo esc_attr( $avatar_url ); ?>" width="47" height="47" border="0" style="margin:0; padding:0; border:none; display:block; max-width: 47px; border-radius: 50%;" />
+									<img alt="" src="<?php echo esc_url( $avatar_url ); ?>" width="47" height="47" border="0" style="margin:0; padding:0; border:none; display:block; max-width: 47px; border-radius: 50%;" />
 								</a>
 							</td>
 							<td width="88%" style="vertical-align: middle;">
@@ -739,7 +739,7 @@ class BP_Email_Tokens {
 							<tbody>
 								<tr>
 									<td valign="middle" width="65px" style="vertical-align: middle;">
-										<a style="display: block; width: 47px;" href="<?php echo esc_attr( bp_core_get_user_domain( $this->_message_sender_id ) ); ?>" target="_blank" rel="nofollow"><?php
+										<a style="display: block; width: 47px;" href="<?php echo esc_url( bp_core_get_user_domain( $this->_message_sender_id ) ); ?>" target="_blank" rel="nofollow"><?php
 											$avatar_url = bp_core_fetch_avatar(
 												array(
 													'item_id' => $this->_message_sender_id,
@@ -750,12 +750,12 @@ class BP_Email_Tokens {
 												)
 											);
 											?>
-											<img alt="" src="<?php echo esc_attr( $avatar_url ); ?>" width="47" height="47" border="0" style="margin:0; padding:0; border:none; display:block; max-width: 47px; border-radius: 50%;" />
+											<img alt="" src="<?php echo esc_url( $avatar_url ); ?>" width="47" height="47" border="0" style="margin:0; padding:0; border:none; display:block; max-width: 47px; border-radius: 50%;" />
 										</a>
 									</td>
 									<td width="88%" style="vertical-align: middle;">
 										<div style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: <?php echo esc_attr( $settings['body_text_size'] . 'px' ); ?>; line-height: <?php echo esc_attr( $settings['body_text_size'] . 'px' ); ?>; letter-spacing: -0.24px;">
-											<a href="<?php echo esc_attr( bp_core_get_user_domain( $this->_message_sender_id ) ); ?>" target="_blank" rel="nofollow" style="color: <?php echo esc_attr( $settings['body_secondary_text_color'] ); ?> !important;"><?php echo $tokens['sender.name']; ?></a>
+											<a href="<?php echo esc_url( bp_core_get_user_domain( $this->_message_sender_id ) ); ?>" target="_blank" rel="nofollow" style="color: <?php echo esc_attr( $settings['body_secondary_text_color'] ); ?> !important;"><?php echo $tokens['sender.name']; ?></a>
 										</div>
 									</td>
 								</tr>
@@ -801,7 +801,7 @@ class BP_Email_Tokens {
 																?>
 																<div class="bb-activity-media-elem"  style="display: inline-block; max-width: 120px; vertical-align: top; max-height: 120px; overflow: hidden; padding: 4px 0;">
 																	<a href="<?php echo esc_attr( $tokens['message.url'] ); ?>">
-																		<img src="<?php echo esc_attr( wp_get_attachment_image_url( bp_get_media_attachment_id() ) ); ?>" alt="<?php echo esc_attr( bp_get_media_title() ); ?>" />
+																		<img src="<?php echo esc_url( wp_get_attachment_image_url( bp_get_media_attachment_id() ) ); ?>" alt="<?php echo esc_attr( bp_get_media_title() ); ?>" />
 																	</a>
 																</div>
 																<?php
