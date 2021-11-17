7eb7d2bde2e135a6ef3d93934cb097e4cd943000
Chetan Satasiya <chetan@buddyboss.com>
2021-11-17 18:11:34 +0530

    #3044 - phpcs

--- src/bp-templates/bp-nouveau/buddypress/media/albums.php
+++ src/bp-templates/bp-nouveau/buddypress/media/albums.php
@@ -39,31 +39,34 @@ if ( bp_has_albums( bp_ajax_querystring( 'albums' ) ) ) :
 		if ( empty( $_POST['page'] ) || 1 === (int) $_POST['page'] ) :
 			?>
 			<ul class="bb-albums-list">
-				<?php endif; ?>
-
 				<?php
-				while ( bp_album() ) :
-					bp_the_album();
-
-					bp_get_template_part( 'media/album-entry' );
-
-				endwhile;
-				?>
-
-				<?php if ( bp_album_has_more_items() ) : ?>
-
-					<li class="load-more">
-						<a class="button outline"
-						   href="<?php bp_album_has_more_items(); ?>"><?php esc_html_e( 'Load More', 'buddyboss' ); ?></a>
-					</li>
-
-				<?php endif; ?>
-
-				<?php if ( empty( $_POST['page'] ) || 1 === (int) $_POST['page'] ) : ?>
-			</ul>
-					<?php
 		endif;
-				?>
+
+
+		while ( bp_album() ) :
+			bp_the_album();
+
+			bp_get_template_part( 'media/album-entry' );
+
+		endwhile;
+
+		if ( bp_album_has_more_items() ) :
+			?>
+
+			<li class="load-more">
+				<a class="button outline"
+				   href="<?php bp_album_has_more_items(); ?>"><?php esc_html_e( 'Load More', 'buddyboss' ); ?></a>
+			</li>
+
+			<?php
+		endif;
+
+		if ( empty( $_POST['page'] ) || 1 === (int) $_POST['page'] ) :
+			?>
+			</ul>
+			<?php
+		endif;
+		?>
 	</div>
 	<?php
 
