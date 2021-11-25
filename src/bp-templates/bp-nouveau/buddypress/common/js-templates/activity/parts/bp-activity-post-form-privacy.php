6aabac8336b5fb3fba1040fd79104551cf459fbd
amincdev <amin@buddyboss.com>
2021-11-25 16:29:49 +0530

    #3189 - Replace privacy in select box with dropdown

--- src/bp-templates/bp-nouveau/buddypress/common/js-templates/activity/parts/bp-activity-post-form-privacy.php
+++ src/bp-templates/bp-nouveau/buddypress/common/js-templates/activity/parts/bp-activity-post-form-privacy.php
@@ -1,7 +1,10 @@
 <script type="text/html" id="tmpl-activity-post-form-privacy">
-    <select id="bp-activity-privacy" class="bp-activity-privacy" name="privacy">
+    <span class="bp-tooltip privacy-wrap" data-bp-tooltip-pos="up" data-bp-tooltip="Post in: Profile">
+        <span class="privacy profile"><i class="bb-icon-globe" style="font-size:20px;"></i></span>
+    </span>
+    <div id="bp-activity-privacy" class="bp-activity-privacy" name="privacy" style="display:none;">
 	    <?php foreach( bp_activity_get_visibility_levels() as $key => $privacy ) : ?>
-            <option value="<?php echo $key; ?>"><?php echo $privacy; ?></option>
+            <li data-value="<?php echo $key; ?>"><?php echo $privacy; ?></li>
 	    <?php endforeach; ?>
-    </select>
+    </div>
 </script>
