4554372fd79d22a745b95e9482f32d8c40792045
Chetan Satasiya <chetan@buddyboss.com>
2021-11-29 18:17:50 +0530

    API changes

--- src/bp-performance/classes/integrations/class-integration-abstract.php
+++ src/bp-performance/classes/integrations/class-integration-abstract.php
@@ -418,6 +418,7 @@ abstract class Integration_Abstract {
 	 */
 	public function endpoint_cache_render() {
 		if ( $this->api_cache_data ) {
+			$current_endpoint = $this->get_current_endpoint();
 
 			// Security Check.
 			// When the cache generated to user is not matched with it's being delivered to output error.
@@ -446,7 +447,7 @@ abstract class Integration_Abstract {
 				}
 			}
 
-			$this->api_cache_data = apply_filters( 'rest_post_dispatch_cache', $this->api_cache_data['data'] );
+			$this->api_cache_data = apply_filters( 'rest_post_dispatch_cache', $this->api_cache_data['data'], $current_endpoint );
 			echo wp_json_encode( $this->api_cache_data );
 			exit;
 		}
