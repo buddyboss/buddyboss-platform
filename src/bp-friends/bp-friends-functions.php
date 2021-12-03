fe5f1fd353e9253be53f3fbc2a16551ec8f91404
Chetan Satasiya <chetan@buddyboss.com>
2021-12-03 14:08:06 +0530

    #3203 - condition friendship accepted

--- src/bp-friends/bp-friends-functions.php
+++ src/bp-friends/bp-friends-functions.php
@@ -906,7 +906,7 @@ add_action( 'friends_friendship_requested', 'friends_notification_new_request', 
  * @param int $friend_id     ID of the request recipient.
  */
 function friends_notification_accepted_request( $friendship_id, $initiator_id, $friend_id ) {
-	if ( 'no' == bp_get_user_meta( (int) $initiator_id, 'notification_friends_friendship_accepted', true ) ) {
+	if ( false === bb_is_notification_enabled( (int) $initiator_id, 'notification_friends_friendship_accepted' ) ) {
 		return;
 	}
 
