2befc4976a0932209b5cae037c1c61aed15640f6
amincdev <amin@buddyboss.com>
2021-12-07 15:02:45 +0530

    #3189 - Activity Formatting option should be available even if media component is disabled

--- src/bp-templates/bp-nouveau/buddypress/common/js-templates/activity/parts/bp-editor-toolbar.php
+++ src/bp-templates/bp-nouveau/buddypress/common/js-templates/activity/parts/bp-editor-toolbar.php
@@ -1,11 +1,9 @@
 <script type="text/html" id="tmpl-editor-toolbar">
-	<?php if ( bp_is_active( 'media' ) ) : ?>
 		<div class="post-elements-buttons-item show-toolbar" data-bp-tooltip-pos="up-left" data-bp-tooltip="<?php esc_attr_e( 'Show formatting', 'buddyboss' ); ?>" data-bp-tooltip-hide="<?php esc_html_e( 'Hide formatting', 'buddyboss' ); ?>" data-bp-tooltip-show="<?php esc_attr_e( 'Show formatting', 'buddyboss' ); ?>">
 			<a href="#" id="show-toolbar-button" class="toolbar-button bp-tooltip">
 				<span class="bb-icon bb-icon-text-format"></span>
 			</a>
 		</div>
-	<?php endif; ?>
 	<div class="post-elements-buttons-item post-mention bp-tooltip" data-bp-tooltip-pos="up" data-bp-tooltip="<?php esc_attr_e( 'Mention Member', 'buddyboss' ); ?>">
 		<span class="toolbar-button">
 			<i class="bb-icon bb-icon-at-sign"></i>
