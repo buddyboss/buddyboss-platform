7b6fe640ad3e46b47d7228eaf1e0f1a7fc852e73
jitendrabanjara <jitendrabanjara@gmail.com>
2021-12-09 10:53:54 +0530

    #2324 - Revert code

--- src/bp-core/bp-core-options.php
+++ src/bp-core/bp-core-options.php
@@ -1806,11 +1806,23 @@ function bp_core_display_name_format( $default = 'first_name' ) {
  */
 function bp_rest_enable_private_network() {
 
-	if ( function_exists( 'bbapp_is_private_app_enabled' ) ) {
-		return (bool) bbapp_is_private_app_enabled();
-	} else {
-		$retval = (bool) bp_enable_private_rest_apis();
-	}
+	$retval = (
+		function_exists( 'bp_enable_private_network' ) &&
+		true !== bp_enable_private_network() &&
+		(
+			! function_exists( 'bbapp_is_private_app_enabled' ) ||
+			(
+				function_exists( 'bbapp_is_private_app_enabled' ) &&
+				true === bbapp_is_private_app_enabled() // Check for buddyboss app private network.
+			)
+		)
+	);
+
+//	if ( function_exists( 'bbapp_is_private_app_enabled' ) ) {
+//		return (bool) bbapp_is_private_app_enabled();
+//	} else {
+//		$retval = (bool) bp_enable_private_rest_apis();
+//	}
 
 	/**
 	 * Filters whether private private REST APIs is enabled.
