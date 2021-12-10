85648896948b0ee181f5b5c43dee67424d798f7d
annrra <andrey.raychev@buddyboss.com>
2021-12-10 15:20:50 +0200

    #3189 - Activity post user name wrapper

--- src/bp-templates/bp-nouveau/buddypress/common/js-templates/activity/parts/bp-activity-post-case-heading.php
+++ src/bp-templates/bp-nouveau/buddypress/common/js-templates/activity/parts/bp-activity-post-case-heading.php
@@ -1,7 +1,7 @@
 <script type="text/html" id="tmpl-activity-post-case-heading">
 	<# if ( data.display_avatar ) {  #>
 		<span class="activity-post-user-name-container">
-            <a class="activity-post-user-name" href="{{data.user_domain}}"><span class="user-name">{{data.user_display_name}}</span></a>
+            <h5><a class="activity-post-user-name" href="{{data.user_domain}}"><span class="user-name">{{data.user_display_name}}</span></a></h5>
         </span>
 	<# } #>
 </script>
