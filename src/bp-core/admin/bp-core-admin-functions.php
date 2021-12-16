61b0c4657f255ae1cd4c13b4b1ddfb942ae28f87
Chetan Satasiya <chetan@buddyboss.com>
2021-12-16 16:23:07 +0530

    #3203 - disable multi select in email situations

--- src/bp-core/admin/bp-core-admin-functions.php
+++ src/bp-core/admin/bp-core-admin-functions.php
@@ -3227,3 +3227,33 @@ function bp_core_get_moderation_admin_tabs( $active_tab = '' ) {
 	 */
 	return apply_filters( 'bp_core_get_moderation_admin_tabs', $tabs );
 }
+
+add_action( 'admin_head', 'bb_disable_multiple_select_situation', 99999 );
+
+/**
+ * Disable the multi select in situation.
+ *
+ * @return void
+ *
+ * @since BuddyBoss [BBVERSION]
+ */
+function bb_disable_multiple_select_situation() {
+
+	global $typenow;
+
+
+	if ( function_exists( 'bp_get_email_post_type' ) && bp_get_email_post_type() === $typenow ) {
+		?>
+        <script type="text/javascript">
+			jQuery( document ).ready( function ( $ ) {
+				jQuery('#taxonomy-<?php echo esc_js( bp_get_email_post_type() ); ?>-type input:checkbox').click(function(){
+					var group = "input:checkbox[name='"+jQuery(this).attr("name")+"']";
+					jQuery(group).attr("checked",false);
+					jQuery(this).attr("checked",true);
+				});
+			} );
+        </script>
+		<?php
+	}
+
+}
