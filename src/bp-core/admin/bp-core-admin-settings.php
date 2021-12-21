dee0731e34f8c673ee3e71de2e5e1649d008d1fc
jitendrabanjara <jitendrabanjara@gmail.com>
2021-12-21 18:43:48 +0530

    #2324 - If app disable then notice should not be display

--- src/bp-core/admin/bp-core-admin-settings.php
+++ src/bp-core/admin/bp-core-admin-settings.php
@@ -1625,18 +1625,20 @@ function bb_admin_setting_callback_private_rest_apis() {
 			)
 		)
 	);
-	printf(
-		'<div class="bp-feedback info"><span class="bp-icon" aria-hidden="true"></span><p>%s</p></div>',
-		sprintf(
-			__( 'BuddyBoss App is enabled. Please go to <a href="%s">settings</a> here to restrict App and REST APIs access to logged-in members only.', 'buddyboss' ),
-			add_query_arg(
-				array(
-					'page' => 'bbapp-settings',
-				),
-				admin_url( 'admin.php' )
+	if ( function_exists( 'bbapp_is_private_app_enabled' ) ) {
+		printf(
+			'<div class="bp-feedback info"><span class="bp-icon" aria-hidden="true"></span><p>%s</p></div>',
+			sprintf(
+				__( 'BuddyBoss App is enabled. Please go to <a href="%s">settings</a> here to restrict App and REST APIs access to logged-in members only.', 'buddyboss' ),
+				add_query_arg(
+					array(
+						'page' => 'bbapp-settings',
+					),
+					admin_url( 'admin.php' )
+				)
 			)
-		)
-	);
+		);
+	}
 }
 
 /**
