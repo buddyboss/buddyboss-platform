76b63ddccfb4ee12fd0d4ccf2aeaf0241ea18e4e
meetmakadiya <meet@buddyboss.com>
2021-12-30 16:46:08 +0530

    #3265-fix-Extra character when the discussion subscribe button was toggle.

--- src/bp-forums/users/template.php
+++ src/bp-forums/users/template.php
@@ -1092,7 +1092,7 @@ function bbp_get_user_subscribe_link( $args = '', $user_id = 0, $wrap = true ) {
 			'user_id'     => 0,
 			'topic_id'    => 0,
 			'forum_id'    => 0,
-			'before'      => '&nbsp;|&nbsp;',
+			'before'      => '',
 			'after'       => '',
 		),
 		'get_user_subscribe_link'
