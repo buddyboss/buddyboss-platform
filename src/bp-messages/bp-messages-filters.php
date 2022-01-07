5275b48bc9e6cd097f87a78c5c0cf73397d6af03
Kartik Suthar <kartik@buddyboss.com>
2022-01-07 12:35:35 +0530

    PROD-674 - notification message changes

--- src/bp-messages/bp-messages-filters.php
+++ src/bp-messages/bp-messages-filters.php
@@ -844,59 +844,60 @@ function bb_load_messages_notifications() {
  */
 function messages_screen_notification_settings() {
 
-	if ( ! bb_enabled_legacy_email_preference() ) {
-		bb_render_notification( buddypress()->messages->id );
-	} else {
-		if ( bp_action_variables() ) {
-			bp_do_404();
-			return;
-		}
+	if ( bp_action_variables() ) {
+		bp_do_404();
+		return;
+	}
 
-		if ( ! $new_messages = bp_get_user_meta( bp_displayed_user_id(), 'notification_messages_new_message', true ) ) {
-			$new_messages = 'yes';
-		} ?>
+	// Bail out if legacy method not enabled.
+	if ( false === bb_enabled_legacy_email_preference() ) {
+		return;
+	}
 
-		<table class="notification-settings" id="messages-notification-settings">
-			<thead>
-			<tr>
-				<th class="icon"></th>
-				<th class="title"><?php esc_html_e( 'Messages', 'buddyboss' ); ?></th>
-				<th class="yes"><?php esc_html_e( 'Yes', 'buddyboss' ); ?></th>
-				<th class="no"><?php esc_html_e( 'No', 'buddyboss' ); ?></th>
-			</tr>
-			</thead>
+	if ( ! $new_messages = bp_get_user_meta( bp_displayed_user_id(), 'notification_messages_new_message', true ) ) {
+		$new_messages = 'yes';
+	} ?>
 
-			<tbody>
-			<tr id="messages-notification-settings-new-message">
-				<td></td>
-				<td><?php esc_html_e( 'A member sends you a new message', 'buddyboss' ); ?></td>
-				<td class="yes">
-					<div class="bp-radio-wrap">
-						<input type="radio" name="notifications[notification_messages_new_message]" id="notification-messages-new-messages-yes" class="bs-styled-radio" value="yes" <?php checked( $new_messages, 'yes', true ); ?> />
-						<label for="notification-messages-new-messages-yes"><span class="bp-screen-reader-text"><?php esc_html_e( 'Yes, send email', 'buddyboss' ); ?></span></label>
-					</div>
-				</td>
-				<td class="no">
-					<div class="bp-radio-wrap">
-						<input type="radio" name="notifications[notification_messages_new_message]" id="notification-messages-new-messages-no" class="bs-styled-radio" value="no" <?php checked( $new_messages, 'no', true ); ?> />
-						<label for="notification-messages-new-messages-no"><span class="bp-screen-reader-text"><?php esc_html_e( 'No, do not send email', 'buddyboss' ); ?></span></label>
-					</div>
-				</td>
-			</tr>
+	<table class="notification-settings" id="messages-notification-settings">
+		<thead>
+		<tr>
+			<th class="icon"></th>
+			<th class="title"><?php esc_html_e( 'Messages', 'buddyboss' ); ?></th>
+			<th class="yes"><?php esc_html_e( 'Yes', 'buddyboss' ); ?></th>
+			<th class="no"><?php esc_html_e( 'No', 'buddyboss' ); ?></th>
+		</tr>
+		</thead>
 
-			<?php
-
-			/**
-			 * Fires inside the closing </tbody> tag for messages screen notification settings.
-			 *
-			 * @since BuddyPress 1.0.0
-			 */
-			do_action( 'messages_screen_notification_settings' );
-			?>
-			</tbody>
-		</table>
+		<tbody>
+		<tr id="messages-notification-settings-new-message">
+			<td></td>
+			<td><?php esc_html_e( 'A member sends you a new message', 'buddyboss' ); ?></td>
+			<td class="yes">
+				<div class="bp-radio-wrap">
+					<input type="radio" name="notifications[notification_messages_new_message]" id="notification-messages-new-messages-yes" class="bs-styled-radio" value="yes" <?php checked( $new_messages, 'yes', true ); ?> />
+					<label for="notification-messages-new-messages-yes"><span class="bp-screen-reader-text"><?php esc_html_e( 'Yes, send email', 'buddyboss' ); ?></span></label>
+				</div>
+			</td>
+			<td class="no">
+				<div class="bp-radio-wrap">
+					<input type="radio" name="notifications[notification_messages_new_message]" id="notification-messages-new-messages-no" class="bs-styled-radio" value="no" <?php checked( $new_messages, 'no', true ); ?> />
+					<label for="notification-messages-new-messages-no"><span class="bp-screen-reader-text"><?php esc_html_e( 'No, do not send email', 'buddyboss' ); ?></span></label>
+				</div>
+			</td>
+		</tr>
 
 		<?php
-	}
+
+		/**
+		 * Fires inside the closing </tbody> tag for messages screen notification settings.
+		 *
+		 * @since BuddyPress 1.0.0
+		 */
+		do_action( 'messages_screen_notification_settings' );
+		?>
+		</tbody>
+	</table>
+
+	<?php
 
 }
