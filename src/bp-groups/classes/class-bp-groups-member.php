380d91eb44f8ed8a780066c0e401b84ea6e648fe
Kartik Suthar <kartik@buddyboss.com>
2022-01-25 14:36:15 +0530

    PROD-1227 - updated logic for cache

--- src/bp-groups/classes/class-bp-groups-member.php
+++ src/bp-groups/classes/class-bp-groups-member.php
@@ -557,10 +557,11 @@ class BP_Groups_Member {
 		static $cache = array();
 
 		$pag_sql = '';
-		$cache_key = 'bb_group_ids_for_user' . $user_id;
+
+		$cache_key = 'bp_group_ids_for_user_' . $user_id . '_' . bp_loggedin_user_id();
 		if ( ! empty( $limit ) && ! empty( $page ) ) {
 			$pag_sql = $wpdb->prepare( ' LIMIT %d, %d', intval( ( $page - 1 ) * $limit ), intval( $limit ) );
-			$cache_key = 'bb_group_ids_for_user' . $user_id . '_' . $limit . '_' . $page;
+			$cache_key = 'bp_group_ids_for_user_' . $user_id . '_' . bp_loggedin_user_id() . '_' . $limit . '_' . $page;
 		}
 
 		$bp = buddypress();
