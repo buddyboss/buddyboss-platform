cf286c8df2cad57c5d07d675dc4454e5b3daeb50
meetmakadiya <meet@buddyboss.com>
2022-02-04 15:10:25 +0530

    #PROD-778-fix the groups landing page and the mygroups title

--- src/bp-core/compatibility/bp-rankmath-plugin-helpers.php
+++ src/bp-core/compatibility/bp-rankmath-plugin-helpers.php
@@ -30,11 +30,17 @@ class BP_RankMath_Title implements IPaper {
 			}
 		} else {
 			$action = bp_current_action();
-			$title  = isset( buddypress()->groups->current_group->name ) ? buddypress()->groups->current_group->name : __( 'Social Group', 'buddyboss' );
+			var_dump($action);
+			$title  = isset( buddypress()->groups->current_group->name ) ? buddypress()->groups->current_group->name : esc_html__( 'Social Group', 'buddyboss' );
 			if( 'admin' === $action ) {
-				$action = __( 'Manage', 'buddyboss' );
+				$action = esc_html__( 'Manage', 'buddyboss' );
 			}
-			$title  = ucfirst( $action ) . ' - ' . $title;
+			if( 'my-groups' === $action ) {
+				$action = esc_html__( 'Groups', 'buddyboss' );
+			}
+			if( isset( $action ) && !empty( $action ) ) {
+				$title = ucfirst( $action ) . ' - ' . $title;
+			}
 		}
 
 		return $title . ' - ' . bp_get_site_name();
@@ -96,7 +102,13 @@ function bp_helper_rankmath_group_page_support( $title ) {
 		return;
 	}
 
-	if ( bp_is_active( 'groups' ) && ( ! empty( buddypress()->groups->current_group ) || bp_is_current_component( 'groups' ) ) ) {
+	if (
+		bp_is_active( 'groups' ) &&
+		(
+			! empty( buddypress()->groups->current_group ) ||
+			bp_is_current_component( 'groups' )
+		)
+	) {
 		$group_page = new BP_RankMath_Title();
 		$title      = $group_page->title();
 	}
