b0f87d0ce59cf30f4ab1f1f0c10da6e651bbe513
Krunal Palaniya <krunal@buddyboss.com>
2022-02-07 12:54:47 +0530

    PROD-1899 - Added member link on group directory

--- src/bp-groups/bp-groups-functions.php
+++ src/bp-groups/bp-groups-functions.php
@@ -4546,8 +4546,10 @@ function bb_groups_loop_members( $group_id = 0, $role = array( 'member', 'mod', 
 			);
 			?>
 			<span class="bs-group-member" data-bp-tooltip-pos="up-left" data-bp-tooltip="<?php echo esc_attr( bp_core_get_user_displayname( $member->ID ) ); ?>">
-				<img src="<?php echo $avatar; ?>"
-					alt="<?php echo esc_attr( bp_core_get_user_displayname( $member->ID ) ); ?>" class="round" />
+				<a href="<?php echo esc_url( bp_core_get_user_domain( $member->ID ) ); ?>">
+					<img src="<?php echo $avatar; ?>"
+						alt="<?php echo esc_attr( bp_core_get_user_displayname( $member->ID ) ); ?>" class="round" />
+				</a>
 			</span>
 			<?php
 		}
