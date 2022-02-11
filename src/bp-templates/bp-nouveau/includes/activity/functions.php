4e9a7ecf63bff885d9187c1094bc1af0c0815da6
jitendrabanjara <jitendrabanjara@gmail.com>
2022-02-11 18:11:53 +0530

    PROD-2041 - Remove unwanted code which addded for debuggin

--- src/bp-templates/bp-nouveau/includes/activity/functions.php
+++ src/bp-templates/bp-nouveau/includes/activity/functions.php
@@ -32,7 +32,7 @@ function bp_nouveau_activity_register_scripts( $scripts = array() ) {
 				'footer'       => true,
 			),
 			'bp-nouveau-activity-post-form' => array(
-				'file'         => 'js/buddypress-activity-post-form.js', //%s
+				'file'         => 'js/buddypress-activity-post-form%s.js',
 				'dependencies' => array( 'bp-nouveau', 'bp-nouveau-activity', 'json2', 'wp-backbone' ),
 				'footer'       => true,
 			),
