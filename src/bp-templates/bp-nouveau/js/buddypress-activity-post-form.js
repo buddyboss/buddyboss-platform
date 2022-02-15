3a974d28b31800dd6fddced9cff3be3cc4ded088
jitendrabanjara <jitendrabanjara@gmail.com>
2022-02-15 10:24:38 +0530

    PROD-2041 Debug code to improve post form popup

--- src/bp-templates/bp-nouveau/js/buddypress-activity-post-form.js
+++ src/bp-templates/bp-nouveau/js/buddypress-activity-post-form.js
@@ -3965,42 +3965,42 @@ window.bp = window.bp || {};
 				console.log('3');
 				console.log(new Date().getTime());
 				
-				if ( ! _.isUndefined( BP_Nouveau.media ) &&
-					! _.isUndefined( BP_Nouveau.media.emoji ) &&
-					(
-						( ! _.isUndefined( BP_Nouveau.media.emoji.profile ) && BP_Nouveau.media.emoji.profile ) ||
-						( ! _.isUndefined( BP_Nouveau.media.emoji.groups ) && BP_Nouveau.media.emoji.groups )
-					)
-				) {
-
-					$( '#whats-new' ).emojioneArea(
-						{
-							standalone: true,
-							hideSource: false,
-							container: '#editor-toolbar > .post-emoji',
-							autocomplete: false,
-							pickerPosition: 'bottom',
-							hidePickerOnBlur: true,
-							useInternalCDN: false,
-							events: {
-								emojibtn_click: function () {
-									$( '#whats-new' )[ 0 ].emojioneArea.hidePicker();
-									if (window.getSelection && document.createRange) { //Get caret position when user adds emoji
-										var sel = window.getSelection && window.getSelection();
-										if (sel && sel.rangeCount > 0) {
-											window.activityCaretPosition = sel.getRangeAt(0);
-										}
-									} else {
-										window.activityCaretPosition = document.selection.createRange();
-									}
-
-									// Enable post submit button
-									$( '#whats-new-form' ).removeClass( 'focus-in--empty' );
-								},
-							}
-						}
-					);
-				}
+				// if ( ! _.isUndefined( BP_Nouveau.media ) &&
+				// 	! _.isUndefined( BP_Nouveau.media.emoji ) &&
+				// 	(
+				// 		( ! _.isUndefined( BP_Nouveau.media.emoji.profile ) && BP_Nouveau.media.emoji.profile ) ||
+				// 		( ! _.isUndefined( BP_Nouveau.media.emoji.groups ) && BP_Nouveau.media.emoji.groups )
+				// 	)
+				// ) {
+				//
+				// 	$( '#whats-new' ).emojioneArea(
+				// 		{
+				// 			standalone: true,
+				// 			hideSource: false,
+				// 			container: '#editor-toolbar > .post-emoji',
+				// 			autocomplete: false,
+				// 			pickerPosition: 'bottom',
+				// 			hidePickerOnBlur: true,
+				// 			useInternalCDN: false,
+				// 			events: {
+				// 				emojibtn_click: function () {
+				// 					$( '#whats-new' )[ 0 ].emojioneArea.hidePicker();
+				// 					if (window.getSelection && document.createRange) { //Get caret position when user adds emoji
+				// 						var sel = window.getSelection && window.getSelection();
+				// 						if (sel && sel.rangeCount > 0) {
+				// 							window.activityCaretPosition = sel.getRangeAt(0);
+				// 						}
+				// 					} else {
+				// 						window.activityCaretPosition = document.selection.createRange();
+				// 					}
+				//
+				// 					// Enable post submit button
+				// 					$( '#whats-new-form' ).removeClass( 'focus-in--empty' );
+				// 				},
+				// 			}
+				// 		}
+				// 	);
+				// }
 				console.log('4');
 				console.log(new Date().getTime());
 				// Wrap Toolbar and submit Wrapper into footer.
