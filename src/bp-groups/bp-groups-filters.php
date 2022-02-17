ba75cdf875f5f0de74c461bf50c7d8c47c30f09a
Krunal Palaniya <krunal@buddyboss.com>
2022-02-17 15:32:15 +0530

    PROD-1920 - Remove filter to allow class in group excerpt

--- src/bp-groups/bp-groups-filters.php
+++ src/bp-groups/bp-groups-filters.php
@@ -34,7 +34,6 @@ add_filter( 'bp_get_group_description_excerpt', 'make_clickable', 9 );
 add_filter( 'bp_get_group_name', 'wp_filter_kses', 1 );
 add_filter( 'bp_get_group_permalink', 'wp_filter_kses', 1 );
 add_filter( 'bp_get_group_description', 'bp_groups_filter_kses', 1 );
-add_filter( 'bp_get_group_description_excerpt', 'wp_filter_kses', 1 );
 add_filter( 'groups_group_name_before_save', 'wp_filter_kses', 1 );
 add_filter( 'groups_group_description_before_save', 'bp_groups_filter_kses', 1 );
 
