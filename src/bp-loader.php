9b4b1cd87274ae8e5cbdefd6c090dd2a7fb91128
jitendrabanjara <jitendrabanjara@gmail.com>
2022-02-17 19:19:48 +0530

    PROD-2041 - update code

--- src/bp-loader.php
+++ src/bp-loader.php
@@ -24,7 +24,7 @@ if ( ! defined( 'BP_SOURCE_SUBDIRECTORY' ) && file_exists( dirname( __FILE__ ) .
 }
 
 if ( ! defined( 'BP_PLATFORM_VERSION' ) ) {
-	define( 'BP_PLATFORM_VERSION', '1.8.7.54' );
+	define( 'BP_PLATFORM_VERSION', '1.8.7.55' );
 }
 
 if ( ! defined( 'BP_PLATFORM_API' ) ) {
