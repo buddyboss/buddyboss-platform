31934ce3525166c8bb57ca35482587d2c287d582
Michael Eisenwasser <michael@buddyboss.com>
2022-02-21 13:58:45 -0800

    PROD-1988 - changing link for BB Platform Pro to a URL that is currently live

--- src/bp-core/admin/bp-core-admin-functions.php
+++ src/bp-core/admin/bp-core-admin-functions.php
@@ -3338,7 +3338,7 @@ function bb_get_pro_label_notice() {
 		);
 	} else {
 		$bb_pro_notice = sprintf(
-			'<br/><span class="bb-head-notice"> %1$s <a target="_blank" href="https://www.buddyboss.com/pro">%2$s</a> %3$s</span>',
+			'<br/><span class="bb-head-notice"> %1$s <a target="_blank" href="https://www.buddyboss.com/platform/">%2$s</a> %3$s</span>',
 			esc_html__( 'Install', 'buddyboss' ),
 			esc_html__( 'BuddyBoss Platform Pro', 'buddyboss' ),
 			esc_html__( 'to unlock', 'buddyboss' ),
