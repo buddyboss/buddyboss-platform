bc6df18e84ce8ed42f12fb2476cd91b801a16eb0
Krunal Palaniya <krunal@buddyboss.com>
2022-02-23 11:46:08 +0530

    PROD-1988 - Fixed phpcs and phpcbf

--- src/bp-core/classes/class-bp-admin-tab.php
+++ src/bp-core/classes/class-bp-admin-tab.php
@@ -544,14 +544,14 @@ if ( ! class_exists( 'BP_Admin_Tab' ) ) :
 				echo "<div id='{$section['id']}' class='bp-admin-card section-{$section['id']}'>";
 				$has_tutorial_btn = ( isset( $section['tutorial_callback'] ) && ! empty( $section['tutorial_callback'] ) ) ? 'has_tutorial_btn' : '';
 				if ( $section['title'] ) {
-					echo '<h2 class=' . $has_tutorial_btn . ">{$section['title']}";
+					echo '<h2 class=' . esc_attr( $has_tutorial_btn ) . '>' . esc_attr( $section['title'] );
 					if ( isset( $section['tutorial_callback'] ) && ! empty( $section['tutorial_callback'] ) ) {
 						?>
-							 <div class="bbapp-tutorial-btn">
+							<div class="bbapp-tutorial-btn">
 							<?php
 							call_user_func( $section['tutorial_callback'], $section );
 							?>
-							 </div>
+							</div>
 							<?php
 					}
 					echo "</h2>\n";
