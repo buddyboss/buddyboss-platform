e7d33cb15c8016e32ed1c42548b0395ee3c114cb
Kartik Suthar <kartik@buddyboss.com>
2022-03-02 10:34:04 +0530

    PROD-1988 - phpcbf fixes

--- src/bp-templates/bp-nouveau/buddypress/groups/single/parts/header-item-actions.php
+++ src/bp-templates/bp-nouveau/buddypress/groups/single/parts/header-item-actions.php
@@ -15,20 +15,26 @@
 		<?php bp_group_list_parents(); ?>
 	<?php endif; ?>
 
-	<?php if ( bp_current_user_can( 'groups_access_group' ) ) : ?>
+	<?php
+	if ( bp_current_user_can( 'groups_access_group' ) ) :
+		if ( bb_platform_group_headers_element_enable( 'group-organizers' ) ) :
+			?>
+			<h2 class="bp-screen-reader-text">
+				<?php
+				/* translators: Group %s */
+				printf( esc_html__( 'Group %s', 'buddyboss' ), esc_attr( get_group_role_label( bp_get_current_group_id(), 'organizer_plural_label_name' ) ) );
+				?>
+			</h2>
 
-		<?php if ( bb_platform_group_headers_element_enable( 'group-organizers' ) ) : ?>
-			<?php /* translators: Group %s */ ?>
-		<h2 class="bp-screen-reader-text"><?php printf( esc_html__( 'Group %s', 'buddyboss' ), esc_attr( get_group_role_label( bp_get_current_group_id(), 'organizer_plural_label_name' ) ) ); ?></h2>
-
-		<dl class="moderators-lists">
-			<dt class="moderators-title"><?php esc_html_e( 'Organized by', 'buddyboss' ); ?></dt>
-			<dd class="user-list admins"><?php bp_group_list_admins(); ?>
-				<?php bp_nouveau_group_hook( 'after', 'menu_admins' ); ?>
-			</dd>
-		</dl>
-		<?php endif; ?>
-
-	<?php endif; ?>
+			<dl class="moderators-lists">
+				<dt class="moderators-title"><?php esc_html_e( 'Organized by', 'buddyboss' ); ?></dt>
+				<dd class="user-list admins"><?php bp_group_list_admins(); ?>
+					<?php bp_nouveau_group_hook( 'after', 'menu_admins' ); ?>
+				</dd>
+			</dl>
+			<?php
+		endif;
+	endif;
+	?>
 
 </div><!-- .item-actions -->
