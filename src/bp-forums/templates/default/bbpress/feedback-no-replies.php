63aaaa051883ed5118b7270bf8347710398eb456
Michael Eisenwasser <michael@buddyboss.com>
2022-03-08 05:47:02 -0800

    PROD-2229 - updating text for “Sorry, there were no replies found.”
    "

--- src/bp-forums/templates/default/bbpress/feedback-no-replies.php
+++ src/bp-forums/templates/default/bbpress/feedback-no-replies.php
@@ -10,5 +10,5 @@
 
 <div class="bp-feedback info">
 	<span class="bp-icon" aria-hidden="true"></span>
-	<p><?php _e( 'No replies added here yet.', 'buddyboss' ); ?></p>
+	<p><?php _e( 'Sorry, there were no replies found.', 'buddyboss' ); ?></p>
 </div>
