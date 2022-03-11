0491bfc452e1c7339e8e1734f7806a79a6e2b0f1
Kartik Suthar <kartik@buddyboss.com>
2022-03-11 18:58:38 +0530

    PROD-2229 - revert the changes

--- src/bp-forums/templates/default/js/topic.js
+++ src/bp-forums/templates/default/js/topic.js
@@ -1,39 +1,28 @@
 jQuery( document ).ready(
 	function ( $ ) {
 
-		var xhr_favorite;
-		var xhr_subscription;
-
 		function bbp_ajax_call( action, topic_id, nonce, update_selector ) {
-			  var $data = {
-					action : action,
-					id     : topic_id,
-					nonce  : nonce
+			var $data = {
+				action : action,
+				id     : topic_id,
+				nonce  : nonce
 			};
 
 			/*globals bbpTopicJS:false */
-			$bbp_xhr = $.post(
+			$.post(
 				bbpTopicJS.bbp_ajaxurl,
 				$data,
 				function ( response ) {
 					if ( response.success ) {
-						 $( update_selector ).html( response.content );
+						$( update_selector ).html( response.content );
 					} else {
 						if ( ! response.content ) {
 							response.content = bbpTopicJS.generic_ajax_error;
 						}
 						alert( response.content );
 					}
-
-					if ( action === 'favorite' ) {
-						xhr_favorite = false;
-					} else if ( action === 'subscribe' ) {
-						xhr_subscription = false;
-					}
 				}
 			);
-
-			return $bbp_xhr;
 		}
 
 		$( '#favorite-toggle' ).on(
@@ -41,11 +30,7 @@ jQuery( document ).ready(
 			'span a.favorite-toggle',
 			function( e ) {
 				e.preventDefault();
-				if ( xhr_favorite ) {
-					return;
-				}
-
-				xhr_favorite = bbp_ajax_call( 'favorite', $( this ).attr( 'data-topic' ), bbpTopicJS.fav_nonce, '#favorite-toggle' );
+				bbp_ajax_call( 'favorite', $( this ).attr( 'data-topic' ), bbpTopicJS.fav_nonce, '#favorite-toggle' );
 			}
 		);
 
@@ -54,10 +39,7 @@ jQuery( document ).ready(
 			'span a.subscription-toggle',
 			function( e ) {
 				e.preventDefault();
-				if ( xhr_subscription ) {
-					return;
-				}
-				xhr_subscription = bbp_ajax_call( 'subscription', $( this ).attr( 'data-topic' ), bbpTopicJS.subs_nonce, '#subscription-toggle' );
+				bbp_ajax_call( 'subscription', $( this ).attr( 'data-topic' ), bbpTopicJS.subs_nonce, '#subscription-toggle' );
 			}
 		);
 	}
