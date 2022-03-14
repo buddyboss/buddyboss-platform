af69a28c4557ac4073188d6cee0bc27047d0f44f
Kartik Suthar <kartik@buddyboss.com>
2022-03-14 17:53:42 +0530

    PROD-675 - fix the edit template url when have only one email and fix the label

--- src/bp-notifications/bp-notifications-settings.php
+++ src/bp-notifications/bp-notifications-settings.php
@@ -234,6 +234,7 @@ function bb_admin_setting_callback_on_automatic_notification_fields() {
 									<?php
 									$registered_emails = bb_register_notification_email_templates( $field['key'] );
 									$total_email_count = 0;
+
 									if ( ! empty( $registered_emails ) ) {
 										foreach ( $registered_emails as $email_type ) {
 											$total_email_count += get_terms(
@@ -251,16 +252,24 @@ function bb_admin_setting_callback_on_automatic_notification_fields() {
 											<a class="no-email-info" href="<?php echo esc_url( $email_url ); ?>"><?php esc_html_e( 'Missing Email Template', 'buddyboss' ); ?></a>
 										<?php
 									} elseif ( ! empty( $registered_emails ) ) {
-										$url = ( count( $registered_emails ) === 1 ) ?
-											bp_get_admin_url(
-												add_query_arg(
+										$posts = get_posts(
+											array(
+												'showposts' => 1,
+												'post_type' => bp_get_email_post_type(),
+												'tax_query' => array(
 													array(
-														'post_type' => bp_get_email_post_type(),
 														'taxonomy' => bp_get_email_tax_type(),
-														'term' => current( $registered_emails ),
+														'field' => 'slug',
+														'terms' => $registered_emails,
 													),
-													'edit.php'
-												)
+												),
+												'fields' => 'ids',
+											)
+										);
+
+										$url = ( count( $registered_emails ) === 1 && ! empty( $posts ) ) ?
+											get_edit_post_link(
+												current( $posts )
 											) : add_query_arg(
 												array(
 													'post_type' => bp_get_email_post_type(),
