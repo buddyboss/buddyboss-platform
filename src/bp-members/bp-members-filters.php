8ac2dac3a4a4f01b0a087b72b0255fb02739e5bd
Yudhisthir Nahar <yudhisthir@buddyboss.com>
2022-03-14 16:37:29 +0530

    #PROD-2347 For default suggessions also used the bp_activity_get_user_mentionname function for the mention name
    PROD-2347 #comment For default suggessions also used the bp_activity_get_user_mentionname function for the mention name in the bb_core_prime_mentions_results function

--- src/bp-members/bp-members-filters.php
+++ src/bp-members/bp-members-filters.php
@@ -569,7 +569,7 @@ function bb_core_prime_mentions_results() {
 
 	foreach ( $members_query->results as $user ) {
 		$result        = new stdClass();
-		$result->ID    = $user->user_nicename;
+		$result->ID    = bp_activity_get_user_mentionname( $user->ID );
 		$result->image = bp_core_fetch_avatar(
 			array(
 				'html'    => false,
@@ -582,6 +582,7 @@ function bb_core_prime_mentions_results() {
 		} else {
 			$result->name = bp_core_get_user_displayname( $user->ID );
 		}
+		$result->user_id = $user->ID;
 
 		$members[] = $result;
 	}
