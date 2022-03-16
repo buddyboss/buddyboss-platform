b2dd429f525aa581c612a4e9b2da7a0ec4df2648
Chetan Satasiya <chetan@buddyboss.com>
2022-03-16 20:06:04 +0530

    [PROD-1932] topic changes

--- src/bp-templates/bp-nouveau/buddypress/search/loop/topic.php
+++ src/bp-templates/bp-nouveau/buddypress/search/loop/topic.php
@@ -16,7 +16,7 @@ $total    = bbp_get_topic_reply_count( $topic_id ) ?>
 		<div class="item-avatar">
 			<a href="<?php bbp_topic_permalink( $topic_id ); ?>">
 				<img
-					src="<?php echo bbp_get_forum_thumbnail_src( bbp_get_forum_id( $topic_id ) ) ?: bp_search_get_post_thumbnail_default( get_post_type() ); ?>"
+					src="<?php echo esc_url( bbp_get_forum_thumbnail_src( bbp_get_forum_id( $topic_id ) ) ?: bp_search_get_post_thumbnail_default( get_post_type() ) ); ?>"
 					class="avatar forum-avatar"
 					height="150"
 					width="150"
@@ -30,7 +30,7 @@ $total    = bbp_get_topic_reply_count( $topic_id ) ?>
 				<a href="<?php bbp_topic_permalink( $topic_id ); ?>"><?php bbp_topic_title( $topic_id ); ?></a>
 			</h3>
 			<div class="entry-content entry-summary">
-				<?php echo wp_trim_words( bbp_get_topic_content( $topic_id ), 30, '...' ); ?>
+				<?php echo wp_kses_post( wp_trim_words( bbp_get_topic_content( $topic_id ), 30, '...' ) ); ?>
 			</div>
 			<div class="entry-meta">
 				<span class="reply-count">
@@ -41,38 +41,6 @@ $total    = bbp_get_topic_reply_count( $topic_id ) ?>
 					<?php bbp_topic_freshness_link( $topic_id ); ?>
 				</span>
 			</div>
-			<?php
-			$discussion_tags = get_the_terms( $topic_id, bbpress()->topic_tag_tax_id );
-
-			if ( ! empty( $discussion_tags ) ) {
-				?>
-				<div class="item-tags">
-					<span class="item-tag-cap">
-						<?php
-						esc_html_e( 'Tags:', 'buddyboss' );
-						?>
-					</span>
-					<?php
-					$tags_count = ( is_array( $discussion_tags ) || is_object( $discussion_tags ) ) ? count( $discussion_tags ) : 0;
-					$loop_count = 1;
-					foreach ( $discussion_tags as $key => $discussion_tag ) {
-						?>
-						<span class="discussion-tag">
-							<?php
-							echo esc_html( $discussion_tag->name );
-							if ( $tags_count != $loop_count ) {
-								echo ', ';
-							}
-							?>
-						</span>
-						<?php
-						$loop_count++;
-					}
-					?>
-				</div>
-				<?php
-			}
-			?>
 		</div>
 	</div>
 </li>
