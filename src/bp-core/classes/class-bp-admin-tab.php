700fdc056dc0e1cea5edf96712568fa85d34e37e
jitendrabanjara <jitendrabanjara@gmail.com>
2022-03-18 12:34:14 +0530

    PROD-2311 - use esc_html__ instead of __

--- src/bp-core/classes/class-bp-admin-tab.php
+++ src/bp-core/classes/class-bp-admin-tab.php
@@ -111,19 +111,19 @@ if ( ! class_exists( 'BP_Admin_Tab' ) ) :
 
 			$localize_arg = array(
 				'ajax_url'            => admin_url( 'admin-ajax.php' ),
-				'select_document'     => esc_js( __( 'Please upload a file to check the MIME Type.', 'buddyboss' ) ),
+				'select_document'     => esc_js( esc_html__( 'Please upload a file to check the MIME Type.', 'buddyboss' ) ),
 				'tools'               => array(
 					'default_data'  => array(
-						'submit_button_message' => esc_js( __( 'Are you sure you want to import data? This action is going to alter your database. If this is a live website you may want to create a backup of your database first.', 'buddyboss' ) ),
-						'clear_button_message'  => esc_js( __( 'Are you sure you want to delete all Default Data content? Content that was created by you and others, and not by this default data installer, will not be deleted.', 'buddyboss' ) ),
+						'submit_button_message' => esc_js( esc_html__( 'Are you sure you want to import data? This action is going to alter your database. If this is a live website you may want to create a backup of your database first.', 'buddyboss' ) ),
+						'clear_button_message'  => esc_js( esc_html__( 'Are you sure you want to delete all Default Data content? Content that was created by you and others, and not by this default data installer, will not be deleted.', 'buddyboss' ) ),
 					),
 					'repair_forums' => array(
 						'validate_site_id_message' => esc_html__( 'Select site to repair the forums', 'buddyboss' ),
 					),
 				),
 				'moderation'          => array(
-					'suspend_confirm_message'   => esc_js( __( 'Please confirm you want to suspend this member. Members who are suspended will be logged out and not allowed to login again. Their content will be hidden from all members in your network. Please allow a few minutes for this process to complete.', 'buddyboss' ) ),
-					'unsuspend_confirm_message' => esc_js( __( 'Please confirm you want to unsuspend this member. Members who are unsuspended will be allowed to login again, and their content will no longer be hidden from other members in your network. Please allow a few minutes for this process to complete.', 'buddyboss' ) ),
+					'suspend_confirm_message'   => esc_js( esc_html__( 'Please confirm you want to suspend this member. Members who are suspended will be logged out and not allowed to login again. Their content will be hidden from all members in your network. Please allow a few minutes for this process to complete.', 'buddyboss' ) ),
+					'unsuspend_confirm_message' => esc_js( esc_html__( 'Please confirm you want to unsuspend this member. Members who are unsuspended will be allowed to login again, and their content will no longer be hidden from other members in your network. Please allow a few minutes for this process to complete.', 'buddyboss' ) ),
 				),
 				'cover_size_alert'    => array(
 					'profile' => esc_html__( 'Changing the Cover Image Size will reposition all of your members cover images. Are you sure you wish to save these changes?', 'buddyboss' ),
