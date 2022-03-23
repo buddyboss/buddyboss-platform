ce976e06791f8c916464e490a38578037203528a
Mahedi Hasan <mahedihasannoman@gmail.com>
2022-03-23 21:53:49 +0600

    [PROD-2425] bp_before_member_header_meta hook multiple triggering fixed

--- src/bp-templates/bp-nouveau/buddypress/members/single/member-header.php
+++ src/bp-templates/bp-nouveau/buddypress/members/single/member-header.php
@@ -108,8 +108,6 @@ if ( ! bp_is_user_messages() && ! bp_is_user_settings() && ! bp_is_user_notifica
 										<?php
 									endif;
 
-									bp_nouveau_member_hook( 'before', 'header_meta' );
-
 									if ( bp_get_last_activity() && $is_enabled_last_active ) :
 										echo wp_kses_post( bp_get_last_activity() );
 									endif;
