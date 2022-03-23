a8284d17fbd1dd58a6f679c6eb239c7143f87949
Chetan Satasiya <chetan@buddyboss.com>
2022-03-23 14:09:40 +0530

    PROD-1905 - messages filters changes

--- src/bp-messages/classes/class-bp-messages-notification.php
+++ src/bp-messages/classes/class-bp-messages-notification.php
@@ -93,15 +93,9 @@ class BP_Messages_Notification extends BP_Core_Notification_Abstract {
 		$this->register_notification(
 			'messages',
 			'new_message',
-			'notification_messages_new_message'
-		);
-
-		$this->register_notification_filter(
-			'new_message',
-			__( 'New private messages', 'buddyboss' ),
-			155,
 			'notification_messages_new_message',
-			'messages'
+			__( 'New private messages', 'buddyboss' ),
+			155
 		);
 	}
 
