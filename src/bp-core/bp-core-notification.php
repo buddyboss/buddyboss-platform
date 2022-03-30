3d205e966999887c97b69a6936706100279dd133
Kartik Suthar <kartik@buddyboss.com>
2022-03-30 15:00:47 +0530

    PROD-1905 - phpunit fix

--- src/bp-core/bp-core-notification.php
+++ src/bp-core/bp-core-notification.php
@@ -22,23 +22,25 @@ add_action( 'bb_core_before_install', 'bb_core_default_install_emails' );
  */
 function bb_core_default_install_emails( $default_components ) {
 
-	// Load members notification file.
-	if ( file_exists( buddypress()->plugin_dir . 'bp-members/classes/class-bp-members-notification.php' ) ) {
-		require buddypress()->plugin_dir . 'bp-members/classes/class-bp-members-notification.php';
-	}
-
 	// Called members Notification class.
-	if ( class_exists( 'BP_Members_Notification' ) ) {
+	if ( ! class_exists( 'BP_Members_Notification' ) ) {
+
+		// Load members notification file.
+		if ( file_exists( buddypress()->plugin_dir . 'bp-members/classes/class-bp-members-notification.php' ) ) {
+		require buddypress()->plugin_dir . 'bp-members/classes/class-bp-members-notification.php';
+		}
+
 		BP_Members_Notification::instance();
 	}
 
-	// Load members mentions notification file.
-	if ( file_exists( buddypress()->plugin_dir . 'bp-members/classes/class-bp-members-mentions-notification.php' ) ) {
-		require buddypress()->plugin_dir . 'bp-members/classes/class-bp-members-mentions-notification.php';
-	}
-
 	// Called members mentions Notification class.
-	if ( class_exists( 'BP_Members_Mentions_Notification' ) ) {
+	if ( ! class_exists( 'BP_Members_Mentions_Notification' ) ) {
+
+		// Load members mentions notification file.
+		if ( file_exists( buddypress()->plugin_dir . 'bp-members/classes/class-bp-members-mentions-notification.php' ) ) {
+			require buddypress()->plugin_dir . 'bp-members/classes/class-bp-members-mentions-notification.php';
+		}
+
 		BP_Members_Mentions_Notification::instance();
 	}
 
