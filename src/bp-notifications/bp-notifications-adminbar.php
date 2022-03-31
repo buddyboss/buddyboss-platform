8c21d74b22a6578eb81fadb61ae0552e17a266c9
Chetan Satasiya <chetan@buddyboss.com>
2022-03-31 18:24:02 +0530

    PROD-2434 notification changes

--- src/bp-notifications/bp-notifications-adminbar.php
+++ src/bp-notifications/bp-notifications-adminbar.php
@@ -25,79 +25,38 @@ function bp_notifications_toolbar_menu() {
 		return false;
 	}
 
-	if ( bb_enabled_legacy_email_preference() ) {
+	$count       = bp_notifications_get_unread_notification_count( bp_loggedin_user_id() );
+	$alert_class = (int) $count > 0 ? 'pending-count alert' : 'count no-alert';
+	$menu_title  = '<span id="ab-pending-notifications" class="' . $alert_class . '">' . bp_core_number_format( $count ) . '</span>';
+	$menu_link   = trailingslashit( bp_loggedin_user_domain() . bp_get_notifications_slug() );
 
-		$notifications = bp_notifications_get_notifications_for_user( bp_loggedin_user_id(), 'object' );
-		$count         = ! empty( $notifications ) ? count( $notifications ) : 0;
-		$alert_class   = (int) $count > 0 ? 'pending-count alert' : 'count no-alert';
-		$menu_title    = '<span id="ab-pending-notifications" class="' . $alert_class . '">' . bp_core_number_format( $count ) . '</span>';
-		$menu_link     = trailingslashit( bp_loggedin_user_domain() . bp_get_notifications_slug() );
+	// Add the top-level Notifications button.
+	$wp_admin_bar->add_menu(
+		array(
+			'parent' => 'top-secondary',
+			'id'     => 'bp-notifications',
+			'title'  => $menu_title,
+			'href'   => $menu_link,
+		)
+	);
 
-		// Add the top-level Notifications button.
-		$wp_admin_bar->add_menu(
-			array(
-				'parent' => 'top-secondary',
-				'id'     => 'bp-notifications',
-				'title'  => $menu_title,
-				'href'   => $menu_link,
-			)
-		);
+	if ( bp_has_notifications( bp_ajax_querystring( 'notifications' ) . '&per_page=6&user_id=' . get_current_user_id() . '&is_new=1' ) ) {
 
-		if ( ! empty( $notifications ) ) {
-			foreach ( (array) $notifications as $notification ) {
-				$wp_admin_bar->add_menu(
-					array(
-						'parent' => 'bp-notifications',
-						'id'     => 'notification-' . $notification->id,
-						'title'  => $notification->content,
-						'href'   => $notification->href,
-					)
-				);
-			}
-		} else {
-			$wp_admin_bar->add_menu(
-				array(
-					'parent' => 'bp-notifications',
-					'id'     => 'no-notifications',
-					'title'  => __( 'No new notifications', 'buddyboss' ),
-					'href'   => $menu_link,
-				)
-			);
-		}
-	} else {
+		$total = buddypress()->notifications->query_loop->total_notification_count;
+		while ( bp_the_notifications() ) :
+			bp_the_notification();
 
-		$count       = bp_notifications_get_unread_notification_count( bp_loggedin_user_id() );
-		$alert_class = (int) $count > 0 ? 'pending-count alert' : 'count no-alert';
-		$menu_title  = '<span id="ab-pending-notifications" class="' . $alert_class . '">' . bp_core_number_format( $count ) . '</span>';
-		$menu_link   = trailingslashit( bp_loggedin_user_domain() . bp_get_notifications_slug() );
-
-		// Add the top-level Notifications button.
-		$wp_admin_bar->add_menu(
-			array(
-				'parent' => 'top-secondary',
-				'id'     => 'bp-notifications',
-				'title'  => $menu_title,
-				'href'   => $menu_link,
-			)
-		);
-
-		if ( bp_has_notifications( bp_ajax_querystring( 'notifications' ) . '&per_page=6&user_id=' . get_current_user_id() . '&is_new=1' ) ) {
-
-			$total = buddypress()->notifications->query_loop->total_notification_count;
-			while ( bp_the_notifications() ) :
-				bp_the_notification();
-
-				ob_start();
-				?>
+			ob_start();
+			?>
 				<span class="bb-full-link">
-					<?php bp_the_notification_description(); ?>
+				<?php bp_the_notification_description(); ?>
 				</span>
 				<div class="notification-avatar">
-					<?php bb_notification_avatar(); ?>
+				<?php bb_notification_avatar(); ?>
 				</div>
 				<div class="notification-content">
 					<span class="bb-full-link">
-						<?php bp_the_notification_description(); ?>
+					<?php bp_the_notification_description(); ?>
 					</span>
 					<span><?php bp_the_notification_description(); ?></span>
 					<span class="posted"><?php bp_the_notification_time_since(); ?></span>
@@ -121,28 +80,28 @@ function bp_notifications_toolbar_menu() {
 
 			endwhile;
 
-			if ( $total ) {
-				$menu_link = trailingslashit( bp_loggedin_user_domain() . bp_get_notifications_slug() );
-				$wp_admin_bar->add_menu(
-					array(
-						'parent' => 'bp-notifications',
-						'id'     => 'notification-view-all',
-						'title'  => esc_html__( 'View Notifications', 'buddyboss' ),
-						'href'   => $menu_link,
-					)
-				);
-
-			}
-		} else {
+		if ( $total ) {
+			$menu_link = trailingslashit( bp_loggedin_user_domain() . bp_get_notifications_slug() );
 			$wp_admin_bar->add_menu(
 				array(
 					'parent' => 'bp-notifications',
-					'id'     => 'no-notifications',
-					'title'  => __( 'No new notifications', 'buddyboss' ),
+					'id'     => 'notification-view-all',
+					'title'  => esc_html__( 'View Notifications', 'buddyboss' ),
 					'href'   => $menu_link,
 				)
 			);
+
 		}
+	} else {
+		$wp_admin_bar->add_menu(
+			array(
+				'parent' => 'bp-notifications',
+				'id'     => 'no-notifications',
+				'title'  => __( 'No new notifications', 'buddyboss' ),
+				'href'   => $menu_link,
+			)
+		);
 	}
+
 }
 add_action( 'admin_bar_menu', 'bp_members_admin_bar_notifications_menu', 90 );
