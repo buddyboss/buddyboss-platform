e4b1298b5d4d64cef982229f1ec25201c163b85b
Sourabh <sourabh@buddyboss.com>
2022-04-07 23:14:23 +0530

    [API-1] grunt bp_rest grunt bp_performance

--- src/endpoints/api_project.js
+++ src/endpoints/api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2022-04-07T13:14:04.133Z",
+    "time": "2022-04-07T17:43:52.808Z",
     "url": "http://apidocjs.com",
     "version": "0.22.1"
   }
