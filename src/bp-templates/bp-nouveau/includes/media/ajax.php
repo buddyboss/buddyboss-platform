ab364de50414607ac0b69d02269a95bd1ef4c8e4
jitendrabanjara1991 <jitendra@buddyboss.com>
2022-04-07 20:01:53 +0530

    PROD-2514 - Add count with number format and run grunt

--- src/bp-templates/bp-nouveau/includes/media/ajax.php
+++ src/bp-templates/bp-nouveau/includes/media/ajax.php
@@ -300,17 +300,17 @@ function bp_nouveau_ajax_media_save() {
 
 		add_filter( 'bp_ajax_querystring', 'bp_media_object_results_media_all_scope', 20 );
 		bp_has_media( bp_ajax_querystring( 'media' ) );
-		$media_all_count = $GLOBALS['media_template']->total_media_count;
+		$media_all_count = bp_core_number_format( $GLOBALS['media_template']->total_media_count );
 		remove_filter( 'bp_ajax_querystring', 'bp_media_object_results_media_all_scope', 20 );
 
 		add_filter( 'bp_ajax_querystring', 'bp_media_object_template_results_media_personal_scope', 20 );
 		bp_has_media( bp_ajax_querystring( 'media' ) );
-		$media_personal_count = $GLOBALS['media_template']->total_media_count;
+		$media_personal_count = bp_core_number_format( $GLOBALS['media_template']->total_media_count );
 		remove_filter( 'bp_ajax_querystring', 'bp_media_object_template_results_media_personal_scope', 20 );
 
 		add_filter( 'bp_ajax_querystring', 'bp_media_object_template_results_media_groups_scope', 20 );
 		bp_has_media( bp_ajax_querystring( 'groups' ) );
-		$media_group_count = $GLOBALS['media_template']->total_media_count;
+		$media_group_count = bp_core_number_format( $GLOBALS['media_template']->total_media_count );
 		remove_filter( 'bp_ajax_querystring', 'bp_media_object_template_results_media_groups_scope', 20 );
 
 	}
