14744d810f407f33b052553d9cf6462a9483eb05
Kartik Suthar <kartik@buddyboss.com>
2022-04-12 10:27:42 +0530

    PROD-883 - phpcbf fixes

--- src/bp-core/js/vendor/medium-editor.js
+++ src/bp-core/js/vendor/medium-editor.js
@@ -6855,9 +6855,10 @@ MediumEditor.extensions = {};
     function handleKeyup(event) {
         
         // Ignore composing keyUp event, prevent from duplicated first char with CJK IME.
-        if (event.isComposing || event.keyCode === 229) {
+        if ( event.isComposing || event.keyCode === 229 ) {
             return;
         }
+
         var node = MediumEditor.selection.getSelectionStart(this.options.ownerDocument),
             tagName;
 
