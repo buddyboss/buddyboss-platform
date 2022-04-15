98fbd8611169d20a72d38dc22c6a87fb6d3a74fc
Amin Charoliya <amin@buddyboss.com>
2022-04-15 17:26:15 +0530

    PROD-2425 - Add missing 'in_header_meta' in member header

--- src/bp-templates/bp-nouveau/buddypress/members/single/member-header.php
+++ src/bp-templates/bp-nouveau/buddypress/members/single/member-header.php
@@ -108,6 +108,8 @@ if ( ! bp_is_user_messages() && ! bp_is_user_settings() && ! bp_is_user_notifica
 										<?php
 									endif;
 
+									<?php bp_nouveau_member_hook( 'before', 'in_header_meta' ); ?>
+
 									if ( bp_get_last_activity() && $is_enabled_last_active ) :
 										echo wp_kses_post( bp_get_last_activity() );
 									endif;
