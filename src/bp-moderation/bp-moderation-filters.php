e624613d461b3e322c12cc001830064c74cd056a
Chetan Satasiya <chetan@buddyboss.com>
2022-04-29 11:29:49 +0530

    [PROD-2686] added condtion to active friends component

--- src/bp-moderation/bp-moderation-filters.php
+++ src/bp-moderation/bp-moderation-filters.php
@@ -256,11 +256,10 @@ function bp_moderation_block_member() {
 		if ( ! empty( $moderation->id ) && ! empty( $moderation->report_id ) ) {
 			$response['moderation'] = $moderation;
 
-			$friend_status = function_exists( 'bp_is_friend' ) ? bp_is_friend( $item_id ) : array();
+			$friend_status = function_exists( 'bp_is_friend' ) && bp_is_active( 'friends' ) ? bp_is_friend( $item_id ) : array();
 			if ( ! empty( $friend_status ) && in_array( $friend_status, array( 'is_friend', 'pending', 'awaiting_response' ), true ) ) {
 				friends_remove_friend( bp_loggedin_user_id(), $item_id );
 			}
-
 			if ( bp_is_following(
 				array(
 					'leader_id'   => $item_id,
