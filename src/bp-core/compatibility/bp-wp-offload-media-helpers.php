8fcdd13654ee5c73935ebbace7d1b5e783abf314
jitendrabanjara1991 <jitendra@buddyboss.com>
2022-05-09 10:15:12 +0530

    PROD-2734 - phpcs

--- src/bp-core/compatibility/bp-wp-offload-media-helpers.php
+++ src/bp-core/compatibility/bp-wp-offload-media-helpers.php
@@ -174,7 +174,7 @@ class BB_AS3CF_Plugin_Compatibility {
 				$attachment_url = wp_get_attachment_image_url( $attachment_id, 'full' );
 			}
 
-			$image_array = ( $attachment_url !== false ? @getimagesize( $attachment_url ) : array() );
+			$image_array = ( false !== $attachment_url ? @getimagesize( $attachment_url ) : array() );
 
 			if ( ! $attachment_url || empty( $image_array ) ) {
 
