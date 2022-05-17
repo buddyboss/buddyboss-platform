f831f00f884d07ce72c27a5e9f4bef643ac68637
KartikSuthar <kartik@buddyboss.com>
2022-05-17 13:13:43 +0530

    [API-2] [PROD-2769] - API changes and fixes

--- src/endpoints/api_project.js
+++ src/endpoints/api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2022-05-11T11:37:12.598Z",
+    "time": "2022-05-17T07:38:14.071Z",
     "url": "http://apidocjs.com",
     "version": "0.22.1"
   }
