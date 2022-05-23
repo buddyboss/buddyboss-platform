2fb8863803f6ef3aa929866ec26a3ab5e09ef5f0
Amin Charoliya <amin@buddyboss.com>
2022-05-23 18:37:35 +0530

    [PROD-2775] Make "Options" text translatable

--- src/bp-templates/bp-nouveau/includes/members/template-tags.php
+++ src/bp-templates/bp-nouveau/includes/members/template-tags.php
@@ -217,7 +217,7 @@ function bp_nouveau_member_header_bubble_buttons( $args = array() ) {
 		);
 	}
 
-	$output = sprintf( '<a href="#" class="bb_more_options_action"><i class="bb-icon-f bb-icon-ellipsis-h"></i></a><div class="bb_more_options_list bb_more_dropdown"><div class="bb_more_dropdown__title"><span class="bb_more_dropdown__title__text">Options</span><span class="bb_more_dropdown__close_button" role="button"><i class="bb-icon-l bb-icon-times"></i></span></div>%s</div><div class="bb_more_dropdown_overlay"></div>', $output );
+	$output = sprintf( '<a href="#" class="bb_more_options_action"><i class="bb-icon-f bb-icon-ellipsis-h"></i></a><div class="bb_more_options_list bb_more_dropdown"><div class="bb_more_dropdown__title"><span class="bb_more_dropdown__title__text">%1$s</span><span class="bb_more_dropdown__close_button" role="button"><i class="bb-icon-l bb-icon-times"></i></span></div>%2$s</div><div class="bb_more_dropdown_overlay"></div>', esc_html__( 'Options', 'buddyboss' ), $output );
 
 	bp_nouveau_wrapper( array_merge( $args, array( 'output' => $output ) ) );
 }
