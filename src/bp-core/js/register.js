5623e1e08cb2664c008e902e15d12ffcd09deb7b
jitendrabanjara1991 <jitendra@buddyboss.com>
2022-05-24 14:50:10 +0530

    PROD-2777 - Add invalid class for each required field

--- src/bp-core/js/register.js
+++ src/bp-core/js/register.js
@@ -219,33 +219,54 @@ jQuery( document ).ready( function() {
 
 		if ( jQuery( document ).find( signup_email_confirm ).length && jQuery( document ).find( signup_email_confirm ).val() == '' ) {
 			jQuery( document ).find( signup_email_confirm ).before( html_error );
+			jQuery( document ).find( signup_email_confirm ).addClass( 'invalid' );
 			return_val = false;
+		} else {
+			jQuery( document ).find( signup_email_confirm ).removeClass( 'invalid' );
 		}
 		if ( jQuery( document ).find( signup_password ).length && jQuery( document ).find( signup_password ).val() == '' ) {
 			jQuery( document ).find( signup_password ).before( html_error );
+			jQuery( document ).find( signup_password ).addClass( 'invalid' );
 			return_val = false;
+		} else {
+			jQuery( document ).find( signup_password ).removeClass( 'invalid' );
 		}
 		if ( jQuery( document ).find( signup_password_confirm ).length && jQuery( document ).find( signup_password_confirm ).val() == '' ) {
 			jQuery( document ).find( signup_password_confirm ).before( html_error );
+			jQuery( document ).find( signup_password_confirm ).addClass( 'invalid' );
 			return_val = false;
+		} else {
+			jQuery( document ).find( signup_password_confirm ).removeClass( 'invalid' );
 		}
 		jQuery( '.required-field' ).each( function() {
 
 			if ( jQuery( this ).find( 'input[type="text"]' ).length && jQuery( this ).find( 'input[type="text"] ').val() == '' ) {
 				jQuery( this ).find( 'input[type="text"]' ).before( html_error );
+				jQuery( this ).find( 'input[type="text"]' ).addClass( 'invalid' );
 				return_val = false;
+			} else {
+				jQuery( this ).find( 'input[type="text"]' ).removeClass( 'invalid' );
 			}
 			if ( jQuery( this ).find( 'input[type="number"]' ).length && jQuery( this ).find( 'input[type="number"] ').val() == '' ) {
 				jQuery( this ).find( 'input[type="number"]' ).before( html_error );
+				jQuery( this ).find( 'input[type="number"]' ).addClass( 'invalid' );
 				return_val = false;
+			} else {
+				jQuery( this ).find( 'input[type="number"]' ).removeClass( 'invalid' );
 			}
-			if ( jQuery( this ).find( 'textarea' ).length && jQuery( this ).find( 'textarea' ).val() == '' ) {
+			if ( jQuery( this ).find( 'textarea' ).length && jQuery( this ).find( 'textarea' ).val() == '' || undefined === typeof jQuery( this ).find( 'textarea' ).val() ) {
 				jQuery( this ).find( 'textarea' ).before( html_error );
+				jQuery( this ).find( 'textarea' ).addClass( 'invalid' );
 				return_val = false;
+			} else {
+				jQuery( this ).find( 'textarea' ).removeClass( 'invalid' );
 			}
 			if ( jQuery( this ).find( 'select' ).length && jQuery( this ).find( 'select' ).val() == '' ) {
 				jQuery( this ).find( 'select' ).before( html_error );
+				jQuery( this ).find( 'select' ).addClass( 'invalid' );
 				return_val = false;
+			} else {
+				jQuery( this ).find( 'select' ).removeClass( 'invalid' );
 			}
 			if ( jQuery( this ).find( 'input[type="checkbox"]' ).length ) {
 					var checked_check = 0;
@@ -262,6 +283,7 @@ jQuery( document ).ready( function() {
 		});
 		if ( jQuery( document ).find( signup_email ).length && jQuery( document ).find( signup_email ).val() == '' ) {
 			jQuery( document ).find( signup_email ).before( html_error );
+			jQuery( document ).find( signup_email ).addClass( 'invalid' );
 			return_val = false;
 		}else{
 			bp_register_validate_confirm_email();
@@ -273,26 +295,32 @@ jQuery( document ).ready( function() {
 				async: false,
 			    data: jQuery( 'body #buddypress #register-page #signup-form' ).serialize() + '&action=check_email',
 			    success: function ( response ) {
-			    	if (response.signup_email) {
-				    	var html_serror = '<div class="bp-messages bp-feedback error">';
-							html_serror += '<span class="bp-icon" aria-hidden="true"></span>';
-							html_serror += '<p>' + response.signup_email + '</p>';
-							html_serror += '</div>';
-
-                		jQuery( document ).find( signup_email ).before( html_serror );
-                		return_val = false;
-                	}
-                	var nickname = 'field_'+response.field_id;
-                	if (response.signup_username) {
-                		var html_uerror = '<div class="bp-messages bp-feedback error">';
-							html_uerror += '<span class="bp-icon" aria-hidden="true"></span>';
-							html_uerror += '<p>' + response.signup_username + '</p>';
-							html_uerror += '</div>';
-                		jQuery( document ).find( '#'+nickname ).before( html_uerror );
-                		return_val = false;
-                	}
-	                return true;
-	           	}
+				    if ( response.signup_email ) {
+					    var html_serror = '<div class="bp-messages bp-feedback error">';
+					    html_serror += '<span class="bp-icon" aria-hidden="true"></span>';
+					    html_serror += '<p>' + response.signup_email + '</p>';
+					    html_serror += '</div>';
+					
+					    jQuery( document ).find( signup_email ).before( html_serror );
+					    jQuery( document ).find( signup_email ).addClass( 'invalid' );
+					    return_val = false;
+				    } else {
+					    jQuery( document ).find( signup_email ).removeClass( 'invalid' );
+				    }
+				    var nickname = 'field_' + response.field_id;
+				    if ( response.signup_username ) {
+					    var html_uerror = '<div class="bp-messages bp-feedback error">';
+					    html_uerror += '<span class="bp-icon" aria-hidden="true"></span>';
+					    html_uerror += '<p>' + response.signup_username + '</p>';
+					    html_uerror += '</div>';
+					    jQuery( document ).find( '#' + nickname ).before( html_uerror );
+					    jQuery( document ).find( '#' + nickname ).addClass( 'invalid' );
+					    return_val = false;
+				    } else {
+					    jQuery( document ).find( '#' + nickname ).removeClass( 'invalid' );
+				    }
+				    return true;
+			    }
 			});
 		}
 		if ( ! return_val ) {
