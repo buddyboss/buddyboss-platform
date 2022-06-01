2b6481dffff9cb00bec4d40c69512414b843932b
jitendrabanjara1991 <jitendra@buddyboss.com>
2022-06-01 15:45:23 +0530

    PROD-2895 - Allow comma separate group ids

--- src/bp-groups/bp-groups-functions.php
+++ src/bp-groups/bp-groups-functions.php
@@ -4725,8 +4725,8 @@ function bb_get_all_members_for_groups( $args = array() ) {
 			'is_banned = 0',
 		);
 
-		if ( ! empty( $r['group_ids'] ) && is_array( $r['group_ids'] ) ) {
-			$sql['where'][] = 'group_id IN ( ' . implode( ', ', $r['group_ids'] ) . ' )';
+		if ( ! empty( $r['group_ids'] ) ) {
+			$sql['where'][] = 'group_id IN ( ' . implode( ',', wp_parse_id_list( $r['group_ids'] ) ) . ' )';
 		}
 
 		/**
