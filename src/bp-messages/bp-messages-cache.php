ab838ebb5922a1c922b444bcea20ff75559afae9
Kartik Suthar <kartik@buddyboss.com>
2022-06-06 12:52:06 +0530

    [PROD-2520] removed duplicate call

--- src/bp-messages/bp-messages-cache.php
+++ src/bp-messages/bp-messages-cache.php
@@ -143,5 +143,4 @@ add_action( 'messages_message_after_save', 'bb_core_clear_message_cache' );
 add_action( 'messages_delete_thread', 'bb_core_clear_message_cache' );
 add_action( 'messages_send_notice', 'bb_core_clear_message_cache' );
 add_action( 'messages_message_sent', 'bb_core_clear_message_cache' );
-add_action( 'messages_message_after_save', 'bb_core_clear_message_cache' );
 add_action( 'messages_thread_mark_as_read', 'bb_core_clear_message_cache' );
