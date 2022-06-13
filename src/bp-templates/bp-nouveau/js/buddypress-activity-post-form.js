69ea01c376498b8067cc488f04c16464695e8e7f
jitendrabanjara1991 <jitendra@buddyboss.com>
2022-06-13 19:54:06 +0530

    PROD-896 - Fixed thumbnail is not displaying in comment for activity

--- src/bp-templates/bp-nouveau/js/buddypress-activity-post-form.js
+++ src/bp-templates/bp-nouveau/js/buddypress-activity-post-form.js
@@ -122,6 +122,7 @@ window.bp = window.bp || {};
 				dictMaxFilesExceeded		: BP_Nouveau.media.media_dict_file_exceeded,
 				dictCancelUploadConfirmation: BP_Nouveau.media.dictCancelUploadConfirmation,
 				// previewTemplate : document.getElementsByClassName( 'activity-post-media-template' )[0].innerHTML.
+				maxThumbnailFilesize: ! _.isUndefined( BP_Nouveau.media.max_upload_size ) ? BP_Nouveau.media.max_upload_size : 2,
 			};
 
 			// if defined, add custom dropzone options.
--- src/bp-templates/bp-nouveau/js/buddypress-activity.js
+++ src/bp-templates/bp-nouveau/js/buddypress-activity.js
@@ -1497,6 +1497,7 @@ window.bp = window.bp || {};
 						dictMaxFilesExceeded		: BP_Nouveau.media.media_dict_file_exceeded,
 						previewTemplate		 		: acCommentDefaultTemplate,
 						dictCancelUploadConfirmation: BP_Nouveau.media.dictCancelUploadConfirmation,
+						maxThumbnailFilesize    : typeof BP_Nouveau.media.max_upload_size !== 'undefined' ? BP_Nouveau.media.max_upload_size : 2,
 					};
 
 					// init dropzone.
