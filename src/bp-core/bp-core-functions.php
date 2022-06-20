90186673eecd59d88b862775080e58dc74881185
Kartik Suthar <kartik@buddyboss.com>
2022-06-20 16:17:57 +0530

    [PROD-1915] enable group notification for web push

--- src/bp-core/bp-core-functions.php
+++ src/bp-core/bp-core-functions.php
@@ -6726,6 +6726,11 @@ function bp_can_send_notification( $user_id, $component_name, $component_action 
 					in_array( $component_name, array( 'activity', 'forums', 'members' ), true )
 				) {
 					return $n['notification_type'];
+				} elseif (
+					'bb_groups_new_message' === $component_action &&
+					in_array( $component_name, array( 'messages', 'groups' ), true )
+				) {
+					return $n['notification_type'];
 				} elseif ( ! empty( $n['component'] ) && ! empty( $n['component_action'] ) && $component_name === $n['component'] && $component_action === $n['component_action'] ) {
 					return $n['notification_type'];
 				}
