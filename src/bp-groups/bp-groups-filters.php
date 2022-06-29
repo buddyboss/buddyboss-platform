15776455e4bf32004e88675df3b4aa1b32ad8591
Milankumar Hirapra <milan@buddyboss.com>
2022-06-29 12:11:32 +0530

    [PROD-2140] Basic HTML Codes not working on group description

--- src/bp-groups/bp-groups-filters.php
+++ src/bp-groups/bp-groups-filters.php
@@ -104,30 +104,27 @@ add_action( 'bp_groups_includes', 'bb_load_groups_notifications' );
  */
 function bp_groups_filter_kses( $content = '' ) {
 
+	$allowed_tags = array();
 	/**
 	 * Note that we don't immediately bail if $content is empty. This is because
 	 * WordPress's KSES API calls several other filters that might be relevant
 	 * to someone's workflow (like `pre_kses`)
 	 */
 
-	// Get allowed tags using core WordPress API allowing third party plugins
-	// to target the specific `buddypress-groups` context.
-	$allowed_tags = wp_kses_allowed_html( 'buddypress-groups' );
-
 	// Add our own tags allowed in group descriptions.
-	$allowed_tags['a']['class']    = array();
-	$allowed_tags['img']           = array();
-	$allowed_tags['img']['src']    = array();
-	$allowed_tags['img']['alt']    = array();
-	$allowed_tags['img']['width']  = array();
-	$allowed_tags['img']['height'] = array();
-	$allowed_tags['img']['class']  = array();
-	$allowed_tags['img']['id']     = array();
-	$allowed_tags['code']          = array();
-	$allowed_tags['ol']            = array();
-	$allowed_tags['ul']            = array();
-	$allowed_tags['li']            = array();
-	$allowed_tags['a']['target']   = array();
+	$allowed_tags['a']           = array();
+	$allowed_tags['a']['href']   = true;
+	$allowed_tags['a']['title']  = true;
+	$allowed_tags['a']['class']  = array();
+	$allowed_tags['a']['target'] = array();
+	$allowed_tags['i']           = array();
+	$allowed_tags['b']           = array();
+	$allowed_tags['strong']      = array();
+	$allowed_tags['blockquote']  = array();
+	$allowed_tags['ol']          = array();
+	$allowed_tags['ul']          = array();
+	$allowed_tags['li']          = array();
+	$allowed_tags['pre']         = array();
 
 	/**
 	 * Filters the HTML elements allowed for a given context.
