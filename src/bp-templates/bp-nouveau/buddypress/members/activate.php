0e5499d6140898244c2bd10d67caa13929fb7395
Yudhisthir Nahar <yudhisthir@buddyboss.com>
2022-07-04 17:04:04 +0530

    #PROD-3044 Activation page add button classes to login [PROD-3044] PROD-3044 #comment Activation page add button classes to login

--- src/bp-templates/bp-nouveau/buddypress/members/activate.php
+++ src/bp-templates/bp-nouveau/buddypress/members/activate.php
@@ -27,7 +27,7 @@
 
 			<?php
 				printf(
-					'<p><a href="%1$s">%2$s</a></p>',
+					'<p><a class="button button-primary" href="%1$s">%2$s</a></p>',
 					esc_url( wp_login_url( bp_get_root_domain() ) ),
 					esc_html__( 'Log In', 'buddyboss' )
 				);
