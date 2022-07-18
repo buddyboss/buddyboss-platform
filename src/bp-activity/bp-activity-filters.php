de78997d14e786a6ad66c096ae47f0cdb6d703f9
jitendrabanjara1991 <jitendra@buddyboss.com>
2022-07-18 13:33:11 +0530

    PROD-3050 - When click on confirm then and then save image in DB

--- src/bp-activity/bp-activity-filters.php
+++ src/bp-activity/bp-activity-filters.php
@@ -295,7 +295,7 @@ function bp_activity_save_link_data( $activity ) {
 		}
 	}
 
-	$preview_data['link_image_index'] = ! empty( $_POST['link_image_index'] ) ? filter_var( $_POST['link_image_index'] ) : 0;
+	$preview_data['link_image_index'] = ! empty( $_POST['link_image_index_confirm'] ) ? filter_var( $_POST['link_image_index_confirm'] ) : '';
 
 	if ( ! empty( $link_title ) ) {
 		$preview_data['title'] = $link_title;
