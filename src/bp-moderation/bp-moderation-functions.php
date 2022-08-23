130d0010cbbf31ef85896893b67197d40ea283de
Kartik Suthar <kartik@buddyboss.com>
2022-08-23 20:12:50 +0530

    PROD-3453 - created function to fetch user was blocked by

--- src/bp-moderation/bp-moderation-functions.php
+++ src/bp-moderation/bp-moderation-functions.php
@@ -1150,3 +1150,37 @@ function bp_moderation_get_reported_button_text( $item_type, $item_id ) {
 	 */
 	return apply_filters( "bb_moderation_{$item_type}_reported_button_text", esc_html__( 'Reported', 'buddyboss' ), $item_id );
 }
+
+/**
+ * Fetch the user id by blocked by.
+ *
+ * @since BuddyBoss [BBVERSION]
+ *
+ * @param int  $user_id User id.
+ * @param bool $force   Whether to bypass the static cache or not.
+ *
+ * @return array|mixed
+ */
+function bb_moderation_get_blocked_by_user_ids( $user_id = 0, $force = false ) {
+	static $cache = array();
+	global $wpdb;
+	$bp = buddypress();
+	if ( empty( $user_id ) ) {
+		$user_id = bp_loggedin_user_id();
+	}
+
+	$cache_key = 'bb_moderation_blocked_by_' . $user_id;
+	if ( ! isset( $cache[ $cache_key ] ) || $force ) {
+		$type = BP_Moderation_Members::$moderation_type;
+		// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+		$sql  = $wpdb->prepare( "SELECT DISTINCT m.user_id FROM {$bp->moderation->table_name} s LEFT JOIN {$bp->moderation->table_name_reports} m ON m.moderation_id = s.id WHERE s.item_type = %s AND s.item_id = %d", $type, $user_id );
+		$data = $wpdb->get_col( $sql ); // phpcs:ignore
+		$data = ! empty( $data ) ? $data : array();
+
+		$cache[ $cache_key ] = $data;
+	} else {
+		$data = $cache[ $cache_key ];
+	}
+
+	return $data;
+}
