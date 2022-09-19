5d75fcaf0872a7685a4f6029946762aab6c71f3c
Kartik Suthar <kartik@buddyboss.com>
2022-09-19 19:57:20 +0530

    [PROD-1948] PROD-1919 - fix the media/video display issue

--- src/bp-templates/bp-nouveau/includes/messages/functions.php
+++ src/bp-templates/bp-nouveau/includes/messages/functions.php
@@ -134,6 +134,7 @@ function bp_nouveau_messages_localize_scripts( $params = array() ) {
 		'rootUrl'                    => urldecode( wp_parse_url( trailingslashit( bp_displayed_user_domain() . bp_get_messages_slug() ), PHP_URL_PATH ) ),
 		'hasThreads'                 => bp_has_message_threads( bp_ajax_querystring( 'messages' ) ),
 		'today'                      => __( 'Today', 'buddyboss' ),
+		'video_default_url'          => ( function_exists( 'bb_get_video_default_placeholder_image' ) && ! empty( bb_get_video_default_placeholder_image() ) ? bb_get_video_default_placeholder_image() : '' ),
 	);
 
 	// Star private messages.
