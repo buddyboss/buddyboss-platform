e3711e9604f03e8bf10474ebc8bf5eb990fb4316
Kartik Suthar <kartik@buddyboss.com>
2022-10-13 15:21:39 +0530

    [PROD-3823] PROD-3828 - fix the depricated notice

--- src/bp-core/deprecated/buddyboss/2.2.0.php
+++ src/bp-core/deprecated/buddyboss/2.2.0.php
@@ -19,7 +19,7 @@ defined( 'ABSPATH' ) || exit;
  */
 function bb_current_user_status( $user_id ) {
 
-	_deprecated_function( __FUNCTION__, '2.2.0', 'bb_get_user_status' );
+	_deprecated_function( __FUNCTION__, '2.2.0', 'bb_get_user_presence_html' );
 
 	if ( bb_is_online_user( $user_id ) ) {
 		echo wp_kses_post( apply_filters( 'bb_user_online_html', '<span class="member-status online"></span>', $user_id ) );
