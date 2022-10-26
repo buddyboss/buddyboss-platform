bb276f9e194f217c43bd4ff70a75def996fbbaee
Kartik Suthar <kartik@buddyboss.com>
2022-10-26 17:26:48 +0530

    PROD-3910 - removed code to load the integration

--- src/bp-integrations/pusher/bb-pusher-loader.php
+++ src/bp-integrations/pusher/bb-pusher-loader.php
@@ -10,21 +10,3 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-/**
- * Set up the BB Pusher integration.
- *
- * @since BuddyBoss [BBVERSION]
- */
-function bb_register_pusher_integration() {
-
-	if (
-		( function_exists( 'bb_platform_pro' ) && version_compare( bb_platform_pro()->version, '2.2.0', '>=' ) ) ||
-		class_exists( 'BB_Pusher_Integration' )
-	) {
-		return;
-	}
-
-	require_once dirname( __FILE__ ) . '/bb-pusher-integration.php';
-	buddypress()->integrations['pusher'] = new BB_Pusher_Integration();
-}
-add_action( 'bp_setup_integrations', 'bb_register_pusher_integration', 20 );
