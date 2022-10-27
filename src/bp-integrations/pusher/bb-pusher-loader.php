6c0857aec6e45c00cc494af5d3a03a6d76152a35
Chetan Satasiya <chetan@buddyboss.com>
2022-10-27 11:39:04 +0530

    [PROD-3910] removed pusher integration

--- src/bp-integrations/pusher/bb-pusher-loader.php
+++ src/bp-integrations/pusher/bb-pusher-loader.php
@@ -4,27 +4,8 @@
  *
  * @package BuddyBoss\Pusher
  *
- * @since BuddyBoss [BBVERSION]
+ * @since BuddyBoss 2.1.1.1
  */
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
-
-///**
-// * Set up the BB Pusher integration.
-// *
-// * @since BuddyBoss [BBVERSION]
-// */
-//function bb_register_pusher_integration() {
-//
-//	if (
-//		( function_exists( 'bb_platform_pro' ) && version_compare( bb_platform_pro()->version, '2.2.0', '>=' ) ) ||
-//		class_exists( 'BB_Pusher_Integration' )
-//	) {
-//		return;
-//	}
-//
-//	require_once dirname( __FILE__ ) . '/bb-pusher-integration.php';
-//	buddypress()->integrations['pusher'] = new BB_Pusher_Integration();
-//}
-//add_action( 'bp_setup_integrations', 'bb_register_pusher_integration', 20 );
