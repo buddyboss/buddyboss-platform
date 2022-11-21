15bf802f4f0293f6cadfc1dd51c19a3138d2ca60
Milankumar Hirapra <milan@buddyboss.com>
2022-11-21 18:33:20 +0530

    PROD-870 : Fixed phpcbf

--- src/bp-core/admin/bp-core-admin-functions.php
+++ src/bp-core/admin/bp-core-admin-functions.php
@@ -3272,7 +3272,23 @@ function bp_core_get_moderation_admin_tabs( $active_tab = '' ) {
 
 	if ( bp_is_moderation_member_blocking_enable() ) {
 		$tabs[] = array(
-			'href'  => ( is_multisite() ) ? get_admin_url( get_current_blog_id(), add_query_arg( array( 'page' => 'bp-moderation', ), 'admin.php' ) ) : bp_get_admin_url( add_query_arg( array( 'page' => 'bp-moderation', ), 'admin.php'
+			'href'  => ( is_multisite() ?
+				get_admin_url(
+					get_current_blog_id(),
+					add_query_arg(
+						array(
+							'page' => 'bp-moderation',
+						),
+						'admin.php'
+					)
+				)
+				: bp_get_admin_url(
+					add_query_arg(
+						array(
+							'page' => 'bp-moderation',
+						),
+						'admin.php'
+					)
 				)
 			),
 			'name'  => esc_html__( 'Flagged Members', 'buddyboss' ),
@@ -3292,9 +3308,25 @@ function bp_core_get_moderation_admin_tabs( $active_tab = '' ) {
 	);
 
 	$tabs[] = array(
-		'href'  => ( is_multisite() ) ? get_admin_url( get_current_blog_id(), add_query_arg( array( 'taxonomy' => 'bpm_category', 'tab' => 'report-categories' ), 'edit-tags.php' ) ) : bp_get_admin_url( add_query_arg( array( 'taxonomy' => 'bpm_category', 'tab' => 'report-categories' ,
-				),
-				'edit-tags.php'
+		'href'  => ( is_multisite() ?
+			get_admin_url(
+				get_current_blog_id(),
+				add_query_arg(
+					array(
+						'taxonomy' => 'bpm_category',
+						'tab'      => 'report-categories',
+					),
+					'edit-tags.php'
+				)
+			)
+			: bp_get_admin_url(
+				add_query_arg(
+					array(
+						'taxonomy' => 'bpm_category',
+						'tab'      => 'report-categories',
+					),
+					'edit-tags.php'
+				)
 			)
 		),
 		'name'  => esc_html__( 'Reporting Categories', 'buddyboss' ),
