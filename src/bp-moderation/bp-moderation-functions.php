50b9885f554fa3cfac5125f9d3e565cc790dee8c
jitendrabanjara1991 <jitendra@buddyboss.com>
2022-12-05 18:37:15 +0530

    PROD-4097 - Optimize query

--- src/bp-moderation/bp-moderation-functions.php
+++ src/bp-moderation/bp-moderation-functions.php
@@ -1267,7 +1267,7 @@ function bb_moderation_get_blocked_by_user_ids( $user_id = 0, $force = false ) {
 	if ( ! isset( $cache[ $cache_key ] ) || $force ) {
 		$type = BP_Moderation_Members::$moderation_type;
 		// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
-		$sql  = $wpdb->prepare( "SELECT DISTINCT m.user_id FROM {$bp->moderation->table_name} s LEFT JOIN {$bp->moderation->table_name_reports} m ON m.moderation_id = s.id WHERE s.item_type = %s AND s.item_id = %d AND s.reported != 0 AND m.user_report != 1", $type, $user_id );
+		$sql  = bb_moderation_get_blocked_by_sql( $user_id );
 		$data = $wpdb->get_col( $sql ); // phpcs:ignore
 		$data = ! empty( $data ) ? array_map( 'intval', $data ) : array();
 
@@ -1277,6 +1277,29 @@ function bb_moderation_get_blocked_by_user_ids( $user_id = 0, $force = false ) {
 	}
 
 	return $data;
+}
+
+/**
+ * Return SQL to fetch the query for blocked by users.
+ *
+ * @since BuddyBoss [BBVERSION]
+ *
+ * @param int $user_id User ID.
+ *
+ * @return string|void
+ */
+function bb_moderation_get_blocked_by_sql( $user_id = 0 ) {
+	global $wpdb;
+	$bp = buddypress();
+
+	if ( empty( $user_id ) ) {
+		$user_id = bp_loggedin_user_id();
+	}
+
+	$type = BP_Moderation_Members::$moderation_type;
+
+	// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+	return $wpdb->prepare( "SELECT DISTINCT m.user_id FROM {$bp->moderation->table_name} s LEFT JOIN {$bp->moderation->table_name_reports} m ON m.moderation_id = s.id WHERE s.item_type = %s AND s.item_id = %d AND s.reported != 0 AND m.user_report != 1", $type, $user_id );
 }
 
 /**
