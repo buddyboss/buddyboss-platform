e545ee577f98a620bd112f15745b8a198c7df62c
jitendrabanjara1991 <jitendra@buddyboss.com>
2022-12-16 17:21:11 +0530

    PROD-4127 - Update condition

--- src/bp-core/classes/class-bp-email-tokens.php
+++ src/bp-core/classes/class-bp-email-tokens.php
@@ -2260,16 +2260,13 @@ class BP_Email_Tokens {
 	 * @return string html for the output
 	 */
 	public function token__activity_content( $bp_email, $formatted_tokens, $tokens ) {
-		$output         = '';
-		$settings       = bp_email_get_appearance_settings();
-		$activity       = isset( $tokens['activity'] ) ? $tokens['activity'] : '';
-		$followers_user = bp_get_followers( array( 'user_id' => bp_loggedin_user_id() ) );
+		$output   = '';
+		$settings = bp_email_get_appearance_settings();
+		$activity = isset( $tokens['activity'] ) ? $tokens['activity'] : '';
 
 		if (
 			empty( $activity ) ||
-			'activity' !== $activity->component ||
-			in_array( $activity->privacy, array( 'document', 'media', 'video' ), true ) ||
-			empty( $followers_user )
+			in_array( $activity->privacy, array( 'document', 'media', 'video' ), true )
 		) {
 			return $output;
 		}
