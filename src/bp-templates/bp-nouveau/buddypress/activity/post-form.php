a2930048b9b7ed761149dd182508ed26da86396b
Milankumar Hirapra <milan@buddyboss.com>
2022-12-23 10:57:41 +0530

    [PROD-3837] PROd-3837 : Update condition and add in placeholder activity form

--- src/bp-templates/bp-nouveau/buddypress/activity/post-form.php
+++ src/bp-templates/bp-nouveau/buddypress/activity/post-form.php
@@ -12,12 +12,12 @@
  * Template tag to prepare the activity post form checks capability and enqueue needed scripts.
  */
 bp_nouveau_before_activity_post_form();
-$bp_hide_class = ( in_array( bp_current_action(), array( 'favorites', 'friends', 'groups', 'mentions', 'following' ), true ) ) ? 'bp-hide is-bp-hide' : '';
+$bp_hide_class = ( ! empty( bp_current_action() ) && ( bp_current_action() !== 'just-me' && bp_current_action() !== 'activity' ) ) ? 'bp-hide is-bp-hide' : '';
 ?>
 
 <h2 class="bp-screen-reader-text"><?php esc_html_e( 'Post Update', 'buddyboss' ); ?></h2>
 
-<div id="bp-nouveau-activity-form-placeholder" class="bp-nouveau-activity-form-placeholder-<?php if ( ! bp_is_active( 'media' ) ) { echo ' media-off'; } ?>"></div>
+<div id="bp-nouveau-activity-form-placeholder" class="bp-nouveau-activity-form-placeholder-<?php if ( ! bp_is_active( 'media' ) ) { echo ' media-off'; } ?> <?php echo esc_attr( $bp_hide_class ); ?>"></div>
 
 <div id="bp-nouveau-activity-form" class="activity-update-form<?php if ( ! bp_is_active( 'media' ) ) { echo ' media-off'; } ?> <?php echo esc_attr( $bp_hide_class ); ?>"></div>
 
