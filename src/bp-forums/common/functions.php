b9c5f22efc310f80c965f63f83fb418e5d59f8a9
Milankumar Hirapra <milan@buddyboss.com>
2023-01-17 17:04:38 +0530

    PROD-4410 : Restrict the trigger notification when forum is attached to the group

--- src/bp-forums/common/functions.php
+++ src/bp-forums/common/functions.php
@@ -1152,6 +1152,11 @@ function bbp_notify_topic_subscribers( $reply_id = 0, $topic_id = 0, $forum_id =
 		return false;
 	}
 
+	// Bail if discussion is attached in a group.
+	if ( function_exists( 'bbp_get_forum_group_ids' ) && ! empty( bbp_get_forum_group_ids( $forum_id ) ) ) {
+		return false;
+	}
+
 	// Poster name.
 	$reply_author_name = bbp_get_reply_author_display_name( $reply_id );
 
@@ -1276,6 +1281,11 @@ function bbp_notify_forum_subscribers( $topic_id = 0, $forum_id = 0, $anonymous_
 
 	// Bail if topic is not published.
 	if ( ! bbp_is_topic_published( $topic_id ) ) {
+		return false;
+	}
+
+	// Bail if discussion is attached in a group.
+	if ( function_exists( 'bbp_get_forum_group_ids' ) && ! empty( bbp_get_forum_group_ids( $forum_id ) ) ) {
 		return false;
 	}
 
