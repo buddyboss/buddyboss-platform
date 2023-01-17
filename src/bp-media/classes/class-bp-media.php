4aa013ebc32a847a1e31baf14fec01d942664639
jitendrabanjara1991 <jitendra@buddyboss.com>
2023-01-17 16:35:57 +0530

    PROD-4447 - Fixed - Activity is deleted after editing it a second time with media

--- src/bp-media/classes/class-bp-media.php
+++ src/bp-media/classes/class-bp-media.php
@@ -1193,12 +1193,15 @@ class BP_Media {
 
 						// Deleting an activity.
 					} else {
-						if ( 'activity' !== $from && bp_activity_delete(
-							array(
-								'id'      => $activity->id,
-								'user_id' => $activity->user_id,
+						if (
+							! empty( $activity->secondary_item_id ) &&
+							'activity' !== $from && bp_activity_delete(
+								array(
+									'id'      => $activity->id,
+									'user_id' => $activity->user_id,
+								)
 							)
-						) ) {
+						) {
 							/** This action is documented in bp-activity/bp-activity-actions.php */
 							do_action( 'bp_activity_action_delete_activity', $activity->id, $activity->user_id );
 						}
