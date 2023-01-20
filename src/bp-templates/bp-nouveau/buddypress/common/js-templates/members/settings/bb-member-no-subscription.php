15fd2c494b7844a5f5587951139fe4eb88910745
Kartik Suthar <kartik@buddyboss.com>
2023-01-20 17:54:18 +0530

    PROD-4121 - phpcbf fixes

--- src/bp-templates/bp-nouveau/buddypress/common/js-templates/members/settings/bb-member-no-subscription.php
+++ src/bp-templates/bp-nouveau/buddypress/common/js-templates/members/settings/bb-member-no-subscription.php
@@ -12,9 +12,9 @@
 <script type="text/html" id="tmpl-bb-member-no-subscription">
 	<p>
 		<#
-			var no_results = BP_Nouveau.subscriptions.no_result;
-				no_results = no_results.replace( "%s", data.pluralLabel );
-			print( no_results );
+		var no_results = BP_Nouveau.subscriptions.no_result;
+		no_results = no_results.replace( "%s", data.pluralLabel );
+		print( no_results );
 		#>
 	</p>
 </script>
