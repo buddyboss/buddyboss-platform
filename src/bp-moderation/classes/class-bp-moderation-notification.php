f2747bef8a2f11e1285a40caea0ea08429eabc39
jitendrabanjara1991 <jitendra@buddyboss.com>
2023-01-24 10:15:31 +0530

    PROD-4103 - Remove code for hidden/blocked/suspened query for notifications

--- src/bp-moderation/classes/class-bp-moderation-notification.php
+++ src/bp-moderation/classes/class-bp-moderation-notification.php
@@ -86,25 +86,9 @@ class BP_Moderation_Notification extends BP_Moderation_Abstract {
 	 * @return string
 	 */
 	public function update_where_sql( $sql_where, $tbl_alias, $args = array() ) {
-		global $wpdb;
-		$bp = buddypress();
 
 		if ( isset( $args['moderation_query'] ) && false === $args['moderation_query'] ) {
 			return $sql_where;
-		}
-
-		if ( bp_is_moderation_member_blocking_enable( 0 ) ) {
-			// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
-			$moderation_query = $wpdb->prepare( "SELECT item_id FROM {$bp->table_prefix}bp_suspend s INNER JOIN {$bp->table_prefix}bp_moderation mr ON mr.moderation_id = s.id WHERE mr.user_id = %d AND mr.user_report = %d AND ( hide_parent = %d OR hide_sitewide = %d OR reported = %d ) AND item_type = %s", bp_loggedin_user_id(), 0, 1, 1, 1, 'user' );
-
-			// phpcs:ignore Squiz.Strings.DoubleQuoteUsage.NotRequired
-			$sql_where .= " AND {$tbl_alias}.secondary_item_id NOT IN ( " . $moderation_query . " )";
-
-			$hidden_users_ids = bp_moderation_get_hidden_user_ids();
-			if ( ! empty( $hidden_users_ids ) ) {
-				// phpcs:ignore Squiz.Strings.DoubleQuoteUsage.NotRequired
-				$sql_where .= " AND ( {$tbl_alias}.item_id NOT IN ( " . implode( ',', $hidden_users_ids ) . " ) )";
-			}
 		}
 
 		/**
