0507eb2d2f9792938020d91cc36a58cdd01d822b
jitendrabanjara1991 <jitendra@buddyboss.com>
2023-02-14 14:53:29 +0530

    PROD-3134 - Update condition as per new setting " A group's details are updated"

--- src/bp-groups/bp-groups-functions.php
+++ src/bp-groups/bp-groups-functions.php
@@ -319,19 +319,11 @@ function groups_edit_base_group_details( $args = array() ) {
 		groups_add_groupmeta( $group->id, 'previous_slug', $old_group->slug );
 	}
 
-	if ( bb_get_modern_notification_admin_settings_is_enabled( 'bb_groups_details_updated', 'groups' ) ) {
-		if ( $r['notify_members'] && ! $r['slug'] ) {
-			groups_notification_group_updated( $group->id, $old_group );
-			// fire notification from front side.
-			if ( ! bb_enabled_legacy_email_preference() ) {
-				bb_groups_notification_groups_updated( $group->id );
-			}
-		}
-		if ( ! $r['notify_members'] && $r['slug'] ) {
-			// fire notification from admin side.
-			if ( ! bb_enabled_legacy_email_preference() ) {
-				bb_groups_notification_groups_updated( $group->id );
-			}
+	if ( bb_get_modern_notification_admin_settings_is_enabled( 'bb_groups_details_updated', 'groups' ) && $r['notify_members'] ) {
+		groups_notification_group_updated( $group->id, $old_group );
+
+		if ( ! bb_enabled_legacy_email_preference() ) {
+			bb_groups_notification_groups_updated( $group->id );
 		}
 	}
 
