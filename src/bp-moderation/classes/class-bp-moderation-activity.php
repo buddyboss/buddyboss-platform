22c47147fce24dda3892a7187290698c4292c505
Milankumar Hirapra <milan@buddyboss.com>
2023-03-16 16:46:58 +0530

    PROD-4105 : Allow to show blocked user group activities

--- src/bp-moderation/classes/class-bp-moderation-activity.php
+++ src/bp-moderation/classes/class-bp-moderation-activity.php
@@ -140,6 +140,11 @@ class BP_Moderation_Activity extends BP_Moderation_Abstract {
 			$where['moderation_where'] = $sql;
 		}
 
+		if ( isset( $where['moderation_where'] ) && ! empty( $where['moderation_where'] ) ) {
+			$where['moderation_where'] .= ' AND ';
+		}
+		$where['moderation_where'] .= '( a.user_id NOT IN ( ' . bb_moderation_get_blocked_by_sql() . ' ) )';
+
 		return $where;
 	}
 
