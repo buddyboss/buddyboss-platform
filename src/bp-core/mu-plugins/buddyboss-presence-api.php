4f4275f567e6dc1d0ea7ef93aa14639846b4289e
jitendrabanjara1991 <jitendra@buddyboss.com>
2023-03-16 12:03:48 +0530

    PROD-4846 - load BB Presence file in mu plugin

--- src/bp-core/mu-plugins/buddyboss-presence-api.php
+++ src/bp-core/mu-plugins/buddyboss-presence-api.php
@@ -16,3 +16,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! function_exists( 'is_plugin_active' ) ) {
 	require_once ABSPATH . '/wp-admin/includes/plugin.php';
 }
+
+// File from the build version.
+$buddyboss_presence = WP_PLUGIN_DIR . '/buddyboss-platform/bp-core/classes/class-bb-presence.php';
+
+// File from the development version.
+$buddyboss_presence_dev = WP_PLUGIN_DIR . '/buddyboss-platform/src/bp-core/classes/class-bb-presence.php';
+
+if ( is_plugin_active( 'buddyboss-platform/bp-loader.php' ) ) {
+	if ( file_exists( $buddyboss_presence ) ) {
+		/**
+		 * Included File.
+		 */
+		require_once $buddyboss_presence;
+	} elseif ( file_exists( $buddyboss_presence_dev ) ) {
+		/**
+		 * Included File.
+		 */
+		require_once $buddyboss_presence_dev;
+	}
+}
