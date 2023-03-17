8bd0c375a3405831e1b41c9ce4109d8c76ca03a6
jitendrabanjara1991 <jitendra@buddyboss.com>
2023-03-17 16:20:12 +0530

    PROD-4846 - If we use SHORTINIT true then we need to allow some file for get current user id

--- src/bp-core/bb-core-native-presence.php
+++ src/bp-core/bb-core-native-presence.php
@@ -1,14 +1,49 @@
 <?php
 // specify that we need a minimum from WP
-//define( 'SHORTINIT', true );
+define( 'SHORTINIT', true );
 
 // Loading the WordPress environment
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php' );
 
+// Require files used for cookie-based user authentication
+require ABSPATH . WPINC . '/pluggable.php';
+require ABSPATH . WPINC . '/kses.php';
+require ABSPATH . WPINC . '/user.php';
+require ABSPATH . WPINC . '/capabilities.php';
+require ABSPATH . WPINC . '/class-wp-role.php';
+require ABSPATH . WPINC . '/class-wp-roles.php';
+require ABSPATH . WPINC . '/class-wp-user.php';
+require ABSPATH . WPINC . '/class-wp-session-tokens.php';
+require ABSPATH . WPINC . '/class-wp-user-meta-session-tokens.php';
+require ABSPATH . WPINC . '/l10n.php';
+require ABSPATH . WPINC . '/rest-api.php';
+/**
+ * 'WP_PLUGIN_URL' and others are used by: wp_cookie_constants()
+ */
+wp_plugin_directory_constants();
+
+/**
+ * 'ADMIN_COOKIE_PATH' and others are used by: wp_set_auth_cookie()
+ */
+if ( is_multisite() ) {
+	ms_cookie_constants();
+}
+
+/**
+ * 'SECURE_AUTH_COOKIE' and others are used by: wp_parse_auth_cookie()
+ */
+wp_cookie_constants();
+
+/**
+ * Sets: 'FORCE_SSL_ADMIN' and 'FORCE_SSL_LOGIN'
+ */
+wp_ssl_constants();
+
+
+// If user not logged in then return.
 if ( ! is_user_logged_in() ) {
 	return;
 }
-
 
 if ( isset( $_GET['direct_allow'] ) ) {
 	$return = array(
