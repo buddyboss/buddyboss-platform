c6305f2481008261b791c162dc3caefc7ec809c0
jitendrabanjara1991 <jitendra@buddyboss.com>
2023-03-17 12:36:12 +0530

    PROD-4846 - Disable shortint because if we enable then we didn't get current user id. Also called bb_is_online_user_mu_cache function from BB_Presence class to check user is online or not

--- src/bp-core/bb-core-native-presence.php
+++ src/bp-core/bb-core-native-presence.php
@@ -1,13 +1,37 @@
 <?php
 // specify that we need a minimum from WP
-define( 'SHORTINIT', true );
+//define( 'SHORTINIT', true );
 
 // Loading the WordPress environment
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php' );
+
+if ( ! is_user_logged_in() ) {
+	return;
+}
+
 
 if ( isset( $_GET['direct_allow'] ) ) {
 	$return = array(
 		'direct_allow' => true,
 	);
 	wp_send_json_success( $return );
+}
+
+// Include BB_Presence class.
+include_once __DIR__ . '/classes/class-bb-presence.php';
+
+if ( isset( $_POST['ids'] ) ) {
+
+	BB_Presence::bb_update_last_activity( get_current_user_id() );
+
+	$users         = $_POST['ids'];
+	$presence_data = array();
+	foreach ( array_unique( $users ) as $user_id ) {
+		$presence_data[] = array(
+			'id'     => $user_id,
+			'status' => BB_Presence::bb_is_online_user_mu_cache( $user_id ),
+		);
+	}
+
+	return wp_send_json( $presence_data );
 }
