325ce8faf190c43d429408a176ca3b150f3b8984
Krunal Palaniya <krunal@buddyboss.com>
2023-03-20 19:30:29 +0530

    PROD-4362 - Fixed QA reported PHP Deprecated errors

--- src/bp-video/bp-video-functions.php
+++ src/bp-video/bp-video-functions.php
@@ -850,7 +850,7 @@ function bp_video_add_generate_thumb_background_process( $video_id ) {
 		global $bp_background_updater;
 		$ffmpeg = bb_video_check_is_ffmpeg_binary();
 
-		if ( ! empty( trim( $ffmpeg->error ) ) ) {
+		if ( ! empty( $ffmpeg->error ) && ! empty( trim( $ffmpeg->error ) ) ) {
 			return;
 		}
 
