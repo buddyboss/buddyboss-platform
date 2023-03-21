398c8d770b665297ba974c64864491f4610644d5
Kartik Suthar <kartik@buddyboss.com>
2023-03-21 16:28:35 +0530

    PROD-4846 - implement raw code to support presence API in MU level

--- src/bp-core/classes/class-bb-presence.php
+++ src/bp-core/classes/class-bb-presence.php
@@ -427,9 +427,367 @@ if ( ! class_exists( 'BB_Presence' ) ) {
 		 * @return void
 		 */
 		public function bb_presence_mu_loader() {
+			add_action( 'set_current_user', array( $this, 'bb_is_set_current_user' ), 1 );
 
-			error_log( print_r( 'mu loaded', 1 ) );
+			add_filter( 'rest_cache_pre_current_user_id', array( $this, 'bb_cookie_support' ), 1 );
+			add_filter( 'rest_cache_pre_current_user_id', array( $this, 'bb_jwt_auth_support' ), 2 );
+
+			$this->prepare_presence_mu();
 		}
+
+		/**
+		 * Identify the current user is set by WordPress.
+		 */
+		public function bb_is_set_current_user() {
+			global $bb_is_current_user_available;
+			$bb_is_current_user_available = true;
+		}
+
+		/**
+		 * Tells if the current user information is available on WordPress.
+		 */
+		public function is_current_user_available() {
+			global $bb_is_current_user_available;
+			if ( isset( $bb_is_current_user_available ) ) {
+				return $bb_is_current_user_available;
+			}
+
+			return false;
+		}
+
+		/**
+		 * Check if user logged in or not.
+		 */
+		public function get_loggedin_user_id() {
+
+			if ( $this->is_current_user_available() ) {
+				return get_current_user_id();
+			} else {
+				$guessed_user_id = $this->get_guessed_user_id();
+				if ( ! $guessed_user_id ) {
+					return 0;
+				}
+
+				return $guessed_user_id;
+			}
+		}
+
+		/**
+		 * Guessed user ID.
+		 *
+		 * @return int|boolean
+		 */
+		public function get_guessed_user_id() {
+			$guessed_user_id = apply_filters( 'rest_cache_pre_current_user_id', false );
+
+			return $guessed_user_id;
+		}
+
+		/**
+		 * Get Pre User ID from WordPress Cookie.
+		 *
+		 * @param int $user_id User ID.
+		 *
+		 * @return int
+		 */
+		public function bb_cookie_support( $user_id ) {
+			$scheme          = apply_filters( 'auth_redirect_scheme', '' );
+			$cookie_elements = $this->wp_parse_auth_cookie( '', $scheme );
+
+			if ( $cookie_elements && isset( $cookie_elements['username'] ) ) {
+				global $wpdb;
+
+				// @todo: any idea to avoid this query ?
+				// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
+				$get_user = $wpdb->get_row( $wpdb->prepare( "SELECT ID FROM $wpdb->users WHERE user_login=%s", $cookie_elements['username'] ) );
+
+				if ( $get_user ) {
+					return $get_user->ID;
+				}
+			}
+		}
+
+		/**
+		 * Copied from wp-includes/pluggable.php.
+		 *
+		 * @see wp-includes/pluggable.php
+		 *
+		 * @param string $cookie Cookie value.
+		 * @param string $scheme Schema.
+		 *
+		 * @return array|bool
+		 */
+		public function wp_parse_auth_cookie( $cookie = '', $scheme = '' ) {
+			if ( empty( $cookie ) ) {
+
+				// @see wp_cookie_constants()..
+				$siteurl = get_site_option( 'siteurl' );
+				if ( $siteurl ) {
+					$cookie_hash = md5( $siteurl );
+				} else {
+					$cookie_hash = '';
+				}
+
+				// @see wp_cookie_constants()..
+				if ( is_ssl() ) {
+					$cookie_name = 'wordpress_sec_' . $cookie_hash;
+					$scheme      = 'secure_auth';
+				} else {
+					$cookie_name = 'wordpress_' . $cookie_hash;
+					$scheme      = 'auth';
+				}
+
+				if ( empty( $_COOKIE[ $cookie_name ] ) ) {
+					return false;
+				}
+
+				// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.ValidatedSanitizedInput.MissingUnslash
+				$cookie = $_COOKIE[ $cookie_name ];
+			}
+
+			$cookie_elements = explode( '|', $cookie );
+			if ( count( $cookie_elements ) !== 4 ) {
+				return false;
+			}
+
+			list( $username, $expiration, $token, $hmac ) = $cookie_elements;
+
+			return compact( 'username', 'expiration', 'token', 'hmac', 'scheme' );
+		}
+
+		/**
+		 * Get the Pre User ID from BuddyBoss APP JWT Token.
+		 *
+		 * @param int $user_id User ID.
+		 *
+		 * @return int|void
+		 */
+		public function bb_jwt_auth_support( $user_id ) {
+
+			$header = $this->get_all_headers();
+
+			$jwt_token = false;
+			if ( ! empty( $header ) ) {
+				foreach ( $header as $k => $v ) {
+					if ( strtolower( $k ) === 'accesstoken' ) {
+						$jwt_token = $v;
+						break;
+					}
+				}
+			}
+
+			if ( $jwt_token ) {
+
+				$token = explode( '.', $jwt_token );
+				// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_decode
+				$token = (array) json_decode( base64_decode( $token[1] ) );
+
+				if ( isset( $token['data'] ) && isset( $token['data']->user ) && isset( $token['data']->user->id ) ) {
+					$user_id = $token['data']->user->id;
+
+					// Check if there is any switch user.
+					$switch_data = get_user_meta( $user_id, '_bbapp_jwt_switch_user', true );
+					$switch_data = ( ! is_array( $switch_data ) ) ? array() : $switch_data;
+					$jti         = ( isset( $token['jti'] ) ) ? $token['jti'] : false;
+
+					// if switch user is found for current access token pass it.
+					if ( $jti && isset( $switch_data[ $jti ] ) && is_numeric( $switch_data[ $jti ] ) ) {
+						return (int) $switch_data[ $jti ];
+					}
+
+					// End Switch user logic's.
+
+					return $user_id;
+				}
+			}
+
+		}
+
+		/**
+		 * Get Headers.
+		 *
+		 * @return array|false|string
+		 */
+		public function get_all_headers() {
+
+			if ( function_exists( 'getallheaders' ) ) {
+				return getallheaders();
+			}
+
+			if ( ! is_array( $_SERVER ) ) {
+				return array();
+			}
+
+			$headers = array();
+			foreach ( $_SERVER as $name => $value ) {
+				if ( substr( $name, 0, 5 ) === 'HTTP_' ) {
+					$headers[ str_replace( ' ', '-', ucwords( strtolower( str_replace( '_', ' ', substr( $name, 5 ) ) ) ) ) ] = $value;
+				}
+			}
+
+			return $headers;
+		}
+
+		/**
+		 * Returns the current web location.
+		 */
+		public function get_current_path() {
+			return add_query_arg( null, null );
+		}
+
+		/**
+		 * Get current endpoint.
+		 *
+		 * @todo :- research the correct wp way to get current endpoint. I doubt on this.
+		 */
+		public function get_current_endpoint() {
+			$current_path = $this->get_current_path();
+			if ( strpos( $current_path, 'wp-json/' ) !== false ) {
+
+				$current_path = explode( 'wp-json/', $current_path );
+				$current_path = $current_path[1];
+
+				// remove query vars.
+				if ( strpos( $current_path, '?' ) !== false ) {
+					$current_path = explode( '?', $current_path );
+					$current_path = $current_path[0];
+				}
+
+				return trim( $current_path );
+
+			}
+
+			return false;
+		}
+
+		public function prepare_presence_mu() {
+
+			// Check if we are in WP API.
+			if ( strpos( $this->get_current_path(), 'wp-json/buddyboss/v1/members/presence' ) !== false ) {
+
+				/**
+				 * Remove WordPress Extra Headaches.
+				 */
+
+				// Tell WordPress to Don't Load Theme.
+				add_filter(
+					'wp_using_themes',
+					function ( $wp_use_themes ) {
+						$wp_use_themes = false;
+
+						return $wp_use_themes;
+					}
+				);
+				add_filter(
+					'option_stylesheet',
+					function ( $stylesheet ) {
+						return '';
+					}
+				);
+				add_filter(
+					'option_template',
+					function ( $template ) {
+						return '';
+					}
+				);
+
+				// Disable all plugins for this request as we will fire cache on init hook.
+				add_filter(
+					'option_active_plugins',
+					function ( $plugins ) {
+						if ( ! empty( $plugins ) ) {
+							foreach ( $plugins as $plugin_key => $plugin_val ) {
+								unset( $plugins[ $plugin_key ] );
+							}
+						}
+
+						return $plugins;
+					}
+				);
+
+				// Disable all plugins for this request as we will fire cache on init hook. Network Mode.
+				add_filter(
+					'option_active_sitewide_plugins',
+					function ( $plugins ) {
+						if ( ! empty( $plugins ) ) {
+							foreach ( $plugins as $plugin_key => $plugin_val ) {
+								unset( $plugins[ $plugin_key ] );
+							}
+						}
+
+						return $plugins;
+					}
+				);
+
+				// Disable all plugins for this request as we will fire cache on init hook. Network Mode.
+				add_filter(
+					'site_option_active_sitewide_plugins',
+					function ( $plugins ) {
+						if ( ! empty( $plugins ) ) {
+							foreach ( $plugins as $plugin_key => $plugin_val ) {
+								unset( $plugins[ $plugin_key ] );
+							}
+						}
+
+						return $plugins;
+					}
+				);
+
+				$user_id = $this->get_loggedin_user_id() ?? 0;
+
+				$ids = (array) ( isset( $_POST['ids'] ) ? wp_parse_id_list( $_POST['ids'] ) : array() );
+
+				$this->endpoint_cache_render( $user_id, $ids );
+
+				exit;
+			}
+
+		}
+
+		public function endpoint_cache_render( $user_id, $ids ) {
+			// Security Check.
+			// When the cache generated to user is not matched with it's being delivered to output error.
+			// Here we avoid passing another user cached instead of logged in.
+			if ( empty( $user_id ) ) {
+//				header( 'HTTP/1.0 401 Unauthorized' );
+//				header( 'Content-Type: application/json' );
+				$retval = new WP_Error(
+					'bp_rest_authorization_required_test',
+					__( 'Sorry, you are not allowed to perform this action.', 'buddypress' ),
+					array(
+						'status' => 401,
+					)
+				);
+
+				echo rest_convert_error_to_response( $retval );
+				exit;
+			}
+
+			$current_endpoint = $this->get_current_endpoint();
+
+			$header = apply_filters( 'rest_post_dispatch_header_cache', array(), $current_endpoint );
+
+			$header['bb-presence-mu-api'] = true;
+			if ( ! empty( $header ) ) {
+				foreach ( $header as $header_key => $header_value ) {
+					header( $header_key . ':' . $header_value );
+				}
+			}
+
+			$presence_data = array();
+			foreach ( array_unique( $ids ) as $user_id ) {
+				$presence_data[] = array(
+					'id'     => $user_id,
+					'status' => self::bb_get_user_presence_mu_cache( $user_id ),
+				);
+			}
+
+
+			echo wp_json_encode( apply_filters( 'rest_post_dispatch_cache', $presence_data, $current_endpoint ) );
+			exit;
+		}
+
+
 	}
 
 	BB_Presence::instance();
