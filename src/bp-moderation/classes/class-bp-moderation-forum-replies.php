b025f720c80b9f2d5ce368585c3abd10e4238ab9
jitendrabanjara1991 <jitendra@buddyboss.com>
2023-04-12 12:32:22 +0530

    PROD-4101 - Loggedin member can't see is blocked members replies in widget but Loggedin member can see replies in discussion

--- src/bp-moderation/classes/class-bp-moderation-forum-replies.php
+++ src/bp-moderation/classes/class-bp-moderation-forum-replies.php
@@ -135,12 +135,16 @@ class BP_Moderation_Forum_Replies extends BP_Moderation_Abstract {
 	public function update_where_sql( $where, $suspend ) {
 		$this->alias = $suspend->alias;
 
-		// Remove has blocked members reply from widget.
+		// Remove has blocked/is blocked members replies from widget.
 		if ( function_exists( 'bb_did_filter' ) && bb_did_filter( 'bbp_after_replies_widget_settings_parse_args' ) ) {
+			// Remove has blocked members replies from widget.
 			$sql = $this->exclude_where_query();
 			if ( ! empty( $sql ) ) {
 				$where['moderation_where'] = $sql;
 			}
+
+			// Remove is blocked members replies from widget.
+			$where['moderation_widget_forums'] = '( wp_posts.post_author NOT IN ( ' . bb_moderation_get_blocked_by_sql() . ' ) )';
 		}
 
 		return $where;
