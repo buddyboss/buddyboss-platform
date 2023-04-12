ed16afa6f02d65caae534c6c42f4e075cb16353b
jitendrabanjara1991 <jitendra@buddyboss.com>
2023-04-12 11:55:49 +0530

    PROD-4101 - Add doc comment

--- src/bp-moderation/classes/class-bp-moderation-forum-topics.php
+++ src/bp-moderation/classes/class-bp-moderation-forum-topics.php
@@ -138,6 +138,7 @@ class BP_Moderation_Forum_Topics extends BP_Moderation_Abstract {
 	public function update_where_sql( $where, $suspend ) {
 		$this->alias = $suspend->alias;
 
+		// Remove has blocked members discussion from widget.
 		if ( function_exists( 'bb_did_filter' ) && bb_did_filter( 'bbp_after_topic_widget_settings_parse_args' ) ) {
 			$sql = $this->exclude_where_query();
 			if ( ! empty( $sql ) ) {
