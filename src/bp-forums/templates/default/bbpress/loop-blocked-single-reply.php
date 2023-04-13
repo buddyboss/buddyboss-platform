3d3685024072d6c5faeb4286b060376a25b67f82
jitendrabanjara1991 <jitendra@buddyboss.com>
2023-04-13 19:09:21 +0530

    PROD-4101 - Fixed - If suspend to any member then content is displaying "This content has been hidden from site admin." for loggedin members content instead of loggedin members original content ( for admin user ).

--- src/bp-forums/templates/default/bbpress/loop-blocked-single-reply.php
+++ src/bp-forums/templates/default/bbpress/loop-blocked-single-reply.php
@@ -8,15 +8,17 @@
  * @package BuddyBoss\Theme
  */
 
-$reply_id           = bbp_get_reply_id();
-$reply_author_id    = bbp_get_reply_author_id( $reply_id );
-$is_user_blocked    = false;
-$is_user_suspended  = false;
-$is_user_blocked_by = false;
+$reply_id             = bbp_get_reply_id();
+$reply_author_id      = bbp_get_reply_author_id( $reply_id );
+$is_user_blocked      = false;
+$is_user_suspended    = false;
+$is_user_blocked_by   = false;
+$check_hidden_content = false;
 if ( bp_is_active( 'moderation' ) ) {
-	$is_user_suspended  = bp_moderation_is_user_suspended( $reply_author_id );
-	$is_user_blocked    = bp_moderation_is_user_blocked( $reply_author_id );
-	$is_user_blocked_by = bb_moderation_is_user_blocked_by( $reply_author_id );
+	$is_user_suspended    = bp_moderation_is_user_suspended( $reply_author_id );
+	$is_user_blocked      = bp_moderation_is_user_blocked( $reply_author_id );
+	$is_user_blocked_by   = bb_moderation_is_user_blocked_by( $reply_author_id );
+	$check_hidden_content = BP_Core_Suspend::check_hidden_content( $reply_id, BP_Moderation_Forum_Replies::$moderation_type );
 }
 ?>
 
@@ -48,7 +50,9 @@ if ( bp_is_active( 'moderation' ) ) {
 			} elseif ( $is_user_blocked ) {
 				$reply_content = bb_moderation_has_blocked_message( $reply_content, BP_Moderation_Forum_Replies::$moderation_type, $reply_id );
 			} else {
-				$reply_content = esc_html__( 'This content has been hidden from site admin.', 'buddyboss' );
+				if ( $check_hidden_content ) {
+					$reply_content = esc_html__( 'This content has been hidden from site admin.', 'buddyboss' );
+				}
 			}
 			echo $reply_content; // phpcs:ignore
 			?>
