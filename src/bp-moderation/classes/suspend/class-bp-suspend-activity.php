e618912537a448af6239d5827fc9bef9800b5542
jitendrabanjara1991 <jitendra@buddyboss.com>
2023-04-17 18:13:58 +0530

    PROD-4110 - Logged-in member CAN see posts and comments created by the member in a group for suspended member

--- src/bp-moderation/classes/suspend/class-bp-suspend-activity.php
+++ src/bp-moderation/classes/suspend/class-bp-suspend-activity.php
@@ -262,7 +262,17 @@ class BP_Suspend_Activity extends BP_Suspend_Abstract {
 		$where = apply_filters( 'bp_suspend_activity_get_where_conditions', $where, $this );
 
 		if ( ! empty( array_filter( $where ) ) ) {
-			$where_conditions['suspend_where'] = '( ' . implode( ' AND ', $where ) . ' )';
+
+			$exclude_group_sql = '';
+			// Allow group activities from blocked/suspended users.
+			if (
+				bp_is_active( 'groups' ) &&
+				function_exists( 'did_action' ) && ! did_action( 'get_template_part_activity/widget' )
+			) {
+				$exclude_group_sql = ' OR a.component = "groups"';
+			}
+
+			$where_conditions['suspend_where'] = '( ( ' . implode( ' AND ', $where ) . ' ) ' . $exclude_group_sql . ' )';
 		}
 
 		return $where_conditions;
