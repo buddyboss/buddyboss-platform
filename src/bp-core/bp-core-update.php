dd01c3aa5e1e2512867baae5caa1953a6100483c
Milankumar Hirapra <milan@buddyboss.com>
2023-05-02 11:33:01 +0530

    PROD-5158 : Update the updater function name

--- src/bp-core/bp-core-update.php
+++ src/bp-core/bp-core-update.php
@@ -423,7 +423,7 @@ function bp_version_updater() {
 		}
 
 		if ( $raw_db_version < 20001 ) {
-			bb_update_to_2_3_2();
+			bb_update_to_2_3_3();
 		}
 	}
 
@@ -2624,7 +2624,7 @@ function bb_update_to_2_3_1() {
  *
  * @return void
  */
-function bb_update_to_2_3_2() {
+function bb_update_to_2_3_3() {
 	bb_repair_member_unique_slug();
 }
 
