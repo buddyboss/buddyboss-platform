b82ad2e0901f310b601b7783acf49b632f5f5899
Milankumar Hirapra <milan@buddyboss.com>
2023-05-08 16:31:13 +0530

    PROD-5075 : Make separate function for tools and updater

--- src/bp-members/bp-members-filters.php
+++ src/bp-members/bp-members-filters.php
@@ -747,9 +747,9 @@ function bb_load_member_type_label_custom_css() {
 					isset( $label_color_data['color_type'] ) &&
 					'custom' === $label_color_data['color_type']
 				) {
-					$background_color       = isset( $label_color_data['background-color'] ) ? $label_color_data['background-color'] : '';
-					$text_color             = isset( $label_color_data['color'] ) ? $label_color_data['color'] : '';
-					$class_name             = 'body .bp-member-type.bb-current-member-' . $type;
+					$background_color        = isset( $label_color_data['background-color'] ) ? $label_color_data['background-color'] : '';
+					$text_color              = isset( $label_color_data['color'] ) ? $label_color_data['color'] : '';
+					$class_name              = 'body .bp-member-type.bb-current-member-' . $type;
 					$member_type_custom_css .= $class_name . ' {' . "background-color:$background_color;" . '}';
 					$member_type_custom_css .= $class_name . ' {' . "color:$text_color;" . '}';
 				}
@@ -821,93 +821,55 @@ function bb_repair_member_profile_links( $repair_list ) {
  * @since BuddyBoss 2.3.1
  *
  * @param bool $is_background The current process is background or not.
- * @param int  $paged         The page number.
  *
  * @return array|void
  */
-function bb_repair_member_profile_links_callback( $is_background = false, $paged = 1 ) {
+function bb_repair_member_profile_links_callback( $is_background = false ) {
 	if ( ! bp_is_active( 'members' ) ) {
 		return;
 	}
 
 	global $wpdb;
 
-	// Define default values.
-	$per_page = 50;
-	$offset   = 0;
+	// phpcs:ignore WordPress.Security.NonceVerification.Missing, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.ValidatedSanitizedInput.MissingUnslash
+	$offset   = isset( $_POST['offset'] ) ? (int) ( $_POST['offset'] ) : 0;
+	$per_page = 20;
 
-	if ( ! $is_background ) {
-
-		// phpcs:ignore WordPress.Security.NonceVerification.Missing, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.ValidatedSanitizedInput.MissingUnslash
-		$offset   = isset( $_POST['offset'] ) ? (int) ( $_POST['offset'] ) : 0;
-		$per_page = 20;
-
-		// Set limit while repair the member slug.
-		$user_ids = $wpdb->get_col(
-			$wpdb->prepare(
-				"SELECT u.ID FROM `{$wpdb->prefix}users` AS u LEFT JOIN `{$wpdb->prefix}usermeta` AS um ON ( u.ID = um.user_id AND um.meta_key = %s ) WHERE ( um.user_id IS NULL OR LENGTH(meta_value) = %d ) ORDER BY u.ID ASC LIMIT %d, %d",
-				'bb_profile_slug',
-				40,
-				0,
-				$per_page
-			)
-		);
-	} else {
-
-		// Get all users who have not generate unique slug while it runs from background.
-		$user_ids = $wpdb->get_col(
-			$wpdb->prepare(
-				"SELECT u.ID FROM `{$wpdb->prefix}users` AS u LEFT JOIN `{$wpdb->prefix}usermeta` AS um ON ( u.ID = um.user_id AND um.meta_key = %s ) WHERE um.user_id IS NULL ORDER BY u.ID ASC",
-				'bb_profile_slug'
-			)
-		);
-	}
-
-	global $bp_background_updater;
+	// Set limit while repair the member slug.
+	$user_ids = $wpdb->get_col(
+		$wpdb->prepare(
+			"SELECT u.ID FROM `{$wpdb->prefix}users` AS u LEFT JOIN `{$wpdb->prefix}usermeta` AS um ON ( u.ID = um.user_id AND um.meta_key = %s ) WHERE ( um.user_id IS NULL OR LENGTH(meta_value) = %d ) ORDER BY u.ID ASC LIMIT %d, %d",
+			'bb_profile_slug',
+			40,
+			0,
+			$per_page
+		)
+	);
 
 	if ( ! is_wp_error( $user_ids ) && ! empty( $user_ids ) ) {
-		if ( $is_background ) {
+		bb_set_bulk_user_profile_slug( $user_ids );
 
-			foreach ( array_chunk( $user_ids, $per_page ) as $chunked_user_ids ) {
-				$bp_background_updater->data(
-					array(
-						array(
-							'callback' => 'bb_set_bulk_user_profile_slug',
-							'args'     => array( $chunked_user_ids ),
-						),
-					)
-				);
-				$bp_background_updater->save()->dispatch();
-			}
+		$total           = $offset + count( $user_ids );
+		$records_updated = sprintf(
+		/* translators: total user */
+			_n( '%d user unique identifier generated successfully', '%d users unique identifier generated successfully', $total, 'buddyboss' ),
+			$total
+		);
 
-		} else {
-			bb_set_bulk_user_profile_slug( $user_ids );
-
-			$total           = $offset + count( $user_ids );
-			$records_updated = sprintf(
-				/* translators: total user */
-				_n( '%d user unique identifier generated successfully', '%d users unique identifier generated successfully', $total, 'buddyboss' ),
-				$total
-			);
-
-			return array(
-				'status'  => 'running',
-				'offset'  => $total,
-				'records' => $records_updated,
-			);
-		}
+		return array(
+			'status'  => 'running',
+			'offset'  => $total,
+			'records' => $records_updated,
+		);
 	} else {
-		if ( ! $is_background ) {
+		/* translators: Status of current action. */
+		$statement = __( 'Profile unique identifier generated for all users; %s', 'buddyboss' );
 
-			/* translators: Status of current action. */
-			$statement = __( 'Profile unique identifier generated for all users; %s', 'buddyboss' );
-
-			// All done!
-			return array(
-				'status'  => 1,
-				'message' => sprintf( $statement, __( 'Complete!', 'buddyboss' ) ),
-			);
-		}
+		// All done!
+		return array(
+			'status'  => 1,
+			'message' => sprintf( $statement, __( 'Complete!', 'buddyboss' ) ),
+		);
 	}
 }
 
@@ -946,6 +908,43 @@ function bb_set_default_member_type_to_activate_user_on_admin( $user_id, $member
 			}
 			// Add role.
 			$bp_user->add_role( $selected_member_type_wp_roles[0] );
+		}
+	}
+}
+
+/**
+ * This function will work as migration process which will repair member profile links.
+ *
+ * @since BuddyBoss [BBVERSION]
+ *
+ * @return array|void
+ */
+function bb_generate_member_profile_links_on_update() {
+	if ( ! bp_is_active( 'members' ) ) {
+		return;
+	}
+
+	global $wpdb, $bp_background_updater;
+
+	// Get all users who have not generate unique slug while it runs from background.
+	$user_ids = $wpdb->get_col(
+		$wpdb->prepare(
+			"SELECT u.ID FROM `{$wpdb->prefix}users` AS u LEFT JOIN `{$wpdb->prefix}usermeta` AS um ON ( u.ID = um.user_id AND um.meta_key = %s ) WHERE um.user_id IS NULL ORDER BY u.ID ASC",
+			'bb_profile_slug'
+		)
+	);
+
+	if ( ! is_wp_error( $user_ids ) && ! empty( $user_ids ) ) {
+		foreach ( array_chunk( $user_ids, 50 ) as $chunked_user_ids ) {
+			$bp_background_updater->data(
+				array(
+					array(
+						'callback' => 'bb_set_bulk_user_profile_slug',
+						'args'     => array( $chunked_user_ids ),
+					),
+				)
+			);
+			$bp_background_updater->save()->dispatch();
 		}
 	}
 }
