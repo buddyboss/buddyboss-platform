e2ec3bd0f03a14ed832eda2889eea6457c40ff40
Milankumar Hirapra <milan@buddyboss.com>
2023-05-30 15:20:25 +0530

    PROD-815 : If group leader sync with admin then moderators will not change

--- src/bp-integrations/learndash/library/SyncGenerator.php
+++ src/bp-integrations/learndash/library/SyncGenerator.php
@@ -744,12 +744,12 @@ class SyncGenerator {
 	protected function addUserToBpGroup( $userId, $type, $remove ) {
 		$groupMember = new BP_Groups_Member( $userId, $this->bpGroupId );
 
+		$syncTo = $this->getLdSyncToRole( $type );
+
 		// ignore moderator in syncing as there's no moderator in learndash.
-		if ( 1 === $groupMember->is_mod ) {
+		if ( 1 === $groupMember->is_mod && 'admin' === $syncTo ) {
 			return false;
 		}
-
-		$syncTo = $this->getLdSyncToRole( $type );
 
 		if ( $remove ) {
 			if ( bp_is_active( 'messages' ) ) {
