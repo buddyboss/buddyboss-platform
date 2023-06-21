70724e88d98c6fe1316516794e96546d96b65cdc
Milankumar Hirapra <milan@buddyboss.com>
2023-06-21 09:12:26 +0530

    PROD-3652 : Fixed count of all/active/inactive components

--- src/bp-core/admin/bp-core-admin-components.php
+++ src/bp-core/admin/bp-core-admin-components.php
@@ -70,18 +70,6 @@ function bp_core_admin_components_options() {
 	unset( $optional_components['document'] );
 	unset( $optional_components['video'] );
 
-	if ( isset( $active_components['document'] ) ) {
-		unset( $active_components['document'] );
-	}
-
-	if ( isset( $active_components['video'] ) ) {
-		unset( $active_components['video'] );
-	}
-
-	if ( isset( $active_components['blogs'] ) ) {
-		unset( $active_components['blogs'] );
-	}
-
 	// Merge optional and required together.
 	$all_components = $required_components + $optional_components;
 
@@ -117,9 +105,10 @@ function bp_core_admin_components_options() {
 	 */
 
 	// Get the total count of all plugins.
-	$all_count = count( $all_components );
-	$page      = bp_core_do_network_admin() ? 'admin.php' : 'admin.php';
-	$action    = ! empty( $_GET['action'] ) ? $_GET['action'] : 'all';
+	$all_count    = count( $all_components );
+	$active_count = $all_count - count( $inactive_components );
+	$page         = bp_core_do_network_admin() ? 'admin.php' : 'admin.php';
+	$action       = ! empty( $_GET['action'] ) ? $_GET['action'] : 'all';
 
 	switch ( $action ) {
 		case 'all':
@@ -186,7 +175,7 @@ function bp_core_admin_components_options() {
 	<?php
 	if ( $action === 'active' ) :
 		?>
-		class="current"<?php endif; ?>><?php printf( __( 'Active <span class="count">(%s)</span>', 'buddyboss' ), bp_core_number_format( count( $active_components ) ) ); ?></a> | </li>
+		class="current"<?php endif; ?>><?php printf( __( 'Active <span class="count">(%s)</span>', 'buddyboss' ), bp_core_number_format( $active_count ) ); ?></a> | </li>
 		<li><a href="
 		<?php
 		echo esc_url(
