eea5df3b226982754822823ab4ecf4f596e725c4
Suraj Singh <suraj.sk243@gmail.com>
2023-07-31 14:32:31 +0530

    PROD-5591 Fixed issue to show groups with 0 member

--- src/bp-groups/classes/class-bp-groups-group.php
+++ src/bp-groups/classes/class-bp-groups-group.php
@@ -1180,7 +1180,7 @@ class BP_Groups_Group {
 		);
 
 		if ( ! empty( $r['user_id'] ) || ! empty( $r['show_hidden'] ) ) {
-			$sql['from'] .= " JOIN {$bp->groups->table_name_members} m ON ( g.id = m.group_id )";
+			$sql['from'] .= " LEFT JOIN {$bp->groups->table_name_members} m ON ( g.id = m.group_id )";
 		}
 
 		$where_conditions = array();
