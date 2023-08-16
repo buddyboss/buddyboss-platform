8e6ea99808e8a6634e8fd600ee738a1554ca36ac
jitendrabanjara1991 <jitendra@buddyboss.com>
2023-08-16 11:46:51 +0530

    PROD-5712 - Create new class for reaction

---
+++ src/bp-core/classes/class-bb-reaction.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Reaction class.
+ *
+ * @package BuddyBoss\Core
+ * @since BuddyBoss [BBVERSION]
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'BB_Reaction' ) ) {
+
+	/**
+	 * BuddyBoss Reaction object.
+	 *
+	 * @since BuddyBoss [BBVERSION]
+	 */
+	class BB_Reaction {
+
+		/**
+		 * The single instance of the class.
+		 *
+		 * @since BuddyBoss [BBVERSION]
+		 *
+		 * @var self
+		 */
+		private static $instance = null;
+
+		/**
+		 * Get the instance of this class.
+		 *
+		 * @since BuddyBoss [BBVERSION]
+		 *
+		 * @return Controller|BB_Reaction|null
+		 */
+		public static function instance() {
+
+			if ( null === self::$instance ) {
+				$class_name     = __CLASS__;
+				self::$instance = new $class_name();
+			}
+
+			return self::$instance;
+		}
+	}
+}
