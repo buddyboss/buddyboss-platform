61fbf9d1bf11f515a00ec5c8077869f90482b8f4
jitendrabanjara1991 <jitendra@buddyboss.com>
2023-08-16 13:50:53 +0530

    PROD-5712 - Introduce new function to add reaction, Once reaction update sucessfully then store transient. Also introduce new function to fetch all reactions

--- src/bp-core/classes/class-bb-reaction.php
+++ src/bp-core/classes/class-bb-reaction.php
@@ -129,5 +129,118 @@ if ( ! class_exists( 'BB_Reaction' ) ) {
 				)
 			);
 		}
+
+		/**
+		 * Add new reaction.
+		 *
+		 * @since BuddyBoss [BBVERSION]
+		 *
+		 * @param array $args {
+		 *                    Reaction arguments.
+		 * @type string $name Name of the reaction.
+		 * @type string $icon Icon filename or uploaded file.
+		 *                    }
+		 */
+		public function bb_add_reaction( $args ) {
+			$r = bp_parse_args(
+				$args,
+				array(
+					'name' => '',
+					'icon' => '',
+				)
+			);
+
+			$post_title = ! empty( $r['name'] ) ? sanitize_title( $r['name'] ) : '';
+			if ( empty( $post_title ) ) {
+				return;
+			}
+
+			// Validate if a duplicate name exists before adding.
+			$existing_reaction = get_page_by_path( $post_title, OBJECT, self::$post_type );
+			if ( $existing_reaction ) {
+				return;
+			}
+
+			$post_content = array(
+				'name' => $r['name'],
+				'icon' => ! empty( $r['icon'] ) ? $r['icon'] : '',
+			);
+
+			// Prepare reaction data.
+			$reaction_data = array(
+				'post_title'   => $r['name'],
+				'post_name'    => $post_title,
+				'post_type'    => self::$post_type,
+				'post_status'  => 'publish',
+				'post_content' => maybe_serialize( $post_content ),
+				'post_author'  => bp_loggedin_user_id(),
+			);
+
+			// Insert the new reaction.
+			$reaction_id = wp_insert_post( $reaction_data );
+
+			// If the reaction was successfully added, update the transient.
+			if ( ! is_wp_error( $reaction_id ) ) {
+				// Update bb_reactions transient.
+				$this->bb_update_reactions_transient();
+			}
+		}
+
+		/**
+		 * Update the bb_reactions transient.
+		 *
+		 * @since BuddyBoss [BBVERSION]
+		 */
+		private function bb_update_reactions_transient() {
+			// Clear the existing transient.
+			delete_transient( 'bb_reactions' );
+
+			// Fetch existing reactions.
+			$all_reactions  = $this->bb_get_reactions();
+			$reactions_data = array();
+			if ( ! empty( $all_reactions ) ) {
+				foreach ( $all_reactions as $reaction ) {
+					$reaction_data = ! empty( $reaction->post_content ) ? maybe_unserialize( $reaction->post_content ) : '';
+					if (
+						! empty( $reaction_data ) &&
+						is_array( $reaction_data ) &&
+						isset( $reaction_data['name'] ) &&
+						isset( $reaction_data['icon'] )
+					) {
+						$reactions_data[] = array(
+							'id'   => $reaction->ID,
+							'name' => $reaction_data['name'],
+							'icon' => $reaction_data['icon'],
+						);
+					}
+				}
+			}
+
+			$reactions_data = ! empty( $reactions_data ) ? maybe_serialize( $reactions_data ) : '';
+			// Update the transient.
+			set_transient( 'bb_reactions', $reactions_data );
+		}
+
+		/**
+		 * Get all reaction data.
+		 *
+		 * @since BuddyBoss [BBVERSION]
+		 *
+		 * @return array
+		 */
+		private function bb_get_reactions() {
+			$args = array(
+				'fields'                 => array( 'ids', 'post_title', 'post_content' ),
+				'post_type'              => self::$post_type,
+				'posts_per_page'         => - 1,
+				'orderby'                => 'menu_order',
+				'post_status'            => 'publish',
+				'suppress_filters'       => false,
+				'update_post_meta_cache' => false,
+				'update_post_term_cache' => false,
+			);
+
+			return get_posts( $args );
+		}
 	}
 }
