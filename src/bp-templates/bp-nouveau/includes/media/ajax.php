ef729ba56e0cbdcbdca9f99f1e0bb3907cdacd27
Milankumar Hirapra <milan@buddyboss.com>
2023-10-04 10:33:19 +0530

    PROD-6054 : Updated text based on directory page

--- src/bp-templates/bp-nouveau/includes/media/ajax.php
+++ src/bp-templates/bp-nouveau/includes/media/ajax.php
@@ -457,7 +457,7 @@ function bp_nouveau_ajax_media_delete() {
 			?>
 			<aside class="bp-feedback bp-messages info">
 				<span class="bp-icon" aria-hidden="true"></span>
-				<p><?php esc_html_e( 'Sorry, no photos were found', 'buddyboss' ); ?></p>
+				<p><?php ( bp_is_active( 'video' ) && ( bp_is_profile_video_support_enabled() && bp_is_user_albums() ) || ( bp_is_group_video_support_enabled() && bp_is_group_albums() ) ) ? esc_html_e( 'Sorry, no photos or videos were found.', 'buddyboss' ) : esc_html_e( 'Sorry, no photos were found.', 'buddyboss' ); ?></p>
 			</aside>
 			<?php
 		}
@@ -498,7 +498,7 @@ function bp_nouveau_ajax_media_delete() {
 			?>
 			<aside class="bp-feedback bp-messages info">
 				<span class="bp-icon" aria-hidden="true"></span>
-				<p><?php esc_html_e( 'Sorry, no photos were found', 'buddyboss' ); ?></p>
+				<p><?php ( bp_is_active( 'video' ) && ( bp_is_profile_video_support_enabled() && bp_is_user_albums() ) || ( bp_is_group_video_support_enabled() && bp_is_group_albums() ) ) ? esc_html_e( 'Sorry, no photos & videos were found.', 'buddyboss' ) : esc_html_e( 'Sorry, no photos were found.', 'buddyboss' ); ?></p>
 			</aside>
 			<?php
 		}
