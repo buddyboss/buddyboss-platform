4cd7022853f8749997c664c22b23561c15e31a31
Kartik Suthar <kartik@buddyboss.com>
2023-10-16 16:28:24 +0530

    PROD-6275 - fix the issue with the bb_activity_like_reaction background jobs flooding

--- src/bp-activity/bp-activity-functions.php
+++ src/bp-activity/bp-activity-functions.php
@@ -6134,10 +6134,14 @@ function bp_activity_get_types_list() {
  * Activity migration.
  *
  * @since BuddyBoss 2.4.30
+ * @since BuddyBoss [BBVERSION] Added support for the $raw_db_version and $current_db.
+ *
+ * @param int $raw_db_version Raw database version.
+ * @param int $current_db Current DB version.
  *
  * @return void
  */
-function bb_activity_migration() {
+function bb_activity_migration( $raw_db_version, $current_db ) {
 
 	/**
 	 * Like migration into reaction.
@@ -6148,8 +6152,18 @@ function bb_activity_migration() {
 		bb_load_reaction()->create_table();
 		bb_load_reaction()->bb_register_activity_like();
 
+		$is_already_run = get_transient( 'bb_migrate_activity_reaction' );
+
 		// Migration Like to Reaction.
-		if ( function_exists( 'bb_migrate_activity_like_reaction' ) ) {
+		if (
+			! $is_already_run &&
+			(
+				$raw_db_version < 20601 || // Reaction release version.
+				$raw_db_version < 20674 // Last release version.
+			) &&
+			$current_db >= 20674 // Current DB version.
+		) {
+			set_transient( 'bb_migrate_activity_reaction', 'yes', HOUR_IN_SECONDS );
 			bb_migrate_activity_like_reaction();
 		}
 	}
@@ -6178,7 +6192,7 @@ function bb_migrate_activity_like_reaction( $paged = 1 ) {
 
 	$results = $wpdb->get_results(
 		$wpdb->prepare(
-			"SELECT * FROM {$bp->activity->table_name_meta} WHERE meta_key = 'bp_favorite_users' ORDER BY activity_id LIMIT %d offset %d",
+			"SELECT * FROM {$bp->activity->table_name_meta} WHERE meta_key = 'bp_favorite_users' ORDER BY activity_id ASC LIMIT %d offset %d",
 			$per_page,
 			$offset
 		)
@@ -6191,13 +6205,24 @@ function bb_migrate_activity_like_reaction( $paged = 1 ) {
 	$bb_background_updater->push_to_queue(
 		array(
 			'type'     => 'migration',
-			'group'    => 'bb_activity_like_reaction',
-			'priority' => 5,
+			'group'    => 'bb_activity_like_reaction_migration',
+			'priority' => 4,
 			'callback' => 'bb_activity_like_reaction_background_process',
 			'args'     => array( $results, $paged, $reaction_id ),
 		)
 	);
 	$bb_background_updater->save()->schedule_event();
+
+	// Delete previous existing migration from background jobs table.
+	$table_name = $bb_background_updater::$table_name;
+	// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
+	$wpdb->query(
+		$wpdb->prepare(
+			"DELETE FROM {$table_name} WHERE `type` = %s AND `group` = %s ORDER BY id ASC limit 500", // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+			'migration',
+			'bb_activity_like_reaction'
+		)
+	);
 }
 
 /**
--- src/bp-core/bp-core-update.php
+++ src/bp-core/bp-core-update.php
@@ -494,7 +494,7 @@ function bp_version_updater() {
 
 			// Run migration about activity.
 			if ( function_exists( 'bb_activity_migration' ) ) {
-				bb_activity_migration();
+				bb_activity_migration( $raw_db_version, $current_db );
 			}
 		}
 	}
