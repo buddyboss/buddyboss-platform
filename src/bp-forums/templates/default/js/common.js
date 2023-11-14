1d2fdf2ea8998a3e9281e57c189995b1e850c9f6
Md. Rezaul Islam Khan <rezaul@buddyboss.com>
2023-11-14 10:55:07 +0600

    PROD-6350 - Removed extra spacing from topic and reply when removing URLs from the post
    PROD-6350 - Removed extra spacing from topic and reply when removing URLs from the post

--- src/bp-forums/templates/default/js/common.js
+++ src/bp-forums/templates/default/js/common.js
@@ -342,5 +342,23 @@ jQuery( document ).ready(
 			} );
 		}
 
+        jQuery( document ).on( 'input', '.bbp_editor_topic_content', function ( e ) {
+            var content = jQuery( e.currentTarget)[0];
+            if ( content.innerHTML.replace( /<p>/gi, '' ).replace( /<\/p>/gi, '' ).replace( /<br>/gi, '' ) === '' ) {
+                var topic_content = '';
+                content.innerHTML = topic_content;
+                jQuery( '#bbp_topic_content' ).val( topic_content );
+            }
+        });
+
+        jQuery( document ).on( 'input', '.bbp_editor_reply_content', function ( e ) {
+            var content = jQuery( e.currentTarget)[0];
+            if ( content.innerHTML.replace( /<p>/gi, '' ).replace( /<\/p>/gi, '' ).replace( /<br>/gi, '' ) === '' ) {
+                var reply_content = '';
+                content.innerHTML = reply_content;
+                jQuery( '#bbp_reply_content' ).val( reply_content );
+            }
+        });
+
 	}
 );
