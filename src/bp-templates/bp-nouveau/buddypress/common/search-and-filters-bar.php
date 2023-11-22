221b7fd40fc575846a479e50c92a19092a5096fe
Kartik Suthar <kartik@buddyboss.com>
2023-11-22 10:50:15 +0530

    PROD-6379 - phpcbf fixes

--- src/bp-templates/bp-nouveau/buddypress/common/search-and-filters-bar.php
+++ src/bp-templates/bp-nouveau/buddypress/common/search-and-filters-bar.php
@@ -13,27 +13,27 @@
 
 	<?php if ( 'friends' !== bp_current_component() ) : ?>
 
-        <?php if ( 'members' !== bp_current_component() || bp_disable_advanced_profile_search() ) : ?>
-            <div class="subnav-search clearfix">
+		<?php if ( 'members' !== bp_current_component() || bp_disable_advanced_profile_search() ) : ?>
+			<div class="subnav-search clearfix">
 
-                <?php bp_nouveau_search_form(); ?>
+				<?php bp_nouveau_search_form(); ?>
 
-            </div>
-        <?php endif; ?>
+			</div>
+		<?php endif; ?>
 
 	<?php endif; ?>
 
-    <?php if ( ( 'members' === bp_current_component() || 'groups' === bp_current_component() || 'friends' === bp_current_component() ) && ! bp_is_current_action( 'requests' ) ): ?>
-        <?php bp_get_template_part( 'common/filters/grid-filters' ); ?>
-    <?php endif; ?>
+	<?php if ( ( 'members' === bp_current_component() || 'groups' === bp_current_component() || 'friends' === bp_current_component() ) && ! bp_is_current_action( 'requests' ) ): ?>
+		<?php bp_get_template_part( 'common/filters/grid-filters' ); ?>
+	<?php endif; ?>
 
 	<?php if ( ( 'members' === bp_current_component() || 'groups' === bp_current_component() ) || ( bp_is_user() && ( ! bp_is_current_action( 'requests' ) && ! bp_is_current_action( 'mutual' ) ) ) ): ?>
-        <?php bp_get_template_part( 'common/filters/directory-filters' ); ?>
+		<?php bp_get_template_part( 'common/filters/directory-filters' ); ?>
 	<?php endif; ?>
 
-    <?php if ( 'members' === bp_current_component() || ( 'friends' === bp_current_component() && 'my-friends' === bp_current_action() ) ): ?>
-        <?php bp_get_template_part( 'common/filters/member-filters' ); ?>
-    <?php endif; ?>
+	<?php if ( 'members' === bp_current_component() || ( 'friends' === bp_current_component() && 'my-friends' === bp_current_action() ) ): ?>
+		<?php bp_get_template_part( 'common/filters/member-filters' ); ?>
+	<?php endif; ?>
 
 	<?php if ( 'groups' === bp_current_component() ): ?>
 		<?php bp_get_template_part( 'common/filters/group-filters' ); ?>
