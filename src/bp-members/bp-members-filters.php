9713e932f24c7fec935bd2e7aca2fe006c7ffbc9
Kartik Suthar <kartik@buddyboss.com>
2023-11-29 17:27:38 +0530

    PROD-6511 - removed unwanted previous background jobs on plugin update

--- src/bp-members/bp-members-filters.php
+++ src/bp-members/bp-members-filters.php
@@ -930,44 +930,6 @@ function bb_set_default_member_type_to_activate_user_on_admin( $user_id, $member
 }
 
 /**
- * This function will work as migration process which will repair member profile links.
- *
- * @since BuddyBoss 2.3.41
- *
- * @return array|void
- */
-function bb_generate_member_profile_links_on_update() {
-	if ( ! bp_is_active( 'members' ) ) {
-		return;
-	}
-
-	global $wpdb, $bp_background_updater;
-	$bp_prefix = bp_core_get_table_prefix();
-
-	// Get all users who have not generate unique slug while it runs from background.
-	$user_ids = $wpdb->get_col(
-		$wpdb->prepare(
-			"SELECT u.ID FROM `{$wpdb->users}` AS u LEFT JOIN `{$wpdb->usermeta}` AS um ON ( u.ID = um.user_id AND um.meta_key = %s ) WHERE um.user_id IS NULL ORDER BY u.ID ASC",
-			'bb_profile_slug'
-		)
-	);
-
-	if ( ! is_wp_error( $user_ids ) && ! empty( $user_ids ) ) {
-		foreach ( array_chunk( $user_ids, 50 ) as $chunked_user_ids ) {
-			$bp_background_updater->data(
-				array(
-					array(
-						'callback' => 'bb_set_bulk_user_profile_slug',
-						'args'     => array( $chunked_user_ids ),
-					),
-				)
-			);
-			$bp_background_updater->save()->dispatch();
-		}
-	}
-}
-
-/**
  * Generate profile slug registration and activation.
  *
  * @since BuddyBoss [BBVERSION]
