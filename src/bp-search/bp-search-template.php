e5e13f1777673a0a8262f5aba55734e453c48b04
Milankumar Hirapra <milan@buddyboss.com>
2023-12-01 13:09:44 +0530

    PROD-6380 : Replace block template to our template

--- src/bp-search/bp-search-template.php
+++ src/bp-search/bp-search-template.php
@@ -99,26 +99,73 @@ function bp_search_results() {
  * @since BuddyBoss [BBVERSION]
  *
  * @param WP_Block_Template[] $query_result Array of found block templates.
+ * @param array               $query        {
+ *                                          Arguments to retrieve templates. All arguments are optional.
+ *
+ * @type string[]             $slug__in     List of slugs to include.
+ * @type int                  $wp_id        Post ID of customized template.
+ * @type string               $area         A 'wp_template_part_area' taxonomy value to filter by (for 'wp_template_part' template type only).
+ * @type string               $post_type    Post type to get the templates for.
+ *                                          }
+ *
+ * @param string              $template_type wp_template or wp_template_part.
  *
  * return WP_Block_Template[] $query_result
  */
-function bb_search_set_block_template_content( $query_result ) {
+function bb_search_set_block_template_content( $query_result, $query, $template_type ) {
+	// Check if the current page is buddyboss search page.
 	if (
-		! empty( $query_result ) &&
 		bp_search_is_search() &&
 		function_exists( 'wp_is_block_theme' ) &&
-		wp_is_block_theme()
+		wp_is_block_theme() &&
+		in_array( 'search', $query['slug__in'], true )
 	) {
-		foreach ( $query_result as $template_key => $template ) {
-			if (
-				'search' === $template->slug &&
-				str_contains( $template->content, 'wp:post-excerpt' )
-			) {
-				$query_result[ $template_key ]->content = str_replace( 'wp:post-excerpt', 'wp:post-content', $template->content );
+
+		$new_content = preg_replace('/\/\*\*[\s\S]*?\*\//', '', file_get_contents( bp_get_template_part( 'search/blocks/search' ) ) );
+		$new_content = str_replace( array( '<?php', '?>' ), '', $new_content );
+
+		// Reset the template query array.
+		$query_result     = array();
+		$theme            = get_stylesheet();
+		$slug             = current( $query['slug__in'] );
+		$template_content = $new_content;
+		$blocks           = parse_blocks( $template_content );
+
+		$prepare_template                 = new WP_Block_Template();
+		$prepare_template->id             = $theme . '//' . $slug;
+		$prepare_template->theme          = $theme;
+		$prepare_template->slug           = $slug;
+		$prepare_template->content        = traverse_and_serialize_blocks( $blocks );
+		$prepare_template->source         = 'buddyboss';
+		$prepare_template->type           = $template_type;
+		$prepare_template->title          = ! empty( $template_file['title'] ) ? $template_file['title'] : $slug;
+		$prepare_template->status         = 'publish';
+		$prepare_template->has_theme_file = false;
+		$prepare_template->is_custom      = true;
+		$prepare_template->modified       = null;
+
+
+		// Get block templates.
+		$template_files = _get_block_templates_files( $template_type, $query );
+
+		// Replace all templates to buddyboss templates for search.
+		if ( ! empty( $template_files ) ) {
+			foreach ( $template_files as $template_file ) {
+				$data = _build_block_template_result_from_file( $template_file, $template_type );
+
+				// Set template to query results.
+				if ( $template_file['slug'] === 'search' || $template_file['slug'] === 'index' ) {
+					$query_result[] = $prepare_template;
+				} else {
+					$query_result[] = $data;
+				}
 			}
+		} else {
+			// Set template to query results.
+			$query_result[] = $prepare_template;
 		}
 	}
 
 	return $query_result;
 }
-add_filter( 'get_block_templates', 'bb_search_set_block_template_content', 10, 1 );
+add_filter( 'get_block_templates', 'bb_search_set_block_template_content', 10, 3 );
---
+++ src/bp-templates/bp-nouveau/buddypress/search/blocks/search.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template for displaying the search results of the page.
+ *
+ * This template can be overridden by copying it to yourtheme/buddypress/search/blocks/search.php.
+ *
+ * @package BuddyBoss\Core
+ * @since   BuddyBoss 1.0.0
+ * @version 1.0.0
+ */
+
+?>
+<!-- wp:template-part {"slug":"header","tagName":"header"} /-->
+<!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"var:preset|spacing|50"}}}} -->
+<main class="wp-block-group">
+	<!-- wp:post-content {"layout":{"type":"constrained"}} /-->
+</main>
+<!-- /wp:group -->
+<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
