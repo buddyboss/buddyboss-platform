a98e15ce85840d4a07219fa300df069c99b5081e
Kartik Suthar <kartik@buddyboss.com>
2023-12-20 18:29:49 +0530

    PROD-6354 - phpcbf fixes

--- src/bp-core/bp-core-functions.php
+++ src/bp-core/bp-core-functions.php
@@ -9093,15 +9093,47 @@ function bb_redirect_after_action( $redirect_to, $user_id = 0, $action = 'login'
 }
 
 /**
+ * Remove action which used in thord party class.
+ *
+ * @since BuddyBoss 2.5.00
+ *
+ * @param string $action Action name.
+ * @param string $class  Class name.
+ * @param string $method Method name.
+ */
+function bb_remove_class_action( $action, $class, $method ) {
+	global $wp_filter;
+	if ( isset( $wp_filter[ $action ] ) ) {
+		$len = strlen( $method );
+		foreach ( $wp_filter[ $action ] as $pri => $actions ) {
+			foreach ( $actions as $name => $def ) {
+				if ( substr( $name, - $len ) === $method ) {
+					if ( is_array( $def['function'] ) && ! empty( $def['function'] ) ) {
+						if ( get_class( $def['function'][0] ) === $class ) {
+							if ( is_object( $wp_filter[ $action ] ) && isset( $wp_filter[ $action ]->callbacks ) ) {
+								unset( $wp_filter[ $action ]->callbacks[ $pri ][ $name ] );
+							} else {
+								unset( $wp_filter[ $action ][ $pri ][ $name ] );
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+/**
  * Get the Reactions settings sections.
  *
- * @return array
  * @since BuddyBoss [BBVERSION]
+ *
+ * @return array
  */
 function bb_reactions_get_settings_sections() {
 
 	$settings = array(
-		'bp_reaction_settings_section'    => array(
+		'bp_reaction_settings_section' => array(
 			'page'              => 'reaction',
 			'title'             => esc_html__( 'Reactions', 'buddyboss' ),
 			'tutorial_callback' => 'bp_admin_reaction_setting_tutorial',
@@ -9118,8 +9150,8 @@ function bb_reactions_get_settings_sections() {
  */
 function bp_admin_reaction_setting_tutorial() {
 	?>
-    <p>
-        <a class="button" href="
+	<p>
+		<a class="button" href="
 		<?php
 		echo esc_url(
 			bp_get_admin_url(
@@ -9134,17 +9166,18 @@ function bp_admin_reaction_setting_tutorial() {
 		);
 		?>
 		"><?php esc_html_e( 'View Tutorial', 'buddyboss' ); ?></a>
-    </p>
+	</p>
 	<?php
 }
 
 /**
  * Get reaction settings fields by section.
  *
+ * @since BuddyBoss [BBVERSION]
+ *
  * @param string $section_id Section ID.
  *
  * @return mixed False if section is invalid, array of fields otherwise.
- * @since BuddyBoss [BBVERSION]
  */
 function bb_reactions_get_settings_fields_for_section( $section_id = '' ) {
 
@@ -9162,8 +9195,9 @@ function bb_reactions_get_settings_fields_for_section( $section_id = '' ) {
 /**
  * Get all of the reactions settings fields.
  *
- * @return array
  * @since BuddyBoss [BBVERSION]
+ *
+ * @return array
  */
 function bb_reactions_get_settings_fields() {
 
@@ -9198,35 +9232,4 @@ function bb_reactions_get_settings_fields() {
 	);
 
 	return (array) apply_filters( 'bb_reactions_get_settings_fields', $fields );
-}
-
-/**
- * Remove action which used in thord party class.
- *
- * @since BuddyBoss 2.5.00
- *
- * @param string $action Action name.
- * @param string $class  Class name.
- * @param string $method Method name.
- */
-function bb_remove_class_action( $action, $class, $method ) {
-	global $wp_filter;
-	if ( isset( $wp_filter[ $action ] ) ) {
-		$len = strlen( $method );
-		foreach ( $wp_filter[ $action ] as $pri => $actions ) {
-			foreach ( $actions as $name => $def ) {
-				if ( substr( $name, - $len ) === $method ) {
-					if ( is_array( $def['function'] ) && ! empty( $def['function'] ) ) {
-						if ( get_class( $def['function'][0] ) === $class ) {
-							if ( is_object( $wp_filter[ $action ] ) && isset( $wp_filter[ $action ]->callbacks ) ) {
-								unset( $wp_filter[ $action ]->callbacks[ $pri ][ $name ] );
-							} else {
-								unset( $wp_filter[ $action ][ $pri ][ $name ] );
-							}
-						}
-					}
-				}
-			}
-		}
-	}
 }
