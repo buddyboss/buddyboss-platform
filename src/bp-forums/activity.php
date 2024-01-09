9fd62b33ef3f003ff8034d27bbd62f6bedd9040b
Yudhisthir Nahar <yudhisthir@buddyboss.com>
2024-01-09 14:49:20 +0530

    setup grouped forum actions to fix the translations

--- src/bp-forums/activity.php
+++ src/bp-forums/activity.php
@@ -220,6 +220,29 @@ if ( ! class_exists( 'BBP_BuddyPress_Activity' ) ) :
 				array( 'activity', 'member', 'member_groups', 'group' )
 			);
 
+			// Grouped forums.
+			if ( bp_is_active( 'groups' ) ) {
+				$bp = buddypress();
+
+				bp_activity_set_action(
+					$bp->groups->id,
+					$this->topic_create,
+					esc_html__( 'New forum discussion', 'buddyboss' ),
+					array( $this, 'bbp_format_activity_action_new_topic' ),
+					esc_html__( 'Discussions', 'buddyboss' ),
+					array( 'activity', 'member', 'member_groups', 'group' )
+					
+				);
+	
+				bp_activity_set_action(
+					$bp->groups->id,
+					$this->reply_create,
+					esc_html__( 'New forum reply', 'buddyboss' ),
+					array( $this, 'bbp_format_activity_action_new_reply' ),
+					esc_html__( 'Replies', 'buddyboss' ),
+					array( 'activity', 'member', 'member_groups', 'group' )
+				);
+			}
 		}
 
 		/**
