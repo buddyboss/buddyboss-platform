452f1c6fa28d4817a229d134401622b127d3d5f6
Milankumar Hirapra <milan@buddyboss.com>
2024-01-11 11:36:22 +0530

    PROD-6469 : Restructure blogs templates

--- src/bp-templates/bp-nouveau/buddypress/blogs/create.php
+++ src/bp-templates/bp-nouveau/buddypress/blogs/create.php
@@ -8,25 +8,15 @@
  * @version 1.0.0
  */
 
-bp_nouveau_blogs_create_hook( 'before', 'content_template' ); ?>
+bp_nouveau_blogs_create_hook( 'before', 'content_template' );
+bp_nouveau_template_notices();
+bp_nouveau_blogs_create_hook( 'before', 'content' );
 
-<?php bp_nouveau_template_notices(); ?>
-
-<?php bp_nouveau_blogs_create_hook( 'before', 'content' ); ?>
-
-<?php if ( bp_blog_signup_enabled() ) : ?>
-
-	<?php bp_show_blog_signup_form(); ?>
-
-<?php
+if ( bp_blog_signup_enabled() ) :
+	bp_show_blog_signup_form();
 else :
-
 	bp_nouveau_user_feedback( 'blogs-no-signup' );
-
 endif;
-?>
 
-<?php
 bp_nouveau_blogs_create_hook( 'after', 'content' );
-
 bp_nouveau_blogs_create_hook( 'after', 'content_template' );
