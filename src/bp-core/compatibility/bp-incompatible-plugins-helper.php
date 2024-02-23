94dd1d7ef44701ee6cb2876415cbdcb044b8b9b7
Jitendra Banjara <jitendra@buddyboss.com>
2024-02-23 15:43:39 +0530

    PROD-6840 - Add since tag

--- src/bp-core/compatibility/bp-incompatible-plugins-helper.php
+++ src/bp-core/compatibility/bp-incompatible-plugins-helper.php
@@ -238,8 +238,9 @@ function bp_helper_plugins_loaded_callback() {
 
 	/**
 	 * Include plugin when plugin is activated.
+	 * Support Instructor Role.
 	 *
-	 * Support Instructor Role
+	 * @since BuddyBoss [BBVERSION]
 	 */
 	if ( in_array( 'instructor-role/instructor.php', $bp_plugins ) ) {
 		add_filter( 'ir_filter_remove_private_protected_from_titles', function ( $is_prepend, $prepend, $post ) {
