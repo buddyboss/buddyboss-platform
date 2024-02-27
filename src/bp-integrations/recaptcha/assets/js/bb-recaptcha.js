63b4044977594f0c474cae0b834d44899a60abfc
Kartik Suthar <kartik@buddyboss.com>
2024-02-27 10:35:21 +0530

    PROD-6805 - phpcbf fixes

--- src/bp-integrations/recaptcha/assets/js/bb-recaptcha.js
+++ src/bp-integrations/recaptcha/assets/js/bb-recaptcha.js
@@ -1,4 +1,4 @@
-/* global bbRecaptcha */
+/* global bbRecaptcha, grecaptcha */
 ( function ( $ ) {
 	var BB_Recaptcha = {
 
@@ -7,14 +7,11 @@
 			this.bbrecaptchaAction  = this.bbrecaptchaData && 'undefined' !== typeof this.bbrecaptchaData.action ? this.bbrecaptchaData.action : '';
 			this.bbrecaptchaVersion = this.bbrecaptchaData && 'undefined' !== typeof this.bbrecaptchaData.selected_version ? this.bbrecaptchaData.selected_version : '';
 			this.setupGlobals();
-
-			// Listen to events ("Add hooks!").
-			this.addListeners();
 		},
 
 		setupGlobals: function () {
 			if ( this.bbrecaptchaAction ) {
-				var action = this.bbrecaptchaAction;
+				var action    = this.bbrecaptchaAction;
 				var container = false;
 				if ( 'bb_login' === action ) {
 					container = 'loginform';
@@ -26,56 +23,63 @@
 					container = 'activation-form';
 				}
 				if ( 'recaptcha_v3' === this.bbrecaptchaVersion ) {
-					grecaptcha.ready( function () {
-						grecaptcha.execute( bbRecaptcha.data.site_key, { action: action } ).then( function ( token ) {
-							$( '#bb_recaptcha_response_id' ).val( token );
-						} );
-					} );
+					grecaptcha.ready(
+						function () {
+							grecaptcha.execute( bbRecaptcha.data.site_key, { action: action } ).then(
+								function ( token ) {
+									$( '#bb_recaptcha_response_id' ).val( token );
+								}
+							);
+						}
+					);
 				}
 				if (
 					'recaptcha_v2' === this.bbrecaptchaVersion &&
 					'undefined' !== typeof this.bbrecaptchaData.v2_option
 				) {
 					if ( 'v2_checkbox' === this.bbrecaptchaData.v2_option ) {
-						grecaptcha.ready( function () {
-							var params = {
-								'sitekey': bbRecaptcha.data.site_key,
-								'theme': bbRecaptcha.data.v2_theme,
-								'size': bbRecaptcha.data.v2_size
-							};
+						grecaptcha.ready(
+							function () {
+								var params = {
+									'sitekey': bbRecaptcha.data.site_key,
+									'theme': bbRecaptcha.data.v2_theme,
+									'size': bbRecaptcha.data.v2_size
+								};
 
-							grecaptcha.render( 'bb_recaptcha_v2_element', params );
-						} );
+								grecaptcha.render( 'bb_recaptcha_v2_element', params );
+							}
+						);
 					}
 					if ( 'v2_invisible_badge' === this.bbrecaptchaData.v2_option ) {
-						grecaptcha.ready( function () {
-							var params = {
-								'sitekey': bbRecaptcha.data.site_key,
-								'tabindex': 9999,
-								'badge': bbRecaptcha.data.v2_badge_position,
-								'size': 'invisible',
-								'callback': function ( token ) {
-									$( '#g-recaptcha-response' ).val( token );
-									if ( container ) {
-										document.getElementById( container ).submit();
-									}
-								},
-							};
-							var loginV2 = grecaptcha.render( 'bb_recaptcha_v2_element', params );
-							if ( container ) {
-								$( '#' + container ).on( 'submit', function ( e ) {
-									e.preventDefault();
-									grecaptcha.execute( loginV2 );
-								} );
+						grecaptcha.ready(
+							function () {
+								var params  = {
+									'sitekey': bbRecaptcha.data.site_key,
+									'tabindex': 9999,
+									'badge': bbRecaptcha.data.v2_badge_position,
+									'size': 'invisible',
+									'callback': function ( token ) {
+										$( '#g-recaptcha-response' ).val( token );
+										if ( container ) {
+											document.getElementById( container ).submit();
+										}
+									},
+								};
+								var loginV2 = grecaptcha.render( 'bb_recaptcha_v2_element', params );
+								if ( container ) {
+										$( '#' + container ).on(
+											'submit',
+											function ( e ) {
+												e.preventDefault();
+												grecaptcha.execute( loginV2 );
+											}
+										);
+								}
 							}
-						} );
+						);
 					}
 				}
 			}
-		},
-
-		addListeners: function () {
-
 		},
 	};
 
