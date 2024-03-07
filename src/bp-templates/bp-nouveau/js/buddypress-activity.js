0ea55f7a565f683ccf4ffc92d54376b69943d929
Kishan Jasani <kishan@buddyboss.com>
2024-03-07 11:27:22 +0530

    PROD-6807 - Fix tooltip text issue

--- src/bp-templates/bp-nouveau/js/buddypress-activity.js
+++ src/bp-templates/bp-nouveau/js/buddypress-activity.js
@@ -1751,12 +1751,13 @@ window.bp = window.bp || {};
 									target.closest( 'li.activity-item' ).addClass( 'bb-muted' );
 									target.removeClass( 'bb-icon-bell-slash' );
 									target.addClass( 'bb-icon-bell' );
-
+									target.attr( 'title', BP_Nouveau.activity.strings.unmuteNotification );
 									target.find( 'span' ).html( BP_Nouveau.activity.strings.unmuteNotification );
 								} else if ( 'unmute' === notification_toggle_action ) {
 									target.closest( 'li.activity-item' ).removeClass( 'bb-muted' );
 									target.removeClass( 'bb-icon-bell' );
 									target.addClass( 'bb-icon-bell-slash' );
+									target.attr( 'title', BP_Nouveau.activity.strings.muteNotification );
 									target.find( 'span' ).html( BP_Nouveau.activity.strings.muteNotification );
 								}
 							}
