a998b3e20f9f2050c29dc9551961f11dd5b6b9a8
Chetan Satasiya <chetan@buddyboss.com>
2024-03-18 23:10:57 +0530

    [ARCH-7] Fix the error

--- src/bp-moderation/bp-moderation-filters.php
+++ src/bp-moderation/bp-moderation-filters.php
@@ -1114,18 +1114,20 @@ function bb_as_moderation_async_request_batch_process( $action_id, $context ) {
 		return;
 	}
 
-	$sql = $wpdb->prepare( "SELECT DISTINCT action_id from {$wpdb->prefix}actionscheduler_meta WHERE `group` = %s AND data_id = %s AND type = %s ORDER BY priority, id DESC LIMIT 1", $next_group_name, $item_id, $type );
-	$data = $wpdb->get_results( $sql ); // phpcs:ignore
+	$sql = $wpdb->prepare( "SELECT action_id from {$wpdb->prefix}actionscheduler_meta WHERE `group` = %s AND data_id = %s AND type = %s ORDER BY priority, id DESC LIMIT 1", $next_group_name, $item_id, $type );
+    $data = $wpdb->get_results( $sql ); // phpcs:ignore
 
 	if ( ! empty( $data ) ) {
 		$next_action_id      = current( $data );
 
-		$next_action_status  = ActionScheduler::store()->get_status( $next_action_id );
+		$next_action_status  = ActionScheduler::store()->get_status( $next_action_id->action_id );
 		if ( $next_action_status !== 'pending' ) {
 			return;
 		}
+		error_log( $sql );
+		error_log( 'found 2' );
 
-		$next_batch          = ActionScheduler::store()->fetch_action( $next_action_id );
+		$next_batch          = ActionScheduler::store()->fetch_action( $next_action_id->action_id );
 		$next_batch_args     = $next_batch->get_args();
 		$currrent_batch_args = $get_current_action->get_args();
 
@@ -1142,9 +1144,9 @@ function bb_as_moderation_async_request_batch_process( $action_id, $context ) {
 			(int) $current_args['action_suspend'] === (int) $next_args['action_suspend']
 		) {
 			// Update the status of the action.
-			error_log( sprintf( 'Skip suspend process: `%s` next found: `%s`', $action_id, $next_action_id ) );
+			error_log( sprintf( 'Skip suspend process: `%s` next found: `%s`', $action_id, $next_action_id->action_id ) );
 			ActionScheduler::store()->mark_complete( $action_id ); // Set the status to whatever you need, e.g., 'completed', 'failed', 'pending', etc.
-			error_log( sprintf( 'Skip suspend process: `%s` next found: `%s`', $action_id, $next_action_id ) );
+			error_log( sprintf( 'Skip suspend process: `%s` next found: `%s`', $action_id, $next_action_id->action_id ) );
 
 			// Used for hide_parent argument check.
 		} elseif (
@@ -1153,9 +1155,9 @@ function bb_as_moderation_async_request_batch_process( $action_id, $context ) {
 			(int) $current_args['hide_parent'] !== (int) $next_args['hide_parent']
 		) {
 			// Update the status of the action.
-			error_log( sprintf( 'Skip parent process: `%s` next found: `%s`', $action_id, $next_action_id ) );
+			error_log( sprintf( 'Skip parent process: `%s` next found: `%s`', $action_id, $next_action_id->action_id ) );
 			ActionScheduler::store()->mark_complete( $action_id ); // Set the status to whatever you need, e.g., 'completed', 'failed', 'pending', etc.
-			error_log( sprintf( 'Skip parent process: `%s` next found: `%s`', $action_id, $next_action_id ) );
+			error_log( sprintf( 'Skip parent process: `%s` next found: `%s`', $action_id, $next_action_id->action_id ) );
 			// Used for hide_sitewide argument check.
 		} elseif (
 			! empty( $next_data ) &&
@@ -1165,9 +1167,9 @@ function bb_as_moderation_async_request_batch_process( $action_id, $context ) {
 			(int) $next_data[1] !== (int) $current_data[1]
 		) {
 			// Update the status of the action.
-			error_log( sprintf( 'Skip sitewide process: `%s` next found: `%s`', $action_id, $next_action_id ) );
+			error_log( sprintf( 'Skip sitewide process: `%s` next found: `%s`', $action_id, $next_action_id->action_id ) );
 			ActionScheduler::store()->mark_complete( $action_id ); // Set the status to whatever you need, e.g., 'completed', 'failed', 'pending', etc.
-			error_log( sprintf( 'Skip sitewide process: `%s` next found: `%s`', $action_id, $next_action_id ) );
+			error_log( sprintf( 'Skip sitewide process: `%s` next found: `%s`', $action_id, $next_action_id->action_id ) );
 		}
 	}
 }
