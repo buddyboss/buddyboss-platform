595649b0491e920cd60848b2fa5f87b7155cca80
Md. Rezaul Islam Khan <rezaul@buddyboss.com>
2024-03-25 04:05:26 +0600

    PROD-6954 - Applied fix for email sender last name privacy into some other corner cases
    PROD-6954 - Applied fix for email sender last name privacy into some other corner cases

--- src/bp-activity/bp-activity-filters.php
+++ src/bp-activity/bp-activity-filters.php
@@ -3479,7 +3479,7 @@ function bb_send_email_to_follower( $follower ) {
 		$args                          = array(
 			'tokens' => array(
 				'follower.id'   => $user_id,
-				'follower.name' => bp_core_get_user_displayname( $user_id ),
+				'follower.name' => bb_activity_get_notification_user_displayname( $user_id, $following_user_id ),
 				'follower.url'  => esc_url( bp_core_get_user_domain( $user_id ) ),
 			),
 		);
