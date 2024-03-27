e2685320ac25c388cad92ea144e22e54d73933a7
Kishan Jasani <kishan@buddyboss.com>
2024-03-27 11:13:24 +0530

    PROD-6979 - Update template path

--- src/bp-templates/bp-nouveau/buddypress/schedule-activity/entry.php
+++ src/bp-templates/bp-nouveau/buddypress/schedule-activity/entry.php
@@ -5,7 +5,7 @@
  * This template is used by activity-loop.php and AJAX functions to show
  * each activity.
  *
- * This template can be overridden by copying it to yourtheme/buddypress/activity/entry.php.
+ * This template can be overridden by copying it to yourtheme/buddypress/schedule-activity/entry.php.
  *
  * @since   BuddyPress 3.0.0
  * @version 1.0.0
