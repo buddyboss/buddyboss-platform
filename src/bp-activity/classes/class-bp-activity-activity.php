5bdd8a98fc827329db808570486d462ffe05d0a6
rajinsharwar2 <rajinsharwar@buddyboss.com>
2024-04-02 23:00:26 -0400

    Sorting Activity via date_recorded instead of id

--- src/bp-activity/classes/class-bp-activity-activity.php
+++ src/bp-activity/classes/class-bp-activity-activity.php
@@ -740,7 +740,7 @@ class BP_Activity_Activity {
 			}
 		} else {
 			// Query first for activity IDs.
-			$activity_ids_sql = "{$select_sql} {$from_sql} {$join_sql} {$where_sql} ORDER BY {$order_by} {$sort}, a.id {$sort}";
+			$activity_ids_sql = "{$select_sql} {$from_sql} {$join_sql} {$where_sql} ORDER BY {$order_by} {$sort}, a.date_recorded {$sort}";
 
 			if ( ! empty( $per_page ) && ! empty( $page ) ) {
 				// We query for $per_page + 1 items in order to
