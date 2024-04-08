dde28a5de1a5a36fe5c0ea9b3d9cce47a2ce0fc9
Amin Charoliya <amin@buddyboss.com>
2024-04-08 12:13:46 +0530

    [PROD-6979] Show scheduled posts viewing from scheduled form

--- src/bp-templates/bp-nouveau/js/buddypress-nouveau.js
+++ src/bp-templates/bp-nouveau/js/buddypress-nouveau.js
@@ -884,7 +884,7 @@ window.bp = window.bp || {};
 			$( document ).on( 'click', '.bb-close-action-popup, .action-popup-overlay', this.closeActionPopup );
 			$( document ).on( 'keyup', '.search-form-has-reset input[type="search"], .search-form-has-reset input#bbp_search', _.throttle( this.directorySearchInput, 900 ) );
 			$( document ).on( 'click', '.search-form-has-reset .search-form_reset', this.resetDirectorySearch );
-			$( '#buddypress' ).on( 'click', '.bb-view-schedule-posts', this, this.showSchedulePosts );
+			$( '#buddypress' ).on( 'click', '.bb-view-schedule-posts, .bb-view-all-scheduled-posts', this, this.showSchedulePosts );
 
 			$( document ).on( 'keyup', this, this.keyUp );
 
