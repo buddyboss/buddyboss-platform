93c2278c14007662b4c4f48067a9ef8763f7dd42
Yudhisthir Nahar <yudhisthir@buddyboss.com>
2024-05-03 16:27:12 +0530

    PROD-6979 Check and register the activity schedule cron if not exists on plugin activation

--- src/bp-activity/classes/class-bb-activity-schedule.php
+++ src/bp-activity/classes/class-bb-activity-schedule.php
@@ -52,6 +52,16 @@ if ( ! class_exists( 'BB_Activity_Schedule' ) ) {
 		public function __construct() {
 			add_action( 'bp_activity_after_save', array( $this, 'bb_register_schedule_activity' ), 999, 1 );
 			add_action( 'bb_activity_publish', array( $this, 'bb_check_and_publish_scheduled_activity' ) );
+			
+			// Check if the schedule event transient exists.
+			if ( get_transient( '_bp_activation_schedule_cron' ) ) {
+
+				// Delete transient.
+				delete_transient( '_bp_activation_schedule_cron' );
+
+				// Check and register the activity schedule cron if not exists.
+				add_action( 'wp_loaded', 'bb_create_activity_schedule_cron_event' );
+			}
 		}
 
 		/**
