2d2697ee7ab7fad2e869463111713caef441853b
istiaq-bb <istiaq@buddyboss.com>
2024-05-17 13:30:52 +0600

    PROD-6756 - fix: Added param in localize script and set condition based on it into js

--- src/bp-templates/bp-nouveau/js/buddypress-activity-post-form.js
+++ src/bp-templates/bp-nouveau/js/buddypress-activity-post-form.js
@@ -517,9 +517,15 @@ window.bp = window.bp || {};
 					if ( self.dropzone ) {
 						self.dropzone.files.push( mock_file );
 						self.dropzone.emit( 'addedfile', mock_file );
-						$( self.dropzone.files[i].previewElement ).find( 'img' ).attr( 'src', activity_data.media[i].thumb );
-						self.dropzone.emit( 'thumbnail', activity_data.media[i].thumb );
-						self.dropzone.emit( 'complete', mock_file );
+						
+						if ( BP_Nouveau.is_as3cf_active !== undefined && BP_Nouveau.is_as3cf_active === '1' ) {
+							$( self.dropzone.files[i].previewElement ).find( 'img' ).attr( 'src', activity_data.media[i].thumb );
+							self.dropzone.emit( 'thumbnail', activity_data.media[i].thumb );
+							self.dropzone.emit( 'complete', mock_file );
+						} else {
+							self.createThumbnailFromUrl( mock_file );
+						}
+						
 						self.dropzone.emit( 'dz-success' );
 						self.dropzone.emit( 'dz-complete' );
 					}
