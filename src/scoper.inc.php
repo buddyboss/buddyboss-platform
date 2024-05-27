cc6821be82faa619fdef4b639cf0042a02437a37
Yudhisthir Nahar <yudhisthir@buddyboss.com>
2024-05-27 09:45:35 +0530

    PROD-7243 Add whitelists and patchers to exclude myclabs due to interface stringable not found issue

--- src/scoper.inc.php
+++ src/scoper.inc.php
@@ -6,13 +6,26 @@ use Symfony\Component\Finder\Finder;
 
 $namespace = 'BuddyBossPlatform';
 
-return [
+return array(
+
 	// The prefix configuration. If a non null value will be used, a random prefix will be generated.
-	'prefix' => $namespace,
-	'whitelist' => [
+	'prefix'    => $namespace,
+	'whitelist' => array(
+
 		// Excludes specific namespaces from being prefixed.
-		'Composer\\*'
-	],
-	'finders' => [],
-	'patchers' => [],
-];
+		'Composer\\*',
+		'MyCLabs',
+		'MyCLabs\\*',
+	),
+	'finders'   => array(),
+	'patchers'  => array(
+		function ( $file_path, $prefix, $contents ) {
+
+			// Check the file path and possibly return the original contents.
+			if ( strpos( $file_path, 'myclabs/php-enum' ) !== false ) {
+				return file_get_contents( $file_path ); // Revert to original contents.
+			}
+			return $contents; // Return modified contents for other files.
+		},
+	),
+);
