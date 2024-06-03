1ae8e173014916c71f9b209779430855bbf8d472
Jitendra Banjara <jitendra@buddyboss.com>
2024-06-03 16:04:24 +0530

    PROD-7527 - Fixed - notification screen for member when set page 1

--- src/bp-templates/bp-nouveau/buddypress/members/single/notifications.php
+++ src/bp-templates/bp-nouveau/buddypress/members/single/notifications.php
@@ -10,13 +10,23 @@
 
 bp_get_template_part( 'members/single/parts/item-subnav' );
 
+$is_send_ajax_request = bb_is_send_ajax_request();
+
 switch ( bp_current_action() ) :
 	case 'unread':
 	case 'read':
 		bp_get_template_part( 'common/search-and-filters-bar' );
 		?>
 		<div id="notifications-user-list" class="notifications dir-list" data-bp-list="notifications">
-			<div id="bp-ajax-loader"><?php bp_nouveau_user_feedback( 'member-notifications-loading' ); ?></div>
+			<?php
+			if ( $is_send_ajax_request ) {
+				?>
+				<div id="bp-ajax-loader"><?php bp_nouveau_user_feedback( 'member-notifications-loading' ); ?></div>
+				<?php
+			} else {
+				bp_get_template_part( 'members/single/notifications/notifications-loop' );
+			}
+			?>
 		</div><!-- #groups-dir-list -->
 		<?php
 		break;
