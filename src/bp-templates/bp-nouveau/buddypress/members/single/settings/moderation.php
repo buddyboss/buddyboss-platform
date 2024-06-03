4f028439d06f9a7c773a7f31944594460ea7f9ef
Jitendra Banjara <jitendra@buddyboss.com>
2024-06-03 16:18:42 +0530

    PROD-7527 - Fixed - blocked member screen for member when set page 1

--- src/bp-templates/bp-nouveau/buddypress/members/single/settings/moderation.php
+++ src/bp-templates/bp-nouveau/buddypress/members/single/settings/moderation.php
@@ -9,14 +9,24 @@
  * @version 1.5.6
  */
 
+$is_send_ajax_request = bb_is_send_ajax_request();
+
 switch ( bp_current_action() ) :
 	case 'blocked-members':
 		bp_nouveau_member_hook( 'before', 'moderation_content' );
 		?>
 		<div class="moderation" data-bp-list="moderation">
-			<div id="bp-ajax-loader">
-				<?php bp_nouveau_user_feedback( 'moderation-block-member-loading' ); ?>
-			</div>
+			<?php
+			if ( $is_send_ajax_request ) {
+				?>
+				<div id="bp-ajax-loader">
+					<?php bp_nouveau_user_feedback( 'moderation-block-member-loading' ); ?>
+				</div>
+				<?php
+			} else {
+				bp_get_template_part( 'moderation/moderation-loop' );
+			}
+			?>
 		</div>
 		<?php
 		bp_nouveau_member_hook( 'after', 'moderation_content' );
