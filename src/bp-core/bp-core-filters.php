f6f1eb85ba416da73acac943312c0352e0cb1572
Yudhisthir Nahar <yudhisthir@buddyboss.com>
2024-06-05 15:26:46 +0530

    PROD-7479 Code refactor

--- src/bp-core/bp-core-filters.php
+++ src/bp-core/bp-core-filters.php
@@ -2656,11 +2656,11 @@ function bb_redirection_allowed_third_party_domains( $hosts ) {
 /**
  * Remove notices from the buddyboss upgrade screens.
  *
- * @since BuddyBoss[BBVERSION]
+ * @since BuddyBoss [BBVERSION]
  */
 function bb_remove_admin_notices() {
 	$screen = get_current_screen();
-	if ( in_array( $screen->id, array( 'buddyboss_page_bp-upgrade' ) ) ) {
+	if ( 'buddyboss_page_bp-upgrade' === $screen->id ) {
 		remove_all_actions( 'admin_notices' );
 
 		// Additional check for the common WordPress error/warning hooks
