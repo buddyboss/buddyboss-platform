77d83a571a7985f34e1f2eb0e2e2697ba2afe26a
Amin Charoliya <amin@buddyboss.com>
2024-12-09 17:57:50 +0530

    [PROD-8364] Add new filters static structure

--- src/bp-templates/bp-nouveau/buddypress/common/search-and-filters-bar.php
+++ src/bp-templates/bp-nouveau/buddypress/common/search-and-filters-bar.php
@@ -9,11 +9,11 @@
  */
 
 ?>
-<div class="bb-subnav-filters-container">
+<div class="bb-subnav-filters-container bb-subnav-filters-search">
 	<div class="subnav-filters-opener">
 		<i class="bb-icon-f bb-icon-search"></i>	
 	</div>
-	<div class="subnav-filters filters no-ajax" id="subnav-filters">
+	<div class="subnav-filters filters no-ajax subnav-filters-modal" id="subnav-filters">
 		<?php
 		$bp_current_component = bp_current_component();
 		if (
@@ -71,4 +71,43 @@
 		}
 		?>
 	</div><!-- search & filters -->
+</div>
+
+<span class="bb-subnav-filters-label">Show</span>
+<div class="bb-subnav-filters-container bb-subnav-filters-filtering">
+
+	<div class="subnav-filters-opener">
+		<span class="selected">all posts</span>
+		<i class="bb-icon-l bb-icon-angle-down"></i>
+	</div>
+
+	<div class="subnav-filters-modal">
+		<ul>
+			<li><a href="#" data-value="all_posts">All posts</a></li>
+			<li><a href="#" data-value="created_by_me">Created by me</a></li>
+			<li><a href="#" data-value="from_my_groups">From my groups</a></li>
+			<li><a href="#" data-value="from_my_connections">From my connections</a></li>
+			<li><a href="#" data-value="im_mentioned_in">I'm mentioned in</a></li>
+			<li><a href="#" data-value="im_following">I'm following</a></li>
+			<li><a href="#" data-value="ihv_replied_to">I've replied to</a></li>
+		</ul>
+	</div>
+	<input type="hidden" name="bb_activity_filter_show" value="all_posts" />
+</div>
+
+<span class="bb-subnav-filters-label">by</span>
+<div class="bb-subnav-filters-container bb-subnav-filters-filtering">
+
+	<div class="subnav-filters-opener">
+		<span class="selected">most recent</span>
+		<i class="bb-icon-l bb-icon-angle-down"></i>
+	</div>
+
+	<div class="subnav-filters-modal">
+		<ul>
+			<li><a href="#" data-value="most_recent">Most recent</a></li>
+			<li><a href="#" data-value="recent_activity">Recent activity</a></li>
+		</ul>
+	</div>
+	<input type="hidden" name="bb_activity_filter_by" value="most_recent" />
 </div>
