def39afc00b2f52c60197d50ee77b239a6385724
Amin Charoliya <amin@buddyboss.com>
2024-12-24 17:00:05 +0530

    [PROD-8364] Load new activity filters on main activity feed

--- src/bp-templates/bp-nouveau/buddypress/common/search-and-filters-bar.php
+++ src/bp-templates/bp-nouveau/buddypress/common/search-and-filters-bar.php
@@ -11,7 +11,7 @@
 ?>
 <div class="bb-subnav-filters-container bb-subnav-filters-search">
 	<?php
-	if ( bb_is_activity_search_enabled() ) {
+	if ( bp_is_activity_directory() && bb_is_activity_search_enabled() ) {
 		?>
 		<button class="subnav-filters-opener" aria-expanded="false" aria-controls="subnav-filters">
 			<i class="bb-icon-f bb-icon-search"></i>	
@@ -78,81 +78,83 @@
 		?>
 	</div><!-- search & filters -->
 </div>
-<i class="bb-icon-f bb-icon-loader animate-spin"></i>
-<span class="bb-subnav-filters-label"><?php echo esc_html_e( 'Show', 'buddyboss' ); ?></span>
-<div class="bb-subnav-filters-container bb-subnav-filters-filtering">
-
-	<button class="subnav-filters-opener" aria-expanded="false" aria-controls="bb-subnav-filter-show">
-		<span class="selected"><?php echo strtolower( esc_html__( 'All updates', 'buddyboss' ) ); ?></span>
-		<i class="bb-icon-l bb-icon-angle-down"></i>
-	</button>
-	<div id="bb-subnav-filter-show" class="subnav-filters-modal">
-		<ul role="listbox">
-			<?php
-				$filters_labels   = bb_get_activity_filter_options_labels();
-				$activity_filters = bb_get_enabled_activity_filter_options();
-				if ( ! empty ( $activity_filters ) ) {
-					// Skip filters based on user login or component active.
-					$skip_conditions = [
-						'friends'   => ! bp_is_active( 'friends' ),
-						'following' => ! bp_is_activity_follow_active(),
-						'groups'    => ! bp_is_active( 'groups' ),
-						'mentions'  => ! bp_activity_do_mentions(),
-					];
-					foreach( $activity_filters as $key => $is_enabled ) {
-
-						// Skip filters not enabled or without labels.
-						if ( empty( $is_enabled ) || empty( $filters_labels[ $key ] ) ) {
-							continue;
-						}
-
-						if ( 'all' !== $key && ! is_user_logged_in() ) {
-							continue;
-						}
-				
-						if ( isset( $skip_conditions[ $key ] ) && $skip_conditions[ $key ] ) {
-							continue;
-						}
-						?>
-						<li role="option" data-bp-scope="<?php esc_attr_e( $key ); ?>" data-bp-object="activity"><a href="#"><?php echo $filters_labels[ $key ]; ?></a></li>
-						<?php
-					}
-				}
-			?>
-		</ul>
-	</div>
-</div>
-
-<?php
-$avail_sorting_options = bb_get_enabled_activity_sorting_options();
-arsort( $avail_sorting_options );
-if ( ! empty ( $avail_sorting_options ) && in_array( 1, $avail_sorting_options, false ) && array_count_values( $avail_sorting_options )[1] > 1 ) {
-	?>
-	<span class="bb-subnav-filters-label"><?php echo esc_html_e( 'by', 'buddyboss' ); ?></span>
+<?php if ( bp_is_activity_directory() ) { ?>
+	<i class="bb-icon-f bb-icon-loader animate-spin"></i>
+	<span class="bb-subnav-filters-label"><?php echo esc_html_e( 'Show', 'buddyboss' ); ?></span>
 	<div class="bb-subnav-filters-container bb-subnav-filters-filtering">
-		<?php $sorting_labels = bb_get_activity_sorting_options_labels(); ?>
-		<button class="subnav-filters-opener" aria-expanded="false" aria-controls="bb-subnav-filter-by">
-			<span class="selected"><?php echo strtolower( $sorting_labels[ key( $avail_sorting_options ) ] ); ?></span>
+
+		<button class="subnav-filters-opener" aria-expanded="false" aria-controls="bb-subnav-filter-show">
+			<span class="selected"><?php echo strtolower( esc_html__( 'All updates', 'buddyboss' ) ); ?></span>
 			<i class="bb-icon-l bb-icon-angle-down"></i>
 		</button>
-
-		<div class="subnav-filters-modal" id="bb-subnav-filter-by">
+		<div id="bb-subnav-filter-show" class="subnav-filters-modal">
 			<ul role="listbox">
 				<?php
-				if ( ! empty ( $avail_sorting_options ) ) {
-					foreach( $avail_sorting_options as $key => $is_enabled ) {
-						if ( empty( $is_enabled ) || empty( $sorting_labels[ $key ] ) ) {
-							continue;
+					$filters_labels   = bb_get_activity_filter_options_labels();
+					$activity_filters = bb_get_enabled_activity_filter_options();
+					if ( ! empty ( $activity_filters ) ) {
+						// Skip filters based on user login or component active.
+						$skip_conditions = [
+							'friends'   => ! bp_is_active( 'friends' ),
+							'following' => ! bp_is_activity_follow_active(),
+							'groups'    => ! bp_is_active( 'groups' ),
+							'mentions'  => ! bp_activity_do_mentions(),
+						];
+						foreach( $activity_filters as $key => $is_enabled ) {
+
+							// Skip filters not enabled or without labels.
+							if ( empty( $is_enabled ) || empty( $filters_labels[ $key ] ) ) {
+								continue;
+							}
+
+							if ( 'all' !== $key && ! is_user_logged_in() ) {
+								continue;
+							}
+					
+							if ( isset( $skip_conditions[ $key ] ) && $skip_conditions[ $key ] ) {
+								continue;
+							}
+							?>
+							<li role="option" data-bp-scope="<?php esc_attr_e( $key ); ?>" data-bp-object="activity"><a href="#"><?php echo $filters_labels[ $key ]; ?></a></li>
+							<?php
 						}
-						?>
-						<li role="option" data-bp-order="activity" data-bp-orderby="<?php esc_attr_e( $key ); ?>"><a href="#"><?php echo $sorting_labels[ $key ]; ?></a></li>
-						<?php
 					}
-				}
 				?>
 			</ul>
 		</div>
 	</div>
+
 	<?php
+	$avail_sorting_options = bb_get_enabled_activity_sorting_options();
+	arsort( $avail_sorting_options );
+	if ( ! empty ( $avail_sorting_options ) && in_array( 1, $avail_sorting_options, false ) && array_count_values( $avail_sorting_options )[1] > 1 ) {
+		?>
+		<span class="bb-subnav-filters-label"><?php echo esc_html_e( 'by', 'buddyboss' ); ?></span>
+		<div class="bb-subnav-filters-container bb-subnav-filters-filtering">
+			<?php $sorting_labels = bb_get_activity_sorting_options_labels(); ?>
+			<button class="subnav-filters-opener" aria-expanded="false" aria-controls="bb-subnav-filter-by">
+				<span class="selected"><?php echo strtolower( $sorting_labels[ key( $avail_sorting_options ) ] ); ?></span>
+				<i class="bb-icon-l bb-icon-angle-down"></i>
+			</button>
+
+			<div class="subnav-filters-modal" id="bb-subnav-filter-by">
+				<ul role="listbox">
+					<?php
+					if ( ! empty ( $avail_sorting_options ) ) {
+						foreach( $avail_sorting_options as $key => $is_enabled ) {
+							if ( empty( $is_enabled ) || empty( $sorting_labels[ $key ] ) ) {
+								continue;
+							}
+							?>
+							<li role="option" data-bp-order="activity" data-bp-orderby="<?php esc_attr_e( $key ); ?>"><a href="#"><?php echo $sorting_labels[ $key ]; ?></a></li>
+							<?php
+						}
+					}
+					?>
+				</ul>
+			</div>
+		</div>
+		<?php
+	}
 }
 ?>
