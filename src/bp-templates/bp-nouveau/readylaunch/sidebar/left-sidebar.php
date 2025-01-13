2108ffacb176bc9c894284bd46a29b0df6770066
Jitendra Banjara <jitendra@buddyboss.com>
2025-01-13 14:24:31 +0530

    PRJ-53 - Added code to load Bottom ReadyLaunch Panel menu in the left sidebar

--- src/bp-templates/bp-nouveau/readylaunch/sidebar/left-sidebar.php
+++ src/bp-templates/bp-nouveau/readylaunch/sidebar/left-sidebar.php
@@ -23,5 +23,15 @@ defined( 'ABSPATH' ) || exit;
 			'menu_class'     => 'bb-top-readylaunchpanel-menu',
 		)
 	);
+
+	wp_nav_menu(
+		array(
+			'theme_location' => 'bb-bottom-readylaunchpanel',
+			'menu_id'        => '',
+			'container'      => false,
+			'fallback_cb'    => false,
+			'menu_class'     => 'bb-bottom-readylaunchpanel-menu',
+		)
+	);
 	?>
 </div>
