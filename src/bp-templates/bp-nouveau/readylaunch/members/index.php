300352021ac819b3d2f23401463d515ef07f5d25
Jitendra Banjara <jitendra@buddyboss.com>
2025-01-20 17:43:23 +0530

    PRJ-51 - Initial setup page for member

---
+++ src/bp-templates/bp-nouveau/readylaunch/members/index.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * The ReadyLaunch template for members directory.
+ *
+ * @since   BuddyBoss [BBVERSION]
+ * @version 1.0.0
+ */
+
+$is_send_ajax_request = bb_is_send_ajax_request();
+
+/**
+ * Fires at the begining of the templates BP injected content.
+ *
+ * @since BuddyBoss [BBVERSION]
+ */
+do_action( 'bp_before_directory_members_page' );
+?>
+
+<div class="members-directory-wrapper">
+
+	<?php
+	/**
+	 * Fires before the display of the members.
+	 *
+	 * @since BuddyBoss [BBVERSION]
+	 */
+	do_action( 'bp_before_directory_members' );
+	?>
+
+	<div class="members-directory-container">
+
+		<?php
+		/**
+		 * Fires before the display of the members list tabs.
+		 *
+		 * @since BuddyBoss [BBVERSION]
+		 */
+		do_action( 'bp_before_directory_members_tabs' );
+
+		if ( ! bp_nouveau_is_object_nav_in_sidebar() ) {
+			bp_get_template_part( 'common/nav/directory-nav' );
+		}
+
+		bp_get_template_part( 'common/search-and-filters-bar' );
+
+		/**
+		 * Fires before the display of the members content.
+		 *
+		 * @since BuddyBoss [BBVERSION]
+		 */
+		do_action( 'bp_before_directory_members_content' );
+		?>
+
+		<div class="screen-content members-directory-content">
+
+			<div id="members-dir-list" class="members dir-list" data-bp-list="members" data-ajax="<?php echo esc_attr( $is_send_ajax_request ? 'true' : 'false' ); ?>">
+				<?php
+				if ( $is_send_ajax_request ) {
+					echo '<div id="bp-ajax-loader">';
+					bp_nouveau_user_feedback( 'directory-members-loading' );
+					echo '</div>';
+				} else {
+					bp_get_template_part( 'members/members-loop' );
+				}
+				?>
+			</div><!-- #members-dir-list -->
+
+			<?php
+			/**
+			 * Fires and displays the members content.
+			 *
+			 * @since BuddyBoss [BBVERSION]
+			 */
+			do_action( 'bp_directory_members_content' );
+			?>
+		</div><!-- // .screen-content -->
+
+		<?php
+		/**
+		 * Fires after the display of the members content.
+		 *
+		 * @since BuddyBoss [BBVERSION]
+		 */
+		do_action( 'bp_after_directory_members_content' );
+		?>
+
+	</div>
+
+	<?php
+	/**
+	 * Fires after the display of the members.
+	 *
+	 * @since BuddyBoss [BBVERSION]
+	 */
+	do_action( 'bp_after_directory_members' );
+	?>
+
+</div>
+
+<?php
+/**
+ * Fires at the bottom of the member directory template file.
+ *
+ * @since BuddyBoss [BBVERSION]
+ */
+do_action( 'bp_after_directory_members_page' );
+?>
