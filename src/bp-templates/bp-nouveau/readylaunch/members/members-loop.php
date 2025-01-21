55b1e17af83766ef5b8e48ff3fdf6853ff664cc7
annrra <andrey.raychev@buddyboss.com>
2025-01-21 13:04:09 +0200

    [PRJ-59] Fix merge conflicts

--- src/bp-templates/bp-nouveau/readylaunch/members/members-loop.php
+++ src/bp-templates/bp-nouveau/readylaunch/members/members-loop.php
@@ -34,7 +34,7 @@ $enabled_joined_date   = ! function_exists( 'bb_enabled_member_directory_element
 
 <?php if ( bp_has_members( bp_ajax_querystring( 'members' ) ) ) : ?>
 
-	<ul id="members-list" class="<?php bp_nouveau_loop_classes(); ?> bbrl-flex-grid">
+	<ul id="members-list" class="<?php bp_nouveau_loop_classes(); ?> bb-rl-flex-grid">
 
 		<?php
 		while ( bp_members() ) :
