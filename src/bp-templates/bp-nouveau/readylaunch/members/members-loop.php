a9c9eade4b9a1774fae74bacfe4915e23999be52
Yudhisthir Nahar <yudhisthir@buddyboss.com>
2025-01-22 15:35:27 +0530

    PRJ-59 phpcs

--- src/bp-templates/bp-nouveau/readylaunch/members/members-loop.php
+++ src/bp-templates/bp-nouveau/readylaunch/members/members-loop.php
@@ -226,7 +226,7 @@ $enabled_joined_date   = ! function_exists( 'bb_enabled_member_directory_element
 						</div>
 					</div>
 
-					<?php if ( ! empty( $member_switch_button ) || ! empty( $member_report_button ) || ! empty ( $member_block_button ) ) { ?>
+					<?php if ( ! empty( $member_switch_button ) || ! empty( $member_report_button ) || ! empty( $member_block_button ) ) { ?>
 					<div class="bb_more_options member-dropdown">
 						<a href="#" class="bb_more_options_action bp-tooltip" data-bp-tooltip-pos="up" data-bp-tooltip="<?php esc_html_e( 'More Options', 'buddyboss' ); ?>">
 							<i class="bb-icon-menu-dots-h"></i>
