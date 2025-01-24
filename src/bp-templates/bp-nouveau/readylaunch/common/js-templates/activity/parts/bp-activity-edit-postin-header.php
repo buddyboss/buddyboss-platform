12126b728a136ab3d3f0f6dc09d70a12529c9dde
Amin Charoliya <amin@buddyboss.com>
2025-01-24 16:57:19 +0530

    PRJ-55 - Post form privacy UI and functionality update

---
+++ src/bp-templates/bp-nouveau/readylaunch/common/js-templates/activity/parts/bp-activity-edit-postin-header.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * ReadyLaunch - The template for displaying activity group privacy header.
+ *
+ * @since   BuddyBoss [BBVERSION]
+ * @version 1.0.0
+ */
+
+?>
+<script type="text/html" id="tmpl-activity-edit-postin-header">
+	<h3>
+		<span class="activity-header-data">
+			<?php esc_html_e( 'Select a group', 'buddyboss' ); ?>
+		<span>
+	</h3>
+	<a class="bb-rl-model-close-button" href="#">
+		<span class="bb-icons-rl-x"></span>
+	</a>
+</script>
