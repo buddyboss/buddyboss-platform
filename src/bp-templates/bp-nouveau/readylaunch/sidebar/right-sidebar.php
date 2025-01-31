b0985eaceeedafcd21c4019f8609233199c28438
Yudhisthir Nahar <yudhisthir@buddyboss.com>
2025-01-31 16:46:48 +0530

    PRJ-59 update sidebar id

--- src/bp-templates/bp-nouveau/readylaunch/sidebar/right-sidebar.php
+++ src/bp-templates/bp-nouveau/readylaunch/sidebar/right-sidebar.php
@@ -9,11 +9,11 @@
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
-if ( is_active_sidebar( 'bb-readylaunch-sidebar' ) ) {
+if ( is_active_sidebar( 'bb-readylaunch-members-sidebar' ) ) {
 	?>
 	<div id="bb-rl-right-sidebar" class="bb-rl-widget-sidebar sm-grid-1-1" role="complementary">
 		<?php
-		dynamic_sidebar( 'bb-readylaunch-sidebar' );
+		dynamic_sidebar( 'bb-readylaunch-members-sidebar' );
 		?>
 	</div>
 	<?php
