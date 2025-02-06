8a4020e40c80dfeb9bd20f7b86dac76b7a0d6727
annrra <andrey.raychev@buddyboss.com>
2025-02-06 10:17:12 +0200

    [PRJ-65] Small update in markup for single profile page

--- src/bp-templates/bp-nouveau/readylaunch/members/single/home.php
+++ src/bp-templates/bp-nouveau/readylaunch/members/single/home.php
@@ -9,39 +9,44 @@
 
 bp_nouveau_member_hook( 'before', 'home_content' );
 ?>
+<div class="bb-rl-content-wrapper">
+	<div class="bb-rl-primary-container">
+		<div id="item-header" role="complementary" data-bp-item-id="<?php echo esc_attr( bp_displayed_user_id() ); ?>" data-bp-item-component="members" class="users-header single-headers bb-rl-profile-header">
+			<?php
+			$template = 'member-header';
+			/**
+			 * Fires before the display of a member's header.
+			 *
+			 * @since BuddyPress 1.2.0
+			 */
+			do_action( 'bp_before_member_header' );
 
-<div id="item-header" role="complementary" data-bp-item-id="<?php echo esc_attr( bp_displayed_user_id() ); ?>" data-bp-item-component="members" class="users-header single-headers bb-rl-profile-header">
-	<?php
-	$template = 'member-header';
-	/**
-	 * Fires before the display of a member's header.
-	 *
-	 * @since BuddyPress 1.2.0
-	 */
-	do_action( 'bp_before_member_header' );
+			// Get the template part for the header.
+			bp_nouveau_member_get_template_part( $template );
 
-	// Get the template part for the header.
-	bp_nouveau_member_get_template_part( $template );
+			/**
+			 * Fires after the display of a member's header.
+			 *
+			 * @since BuddyPress 1.2.0
+			 */
+			do_action( 'bp_after_member_header' );
 
-	/**
-	 * Fires after the display of a member's header.
-	 *
-	 * @since BuddyPress 1.2.0
-	 */
-	do_action( 'bp_after_member_header' );
+			bp_nouveau_template_notices();
 
-	bp_nouveau_template_notices();
+			if ( ! bp_nouveau_is_object_nav_in_sidebar() ) {
+				bp_get_template_part( 'members/single/parts/item-nav' );
+			}
+			?>
+		</div><!-- #item-header -->
 
-	if ( ! bp_nouveau_is_object_nav_in_sidebar() ) {
-		bp_get_template_part( 'members/single/parts/item-nav' );
-	}
-	?>
-</div><!-- #item-header -->
+		<div class="bp-wrap">
+			<div id="item-body" class="item-body">
+				<?php bp_nouveau_member_template_part(); ?>
+			</div><!-- #item-body -->
+		</div><!-- // .bp-wrap -->
+	</div>
+	<div class="bb-rl-secondary-container"></div>
 
-<div class="bp-wrap">
-	<div id="item-body" class="item-body">
-		<?php bp_nouveau_member_template_part(); ?>
-	</div><!-- #item-body -->
-</div><!-- // .bp-wrap -->
 
-<?php bp_nouveau_member_hook( 'after', 'home_content' ); ?>
+	<?php bp_nouveau_member_hook( 'after', 'home_content' ); ?>
+</div>
