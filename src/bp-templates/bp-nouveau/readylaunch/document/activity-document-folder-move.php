114c44ef4a4a4392b70e1990ef5073792ba54b9b
Jitendra Banjara <jitendra@buddyboss.com>
2025-02-06 19:20:59 +0530

    PRJ-58 - Change class

--- src/bp-templates/bp-nouveau/readylaunch/document/activity-document-folder-move.php
+++ src/bp-templates/bp-nouveau/readylaunch/document/activity-document-folder-move.php
@@ -9,12 +9,12 @@
 
 ?>
 
-<div class="bb-rl-media-move-folder" id="bb-rl-media-move-folder" style="display: none;">
+<div class="bb-rl-media-move-folder bb-rl-modal-move-file" id="bb-rl-media-move-folder" style="display: none;">
 	<transition name="modal">
-		<div class="modal-mask bb-white bbm-model-wrap">
-			<div class="modal-wrapper">
-				<div id="bb-rl-media-create-album-popup" class="modal-container bb-rl-has-folderlocationUI">
-					<header class="bb-model-header">
+		<div class="bb-rl-modal-mask bb-white bbm-model-wrap">
+			<div class="bb-rl-modal-wrapper">
+				<div id="bb-rl-media-create-album-popup" class="bb-rl-modal-container bb-rl-has-folderlocationUI">
+					<header class="bb-rl-modal-header">
 						<h4><span class="target_name"></span></h4>
 					</header>
 					<div class="bb-rl-field-wrap">
@@ -22,7 +22,7 @@
 						<?php bp_get_template_part( 'document/document-create-folder' ); ?>
 					</div>
 					<div class="error" style="display: none;"></div>
-					<footer class="bb-model-footer">
+					<footer class="bb-rl-model-footer">
 						<a href="#" class="bb-rl-document-open-create-popup-folder"><?php esc_html_e( 'Create new folder', 'buddyboss' ); ?></a>
 						<a class="bb-rl-ac-folder-close-button" href="#"><?php esc_html_e( 'Cancel', 'buddyboss' ); ?></a>
 						<a class="button bb-rl-folder-move" id="" href="#"><?php esc_html_e( 'Move', 'buddyboss' ); ?></a>
