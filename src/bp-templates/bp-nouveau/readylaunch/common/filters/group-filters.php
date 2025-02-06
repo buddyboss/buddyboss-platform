41273620d1c58c5fa569c191b362086865adbb8f
Yudhisthir Nahar <yudhisthir@buddyboss.com>
2025-02-06 11:37:08 +0530

    PRJ-66 Update condition for the member type and scope filter

--- src/bp-templates/bp-nouveau/readylaunch/common/filters/group-filters.php
+++ src/bp-templates/bp-nouveau/readylaunch/common/filters/group-filters.php
@@ -5,70 +5,72 @@
  * This template can be overridden by copying it to yourtheme/readylaunch/common/filters/group-filters.php.
  *
  * @since BuddyBoss [BBVERSION]
- * 
+ *
  * @version 1.0.0
  */
 
 // Check group type enable?
-if ( false === bp_disable_group_type_creation() ) {
-	return '';
-}
+if ( false !== bp_disable_group_type_creation() ) {
 
-// No need to show the group type select dropdown.
-$group_type = bp_get_current_group_directory_type();
-if ( ! empty( $group_type ) ) {
-	return '';
-}
+	// No need to show the group type select dropdown.
+	$group_type = bp_get_current_group_directory_type();
+	if ( ! empty( $group_type ) ) {
+		return '';
+	}
 
 
-$args = array(
-	'orderby'    => 'menu_order',
-	'order'      => 'ASC',
-	'meta_query' => array(
-		array(
-			'key'   => '_bp_group_type_enable_filter',
-			'value' => 1,
+	$args = array(
+		'orderby'    => 'menu_order',
+		'order'      => 'ASC',
+		'meta_query' => array(
+			array(
+				'key'   => '_bp_group_type_enable_filter',
+				'value' => 1,
+			),
 		),
-	),
-);
-
-// Get active group types.
-if ( bp_is_groups_directory() ) {
-	$args['meta_query'][] = array(
-		'key'   => '_bp_group_type_enable_remove',
-		'value' => 0,
 	);
-}
 
-$group_types = bp_get_active_group_types( $args );
+	// Get active group types.
+	if ( bp_is_groups_directory() ) {
+		$args['meta_query'][] = array(
+			'key'   => '_bp_group_type_enable_remove',
+			'value' => 0,
+		);
+	}
 
-if ( ! empty( $group_types ) ) {
-	?>
-	<div id="group-type-filters" class="component-filters clearfix bb-rl-filter">
-		<div id="group-type-select" class="last filter">
-			<label class="bb-rl-filter-label" for="group-type-order-by">
-				<span><?php bp_nouveau_filter_label(); ?></span>
-			</label>
-			<div class="select-wrap">
-				<select id="group-type-order-by" data-bp-group-type-filter="<?php bp_nouveau_search_object_data_attr() ?>">
-					<option value=""><?php _e( 'All Types', 'buddyboss' ); ?></option><?php
-					foreach ( $group_types as $group_type_id ) {
-						$group_type_key   = bp_group_get_group_type_key( $group_type_id );
-						$group_type_label = bp_groups_get_group_type_object( $group_type_key )->labels['name'];
+	$group_types = bp_get_active_group_types( $args );
+
+	if ( ! empty( $group_types ) ) {
+		?>
+		<div id="group-type-filters" class="component-filters clearfix bb-rl-filter">
+			<div id="group-type-select" class="last filter">
+				<label class="bb-rl-filter-label" for="group-type-order-by">
+					<span><?php esc_html_e( 'Type', 'buddyboss' ); ?></span>
+				</label>
+				<div class="select-wrap">
+					<select id="group-type-order-by" data-bp-group-type-filter="<?php bp_nouveau_search_object_data_attr(); ?>">
+						<option value=""><?php esc_html_e( 'All', 'buddyboss' ); ?></option>
+						<?php
+						foreach ( $group_types as $group_type_id ) {
+							$group_type_key   = bp_group_get_group_type_key( $group_type_id );
+							$group_type_label = bp_groups_get_group_type_object( $group_type_key )->labels['name'];
+							?>
+								<option value="<?php echo esc_attr( $group_type_key ); ?>"><?php echo esc_attr( $group_type_label ); ?></option>
+							<?php
+						}
 						?>
-						<option value="<?php echo esc_attr( $group_type_key ); ?>"><?php echo esc_attr( $group_type_label ); ?></option><?php
-					}
-					?>
-				</select>
-				<span class="select-arrow" aria-hidden="true"></span>
+					</select>
+					<span class="select-arrow" aria-hidden="true"></span>
+				</div>
 			</div>
 		</div>
-	</div>
-	<?php
+		<?php
+	}
 }
 
 // Group scope as dropdown.
-if ( bp_nouveau_has_nav( array( 'object' => 'directory' ) ) ) { ?>
+if ( bp_nouveau_has_nav( array( 'object' => 'directory' ) ) ) {
+	?>
 	<div id="bb-rl-groups-scope-filters" class="component-filters clearfix">
 		<div id="bb-rl-groups-scope-select" class="last filter bb-rl-scope-filter bb-rl-filter">
 			<label class="bb-rl-filter-label" for="bb-rl-groups-scope-options">
@@ -84,12 +86,12 @@ if ( bp_nouveau_has_nav( array( 'object' => 'directory' ) ) ) { ?>
 						}
 						?>
 						<option id="<?php bp_nouveau_nav_id(); ?>" <?php bp_nouveau_nav_scope(); ?> data-bp-object="<?php bp_nouveau_directory_nav_object(); ?>">
-							<?php 
-								if ( 'bb-rl-groups-all' === bp_nouveau_get_nav_id() ) {
-									esc_html_e( 'All', 'buddyboss' );
-								} else {
-									bp_nouveau_nav_link_text();
-								}
+							<?php
+							if ( 'bb-rl-groups-all' === bp_nouveau_get_nav_id() ) {
+								esc_html_e( 'All', 'buddyboss' );
+							} else {
+								bp_nouveau_nav_link_text();
+							}
 							?>
 						</option>
 						<?php
@@ -100,5 +102,5 @@ if ( bp_nouveau_has_nav( array( 'object' => 'directory' ) ) ) { ?>
 			</div>
 		</div>
 	</div>
-<?php
+	<?php
 }
