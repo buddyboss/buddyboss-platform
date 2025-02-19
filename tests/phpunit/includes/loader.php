de36bc95f21ed318511716eff1c6380d71a1f1b1
Kartik Suthar <kartik@buddyboss.com>
2025-02-19 16:16:00 +0800

    fix the workflow

--- tests/phpunit/includes/loader.php
+++ tests/phpunit/includes/loader.php
@@ -6,7 +6,7 @@ $multisite = (int) ( defined( 'WP_TESTS_MULTISITE') && WP_TESTS_MULTISITE );
 system( WP_PHP_BINARY . ' ' . escapeshellarg( dirname( __FILE__ ) . '/install.php' ) . ' ' . escapeshellarg( WP_TESTS_CONFIG_PATH ) . ' ' . escapeshellarg( WP_TESTS_DIR ) . ' ' . $multisite );
 
 // Bootstrap BP
-require dirname( __FILE__ ) . '/../../../bp-loader.php';
+require dirname( __FILE__ ) . '/../../../src/bp-loader.php';
 
 // Bail from redirects as they throw 'headers already sent' warnings.
 tests_add_filter( 'wp_redirect', '__return_false' );
