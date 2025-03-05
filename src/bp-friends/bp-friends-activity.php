9f21ec825f23f751d6657e87e57492462fd80920
Yudhisthir Nahar <yudhisthir@buddyboss.com>
2025-03-05 15:34:58 +0530

    PROD-8364 Remove unwanted code after activity tab setting removed

--- src/bp-friends/bp-friends-activity.php
+++ src/bp-friends/bp-friends-activity.php
@@ -281,12 +281,6 @@ function bp_friends_filter_activity_scope( $retval = array(), $filter = array() 
 		}
 	}
 
-	//Determine activity tabs status and filter the activity.
-	if ( is_user_logged_in() && bp_loggedin_user_id() === $user_id ) {
-		$friends[]        = bp_loggedin_user_id();
-		$mutual_friends[] = bp_loggedin_user_id();
-	}
-
 	if ( empty( $friends ) ) {
 		$friends = array( 0 );
 	}
