71f0a4fc7e56dbbe8db9e34b3d9d3fc87d0f18ce
Amin Charoliya <amin@buddyboss.com>
2025-03-07 15:29:30 +0530

    PRJ-81 - Style post form toolbar emoji icon and separator

--- src/bp-templates/bp-nouveau/readylaunch/common/js-templates/messages/parts/bp-messages-formatting-toolbar.php
+++ src/bp-templates/bp-nouveau/readylaunch/common/js-templates/messages/parts/bp-messages-formatting-toolbar.php
@@ -15,13 +15,15 @@
 
 	<?php if ( bp_is_active( 'media' ) ) : ?>
 
+		<div class="bb-rl-separator"></div>
+
+		<div class="post-elements-buttons-item post-emoji bp-tooltip post-media-emoji-support" data-bp-tooltip-pos="down" data-bp-tooltip="<?php esc_attr_e( 'Emoji', 'buddyboss' ); ?>"></div>
+
 		<div class="post-elements-buttons-item show-toolbar"  data-bp-tooltip-pos="down-left" data-bp-tooltip="<?php esc_attr_e( 'Show formatting', 'buddyboss' ); ?>" data-bp-tooltip-hide="<?php esc_attr_e( 'Hide formatting', 'buddyboss' ); ?>" data-bp-tooltip-show="<?php esc_attr_e( 'Show formatting', 'buddyboss' ); ?>">
 			<a href="#" id="show-toolbar-button" class="toolbar-button bp-tooltip" aria-label="<?php esc_attr_e( 'Show formatting', 'buddyboss' ); ?>">
 				<span class="bb-icons-rl-text-aa"></span>
 			</a>
 		</div>
-
-		<div class="post-elements-buttons-item post-emoji bp-tooltip post-media-emoji-support" data-bp-tooltip-pos="down" data-bp-tooltip="<?php esc_attr_e( 'Emoji', 'buddyboss' ); ?>"></div>
 
 	<?php endif; ?>
 
