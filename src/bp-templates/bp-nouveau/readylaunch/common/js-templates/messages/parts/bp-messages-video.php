8ac373790621861379a1402a9e9f89557beb98eb
Amin Charoliya <amin@buddyboss.com>
2025-03-20 13:57:12 +0530

    PRJ-81 - Udpate Message Video dropzone template and functionality

--- src/bp-templates/bp-nouveau/readylaunch/common/js-templates/messages/parts/bp-messages-video.php
+++ src/bp-templates/bp-nouveau/readylaunch/common/js-templates/messages/parts/bp-messages-video.php
@@ -22,7 +22,7 @@
 			<div class="dz-progress-ring-wrap">
 				<i class="bb-icons-rl-fill bb-icons-rl-video-camera"></i>
 				<svg class="dz-progress-ring" width="48" height="48">
-				<circle class="progress-ring__circle" stroke="#4946FE" stroke-width="3" fill="transparent" r="21.5" cx="24" cy="24" stroke-dasharray="185.354, 185.354" stroke-dashoffset="185" />
+					<circle class="progress-ring__circle" stroke="#4946FE" stroke-width="3" fill="transparent" r="21.5" cx="24" cy="24" stroke-dasharray="185.354, 185.354" stroke-dashoffset="185"></circle>
 				</svg>
 			</div>
 			<div class="dz-error-message"><span data-dz-errormessage></span></div>
