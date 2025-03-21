a01e16ede8555091b02c7207286bf18fe0fa0d56
Kartik Suthar <kartik@buddyboss.com>
2025-03-21 11:27:10 +0530

    PRJ-65 - members connections request screen

--- src/bp-templates/bp-nouveau/readylaunch/members/single/friends/requests.php
+++ src/bp-templates/bp-nouveau/readylaunch/members/single/friends/requests.php
@@ -8,43 +8,54 @@
  */
 ?>
 
-<h2 class="screen-heading friendship-requests-screen"><?php esc_html_e( 'Requests to Connect', 'buddyboss' ); ?></h2>
+<div class="bb-rl-container-inner bb-rl-profile-container">
+	<div class="bb-rl-members-directory-container">
+		<div class="screen-content bb-rl-members-directory-content bb-rl-members">
 
-<?php
-bp_nouveau_member_hook( 'before', 'friend_requests_content' );
+			<h2 class="screen-heading friendship-requests-screen"><?php esc_html_e( 'Requests to Connect', 'buddyboss' ); ?></h2>
 
-if ( bp_has_members( 'type=alphabetical&include=' . bp_get_friendship_requests() ) ) {
-	bp_nouveau_pagination( 'top' );
-	?>
+			<?php
+			bp_nouveau_member_hook( 'before', 'friend_requests_content' );
 
-	<ul id="friend-list" class="<?php bp_nouveau_loop_classes(); ?>" data-bp-list="friendship_requests">
-		<?php
-		while ( bp_members() ) :
-			bp_the_member();
+			if ( bp_has_members( 'type=alphabetical&include=' . bp_get_friendship_requests() ) ) {
+				?>
+
+				<ul id="friend-list" class="<?php bp_nouveau_loop_classes(); ?>" data-bp-list="friendship_requests">
+					<?php
+					while ( bp_members() ) :
+						bp_the_member();
+						?>
+
+						<li id="friendship-<?php bp_friend_friendship_id(); ?>" <?php bp_member_class( array( 'item-entry' ) ); ?> data-bp-item-id="<?php bp_friend_friendship_id(); ?>" data-bp-item-component="members">
+							<div class="list-wrap footer-buttons-on">
+								<div class="list-wrap-inner">
+									<div class="item-avatar">
+										<a href="<?php bp_member_link(); ?>"><?php bp_member_avatar( array( 'type' => 'full' ) ); ?></a>
+									</div>
+									<div class="item">
+										<h2 class="item-title list-title member-name"><a href="<?php bp_member_link(); ?>"><?php bp_member_name(); ?></a></h2>
+										<div class="item-meta"><span class="activity"><?php bp_member_last_active(); ?></span></div>
+										<?php bp_nouveau_friend_hook( 'requests_item' ); ?>
+									</div>
+									<div class="bb-rl-member-buttons-wrap">
+										<?php bp_nouveau_members_loop_buttons(); ?>
+									</div>
+								</div>			
+							</div>
+						</li>
+
+					<?php endwhile; ?>
+				</ul>
+
+				<?php
+				bp_nouveau_friend_hook( 'requests_content' );
+				bp_nouveau_pagination( 'bottom' );
+			} else {
+				bp_nouveau_user_feedback( 'member-requests-none' );
+			}
+
+			bp_nouveau_member_hook( 'after', 'friend_requests_content' );
 			?>
-
-			<li id="friendship-<?php bp_friend_friendship_id(); ?>" <?php bp_member_class( array( 'item-entry' ) ); ?> data-bp-item-id="<?php bp_friend_friendship_id(); ?>" data-bp-item-component="members">
-				<div class="item-avatar">
-					<a href="<?php bp_member_link(); ?>"><?php bp_member_avatar( array( 'type' => 'full' ) ); ?></a>
-				</div>
-
-				<div class="item">
-					<div class="item-title"><a href="<?php bp_member_link(); ?>"><?php bp_member_name(); ?></a></div>
-					<div class="item-meta"><span class="activity"><?php bp_member_last_active(); ?></span></div>
-
-					<?php bp_nouveau_friend_hook( 'requests_item' ); ?>
-				</div>
-				<?php bp_nouveau_members_loop_buttons(); ?>
-			</li>
-
-		<?php endwhile; ?>
-	</ul>
-
-	<?php
-	bp_nouveau_friend_hook( 'requests_content' );
-	bp_nouveau_pagination( 'bottom' );
-} else {
-	bp_nouveau_user_feedback( 'member-requests-none' );
-}
-
-bp_nouveau_member_hook( 'after', 'friend_requests_content' );
+		</div>
+	</div>
+</div>
