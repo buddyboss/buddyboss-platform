33b0fc1bde2867878e77488448f8b5eb6f0332c1
annrra <andrey.raychev@buddyboss.com>
2025-03-24 21:50:37 +0200

    Single group/profile albums udated templates without content

--- src/bp-templates/bp-nouveau/readylaunch/media/albums.php
+++ src/bp-templates/bp-nouveau/readylaunch/media/albums.php
@@ -14,22 +14,26 @@ if ( ( ( bp_is_my_profile() && bb_user_can_create_media() ) || ( $bp_is_group &&
 	$bp_is_profile_albums_support_enabled = bp_is_profile_albums_support_enabled();
 	?>
 
-	<div class="bb-media-actions-wrap album-actions-wrap bb-rl-media-actions-wrap">
-		<h2 class="bb-title"><?php esc_html_e( 'Albums', 'buddyboss' ); ?></h2>
+	<?php
+	if ( bp_has_albums( bp_ajax_querystring( 'albums' ) ) ) {
+	?>
+		<div class="bb-media-actions-wrap album-actions-wrap bb-rl-media-actions-wrap">
+			<h2 class="bb-title"><?php esc_html_e( 'Albums', 'buddyboss' ); ?></h2>
+			<?php
+			if ( ( $bp_is_group && $bp_is_group_albums_support_enabled ) || $bp_is_profile_albums_support_enabled ) {
+				?>
+				<div class="bb-media-actions">
+					<a href="#" id="bb-create-album" class="bb-create-album button bb-rl-button bb-rl-button--brandFill bb-rl-button--small"><i class="bb-icons-rl-images"></i> <?php esc_html_e( 'Create Album', 'buddyboss' ); ?></a>
+				</div>
+				<?php
+			}
+			?>
+		</div>
+
 		<?php
 		if ( ( $bp_is_group && $bp_is_group_albums_support_enabled ) || $bp_is_profile_albums_support_enabled ) {
-			?>
-			<div class="bb-media-actions">
-				<a href="#" id="bb-create-album" class="bb-create-album button bb-rl-button bb-rl-button--brandFill bb-rl-button--small"><i class="bb-icons-rl-images"></i> <?php esc_html_e( 'Create Album', 'buddyboss' ); ?></a>
-			</div>
-			<?php
+			bp_get_template_part( 'media/create-album' );
 		}
-		?>
-	</div>
-
-	<?php
-	if ( ( $bp_is_group && $bp_is_group_albums_support_enabled ) || $bp_is_profile_albums_support_enabled ) {
-		bp_get_template_part( 'media/create-album' );
 	}
 } else {
 	?>
@@ -77,7 +81,22 @@ if ( bp_has_albums( bp_ajax_querystring( 'albums' ) ) ) :
 	<?php
 
 else :
-	bp_nouveau_user_feedback( 'media-album-none' );
+	?>
+	<div class="bb-rl-media-none">
+		<div class="bb-rl-media-none-figure"><i class="bb-icons-rl-file-image"></i></div>
+		<?php
+		bp_nouveau_user_feedback( 'media-album-none' );
+		if ( ( $bp_is_group && $bp_is_group_albums_support_enabled ) || $bp_is_profile_albums_support_enabled ) {
+			?>
+			<div class="bb-media-actions bb-rl-media-none-actions">
+				<a href="#" id="bb-create-album" class="bb-create-album button bb-rl-button bb-rl-button--brandFill bb-rl-button--small"><i class="bb-icons-rl-images"></i> <?php esc_html_e( 'Create Album', 'buddyboss' ); ?></a>
+			</div>
+			<?php
+			bp_get_template_part( 'media/create-album' );
+		}
+		?>
+	</div>
+	<?php
 endif;
 
 bp_nouveau_media_hook( 'after', 'media_album_content' );
