a719818d0e693b6da76923516fc86a4b13fa05b9
Jitendra Banjara <jitendra@buddyboss.com>
2025-03-27 12:20:07 +0530

    PRJ-81 - Remove console and remove duplicate jquery code

--- src/bp-templates/bp-nouveau/readylaunch/js/buddypress-messages.js
+++ src/bp-templates/bp-nouveau/readylaunch/js/buddypress-messages.js
@@ -5551,12 +5551,14 @@ window.bp = window.bp || {};
 				}
 
 				// Add the right panel view if not exists.
-				console.log( 'messagesFetched' );
 				if ( ! this.rightPanel || this.rightPanel.model.get( 'id' ) !== this.options.thread.get( 'id' ) ) {
 					this.rightPanel              = new bp.Views.userMessageRightPanel( { model : this.options.thread } );
 					this.rightPanel.el.className = 'bb-rl-messages-right-panel';
-					$( '#bb-rl-messages-right-panel' ).html( this.rightPanel.render().el );
-					$( '#bb-rl-messages-right-panel' ).show();
+					var $rightPanel = $( '#bb-rl-messages-right-panel' );
+					if ( $rightPanel.length ) {
+						$rightPanel.html( this.rightPanel.render().el );
+						$rightPanel.show();
+					}
 				}
 
 				$( '#bp-message-thread-list li' ).each(
