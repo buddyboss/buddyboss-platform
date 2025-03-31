4270d11c5a789f9d25caf4a8157f0a319e16d372
annrra <andrey.raychev@buddyboss.com>
2025-03-31 17:19:43 +0300

    Profile Visibility Settings

--- src/bp-templates/bp-nouveau/readylaunch/js/bb-readylaunch-xprofile.js
+++ src/bp-templates/bp-nouveau/readylaunch/js/bb-readylaunch-xprofile.js
@@ -199,34 +199,30 @@ window.bp = window.bp || {};
 	 */
 
 	/** Profile Visibility Settings *********************************/
-
-	// Initially hide the 'field-visibility-settings' block
-	$( '.field-visibility-settings' ).addClass( 'bp-hide' );
-	// Add initial aria state to button
-	$( '.visibility-toggle-link' ).attr( 'aria-expanded', 'false' );
-
-	$( '.visibility-toggle-link' ).on( 'click', function( event ) {
+    // Change visibility status when radio button changes
+    $( '.field-visibility-toggle-action' ).on( 'click', function( event ) {
 		event.preventDefault();
 
-		$( this ).attr('aria-expanded', 'true');
+		var $visibilityBlock = $( this ).closest( '.bb-rl-field-visibility-block' );
+        var $visibilitySettings = $visibilityBlock.find( '.field-visibility-settings' );
 
-		$( this ).parent().addClass( 'field-visibility-settings-hide bp-hide' )
-
-			.siblings( '.field-visibility-settings' ).removeClass( 'bp-hide' ).addClass( 'field-visibility-settings-open' );
+        $visibilitySettings.show();
 	} );
 
-	$( '.field-visibility-settings-close' ).on( 'click', function( event ) {
-		event.preventDefault();
+    $( '.field-visibility-settings input[type="radio"]' ).on( 'change', function() {
+        var $radio = $( this );
+        var settings_div = $radio.closest( '.field-visibility-settings' );
+        var vis_setting_text = $( '.iradio_minimal' ).length > 0 ? 
+            $radio.parent().next( '.field-visibility-text' ).text() : 
+            $radio.parent().text();
 
-		var settings_div = $( this ).parent(),
-			vis_setting_text = $('.iradio_minimal').length > 0 ? settings_div.find( 'input:checked' ).parent().next('.field-visibility-text').text() : settings_div.find( 'input:checked' ).parent().text();
-
-		settings_div.removeClass( 'field-visibility-settings-open' ).addClass( 'bp-hide' )
-			.siblings( '.field-visibility-settings-toggle' )
-				.children( '.current-visibility-level' ).text( vis_setting_text ).end()
-				.addClass( 'bp-show' ).removeClass( 'field-visibility-settings-hide bp-hide' );
-				$( '.visibility-toggle-link').attr( 'aria-expanded', 'false' );
-	} );
+        // Update the visibility text
+        settings_div.siblings( '.field-visibility-settings-toggle' )
+            .find( '.current-visibility-level' )
+            .text( vis_setting_text );
+            
+        settings_div.hide();
+    } );
 
 	$( '#profile-edit-form input:not(:submit), #profile-edit-form textarea, #profile-edit-form select, #signup_form input:not(:submit), #signup_form textarea, #signup_form select' ).change( function() {
 		var shouldconfirm = true;
