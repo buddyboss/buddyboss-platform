3c29ff03c6a7fdf6f3abadc1c7f42cf94561a5fd
Kartik Suthar <kartik@buddyboss.com>
2025-03-31 16:40:54 +0530

    PRJ-65 - fix the sidebar items display

--- src/bp-templates/bp-nouveau/readylaunch/sidebar/left-sidebar.php
+++ src/bp-templates/bp-nouveau/readylaunch/sidebar/left-sidebar.php
@@ -9,47 +9,48 @@
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
+
 ?>
 
 <div id="secondary" class="bb-rl-left-panel widget-area" role="complementary">
 	<?php
 	if ( BB_Readylaunch::bb_is_group_admin() ) {
 		bp_get_template_part( 'groups/single/parts/admin-subnav' );
-	} if ( bp_is_user_settings() && bp_core_can_edit_settings() ) {
+	} elseif ( bp_is_user_settings() && bp_core_can_edit_settings() ) {
 		bp_get_template_part( 'members/single/parts/item-subnav' );
-	} if ( bp_is_user_change_avatar() || bp_is_user_profile_edit() ) {
+	} elseif ( bp_is_user_change_avatar() || bp_is_user_profile_edit() ) {
 		bp_get_template_part( 'members/single/parts/edit-subnav' );
 	} else {
 		?>
 
 		<div class="bb-rl-left-panel-widget">
-		<?php
-		wp_nav_menu(
-			array(
-				'theme_location' => 'bb-top-readylaunchpanel',
-				'menu_id'        => '',
-				'container'      => false,
-				'fallback_cb'    => false,
-				'menu_class'     => 'bb-readylaunchpanel-menu bb-top-readylaunchpanel-menu',
-			)
-		);
-		?>
-	</div>
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'bb-top-readylaunchpanel',
+					'menu_id'        => '',
+					'container'      => false,
+					'fallback_cb'    => false,
+					'menu_class'     => 'bb-readylaunchpanel-menu bb-top-readylaunchpanel-menu',
+				)
+			);
+			?>
+		</div>
 		<?php
 		$active_left_sidebar_section = bb_load_readylaunch()->bb_is_active_any_left_sidebar_section( true );
 		if ( ! empty( $active_left_sidebar_section['groups']['items'] ) ) {
 			?>
-		<div class="bb-rl-left-panel-widget">
-			<?php bb_load_readylaunch()->bb_render_left_sidebar_middle_html( $active_left_sidebar_section['groups'] ); ?>
-		</div>
+			<div class="bb-rl-left-panel-widget">
+				<?php bb_load_readylaunch()->bb_render_left_sidebar_middle_html( $active_left_sidebar_section['groups'] ); ?>
+			</div>
 			<?php
 		}
 
 		if ( ! empty( $active_left_sidebar_section['courses']['items'] ) ) {
 			?>
-		<div class="bb-rl-left-panel-widget">
-			<?php bb_load_readylaunch()->bb_render_left_sidebar_middle_html( $active_left_sidebar_section['courses'] ); ?>
-		</div>
+			<div class="bb-rl-left-panel-widget">
+				<?php bb_load_readylaunch()->bb_render_left_sidebar_middle_html( $active_left_sidebar_section['courses'] ); ?>
+			</div>
 			<?php
 
 		}
