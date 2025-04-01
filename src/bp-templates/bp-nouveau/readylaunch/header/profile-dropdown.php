03b8887a7baea77f8d941d318854c63b083a8e30
Kartik Suthar <kartik@buddyboss.com>
2025-04-01 11:22:51 +0530

    PRJ-66 - refactor the profile dropdown which was conflicting in group create page

--- src/bp-templates/bp-nouveau/readylaunch/header/profile-dropdown.php
+++ src/bp-templates/bp-nouveau/readylaunch/header/profile-dropdown.php
@@ -33,15 +33,13 @@ $profile_url = trailingslashit( bp_loggedin_user_domain() . bp_get_profile_slug(
 			$old_displayed_user          = buddypress()->displayed_user;
 			buddypress()->displayed_user = buddypress()->loggedin_user;
 
-			// Set up navigation for the logged-in user.
-			bp_setup_nav();
+			$profile_nav = buddypress()->members->nav;
 
 			// Reorder the user's primary nav according to the customizer setting.
-			bp_nouveau_set_nav_item_order( buddypress()->members->nav, bp_nouveau_get_appearance_settings( 'user_nav_order' ) );
+			bp_nouveau_set_nav_item_order( $profile_nav, bp_nouveau_get_appearance_settings( 'user_nav_order' ) );
 
 			// Get the navigation items using bp_get_nav_menu_items().
-			$profile_nav = buddypress()->members->nav->get_item_nav();
-
+			$profile_nav = $profile_nav->get_primary();
 
 			// Restore the original displayed user.
 			buddypress()->displayed_user = $old_displayed_user;
@@ -71,6 +69,14 @@ $profile_url = trailingslashit( bp_loggedin_user_domain() . bp_get_profile_slug(
 					}
 
 					if ( in_array( $nav_item->slug, $excluded_slugs, true ) ) {
+						continue;
+					}
+
+					// Skip if show_for_displayed_user is empty and it's not the invites slug.
+					if (
+						empty( $nav_item->show_for_displayed_user ) &&
+						( ! function_exists( 'bp_get_invites_slug' ) || bp_get_invites_slug() !== $nav_item->slug )
+					) {
 						continue;
 					}
 
@@ -106,11 +112,12 @@ $profile_url = trailingslashit( bp_loggedin_user_domain() . bp_get_profile_slug(
 					$is_enable_profile_avatar = false;
 				}
 
-				if ( $is_enable_profile_avatar && buddypress()->avatar->show_avatars ) { ?>
+				if ( $is_enable_profile_avatar && buddypress()->avatar->show_avatars ) {
+					?>
 					<li class="bb-rl-profile-sublist-link" id="bb-rl-xprofile-change-avatar">
 						<a href="<?php echo esc_url( trailingslashit( $profile_link . 'change-avatar' ) ); ?>"><?php esc_html_e( 'Profile Photo', 'buddyboss' ); ?></a>
 					</li>
-				<?php
+					<?php
 				}
 
 				$edit_profile_link = trailingslashit( bp_loggedin_user_domain() . bp_get_profile_slug() . '/edit/group/' );
@@ -152,35 +159,65 @@ $profile_url = trailingslashit( bp_loggedin_user_domain() . bp_get_profile_slug(
 
 			<ul class="bb-profile-submenu-lists">
 				<?php
-					// Temporarily set displayed user to logged-in user to get correct navigation.
-					$old_displayed_user          = buddypress()->displayed_user;
-					buddypress()->displayed_user = buddypress()->loggedin_user;
+				// Temporarily set displayed user to logged-in user to get correct navigation.
+				$old_displayed_user          = buddypress()->displayed_user;
+				buddypress()->displayed_user = buddypress()->loggedin_user;
 
-					// Set up navigation for the logged-in user.
-					bp_setup_nav();
+				// Get the navigation items.
+				$settings_nav = buddypress()->members->nav->get_secondary( array( 'parent_slug' => bp_get_settings_slug() ) );
 
-					// Get the navigation items.
-					$settings_nav = buddypress()->members->nav->get_secondary(
-						array(
-							'parent_slug'     => 'settings',
-							'user_has_access' => true,
-						)
-					);
+				// Restore the original displayed user.
+				buddypress()->displayed_user = $old_displayed_user;
 
-					// Restore the original displayed user.
-					buddypress()->displayed_user = $old_displayed_user;
+				$excluded_slugs = array( 'capabilities' );
+				if ( bp_disable_account_deletion() || bp_current_user_can( 'bp_moderate' ) ) {
+					$excluded_slugs[] = 'delete-account';
+				}
 
-					if ( ! empty( $settings_nav ) ) {
-						foreach ( $settings_nav as $nav_item ) {
-							?>
-							<li class="bb-rl-profile-sublist-link" id="bb-rl-profile-my-account-settings-<?php echo esc_attr( $nav_item->slug ); ?>">
-								<a href="<?php echo esc_url( $nav_item->link ); ?>">
-									<?php echo esc_html( $nav_item->name ); ?>
-								</a>
-							</li>
-							<?php
+
+				if (
+					(int) bp_loggedin_user_id() !== (int) bp_displayed_user_id() &&
+					! bp_disable_account_deletion() &&
+					! bp_current_user_can( 'bp_moderate' )
+				) {
+					$delete_nav_item = new BP_Core_Nav_Item( array(
+						'name'            => __( 'Delete Account', 'buddyboss' ),
+						'slug'            => 'delete-account',
+						'parent_url'      => $settings_link,
+						'link'            => trailingslashit( $settings_link . 'delete-account' ),
+						'parent_slug'     => bp_get_settings_slug(),
+						'screen_function' => 'bp_settings_screen_delete_account',
+						'position'        => 90,
+						'user_has_access' => ! is_super_admin( bp_displayed_user_id() ),
+					) );
+
+					$settings_nav[ $delete_nav_item->position ] = $delete_nav_item;
+				}
+
+
+				if ( ! empty( $settings_nav ) ) {
+					foreach ( $settings_nav as $nav_item ) {
+						if (
+							(
+								(int) bp_loggedin_user_id() === (int) bp_displayed_user_id() &&
+								empty( $nav_item->user_has_access )
+							) ||
+							in_array( $nav_item->slug, $excluded_slugs, true )
+						) {
+							continue;
 						}
+
+						$menu_link = str_replace( bp_displayed_user_domain(), bp_loggedin_user_domain(), $nav_item->link );
+
+						?>
+						<li class="bb-rl-profile-sublist-link" id="bb-rl-profile-my-account-settings-<?php echo esc_attr( $nav_item->slug ); ?>">
+							<a href="<?php echo esc_url( $menu_link ); ?>">
+							<?php echo esc_html( $nav_item->name ); ?>
+							</a>
+						</li>
+						<?php
 					}
+				}
 				?>
 			</ul>
 		</li>
