314c4b4f78c6c540379c0faa9e1ab51f2fc5a94e
annrra <andrey.raychev@buddyboss.com>
2025-04-11 14:41:49 +0300

    Basic styling for search results

--- src/bp-templates/bp-nouveau/readylaunch/search/results-page.php
+++ src/bp-templates/bp-nouveau/readylaunch/search/results-page.php
@@ -18,17 +18,23 @@ $post_title = '';
 if ( empty( $_GET['s'] ) || '' === $_GET['s'] ) {
 	$post_title = __( 'No results found', "buddyboss" );
 } elseif ( BP_Search::instance()->has_search_results() ) {
-	$post_title = sprintf( __( 'Showing results for \'%s\'', "buddyboss" ), esc_html( $_GET['s'] ) );
+	$post_title = sprintf( __( 'Showing results for <span class="bb-rl-result-label">\'%s\'</span>', "buddyboss" ), esc_html( $_GET['s'] ) );
 } else {
-	$post_title = sprintf( __( 'No results for \'%s\'', "buddyboss" ), esc_html( $_GET['s'] ) );
+	$post_title = sprintf( __( 'No results for <span class="bb-rl-result-label">\'%s\'</span>', "buddyboss" ), esc_html( $_GET['s'] ) );
 }
 ?>
-<header class="entry-header">
-	<h1 class="entry-title">
-		<?php echo stripslashes( $post_title ); ?>
-	</h1>
-</header>
+<div class="bb-rl-container">
+	<div class="bb-rl-search-results-container">
 
-<div id="buddypress">
-	<?php bp_get_template_part("search/results-page-content"); ?>
+		<header class="entry-header">
+			<h1 class="entry-title">
+				<?php echo stripslashes( $post_title ); ?>
+			</h1>
+		</header>
+
+		<div id="buddypress">
+			<?php bp_get_template_part("search/results-page-content"); ?>
+		</div>
+
+	</div>
 </div>
