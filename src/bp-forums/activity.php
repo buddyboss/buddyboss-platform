e6e57a1dad26e875c356a792a673dd1877838dd4
Jitendra Banjara <jitendra@buddyboss.com>
2025-04-11 17:44:29 +0530

    PROD-8682 - Revert condition as not required as we prevent error with bp-core-filters.php file changes

--- src/bp-forums/activity.php
+++ src/bp-forums/activity.php
@@ -346,10 +346,7 @@ if ( ! class_exists( 'BBP_BuddyPress_Activity' ) ) :
 			if ( in_array( $activity_object->type, $disabled_actions ) ) {
 				$link = $activity_object->primary_link;
 				if ( empty( $link ) ) {
-					$item_id = $activity_object->secondary_item_id;
-					if ( bp_is_active( 'activity' ) && bp_is_activity_component() ) {
-						$item_id = ( 'groups' === $activity_object->component ? $activity_object->secondary_item_id : $activity_object->item_id );
-					}
+					$item_id = ( 'groups' === $activity_object->component ? $activity_object->secondary_item_id : $activity_object->item_id );
 					if ( 'bbp_reply_create' == $activity_object->type ) {
 						$link = bbp_get_reply_url( $item_id );
 					} elseif ( 'bbp_topic_create' == $activity_object->type ) {
