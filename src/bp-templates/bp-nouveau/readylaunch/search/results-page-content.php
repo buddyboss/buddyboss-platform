eaefd7455e5d7b7affd12b4634ce23134c1bc084
annrra <andrey.raychev@buddyboss.com>
2025-04-11 22:47:28 +0300

    Enhance search modal styling and layout

--- src/bp-templates/bp-nouveau/readylaunch/search/results-page-content.php
+++ src/bp-templates/bp-nouveau/readylaunch/search/results-page-content.php
@@ -33,7 +33,7 @@ $no_results_class = ! BP_Search::instance()->has_search_results() ?  'bp-search-
 			</transition>
 		</div>
 
-		<div id="bb-rl-network-search-modal" class="bb-rl-network-search-modal bb-rl-search-modal" style="display: none;">
+		<div id="bb-rl-network-search-modal" class="bb-rl-network-search-modal bb-rl-search-modal">
 			<transition name="modal">
 				<div class="modal-mask bb-rl-modal-mask">
 					<div class="bb-rl-modal-wrapper">
@@ -48,10 +48,24 @@ $no_results_class = ! BP_Search::instance()->has_search_results() ?  'bp-search-
 										<span id="button-text" class="bp-screen-reader-text"><?php esc_html_e( 'Search', 'buddyboss' ); ?></span>
 									</button>
 									<a href="" class="bb-rl-network-search-clear"><?php esc_html_e( 'Clear Search', 'buddyboss' ); ?></a>
-									<div class="bb-rl-network-search-filter">
-										<div class="bb-rl-filter-tag">
+									<div class="bb-rl-network-search-filter bb_more_options">
+										<a class="bb-rl-filter-tag bb-rl-context-btn bb_more_options_action">
 											<span class="search-filter-label"><?php esc_html_e( 'All', 'buddyboss' ); ?></span>
 											<i class="bb-icons-rl-caret-down"></i>
+										</a>
+										<div class="bb-rl-search-options bb_more_options_list bb_more_dropdown">
+											<div class="bb-rl-search-filter-option">
+												<a href="#"><?php esc_html_e( 'Groups', 'buddyboss' ); ?></a>
+											</div>
+											<div class="bb-rl-search-filter-option">
+												<a href="#"><?php esc_html_e( 'Forum Discussions', 'buddyboss' ); ?></a>
+											</div>
+											<div class="bb-rl-search-filter-option">
+												<a href="#"><?php esc_html_e( 'Forum Replies', 'buddyboss' ); ?></a>
+											</div>
+											<div class="bb-rl-search-filter-option">
+												<a href="#"><?php esc_html_e( 'Members', 'buddyboss' ); ?></a>
+											</div>
 										</div>
 									</div>
 								</div>
@@ -70,7 +84,25 @@ $no_results_class = ! BP_Search::instance()->has_search_results() ?  'bp-search-
 											</div>
 											<div class="entry-content">The most versatile, and feature-rich engagement platform. Browse beautifully designed templates, effortlessly customize it to meet your specific requirements.</div>
 											<div class="entry-meta">
-												<span class="bb-rl-search-post-date">12 minutes ago</span>
+												<span class="bb-rl-meta-item bb-rl-meta-followers">34 followers</span>
+												<span class="bb-rl-meta-item bb-rl-meta-date">12 minutes ago</span>
+											</div>
+										</div>
+									</li>
+									<li class="bb-rl-search-post bb-rl-search-post--group">
+										<div class="item-avatar">
+											<a href="#" title="" class="bb-rl-avatar-link">
+												<img alt="" src="http://localhost/bb-buddyboss-theme-demo/wp-content/uploads/avatars/1/67f6a78532bc0-bpthumb.jpg" class="avatar" height="80" width="80">
+											</a>
+										</div>
+										<div class="item-content">
+											<div class="item-title">
+												<a href="#" title="" class="bb-rl-author-link">Innovation Club</a>
+											</div>
+											<div class="entry-content">The most versatile, and feature-rich engagement platform. Browse beautifully designed templates, effortlessly customize it to meet your specific requirements.</div>
+											<div class="entry-meta">
+												<span class="bb-rl-meta-item bb-rl-meta-author">Posted by John Muller</span>
+												<span class="bb-rl-meta-item bb-rl-meta-date">12 minutes ago</span>
 											</div>
 										</div>
 									</li>
