09817c8394855b9353bedcb7b70e4fb51a08f408
Kartik Suthar <kartik@buddyboss.com>
2025-04-15 14:04:21 +0530

    PRJ-66 - group activity filter fix

--- src/bp-templates/bp-nouveau/readylaunch/common/filters/groups-screens-filters.php
+++ src/bp-templates/bp-nouveau/readylaunch/common/filters/groups-screens-filters.php
@@ -5,19 +5,44 @@
  * @since 3.0.0
  * @version 3.0.0
  */
+
+$avail_sorting_options = bb_get_enabled_activity_sorting_options();
+arsort( $avail_sorting_options );
+$default_selected = key( $avail_sorting_options );
+if ( ! empty( $avail_sorting_options ) && in_array( 1, $avail_sorting_options, true ) && array_count_values( $avail_sorting_options )[1] > 1 ) {
+	$hide_class = '';
+} else {
+	$hide_class = 'bp-hide';
+	if ( empty( $avail_sorting_options ) || ! in_array( 1, $avail_sorting_options, true ) ) {
+		$avail_sorting_options = array( 'date_recorded' => 1 );
+	}
+}
 ?>
-<div id="comp-filters" class="component-filters clearfix">
-		<div id="<?php bp_nouveau_filter_container_id(); ?>" class="last filter bb-rl-filter bb-rl-filter-rounded">
-			<label for="<?php bp_nouveau_filter_id(); ?>" class="bp-screen-reader-text">
-				<span ><?php bp_nouveau_filter_label(); ?></span>
-			</label>
-			<div class="select-wrap">
-				<select id="<?php bp_nouveau_filter_id(); ?>" data-bp-filter="<?php bp_nouveau_filter_component(); ?>" data-bb-caret="caret-arrow" data-bb-icon="icon-funnel" data-bb-border="rounded">
+<i class="bb-rl-loader"></i>
+<div class='<?php echo esc_attr( $hide_class ); ?> bb-subnav-filters-container-main'>
+	<span class="bb-subnav-filters-label"><?php echo esc_html_e( 'by', 'buddyboss' ); ?></span>
+	<div class="bb-subnav-filters-container bb-subnav-filters-filtering">
+		<?php $sorting_labels = bb_get_activity_sorting_options_labels(); ?>
+		<button class="subnav-filters-opener" aria-expanded="false" aria-controls="bb-subnav-filter-by">
+			<span class="selected"><?php echo strtolower( esc_html( $sorting_labels[ $default_selected ] ) ); ?></span>
+			<i class="bb-icon-l bb-icon-angle-down"></i>
+		</button>
 
-					<?php bp_nouveau_filter_options(); ?>
-
-				</select>
-				<span class="select-arrow" aria-hidden="true"></span>
-			</div>
+		<div class="subnav-filters-modal" id="bb-subnav-filter-by">
+			<ul role="listbox">
+				<?php
+				if ( ! empty( $avail_sorting_options ) ) {
+					foreach ( $avail_sorting_options as $key => $is_enabled ) {
+						if ( empty( $is_enabled ) || empty( $sorting_labels[ $key ] ) ) {
+							continue;
+						}
+						?>
+						<li class="<?php echo ( $key === $default_selected ) ? 'selected' : ''; ?>" role="option" data-bp-order="activity" data-bp-orderby="<?php echo esc_attr( $key ); ?>"><a href="#"><?php echo esc_html( $sorting_labels[ $key ] ); ?></a></li>
+						<?php
+					}
+				}
+				?>
+			</ul>
 		</div>
+	</div>
 </div>
