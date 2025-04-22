2c432a4c499b7ef3ecc702a221e5172bf9016150
annrra <andrey.raychev@buddyboss.com>
2025-04-22 17:34:54 +0300

    [PROD-7236] Updates in autocomplete search AJAX templates to cleanup code and nested links

--- src/bp-templates/bp-nouveau/js/buddypress-search.js
+++ src/bp-templates/bp-nouveau/js/buddypress-search.js
@@ -93,7 +93,7 @@ jQuery(document).ready(function($) {
 							$(ul).data('current_cat', item.type);
 							return $('<li>').attr('class', 'bbls-' + item.type + '-type bbls-category').append('<div>' + item.value + '</div>').appendTo(ul);
 						} else {
-							return $('<li>').attr('class', 'bbls-' + item.type + '-type bbls-sub-item').append('<a class="x">' + item.value + '</a>').appendTo(ul);
+							return $( '<li>' ).attr( 'class', 'bbls-' + item.type + '-type bbls-sub-item' ).append( item.value ).appendTo( ul );
 						}
 
 
