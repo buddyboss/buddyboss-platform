f602514856f8d1f35a46952d7d70b264c0c00027
Amin Charoliya <amin@buddyboss.com>
2025-04-23 17:50:57 +0530

    Make changes in mobile header and style mobile side menu

--- src/bp-templates/bp-nouveau/readylaunch/header/readylaunch-header.php
+++ src/bp-templates/bp-nouveau/readylaunch/header/readylaunch-header.php
@@ -87,48 +87,15 @@ bp_get_template_part( 'sidebar/left-sidebar' );
 
 			<div class="bb-readylaunch-mobile-menu__wrap">
 				<?php
-				if ( is_user_logged_in() ) {
-					?>
-					<div class="bb-rl-mobile-panel-header flex items-center justify-between">
-						<div class="bb-rl-mobile-user-wrap flex items-center">
-							<?php
-							$current_user = wp_get_current_user();
-							$user_link    = function_exists( 'bp_core_get_user_domain' ) ? bp_core_get_user_domain( $current_user->ID ) : get_author_posts_url( $current_user->ID );
-							$display_name = function_exists( 'bp_core_get_user_displayname' ) ? bp_core_get_user_displayname( $current_user->ID ) : $current_user->display_name;
-							?>
-
-							<a class="bb-rl-mobile-user-link" href="<?php echo esc_url( $user_link ); ?>">
-								<?php echo get_avatar( get_current_user_id(), 100 ); ?>
-							</a>
-							<div>
-								<a href="<?php echo esc_url( $user_link ); ?>" class="bb-rl-mobile-user-name">
-									<?php echo esc_html( $display_name ); ?>
-								</a>
-								<?php
-								if ( function_exists( 'bp_is_active' ) && bp_is_active( 'settings' ) ) {
-									$settings_link = trailingslashit( bp_loggedin_user_domain() . bp_get_settings_slug() );
-									?>
-									<div class="bb-rl-my-account-link">
-										<a href="<?php echo esc_url( $settings_link ); ?>"><?php esc_html_e( 'My Account', 'buddyboss' ); ?></a>
-									</div>
-									<?php
-								}
-								?>
-							</div>
-						</div><!-- .bb-rl-mobile-user-wrap -->
-						<a href="#" class="bb-rl-close-panel-mobile"><i class="bb-icons-rl-bold bb-icons-rl-x"></i></a>
-					</div> <!-- .brl-mobile-panel-header -->
-					<?php
-				}
-				wp_nav_menu(
-					array(
-						'theme_location' => 'bb-readylaunch',
-						'menu_id'        => '',
-						'container'      => false,
-						'fallback_cb'    => false,
-						'menu_class'     => 'bb-readylaunch-mobile-menu',
-					)
-				);
+					wp_nav_menu(
+						array(
+							'theme_location' => 'bb-readylaunch',
+							'menu_id'        => '',
+							'container'      => false,
+							'fallback_cb'    => false,
+							'menu_class'     => 'bb-readylaunch-mobile-menu',
+						)
+					);
 				?>
 			</div>
 		</div>
