421e0568be703f0828a672184c85751c6a1aea09
Jitendra Banjara <jitendra@buddyboss.com>
2025-04-24 22:13:29 +0530

    [PROD-8796] Added function exists condition to prevent error

--- src/bp-core/classes/class-bp-admin-tab.php
+++ src/bp-core/classes/class-bp-admin-tab.php
@@ -358,7 +358,7 @@ if ( ! class_exists( 'BP_Admin_Tab' ) ) :
 				}
 			}
 
-			if ( bb_is_enabled_activity_topics() ) {
+			if ( function_exists( 'bb_is_enabled_activity_topics' ) && bb_is_enabled_activity_topics() ) {
 				$localize_arg['delete_topic_confirm'] = esc_html__( 'Are you sure you want to delete this topic?', 'buddyboss' );
 			}
 
