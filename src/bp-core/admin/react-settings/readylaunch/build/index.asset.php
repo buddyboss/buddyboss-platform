2dff5b4cd4c90e35d16970018eead26f7e2fa423
annrra <andrey.raychev@buddyboss.com>
2025-05-01 11:50:17 +0300

    Rebuild react settings component assets

--- src/bp-core/admin/react-settings/readylaunch/build/index.asset.php
+++ src/bp-core/admin/react-settings/readylaunch/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5bf0a5ee4c96d1fdaa11');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '30b8076af73c2b5e7073');
