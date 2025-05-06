d9ffcc5448281753bb6849047b4055f0a4daf455
annrra <andrey.raychev@buddyboss.com>
2025-05-06 21:45:55 +0300

    Content alignment and loading elements styling enhancements

--- src/bp-core/admin/react-settings/readylaunch/build/index.asset.php
+++ src/bp-core/admin/react-settings/readylaunch/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd5280c3cdf0c7bc3360d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '056e3cccd3abc552a3f7');
