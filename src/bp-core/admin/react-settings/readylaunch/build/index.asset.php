f1ad81ce0542ec3e3fd5b730ac1375a9ad60e75f
annrra <andrey.raychev@buddyboss.com>
2025-05-08 14:48:28 +0300

    Adjust and improve layout for responsiveness on smaller screens

--- src/bp-core/admin/react-settings/readylaunch/build/index.asset.php
+++ src/bp-core/admin/react-settings/readylaunch/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd1f9292da46b10531d8e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9c218c57feae5c099bf5');
