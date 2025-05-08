1f1224d9169c8ccb119d1261a2acabec11de765a
annrra <andrey.raychev@buddyboss.com>
2025-05-08 13:12:44 +0300

    Ensure full height for ReadyLaunch settings

--- src/bp-core/admin/react-settings/readylaunch/build/index.asset.php
+++ src/bp-core/admin/react-settings/readylaunch/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '086ab01de7e98d3b5277');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd1f9292da46b10531d8e');
