fb65d5c9c2fc6398905d8541dd1120fd50d20f8b
Jitendra Banjara <jitendra@buddyboss.com>
2025-05-11 13:08:50 +0530

    [PROD-8796] Pass topic_id to heartbeat to load activity topic data

--- src/bp-templates/bp-nouveau/js/buddypress-activity.js
+++ src/bp-templates/bp-nouveau/js/buddypress-activity.js
@@ -333,6 +333,13 @@ window.bp = window.bp || {};
 						heartbeatData.order_by = order_by;
 					}
 
+					// Get the current topic ID from storage.
+					var topicId = bp.Nouveau.getStorage( 'bp-activity', 'topic_id' );
+					if ( topicId ) {
+						data.bp_heartbeat          = data.bp_heartbeat || {};
+						data.bp_heartbeat.topic_id = topicId;
+					}
+
 					return heartbeatData;
 				})()
 			});
@@ -347,7 +354,8 @@ window.bp = window.bp || {};
 		 */
 		heartbeatTick: function( event, data ) {
 			var newest_activities_count, newest_activities, objects = bp.Nouveau.objects,
-				scope = bp.Nouveau.getStorage( 'bp-activity', 'scope' ), self = this;
+				scope = bp.Nouveau.getStorage( 'bp-activity', 'scope' ), self = this,
+				topicId = bp.Nouveau.getStorage( 'bp-activity', 'topic_id' );
 
 			// Only proceed if we have newest activities.
 			if ( undefined === data || ! data.bp_activity_newest_activities ) {
@@ -359,6 +367,14 @@ window.bp = window.bp || {};
 
 			// Parse activities.
 			newest_activities = $( this.heartbeat_data.newest ).filter( '.activity-item' );
+			
+			// If we have a topic filter active, only show activities matching that topic.
+			if ( topicId ) {
+				newest_activities       = newest_activities.filter( function ( activity ) {
+					return activity.topic_id === topicId;
+				} );
+				newest_activities_count = newest_activities.length;
+			}
 
 			// Count them.
 			newest_activities_count = Number( newest_activities.length );
