a42a2b8b8ed997444d91143ad9d26dde66dc84d3
Kartik Suthar <kartik@buddyboss.com>
2025-05-12 16:59:33 +0530

    Fix the register page

--- src/bp-templates/bp-nouveau/readylaunch/register.php
+++ src/bp-templates/bp-nouveau/readylaunch/register.php
@@ -8,21 +8,31 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-wp_head();
 wp_enqueue_style( 'bb-rl-login-fonts', buddypress()->plugin_url . 'bp-templates/bp-nouveau/readylaunch/assets/fonts/fonts.css' );
 wp_enqueue_style( 'bb-rl-login-style', buddypress()->plugin_url . 'bp-templates/bp-nouveau/readylaunch/css/login.css' );
 wp_enqueue_style( 'bb-rl-login-style-icons', buddypress()->plugin_url . 'bp-templates/bp-nouveau/readylaunch/icons/css/bb-icons-rl.min.css' );
+?>
 
-bp_get_template_part( 'common/header-register' );
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class( 'bb-rl-register' ); ?>>
 
-if ( have_posts() ) :
-	/* Start the Loop */
-	while ( have_posts() ) :
-		the_post();
+	<?php
+	bp_get_template_part( 'common/header-register' );
 
-		the_content();
-	endwhile;
-endif;
+	if ( have_posts() ) :
+		/* Start the Loop */
+		while ( have_posts() ) :
+			the_post();
 
-wp_footer();
+			the_content();
+		endwhile;
+	endif;
+	?>
+</body>
+</html>
 
