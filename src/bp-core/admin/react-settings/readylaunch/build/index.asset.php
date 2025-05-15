90b5181acbfea6a2c713ba5f52e8c45c7008da57
annrra <andrey.raychev@buddyboss.com>
2025-05-15 22:09:37 +0300

    Styling enhancements - create specific stylesheets for accordion and helper modal components to arrange stylesheets more modular

--- src/bp-core/admin/react-settings/readylaunch/build/index.asset.php
+++ src/bp-core/admin/react-settings/readylaunch/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3cdf52c1937738759e5d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c252375c469720e3c600');
