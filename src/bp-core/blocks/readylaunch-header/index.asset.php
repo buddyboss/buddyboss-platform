afc3dea127e7ef7f0d4a05e461aa797b284948b5
Amin Charoliya <amin@buddyboss.com>
2025-05-26 21:08:01 +0530

    Add bb-icons-rl css as block editor assets

--- src/bp-core/blocks/readylaunch-header/index.asset.php
+++ src/bp-core/blocks/readylaunch-header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '292fba51e799d85db779');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '0f00c214a11c00e062a6');
