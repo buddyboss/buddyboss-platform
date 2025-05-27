69258f35154d188a20f8349fd21a7ec66e9f0e5b
Jitendra Banjara <jitendra@buddyboss.com>
2025-05-27 17:53:41 +0530

    [PROD-8796] Added none option to reset topic if topic is not requires

--- src/bp-core/js/bb-topics-manager.js
+++ src/bp-core/js/bb-topics-manager.js
@@ -200,14 +200,24 @@ window.bp = window.bp || {};
 						selectTopic : function ( event ) {
 							event.preventDefault();
 
-							var topicId   = $( event.currentTarget ).data( 'topic-id' );
+							var topicId   = parseInt( $( event.currentTarget ).data( 'topic-id' ) );
 							var topicName = $( event.currentTarget ).text().trim();
 
-							this.model.set( 'topics', {
-								topic_id    : topicId,
-								topic_name  : topicName,
-								topic_lists : this.model.get( 'topics' ).topic_lists
-							} );
+							if ( 0 === topicId ) {
+								this.model.set('topics', {
+									topic_id: 0,
+									topic_name: '', // This will trigger the template to show "Select Topic"
+									topic_lists: this.model.get('topics').topic_lists
+								});
+								topicName = this.$el.find( '.bb-topic-selector-button' ).data( 'select-topic-text' );
+							} else {
+								this.model.set('topics', {
+									topic_id: topicId,
+									topic_name: topicName,
+									topic_lists: this.model.get('topics').topic_lists
+								});
+								topicName = topicName;
+							}
 
 							this.$el.find( '.bb-topic-selector-button' ).text( topicName );
 							this.$el.removeClass( 'is-active' );
