0efe91232a37fc4b5fed3562d810dbbe49b635f0
Kartik Suthar <kartik@buddyboss.com>
2025-05-29 13:36:30 +0530

    ARCH-155 - allowed to be restrict for full and wide align

--- src/bp-core/blocks/readylaunch-header/index.asset.php
+++ src/bp-core/blocks/readylaunch-header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a277e5b834dd320bf7e1');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '5b7d0c68faecc877a836');
