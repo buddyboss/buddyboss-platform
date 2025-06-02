4dbb498b5eea33420f75ee051758b509f1d5564d
Jitendra Banjara <jitendra@buddyboss.com>
2025-06-02 15:59:18 +0530

    Removed icon from heading title

--- src/bp-core/admin/bb-settings/readylaunch/build/index.asset.php
+++ src/bp-core/admin/bb-settings/readylaunch/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'dbc10a66efc7657540db');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bc70650047dced8eb2a3');
