e3a7b9012902503b6b2c28a6072a96dc1cfea36d
Jitendra Banjara <jitendra@buddyboss.com>
2025-06-27 20:26:58 +0530

    Update sequence for icons

--- src/bp-templates/bp-nouveau/readylaunch/document/actions.php
+++ src/bp-templates/bp-nouveau/readylaunch/document/actions.php
@@ -19,8 +19,8 @@ if ( bp_is_my_profile() || ( bp_is_active( 'groups' ) && bp_is_group() && ( ( bp
 
 	<header class="bb-member-media-header bb-photos-actions">
 		<div class="bb-media-meta bb-documents-meta">
+			<a data-bp-tooltip-pos="up" data-bp-tooltip="<?php esc_attr_e( 'Select All', 'buddyboss' ); ?>" class="bb-select bp-tooltip" id="bb-select-deselect-all-media" href="#"><i class="bb-icons-rl-check-circle"></i></a>
 			<a data-bp-tooltip-pos="up" data-bp-tooltip="<?php esc_attr_e( 'Delete', 'buddyboss' ); ?>" class="bb-delete bp-tooltip" id="bb-delete-media" href="#"><i class="dashicons dashicons-trash"></i></a>
-			<a data-bp-tooltip-pos="up" data-bp-tooltip="<?php esc_attr_e( 'Select All', 'buddyboss' ); ?>" class="bb-select bp-tooltip" id="bb-select-deselect-all-media" href="#"><i class="dashicons dashicons-yes"></i></a>
 		</div>
 	</header>
 
