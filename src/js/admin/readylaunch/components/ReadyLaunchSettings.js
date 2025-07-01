c9464de5e6f4490489409795e1f8dbed0354143b
Kartik Suthar <kartik@buddyboss.com>
2025-07-01 18:49:42 +0530

    updated video

--- src/js/admin/readylaunch/components/ReadyLaunchSettings.js
+++ src/js/admin/readylaunch/components/ReadyLaunchSettings.js
@@ -1249,7 +1249,7 @@ export const ReadyLaunchSettings = () => {
 					</Button>
 				</div>
 				<div className="bb-rl-welcome-video">
-					<iframe width="560" height="315" src="https://www.youtube.com/embed/nV3BmJgOiQI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/3-JhzDr1gLc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 				</div>
 			</div>
 		</div>
