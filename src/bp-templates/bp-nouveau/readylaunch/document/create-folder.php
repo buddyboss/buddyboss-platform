98b55240ed0335791681dad8fd21a7a4ff9a822e
Jitendra Banjara <jitendra@buddyboss.com>
2025-07-02 14:26:11 +0530

    Update Palceholder

--- src/bp-templates/bp-nouveau/readylaunch/document/create-folder.php
+++ src/bp-templates/bp-nouveau/readylaunch/document/create-folder.php
@@ -27,7 +27,7 @@ defined( 'ABSPATH' ) || exit;
 					</header>
 					<div class="bb-field-wrap">
 						<label for="bb-album-title" class="bb-label"><?php esc_html_e( 'Title', 'buddyboss' ); ?></label>
-						<input id="bb-album-title" value="" type="text" placeholder="<?php esc_html_e( 'Enter Folder Title', 'buddyboss' ); ?>" />
+						<input id="bb-album-title" value="" type="text" placeholder="<?php esc_html_e( 'Enter folder title', 'buddyboss' ); ?>" />
 						<small class="error-box"><?php esc_html_e( 'Following special characters are not supported: \ / ? % * : | " < >', 'buddyboss' ); ?></small>
 					</div>
 					<?php
