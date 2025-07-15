e64044257a3fa7164c109139fed0dff69cc0316f
Jitendra Banjara <jitendra@buddyboss.com>
2025-07-15 10:47:52 +0530

    [FIXES-1] Added class loading to displaye loader when click on tab

--- src/bp-core/js/bb-rl-my-network-widget.js
+++ src/bp-core/js/bb-rl-my-network-widget.js
@@ -16,6 +16,7 @@ function my_network_click_handler() {
 
 			jQuery( currentWidget ).find( widgetTabLinks ).removeClass( 'selected' );
 			jQuery( this ).addClass( 'loading selected' );
+			jQuery( widgetMemberList ).addClass( 'loading' );
 
 			var seeAllLink = jQuery( this ).data( 'see-all-link' );
 			if ( '' !== seeAllLink ) {
@@ -33,6 +34,7 @@ function my_network_click_handler() {
 				},
 				function( response ) {
 					jQuery( link ).removeClass( 'loading' );
+					jQuery( widgetMemberList ).removeClass( 'loading' );
 					var targetList = jQuery( currentWidget ).find( widgetMemberList );
 					if ( 'undefined' !== typeof response.success && response.success === 1 ) {
 						jQuery( targetList ).fadeOut(
