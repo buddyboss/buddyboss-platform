9564197ec73bb347f29f4882609253c2cfcf60f7
annrra <andrey.raychev@buddyboss.com>
2025-08-11 16:26:27 +0300

    [PROD-7236] Updated autocomplete topic and forum search result templates content to wrap item in a single link

--- src/bp-templates/bp-nouveau/buddypress/search/loop/forum-ajax.php
+++ src/bp-templates/bp-nouveau/buddypress/search/loop/forum-ajax.php
@@ -15,8 +15,8 @@ $result      = bp_search_is_post_restricted( $forum_id, get_current_user_id(), '
 $total_reply = bbp_get_forum_reply_count( $forum_id );
 
 ?>
-<div class="bp-search-ajax-item bp-search-ajax-item_forum">
-	<a href="<?php echo esc_url( bbp_get_forum_permalink( $forum_id ) ); ?>">
+<a href="<?php echo esc_url( bbp_get_forum_permalink( $forum_id ) ); ?>" class="bb-search-item-anchor">
+	<div class="bp-search-ajax-item bp-search-ajax-item_forum">
 		<div class="item-avatar">
 			<?php
 			if ( $result['has_thumb'] ) {
@@ -30,48 +30,48 @@ $total_reply = bbp_get_forum_reply_count( $forum_id );
 			}
 			?>
 		</div>
-	</a>
-	<div class="item">
-		<div class="item-title">
-			<a href="<?php echo esc_url( bbp_get_forum_permalink( $forum_id ) ); ?>"><?php bbp_forum_title( $forum_id ); ?></a>
-		</div>
-		<div class="item-desc">
-			<?php echo wp_kses_post( $result['post_content'] ); ?>
-			<div class="entry-meta">
-				<span class="topic-count">
-					<?php
-					printf(
-					/* translators: total topics */
-						_n( '%d topic', '%d topics', $total, 'buddyboss' ),
-						$total
-					);
-					?>
-				</span>
-				<span class="middot">&middot;</span>
-				<span class="reply-count">
-					<?php
-					printf(
-					/* translators: total replies */
-						_n( '%d reply', '%d replies', $total_reply, 'buddyboss' ),
-						$total_reply
-					);
-					?>
-				</span>
-				<?php
-				$last_active = bbp_get_forum_last_active_time( $forum_id );
-				if ( $last_active ) {
-					?>
-					<span class="middot">&middot;</span>
-					<span class="freshness">
+		<div class="item">
+			<div class="item-title">
+				<span class="bb-search-item-author-name"><?php bbp_forum_title( $forum_id ); ?></span>
+			</div>
+			<div class="item-desc">
+				<?php echo wp_kses_post( $result['post_content'] ); ?>
+				<div class="entry-meta">
+					<span class="topic-count">
 						<?php
-						esc_html_e( 'Last active ', 'buddyboss' );
-						echo wp_kses_post( $last_active );
+						printf(
+						/* translators: total topics */
+							_n( '%d topic', '%d topics', $total, 'buddyboss' ),
+							$total
+						);
+						?>
+					</span>
+					<span class="middot">&middot;</span>
+					<span class="reply-count">
+						<?php
+						printf(
+						/* translators: total replies */
+							_n( '%d reply', '%d replies', $total_reply, 'buddyboss' ),
+							$total_reply
+						);
 						?>
 					</span>
 					<?php
-				}
-				?>
+					$last_active = bbp_get_forum_last_active_time( $forum_id );
+					if ( $last_active ) {
+						?>
+						<span class="middot">&middot;</span>
+						<span class="freshness">
+							<?php
+							esc_html_e( 'Last active ', 'buddyboss' );
+							echo wp_kses_post( $last_active );
+							?>
+						</span>
+						<?php
+					}
+					?>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</a>
