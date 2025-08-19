a004c5846b2ca0a16ff6e16da7c26e61abd0539d
Jitendra Banjara <jitendra@buddyboss.com>
2025-08-19 23:43:52 +0530

    PROD-9014 - Added code to display feature in the platform and readylaunch

--- src/bp-templates/bp-nouveau/buddypress/activity/entry.php
+++ src/bp-templates/bp-nouveau/buddypress/activity/entry.php
@@ -237,6 +237,27 @@ $activity_popup_title = sprintf( esc_html__( '%s\'s post', 'buddyboss' ), bp_cor
 
 	<?php endif; ?>
 
+	<?php
+	if (
+		function_exists( 'bb_pro_activity_post_feature_image_instance' ) &&
+		bb_pro_activity_post_feature_image_instance() &&
+		method_exists( bb_pro_activity_post_feature_image_instance(), 'bb_get_feature_image_data' )
+	) {
+		?>
+		<div class="activity-feature-image">
+			<?php
+			$feature_image_data = bb_pro_activity_post_feature_image_instance()->bb_get_feature_image_data( $activity_id );
+			if ( ! empty( $feature_image_data ) ) {
+				?>
+				<img src="<?php echo esc_url( $feature_image_data['url'] ); ?>" alt="<?php echo esc_attr( $feature_image_data['alt'] ); ?>" />
+				<?php
+			}
+			?>
+		</div>
+		<?php
+	}
+	?>
+
 	<div class="activity-content <?php bp_activity_entry_css_class(); ?>">
 
 		<?php
--- src/bp-templates/bp-nouveau/readylaunch/activity/entry.php
+++ src/bp-templates/bp-nouveau/readylaunch/activity/entry.php
@@ -262,6 +262,27 @@ $bb_rl_activity_class_exists = class_exists( 'BB_Activity_Readylaunch' ) ? BB_Ac
 
 		<?php endif; ?>
 
+		<?php
+		if (
+			function_exists( 'bb_pro_activity_post_feature_image_instance' ) &&
+			bb_pro_activity_post_feature_image_instance() &&
+			method_exists( bb_pro_activity_post_feature_image_instance(), 'bb_get_feature_image_data' )
+		) {
+			?>
+			<div class="bb-rl-activity-feature-image">
+				<?php
+				$feature_image_data = bb_pro_activity_post_feature_image_instance()->bb_get_feature_image_data( $activity_id );
+				if ( ! empty( $feature_image_data ) ) {
+					?>
+					<img src="<?php echo esc_url( $feature_image_data['url'] ); ?>" alt="<?php echo esc_attr( $feature_image_data['alt'] ); ?>" />
+					<?php
+				}
+				?>
+			</div>
+			<?php
+		}
+		?>
+
 		<div class="bb-rl-activity-content <?php bp_activity_entry_css_class(); ?>">
 			<?php
 			bp_nouveau_activity_hook( 'before', 'activity_content' );
