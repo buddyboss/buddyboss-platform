8da08cd43860f38a16d5c999cc4cf451cd7a2f08
Amin Charoliya <amin@buddyboss.com>
2025-08-29 16:11:54 +0530

    [PROD-9130] feat: differentiate empty album vs albums directory notices
    - Add single album detection using bp_is_single_album() and bp_is_single_video_album()
    - Show "No photos or videos found" for empty single albums
    - Update action buttons to display "Add Photos/Videos" for single albums vs "Create Album" for directory
    - Add proper permission checks for media/video creation

--- src/bp-templates/bp-nouveau/readylaunch/media/no-media.php
+++ src/bp-templates/bp-nouveau/readylaunch/media/no-media.php
@@ -29,6 +29,14 @@ if (
 ) {
 	$album_dir = true;
 }
+
+// Check if we're viewing a single album page
+$single_album = false;
+if ( function_exists( 'bp_is_single_album' ) && bp_is_single_album() ) {
+	$single_album = true;
+} elseif ( function_exists( 'bp_is_single_video_album' ) && bp_is_single_video_album() ) {
+	$single_album = true;
+}
 ?>
 <div class="bb-rl-media-none">
 	<div class="bb-rl-media-none-figure"><i class="bb-icons-rl-file-image"></i></div>
@@ -36,7 +44,9 @@ if (
 		<span class="bp-icon" aria-hidden="true"></span>
 		<p>
 			<?php
-			if ( $album_dir ) {
+			if ( $single_album ) {
+				esc_html_e( 'No photos or videos found', 'buddyboss' );
+			} elseif ( $album_dir ) {
 				esc_html_e( 'No albums found', 'buddyboss' );
 			} else {
 				esc_html_e( 'No photos found', 'buddyboss' );
@@ -46,7 +56,9 @@ if (
 	</aside>
 	<p class="bb-rl-media-none-description">
 		<?php
-		if ( $album_dir ) {
+		if ( $single_album ) {
+			esc_html_e( 'It looks like there aren\'t any albums in this album.', 'buddyboss' );
+		} elseif ( $album_dir ) {
 			esc_html_e( 'It looks like there aren\'t any albums in this directory.', 'buddyboss' );
 		} else {
 			esc_html_e( 'It looks like there aren\'t any photos in this directory.', 'buddyboss' );
@@ -54,7 +66,21 @@ if (
 		?>
 	</p>
 	<?php
-	if (
+	if ( $single_album ) {
+		// Show add media buttons for single empty album
+		?>
+		<div class="bb-media-actions-wrap">
+			<div class="bb-media-actions bb-rl-media-none-actions">
+				<?php if ( ( bp_is_my_profile() && bb_user_can_create_media() ) || ( bp_is_group() && is_user_logged_in() && groups_can_user_manage_media( bp_loggedin_user_id(), bp_get_current_group_id() ) ) ) : ?>
+					<a href="#" id="bp-add-media" class="bb-album-add-photos button bb-rl-button bb-rl-button--brandFill bb-rl-button--small"><i class="bb-icons-rl-images"></i> <?php esc_html_e( 'Add Photos', 'buddyboss' ); ?></a>
+				<?php endif; ?>
+				<?php if ( bp_is_active( 'video' ) && ( ( bp_is_profile_video_support_enabled() && bp_is_user() ) || ( bp_is_group_video_support_enabled() && bp_is_group() ) ) && ( ( bp_is_my_profile() && bb_user_can_create_video() ) || ( bp_is_group() && is_user_logged_in() && groups_can_user_manage_video( bp_loggedin_user_id(), bp_get_current_group_id() ) ) ) ) : ?>
+					<a href="#" id="bp-add-video" class="bb-album-add-videos button bb-rl-button bb-rl-button--brandFill bb-rl-button--small"><i class="bb-icons-rl-video"></i> <?php esc_html_e( 'Add Videos', 'buddyboss' ); ?></a>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?php
+	} elseif (
 		! bp_is_media_directory() &&
 		! bp_is_user_albums() &&
 		! bp_is_group_albums()
