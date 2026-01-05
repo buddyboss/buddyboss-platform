8361d6573db0ffeb7f8db2334542504ec389c4f2
Amin Charoliya <amin@buddyboss.com>
2026-01-05 13:37:48 +0530

    PROD-9488 - Add permission check before deleting thumbnail

--- src/bp-templates/bp-nouveau/includes/video/ajax.php
+++ src/bp-templates/bp-nouveau/includes/video/ajax.php
@@ -1779,6 +1779,20 @@ function bp_nouveau_ajax_video_thumbnail_delete() {
 	$attachment_id       = filter_input( INPUT_POST, 'attachment_id', FILTER_SANITIZE_NUMBER_INT );
 	$video_attachment_id = filter_input( INPUT_POST, 'video_attachment_id', FILTER_SANITIZE_NUMBER_INT );
 
+	// Check if the current user has permission to edit this video.
+	if ( ! empty( $video_id ) ) {
+		$video = new BP_Video( $video_id );
+
+		if ( ! empty( $video->id ) && ! bp_video_user_can_edit( $video ) ) {
+			$response['feedback'] = sprintf(
+				'<div class="bp-feedback error"><span class="bp-icon" aria-hidden="true"></span><p>%s</p></div>',
+				esc_html__( 'You do not have permission to update this video.', 'buddyboss' )
+			);
+
+			wp_send_json_error( $response );
+		}
+	}
+
 	if ( ! empty( $attachment_id ) && ! empty( $video_attachment_id ) ) {
 		$auto_generated_thumbnails = get_post_meta( $attachment_id, 'video_preview_thumbnails', true );
 		$old_preview_thumbnail_id  = get_post_meta( $attachment_id, 'bp_video_preview_thumbnail_id', true );
