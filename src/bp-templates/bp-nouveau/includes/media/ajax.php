94cf06da8d92a94686ebfef6fb6dfc288026a3e6
Jitendra Banjara <jitendra@buddyboss.com>
2026-01-21 13:14:42 +0530

    [PROD-9552] Fix permission bypass vulnerability in media and video album save - Add edit permission verification before allowing album updates to prevent - unauthorized users from modifying other users' albums by passing album ID. - Media: Use bb_media_user_can_access() to check can_edit permission - Video: Add similar permission check for video album editing

--- src/bp-templates/bp-nouveau/includes/media/ajax.php
+++ src/bp-templates/bp-nouveau/includes/media/ajax.php
@@ -784,6 +784,16 @@ function bp_nouveau_ajax_media_album_save() {
 
 	$user_id = bp_loggedin_user_id();
 	if ( $id ) {
+		$has_access = bp_album_user_can_edit( $id );
+
+		if ( ! $has_access ) {
+			$response['feedback'] = sprintf(
+				'<div class="bp-feedback error"><span class="bp-icon" aria-hidden="true"></span><p>%s</p></div>',
+				esc_html__( "You don't have a permission to rename the document.", 'buddyboss' )
+			);
+			wp_send_json_error( $response );
+		}
+
 		$album   = new BP_Media_Album( $id );
 		$user_id = $album->user_id;
 	}
