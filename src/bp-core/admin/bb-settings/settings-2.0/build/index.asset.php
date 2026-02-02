6c704f7bc6251fd47dfcf7349308ea1011699013
Amin Charoliya <amin@buddyboss.com>
2026-02-02 15:30:16 +0530

    Style radio inputs in settings form

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '86ac420526cadc33b0f3');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'e9f75811a788d96bc21e');
