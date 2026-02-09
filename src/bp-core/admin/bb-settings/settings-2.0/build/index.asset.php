6f6e428a45693740259038303e6413cfc6706288
Jitendra Banjara <jitendra@buddyboss.com>
2026-02-09 15:37:30 +0530

    Grunt

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '411845ae611fe6e5c67e');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bbf5da7b8f94d5088f6b');
