b50ce5f1920968772f3b32e88d616b4c3f56e33f
Amin Charoliya <amin@buddyboss.com>
2026-02-16 13:58:24 +0530

    PROD-9647 - Style Share settings section

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '618a3d9d51de108ce8c5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '51db4ab9a81f660b410d');
