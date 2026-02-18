62dc703139617a99c5b28c78ff1ea3bab93f6535
Kartik Suthar <kartik@buddyboss.com>
2026-02-18 13:47:37 +0530

    PROD-9631 - run build

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3f45558e356d4383b424');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3edf42fa674f5e25d6d7');
