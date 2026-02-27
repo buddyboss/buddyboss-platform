2193f88969473cbe44b9e4c20b95d849140faf43
Amin Charoliya <amin@buddyboss.com>
2026-02-27 14:21:11 +0530

    PROD-9685 - Group image uploader button UI corrections

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '0c44fc72203cb4f187c7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'a08c58b68396463f428d');
