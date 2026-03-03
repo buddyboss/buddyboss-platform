d0500f7dff07a9c04b1cf91f9f090891278c553c
Amin Charoliya <amin@buddyboss.com>
2026-03-03 11:22:05 +0530

    PROD-9692 - Extensions listing UI corrections

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'a00e7e7f46d48a588cea');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '61a3b74d0274cfde0901');
