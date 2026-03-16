b9da90b527a9cef3ed2d3047501954f74181048a
Amin Charoliya <amin@buddyboss.com>
2026-03-16 19:00:29 +0530

    PROD-9756 - Update checkbox with Wordpress CheckboxControl

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '3d4a18084efce2c57973');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '7ac979cad6998d78316e');
