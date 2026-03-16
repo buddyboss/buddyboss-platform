70ab0a8ef1e6b987e6fc3accf2c31ca01d3ad488
Jitendra Banjara <jitendra@buddyboss.com>
2026-03-16 16:23:45 +0530

    [PROD-9756] Build

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'ff7a989defe5be733ff8');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '0536b783db00d906d42a');
