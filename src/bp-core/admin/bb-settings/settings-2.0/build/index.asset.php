0f2f5c4c5a22a420e7dbfef730cd5984e5e2c8f7
Amin Charoliya <amin@buddyboss.com>
2026-03-17 15:56:31 +0530

    PROD-9756 - New Password field type

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '2d31ba0eaa01df0763ac');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '68c5a025d0668e335f4a');
--- src/js/admin/settings-2.0/components/fields/PasswordField.js
+++ src/js/admin/settings-2.0/components/fields/PasswordField.js
@@ -57,15 +57,9 @@ export function PasswordField( props ) {
 				disabled={ disabled }
 			>
 				{ isVisible ? (
-					<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M17.94 17.94A10.07 10.07 0 0112 20c-7 0-11-8-11-8a18.45 18.45 0 015.06-5.94M9.9 4.24A9.12 9.12 0 0112 4c7 0 11 8 11 8a18.5 18.5 0 01-2.16 3.19m-6.72-1.07a3 3 0 11-4.24-4.24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-						<line x1="1" y1="1" x2="23" y2="23" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-					</svg>
+					<i className="bb-icons-rl-eye-slash"></i>
 				) : (
-					<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8S1 12 1 12z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-						<circle cx="12" cy="12" r="3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-					</svg>
+					<i className="bb-icons-rl-eye"></i>
 				) }
 			</button>
 		</div>
