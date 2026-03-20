9232717cbe4f5b157ed17d600d987f6d3a9c53e5
Jitendra Banjara <jitendra@buddyboss.com>
2026-03-20 09:58:35 +0530

    [PROD-9751] Fixed toggle issue

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '3c7ad0c62aef5e1a77a8');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'ba69b07c368fb9653863');
