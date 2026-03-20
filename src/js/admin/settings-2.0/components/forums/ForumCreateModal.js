8888fe38c5f77ec9b488327f08428dfd46c9eb88
Amin Charoliya <amin@buddyboss.com>
2026-03-20 17:49:59 +0530

    PROD-9735 - Create and edit forum modals UI corrections

--- src/js/admin/settings-2.0/components/forums/ForumCreateModal.js
+++ src/js/admin/settings-2.0/components/forums/ForumCreateModal.js
@@ -349,7 +349,7 @@ export function ForumCreateModal( { isOpen, onClose, onCreated, forumBaseSlug, c
 						);
 					}
 					return (
-						<div key={ item.field.id } className={ 'richtext' === item.field.type || nextIsRow ? 'bb-admin-settings-modal__row--separator' : '' }>
+						<div key={ item.field.id } className={ 'components-base-control' + ( 'richtext' === item.field.type || nextIsRow ? ' bb-admin-settings-modal__row--separator' : '' ) }>
 							<RegisteredMetaField
 								field={ item.field }
 								value={ registeredValues[ item.field.id ] }
