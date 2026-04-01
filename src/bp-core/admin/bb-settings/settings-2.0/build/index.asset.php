f5e662a783431cf490f2ebc0690797d1b3154c7b
Jitendra Banjara <jitendra@buddyboss.com>
2026-04-01 12:38:19 +0530

    [PROD-9752] React Fields: - Replace key={index} with stable unique keys in DomainRestrictionsField and EmailRestrictionsField repeater rows (prevents DOM reuse bugs on middle-row deletion)

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '395369f0cdf8401d3d2a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '851802e345ee94e802e0');
