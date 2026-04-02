d11b1fe0dd21bf8273ec874c63d3cec9089c3f87
Jitendra Banjara <jitendra@buddyboss.com>
2026-04-03 01:02:56 +0530

    [PROD-9777] Added hook

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '68fe0100f1b0ffca52a4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '1c339b90a647d1f01fc4');
