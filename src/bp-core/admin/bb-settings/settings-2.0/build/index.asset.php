cc5401768bdd535fcdcb485fc807e9a516caf95c
Amin Charoliya <amin@buddyboss.com>
2026-04-06 11:49:40 +0530

    PROD-9752 - Show Redirects select box arrow

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'c21bc5b154fc84311a3a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '1f617600e92622b407dc');
