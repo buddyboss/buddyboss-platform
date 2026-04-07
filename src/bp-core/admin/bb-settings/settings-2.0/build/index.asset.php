8e607a5598de96d2800c7840c6cfc7aa87e72741
Amin Charoliya <amin@buddyboss.com>
2026-04-07 14:43:25 +0530

    PROD-9369 - Social Groups, Notifications and Private Messaging settings screens responsive for smaller screen devices

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '199b82abb02dd2058d14');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '3cf127abcfa2aa6c3d68');
