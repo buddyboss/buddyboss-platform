d7f35273327a86499ff3e54d213e7ccac970a7c3
Amin Charoliya <amin@buddyboss.com>
2026-04-13 15:17:42 +0530

    PROD-9369 - Fix deprecated createRoot warning

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'b36aaeece6fa2eb500b4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '96fce750da56b663a9d2');
--- src/js/admin/settings-2.0/index.js
+++ src/js/admin/settings-2.0/index.js
@@ -5,17 +5,17 @@
  * @since BuddyBoss [BBVERSION]
  */
 
-import { render } from '@wordpress/element';
+import { createRoot } from '@wordpress/element';
 import { App } from './App';
 import { ErrorBoundary } from './components/ErrorBoundary';
 
 // Initialize the React app.
 const container = document.getElementById( 'bb-admin-settings-2-0' );
-if (container) {
-	render(
+if ( container ) {
+	const root = createRoot( container );
+	root.render(
 		<ErrorBoundary>
 			<App />
-		</ErrorBoundary>,
-		container
+		</ErrorBoundary>
 	);
 }
