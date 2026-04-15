5b16458802ccf861d29b6377cc7da76adf04117d
Jitendra Banjara <jitendra@buddyboss.com>
2026-04-15 13:40:40 +0530

    [PROD-9777] Fix temporal dead zone error in reactions MigrationModal when clicking Start Conversion
    - Move loadWizardData useCallback declaration before the useEffect that references it
    - Resolves ReferenceError "Cannot access 'k' before initialization" when opening migration wizard
    - Reorder event handler declarations so all useCallbacks are defined before dependent useEffects

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '97b41fbb5105fabbd778');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '4532b3d19ccd584f070f');
