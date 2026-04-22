57c489b776c8c233077888cc61c3919395ae3080
Amin Charoliya <amin@buddyboss.com>
2026-04-22 14:57:54 +0530

    PROD-9859 - Logo uploader UI corrections

--- src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings-2.0/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '7b3c5e688ff5d80655cb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '2cda4ab692b99581d389');
