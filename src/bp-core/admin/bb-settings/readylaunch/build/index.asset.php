a759e336a528dae00952e85ea0104dbc7c1f68a1
Kartik Suthar <kartik@buddyboss.com>
2026-04-24 11:01:23 +0530

    PROD-9370 - Remove bb-upgrade admin page and Performance Tester
    - Deletes the admin.php?page=bb-upgrade screen, submenu, admin-bar tab,
      dismissible upgrade notice, BB_Performance_Tester class, and all
      related templates, JS, CSS, SCSS, and images. Settings 2.0 upgrade
      flow (UpgradeModal) is unrelated and preserved.

--- src/bp-core/admin/bb-settings/readylaunch/build/index.asset.php
+++ src/bp-core/admin/bb-settings/readylaunch/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6c4968d2f6272ee08716');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2a22921118b2db6e5a70');
