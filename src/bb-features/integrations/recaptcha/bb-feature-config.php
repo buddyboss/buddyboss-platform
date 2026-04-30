2fdfbd31d84bd62f5333365f3023d7ebcaa11bea
Amin Charoliya <amin@buddyboss.com>
2026-04-30 16:12:09 +0530

    PROD-9370 - Update Integration icons

--- src/bb-features/integrations/recaptcha/bb-feature-config.php
+++ src/bb-features/integrations/recaptcha/bb-feature-config.php
@@ -24,8 +24,8 @@ bb_register_integration(
 		'label'              => __( 'reCAPTCHA', 'buddyboss' ),
 		'description'        => __( 'Protect your community from spam and bot registrations with Google\'s invisible and checkbox verification.', 'buddyboss' ),
 		'icon'               => array(
-			'type'  => 'font',
-			'class' => 'bb-icons-rl bb-icons-rl-shield-check',
+			'type'  => 'svg',
+			'url'  => 'https://bb-features-marketing.s3.amazonaws.com/images/svg/ReCaptcha.svg',
 		),
 		'license_tier'       => 'free',
 		'standalone'         => true,
