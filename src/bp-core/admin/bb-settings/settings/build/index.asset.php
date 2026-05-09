ff3672b9dcf8cd50beb2226e6d7969244bfe1b4e
Jitendra Banjara <jitendra@buddyboss.com>
2026-05-09 17:16:08 +0530

    Updated icon = Default Profile Avatar -> BuddyBoss

--- src/bp-core/admin/bb-settings/settings/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => 'e2b309e59189b5650fe8');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => 'ac8085850203a05d49c2');
