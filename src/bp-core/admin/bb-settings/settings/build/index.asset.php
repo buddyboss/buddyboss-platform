1482bf9d9e9db8825dc1f55b8d41438b2369682a
Amin Charoliya <amin@buddyboss.com>
2026-05-14 17:04:09 +0530

    PROD-9372 - Add remaining footer sections

--- src/bp-core/admin/bb-settings/settings/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => '1c27e4f64411bb5bb048');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => 'fd2760da623f4be7ece1');
--- src/js/admin/settings/screens/HelpScreen.js
+++ src/js/admin/settings/screens/HelpScreen.js
@@ -8,6 +8,7 @@
 import { useState } from '@wordpress/element';
 import { Button } from '@wordpress/components';
 import { __, _n, sprintf } from '@wordpress/i18n';
+import doneForYouImage from '../images/help-done-for-you.png';
 
 /**
  * Help Screen Component
@@ -153,7 +154,119 @@ export function HelpScreen() {
 						} ) }
 					</div>
 				</section>
+
+				<section
+					className="bb-admin-help-cta"
+					aria-labelledby="bb-admin-help-cta-title"
+				>
+					<h2
+						id="bb-admin-help-cta-title"
+						className="bb-admin-help-cta__title"
+					>
+						{ __( 'Can’t find the Answer?', 'buddyboss' ) }
+					</h2>
+					<Button variant="primary" className="bb-admin-help-cta__action">
+						<i
+							className="bb-icons-rl bb-icons-rl-robot bb-admin-help-cta__action-icon"
+							aria-hidden="true"
+						></i>
+						<span className="bb-admin-help-cta__action-label">
+							{ __( 'Ask Buddy', 'buddyboss' ) }
+						</span>
+						<i
+							className="bb-icons-rl bb-icons-rl-arrow-right bb-admin-help-cta__action-icon"
+							aria-hidden="true"
+						></i>
+					</Button>
+				</section>
+
+				<div className="bb-admin-help-row">
+					<section
+						className="bb-admin-help-getting-started"
+						aria-labelledby="bb-admin-help-getting-started-title"
+					>
+						<h2
+							id="bb-admin-help-getting-started-title"
+							className="bb-admin-help-getting-started__title"
+						>
+							{ __( 'Get Started', 'buddyboss' ) }
+						</h2>
+						<ul className="bb-admin-help-getting-started__list">
+							{ [
+								{ key: 'install-theme',     label: __( 'How to install the BuddyBoss Theme', 'buddyboss' ) },
+								{ key: 'default-data',      label: __( 'How to Setup Default Data in BuddyBoss', 'buddyboss' ) },
+								{ key: 'login-register',    label: __( 'How to Customize the Login & Registration Page in BuddyBoss', 'buddyboss' ) },
+								{ key: 'install-theme-2',   label: __( 'How to install the BuddyBoss Theme', 'buddyboss' ) },
+								{ key: 'default-data-2',    label: __( 'How to Setup Default Data in BuddyBoss', 'buddyboss' ) },
+								{ key: 'login-register-2',  label: __( 'How to Customize the Login & Registration Page in BuddyBoss', 'buddyboss' ) },
+							].map( function ( item ) {
+								return (
+									<li key={ item.key } className="bb-admin-help-getting-started__item">
+										<a href="#" className="bb-admin-help-getting-started__link">
+											<i
+												className="bb-icons-rl bb-icons-rl-file-text bb-admin-help-getting-started__icon"
+												aria-hidden="true"
+											></i>
+											<span className="bb-admin-help-getting-started__label">
+												{ item.label }
+											</span>
+										</a>
+									</li>
+								);
+							} ) }
+						</ul>
+					</section>
+
+					<section
+						className="bb-admin-help-promo"
+						aria-labelledby="bb-admin-help-promo-title"
+					>
+						<div className="bb-admin-help-promo__media">
+							<img
+								src={ doneForYouImage }
+								alt={ __( 'Done For You Service preview', 'buddyboss' ) }
+								className="bb-admin-help-promo__image"
+							/>
+						</div>
+						<div className="bb-admin-help-promo__body">
+							<div className="bb-admin-help-promo__text">
+								<p className="bb-admin-help-promo__eyebrow">
+									{ __( 'Done For You Service', 'buddyboss' ) }
+								</p>
+								<h2
+									id="bb-admin-help-promo-title"
+									className="bb-admin-help-promo__title"
+								>
+									{ __( 'Get help launching your site', 'buddyboss' ) }
+								</h2>
+								<p className="bb-admin-help-promo__description">
+									{ __( 'Get your own team that will help you launch your site. Let them know what your business is about, then provide them with your logo and brand colors, then sit back while we do all the heavy lifting.', 'buddyboss' ) }
+								</p>
+							</div>
+							<Button variant="primary" className="bb-admin-help-promo__action">
+								<span className="bb-admin-help-promo__action-label">
+									{ __( 'Get Done For You Web Service', 'buddyboss' ) }
+								</span>
+							</Button>
+						</div>
+					</section>
+				</div>
 			</div>
+
+			<footer
+				className="bb-admin-help-footer"
+				aria-label={ __( 'Chat with Buddy', 'buddyboss' ) }
+			>
+				<Button variant="primary" className="bb-admin-help-footer__action">
+					<i
+						className="bb-icons-rl bb-icons-rl-robot bb-admin-help-footer__action-icon"
+						aria-hidden="true"
+					></i>
+					<span className="bb-admin-help-footer__action-label">
+						{ __( 'Chat with Buddy', 'buddyboss' ) }
+					</span>
+				</Button>
+			</footer>
 		</div>
 	);
 }
