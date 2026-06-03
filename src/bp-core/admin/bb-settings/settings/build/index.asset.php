dc5935890c718060dae24c836de7644a5b403549
Kartik Suthar <kartik@buddyboss.com>
2026-06-03 12:30:08 +0530

    PROD-9936 - grunt

--- src/bp-core/admin/bb-settings/settings/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => '866ac612725fd69c2db5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => '9ddae971b238bef5d4c8');
