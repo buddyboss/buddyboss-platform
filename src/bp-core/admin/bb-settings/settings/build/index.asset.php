bc528ea22fb8736467e8217d014c46eeb5e28c39
Amin Charoliya <amin@buddyboss.com>
2026-07-02 17:29:48 +0530

    [PROD-10021] build

--- src/bp-core/admin/bb-settings/settings/build/index.asset.php
+++ src/bp-core/admin/bb-settings/settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => '99dab10562af58120831');
+<?php return array('dependencies' => array('bb-admin-common', 'react', 'react-dom', 'wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => 'af3603d186dc5116bf01');
